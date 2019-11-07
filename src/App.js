9bce1d2d73f34790e593883cd2ac7a4ca2bc4a19
Serhii-P <serhii.pyskun@gmail.com>
2019-11-07 12:50:11 +0200

    'Solution'

--- src/App.js
+++ src/App.js
@@ -17,20 +17,96 @@ class App extends React.Component {
       './img/9.png',
       './img/10.png',
     ],
+    width: 130,
+    count: 3,
+    position: 0,
+    imageSize: 130,
+    step: 3,
+    height: 130,
+    isActive: false,
   };
 
-  render() {
-    const { images } = this.state;
-
-    return (
-      <div className="App">
-        {/* eslint-disable-next-line */}
-        <h1>Carousel with {images.length} images</h1>
-
-        <Carousel />
-      </div>
-    );
+  changeStep = (event) => {
+    this.setState({
+      step: event.target.value,
+    });
   }
+
+prevClick = () => {
+  // let last = this.state.images.slice(-1);
+  // let rest = this.state.images.slice(0, -1);
+  // let images = [last, ...rest];
+  // this.setState({images});
+
+  this.setState(prevState => ({
+    position: prevState.position + (prevState.width * prevState.step),
+  }));
+}
+
+nextClick = () => {
+  this.setState(prevState => ({
+    position: prevState.position - (prevState.width * prevState.step),
+  }));
+}
+
+changeItemWidth = (event) => {
+  this.setState({
+    imageSize: event.target.value,
+  });
+}
+
+changeElementNumber = (event) => {
+  this.setState({
+    count: event.target.value,
+  });
+}
+
+startRotate = () => {
+  this.timer = setInterval(() => {
+    this.setState(prevState => ({
+      isActive: true,
+      position: prevState.position + (prevState.width * 1),
+    }));
+  }, 2000);
+}
+
+stopRotate = () => {
+  clearInterval(this.timer);
+  this.setState(prevState => ({
+    isActive: prevState === false,
+  }));
+}
+
+render() {
+  const {
+    images, position, width, count, imageSize, step, height, isActive,
+  } = this.state;
+
+  return (
+    <div className="App">
+      {/* eslint-disable-next-line */}
+       <h1>Carousel with {images.length} images</h1>
+
+      <Carousel
+        prevClick={this.prevClick}
+        position={position}
+        width={width}
+        count={count}
+        nextClick={this.nextClick}
+        changeItemWidth={this.changeItemWidth}
+        imageSize={imageSize}
+        changeElementNumber={this.changeElementNumber}
+        step={step}
+        changeStep={this.changeStep}
+        images={images}
+        height={height}
+        isActive={isActive}
+        startRotate={this.startRotate}
+        stopRotate={this.stopRotate}
+      />
+    </div>
+  );
+}
 }
 
 export default App;
--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,17 +1,86 @@
 import React from 'react';
 
-const Carousel = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+import './Carousel.css';
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+const Carousel = ({ images, initialState, prevClick, position, width, count, nextClick, changeItemWidth, value, imageSize, changeElementNumber, step, changeStep, height, isActive, startRotate, stopRotate}) => {
+  const styles = {
+    marginLeft: `-${position}px`,
+  };
+
+  const wrapperStyle = {
+    width: `${imageSize * count}px`,
+    height: `${imageSize * count}px`,
+  };
+
+  const bubbleSize = {
+    width: `${imageSize}px`,
+  };
+
+  return (
+    <div>
+
+      <button
+        type="button"
+        onClick={prevClick}
+      >
+        Prev
+      </button>
+
+      <button
+        type="button"
+        onClick={nextClick}
+      >
+        Next
+      </button>
+
+      Size of pictures:
+      <input
+        type="range"
+        min="130"
+        max="390"
+        value={imageSize}
+        onChange={changeItemWidth}
+      />
+
+      Nubmer of bubbles:
+      <select value={count} onChange={changeElementNumber}>
+        {images.map((img, index) => (
+          <option key={img} value={index + 1}>{index + 1}</option>
+        ))}
+      </select>
+
+      Step:
+      <select value={step} onChange={changeStep}>
+        {images.map((img, i) => (
+          <option key={img} value={i + 1}>{i + 1}</option>
+        ))}
+      </select>
+
+      Rotate:
+      {isActive === false
+        ? <button type="button" onClick={startRotate}>Start</button>
+        : <button type="button" onClick={stopRotate}>Stop</button>
+      }
+
+      <div>
+
+        <div
+          className="Carousel"
+          style={wrapperStyle}
+        >
+          <ul
+            className="Carousel__list"
+            style={styles}
+          >
+            {images.map((img, index) => (
+              <li key={img} style={bubbleSize}><img style={bubbleSize} src={img} alt={index + 1} /></li>
+            ))}
+
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Carousel;
