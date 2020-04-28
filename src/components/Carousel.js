46df32627ed68230aa0280b5a9a01a40a71ffcbb
Rustam Khananov <antmine2017@gmail.com>
2020-04-28 16:15:25 +0300

    fixed

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,67 +1,76 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Carousel = ({ images,
-  step,
-  frameSize,
-  itemWidth,
-  animationDuration }) => {
-  function moveRight(event) {
-    let { left } = event.nativeEvent.srcElement.parentElement.children[0].style;
-    const carouselElement = event
-      .nativeEvent
-      .srcElement
-      .parentElement
-      .children[0];
+class Carousel extends React.Component {
+  state = {
+    left: 0,
+  }
 
-    left = (parseInt(left, 10)) ? parseInt(left, 10) : 0;
+  moveRight = (event) => {
+    const { step, itemWidth } = this.props;
+    const { left } = this.state;
+    let nextLeft;
+
     if (left + step * itemWidth > 0) {
-      carouselElement.style.left = `${0}px`;
+      nextLeft = 0;
     } else {
-      carouselElement.style.left = `${left + step * itemWidth}px`;
+      nextLeft = left + step * itemWidth;
     }
+
+    this.setState({
+      left: nextLeft,
+    });
   }
 
-  function moveLeft(event) {
-    let { left } = event.nativeEvent.srcElement.parentElement.children[0].style;
-    const carouselElement = event
-      .nativeEvent
-      .srcElement
-      .parentElement
-      .children[0];
+  moveLeft = () => {
+    const { images, step, itemWidth } = this.props;
+    const { left } = this.state;
+    let nextLeft;
 
-    left = (parseInt(left, 10)) ? parseInt(left, 10) : 0;
     if (left - step * itemWidth < -(images.length - step) * itemWidth) {
-      carouselElement
-        .style.left = `${-(itemWidth * images.length - step * itemWidth)}px`;
+      nextLeft = -(itemWidth * images.length - step * itemWidth);
     } else {
-      carouselElement.style.left = `${left - step * itemWidth}px`;
+      nextLeft = left - step * itemWidth;
     }
+
+    this.setState({
+      left: nextLeft,
+    });
   }
 
-  return (
-    <div className="Carousel" style={{ width: `${itemWidth * frameSize}px` }}>
-      <ul
-        className="Carousel__list"
-        style={{ transition: `${animationDuration / 1000}s` }}
-      >
-        {images.map((image, index) => (
-          <li
-            key={image}
-            className="Carousel__item"
-            style={{ width: `${itemWidth}px` }}
-          >
-            <img src={image} alt={index} />
-          </li>
-        ))}
-      </ul>
+  render() {
+    const { images,
+      frameSize,
+      itemWidth,
+      animationDuration } = this.props;
 
-      <button onClick={moveRight} type="button">Prev</button>
-      <button onClick={moveLeft} type="button">Next</button>
+    return (
+      <div className="Carousel" style={{ width: `${itemWidth * frameSize}px` }}>
+        <ul
+          className="Carousel__list"
+          style={{
+            transition: `${animationDuration / 1000}s `,
+            left: `${this.state.left}px`,
+          }}
+        >
+          {images.map((image, index) => (
+            <li
+              key={image}
+              className="Carousel__item"
+              style={{ width: `${itemWidth}px` }}
+            >
+              <img src={image} alt={index} />
+            </li>
+          ))}
+        </ul>
 
-    </div>
-  );
-};
+        <button onClick={this.moveRight} type="button">Prev</button>
+        <button onClick={this.moveLeft} type="button">Next</button>
+
+      </div>
+    );
+  }
+}
 
 Carousel.propTypes = {
   images: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
