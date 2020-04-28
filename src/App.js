298f50df655d9f643229b4e3dca538d45ddabe8d
Oleg <fasfas7971@gmail.com>
2020-04-28 20:17:45 +0300

    Added control panel

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Carousel from './components/Carousel';
+import Options from './components/Options';
 
 class App extends React.Component {
   state = {
@@ -16,21 +17,74 @@ class App extends React.Component {
       './img/9.png',
       './img/10.png',
     ],
+    defaultSelectOption: 3,
+    defaultStep: 3,
+    defaultInfinity: true,
   };
 
+  changeFrameSize = (e) => {
+    const newFrameSize = +e.target.value;
+
+    this.setState(() => ({ defaultSelectOption: newFrameSize }));
+  }
+
+  changeStep = (e) => {
+    const newStep = +e.target.value;
+
+    this.setState(() => ({ defaultStep: newStep }));
+  }
+
+  changeInfinity = () => {
+    this.setState(() => ({ defaultInfinity: !this.state.defaultInfinity }));
+  }
+
   render() {
-    const { images } = this.state;
+    /* eslint-disable-next-line max-len */
+    const { images, defaultSelectOption, defaultStep, defaultInfinity } = this.state;
+    /* eslint-disable-next-line max-len */
+    const selectOption = new Array(defaultSelectOption).fill(0).map((el, i) => i + 1);
+    const changeTo = new Array(images.length - 2).fill(0).map((el, i) => i + 2);
+    const changeInf = [true, false];
 
     return (
       <div className="App">
         <Carousel
           images={images}
-          step={3}
-          frameSize={3}
+          step={defaultStep}
+          frameSize={defaultSelectOption}
           itemWidth={130}
           animationDuration={1000}
-          infinite
+          infinite={defaultInfinity}
         />
+        <div className="wrap">
+          <label htmlFor="changeStep">Choose to change step </label>
+          <select
+            value={defaultStep}
+            onChange={this.changeStep}
+            id="changeStep"
+          >
+            <Options selectOption={selectOption} />
+          </select>
+        </div>
+        <div className="wrap">
+          <label htmlFor="changeFrameSize">Choose to change frame size </label>
+          <select
+            value={defaultSelectOption}
+            onChange={this.changeFrameSize}
+            id="changeFrameSize"
+          >
+            <Options selectOption={changeTo} />
+          </select>
+        </div>
+        <div className="wrap">
+          <label htmlFor="infinity">Infinity</label>
+          <select
+            id="infinity"
+            onChange={this.changeInfinity}
+          >
+            <Options selectOption={changeInf} />
+          </select>
+        </div>
       </div>
     );
   }
--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -23,7 +23,7 @@ class Carousel extends React.Component {
         ? frameSize
         : images.length;
     } else if (flow === 1 && count < frameSize + step) {
-      if (count % step !== 0 && count !== frameSize) {
+      if (count > step && count !== frameSize) {
         toLeft = (count % frameSize) * itemWidth + left * flow;
         updateCount = frameSize;
       } else {
@@ -44,6 +44,13 @@ class Carousel extends React.Component {
     }));
   }
 
+  applyChange = () => {
+    this.setState(() => ({
+      count: this.props.frameSize,
+      left: 0,
+    }));
+  }
+
   render() {
     const { images, itemWidth, frameSize, animationDuration } = this.props;
     const { left } = this.state;
@@ -51,38 +58,55 @@ class Carousel extends React.Component {
     const transitionDuration = animationDuration / 1000;
 
     return (
-      <div
-        className="Carousel"
-        style={{
-          width: `${carouselSize}px`,
-        }}
-      >
-        <ul
-          className="Carousel__list"
+      <>
+        <div className="wrap__apply">
+          <p><strong>Click apply after change parameters!</strong></p>
+          <p>
+            (Иначе первый слайд до конца поломается,
+            <br />
+            а потом станет нормальный)
+          </p>
+          <button
+            type="button"
+            onClick={this.applyChange}
+            className="button__apply"
+          >
+            Apply
+          </button>
+        </div>
+        <div
+          className="Carousel"
           style={{
-            transform: `translate(${left}px)`,
-            transition: `all ${transitionDuration}s`,
+            width: `${carouselSize}px`,
           }}
         >
-          {images.map((link, index) => {
-            const id = index + 1;
+          <ul
+            className="Carousel__list"
+            style={{
+              transform: `translate(${left}px)`,
+              transition: `all ${transitionDuration}s`,
+            }}
+          >
+            {images.map((link, index) => {
+              const id = index + 1;
 
-            return (
-              <li key={id}>
-                <img
-                  style={{
-                    width: `${itemWidth}px`,
-                  }}
-                  src={link}
-                  alt={id}
-                />
-              </li>
-            );
-          })}
-        </ul>
+              return (
+                <li key={id}>
+                  <img
+                    style={{
+                      width: `${itemWidth}px`,
+                    }}
+                    src={link}
+                    alt={id}
+                  />
+                </li>
+              );
+            })}
+          </ul>
 
-        <Buttons swipe={this.swipe} />
-      </div>
+          <Buttons swipe={this.swipe} />
+        </div>
+      </>
     );
   }
 }
