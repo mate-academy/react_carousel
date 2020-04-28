03303e5f4a2d29b93ab822edc928f904afe38224
Rustam Khananov <antmine2017@gmail.com>
2020-04-28 13:38:37 +0300

    solution

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,17 +1,74 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Carousel = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+const Carousel = ({ images,
+  step,
+  frameSize,
+  itemWidth,
+  animationDuration }) => {
+  function moveRight(event) {
+    let { left } = event.nativeEvent.srcElement.parentElement.children[0].style;
+    const carouselElement = event
+      .nativeEvent
+      .srcElement
+      .parentElement
+      .children[0];
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+    left = (parseInt(left, 10)) ? parseInt(left, 10) : 0;
+    if (left + step * itemWidth > 0) {
+      carouselElement.style.left = `${0}px`;
+    } else {
+      carouselElement.style.left = `${left + step * itemWidth}px`;
+    }
+  }
+
+  function moveLeft(event) {
+    let { left } = event.nativeEvent.srcElement.parentElement.children[0].style;
+    const carouselElement = event
+      .nativeEvent
+      .srcElement
+      .parentElement
+      .children[0];
+
+    left = (parseInt(left, 10)) ? parseInt(left, 10) : 0;
+    if (left - step * itemWidth < -(images.length - step) * itemWidth) {
+      carouselElement
+        .style.left = `${-(itemWidth * images.length - step * itemWidth)}px`;
+    } else {
+      carouselElement.style.left = `${left - step * itemWidth}px`;
+    }
+  }
+
+  return (
+    <div className="Carousel" style={{ width: `${itemWidth * frameSize}px` }}>
+      <ul
+        className="Carousel__list"
+        style={{ transition: `${animationDuration / 1000}s` }}
+      >
+        {images.map((image, index) => (
+          <li
+            key={image}
+            className="Carousel__item"
+            style={{ width: `${itemWidth}px` }}
+          >
+            <img src={image} alt={index} />
+          </li>
+        ))}
+      </ul>
+
+      <button onClick={moveRight} type="button">Prev</button>
+      <button onClick={moveLeft} type="button">Next</button>
+
+    </div>
+  );
+};
+
+Carousel.propTypes = {
+  images: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
+  step: PropTypes.number.isRequired,
+  frameSize: PropTypes.number.isRequired,
+  itemWidth: PropTypes.number.isRequired,
+  animationDuration: PropTypes.number.isRequired,
+};
 
 export default Carousel;
