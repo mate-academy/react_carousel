d3d5e5e138e825c8a6eb020fd09c730e99ba979e
Tetiana <ishchuktr@gmail.com>
2020-04-29 13:25:35 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ class App extends React.Component {
         {/* eslint-disable-next-line */}
         <h1>Carousel with {images.length} images</h1>
 
-        <Carousel />
+        <Carousel images={images} />
       </div>
     );
   }
--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,17 +1,56 @@
 import React from 'react';
+import PropsTypes from 'prop-types';
+import './Carousel.css';
 
-const Carousel = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+class Carousel extends React.Component {
+  state = {
+    index: 0,
+    visibleItem: 4,
+    widthItem: 130,
+  }
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+  prevClick = () => {
+    this.setState(state => ({
+      index: state.index === 0
+        ? this.props.images.length - state.visibleItem : state.index - 1,
+    }));
+  }
+
+  nextClick = () => {
+    this.setState(state => ({
+      index: state.index + state.visibleItem === this.props.images.length
+        ? 0 : state.index + 1,
+    }));
+  }
+
+  render() {
+    const { images } = this.props;
+    const { index, visibleItem, widthItem } = this.state;
+
+    return (
+      <div
+        style={{ width: `${widthItem * visibleItem}px` }}
+        className="Carousel"
+      >
+        <ul
+          style={{ transform: `translateX(-${index * widthItem}px)` }}
+          className="Carousel__list"
+        >
+          {images.map(el => (
+            <li key={el}><img src={el} alt="index" /></li>
+          ))
+          }
+        </ul>
+
+        <button onClick={this.prevClick} type="button">Prev</button>
+        <button onClick={this.nextClick} type="button">Next</button>
+      </div>
+    );
+  }
+}
+
+Carousel.propTypes = {
+  images: PropsTypes.arrayOf.isRequired,
+};
 
 export default Carousel;
