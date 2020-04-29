eeffdef1e70cb8bcc8ade8b6ebb0b6ae30c98896
Andreas_Just <andreas.wq@gmail.com>
2020-04-29 08:19:27 +0300

    Step and size selection implementation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './App.scss';
 import { emoji } from './api/emoji';
-
 import Carousel from './components/Carousel';
+import Settings from './components/Settings';
 
 class App extends React.Component {
   state = {
     images: emoji,
-    frameSize: 1,
-    step: 1,
+    frameSize: '1',
+    step: '1',
     option: ['One', 'Two', 'Three', 'Four', 'Five'],
-    infinite: true,
+    infinite: false,
   };
 
   handleChangeSetting = (event) => {
@@ -29,53 +29,14 @@ class App extends React.Component {
   };
 
   render = () => {
-    const { images, option, frameSize, step } = this.state;
+    const { images, frameSize, step } = this.state;
 
     return (
       <div className="App">
-        <div className="ui inverted blue segment setting">
-          <div className="ui inverted equal width form mini">
-            <div className="fields">
-              <div className="field setting__frame">
-                Number of emojis in the frame
-                <select
-                  name="frameSize"
-                  value={frameSize}
-                  onChange={this.handleChangeSetting}
-                >
-                  {option.map((num, idx) => (
-                    <option key={num} value={idx + 1}>{num}</option>
-                  ))}
-                </select>
-              </div>
-              <div className="field setting__step">
-                Slide step
-                <select
-                  name="step"
-                  value={step}
-                  onChange={this.handleChangeSetting}
-                >
-                  {option.slice(0, frameSize).map((num, idx) => (
-                    <option key={num} value={idx + 1}>{num}</option>
-                  ))}
-                </select>
-              </div>
-              <div className="field">
-                <div className="ui checkbox">
-                  <input
-                    onChange={this.handleChangeSetting}
-                    checked={this.state.infinite}
-                    id="infinite"
-                    name="infinite"
-                    type="checkbox"
-                    className="hidden"
-                  />
-                  <label htmlFor="infinite">Infinite</label>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <Settings
+          {...this.state}
+          onChangeSetting={this.handleChangeSetting}
+        />
         <Carousel
           images={images}
           step={+step}
