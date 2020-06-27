4c01912c39ef3b849d5e548cc600095fcdea60c7
Denys <gryshko_denys@gmail.com>
2020-06-27 16:47:01 +0300

    save

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-import Carousel from './components/Carousel';
+import { Carousel } from './components/Carousel';
 
 class App extends React.Component {
   state = {
@@ -17,17 +17,60 @@ class App extends React.Component {
       './img/9.png',
       './img/10.png',
     ],
+    initialPosition: 0,
+    step: 3,
+    frameSize: 3,
+    itemWidth: 130,
+    animationDuration: 1000,
   };
 
+  scrollNext = () => {
+    /* eslint-disable-next-line */
+    const length = this.state.images.length;
+    const position = this.state.initialPosition;
+    /* eslint-disable-next-line */
+    const step = this.state.step;
+    // const currentPosition = position * step + step;
+    const leavedSmiles = length - position * step - step;
+
+    if (leavedSmiles < step) {
+      this.setState({
+        step: leavedSmiles,
+      });
+    } else if (leavedSmiles === 0) {
+      return false;
+    } else {
+      this.setState(prevState => ({
+        initialPosition: prevState.initialPosition + 1,
+      }));
+    }
+  }
+
   render() {
-    const { images } = this.state;
+    const {
+      images,
+      step,
+      frameSize,
+      itemWidth,
+      animationDuration,
+      initialPosition,
+    } = this.state;
+    // console.log(images.length, initialPosition, (images.length - initialPosition * step - step));
 
     return (
       <div className="App">
         {/* eslint-disable-next-line */}
         <h1>Carousel with {images.length} images</h1>
 
-        <Carousel />
+        <Carousel
+          images={images}
+          step={step}
+          width={itemWidth}
+          animation={animationDuration}
+          framesize={frameSize}
+          initialPosition={initialPosition}
+          next={this.scrollNext}
+        />
       </div>
     );
   }
--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,17 +1,50 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import './Carousel.css';
 
-const Carousel = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+export class Carousel extends React.PureComponent {
+  render() {
+    const {
+      images,
+      step,
+      framesize,
+      width,
+      animation,
+      initialPosition,
+      next,
+    } = this.props;
+    // console.log(width);
+    const carouselStyle = {
+      width: `${width * framesize}px`,
+      // transform: `translateX(530px)`,
+    };
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+    const carouselListStyle = {
+      width: `${width * framesize}px`,
+      transform: `translateX(-${width * initialPosition * step}px)`,
+    };
 
-export default Carousel;
+    return (
+      <div className="Carousel" style={carouselStyle}>
+        <ul className="Carousel__list" style={carouselListStyle}>
+          {images.map((image, imgIndex) => (
+            <li className="Carousel__item">
+              <img src={image} alt={imgIndex} />
+            </li>
+          ))}
+        </ul>
+
+        <button type="button">Prev</button>
+        <button type="button" onClick={next}>Next</button>
+      </div>
+    );
+  }
+}
+
+Carousel.propTypes = {
+  images: PropTypes.arrayOf(PropTypes.number).isRequired,
+  width: PropTypes.number.isRequired,
+  framesize: PropTypes.number.isRequired,
+  step: PropTypes.number.isRequired,
+  animation: PropTypes.number.isRequired,
+};
