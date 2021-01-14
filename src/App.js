670fa74f4087d90fd41958c2323eaacbcbd62d45
Lora Levitska <larakad94@gmail.com>
2021-01-14 16:12:03 +0200

    testing commit

--- src/App.js
+++ src/App.js
@@ -17,17 +17,58 @@ class App extends React.Component {
       './img/9.png',
       './img/10.png',
     ],
+    width: 130,
+    count: 3,
+    position: -390,
   };
 
+  nextImage = () => {
+    this.setState(state => ({
+      position: Math
+        .max(state.position - (state.width * state.count), -state.width
+          * (document.querySelectorAll('li').length - state.count)),
+    }));
+    document.querySelector('ul')
+      .style.marginLeft = `${this.state.position}px`;
+  }
+
+  prevImage = () => {
+    this.setState(state => ({
+      position: Math.min(state.position + (state.width * state.count), 0),
+    }));
+    document.querySelector('ul')
+      .style.marginLeft = `${this.state.position}px`;
+  }
+
   render() {
-    const { images } = this.state;
+    const { images, width, count } = this.state;
 
     return (
       <div className="App">
-        {/* eslint-disable-next-line */}
-        <h1>Carousel with {images.length} images</h1>
+        <h1>{`Carousel with ${images.length} images`}</h1>
+        <div className="App__container">
+          <button
+            type="button"
+            className="button"
+            onClick={this.prevImage}
+          >
+            &#8656;
+          </button>
 
-        <Carousel />
+          <div className="Carousel">
+            <ul className="Carousel__list">
+              <Carousel images={images} width={width} count={count} />
+            </ul>
+          </div>
+
+          <button
+            type="button"
+            className="button"
+            onClick={this.nextImage}
+          >
+            &#8658;
+          </button>
+        </div>
       </div>
     );
   }
--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,19 +1,48 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
 import './Carousel.scss';
 
-const Carousel = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+const Carousel = ({ images, width, count }) => (
+  <>
+    {images.map((elem, index) => (
+      <li
+        key={elem + 1}
+        className="Carousel__list-element"
+      >
+        <img
+          src={elem}
+          alt={index + 1}
+          className="Carousel__list-picture"
+        />
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
+        <span style={{
+          position: 'absolute',
+          left: '0',
+          top: '0',
+        }}
+        >
+          {index + 1}
+        </span>
+      </li>
+    ))}
+  </>
+  // <div className="Carousel">
+  //   <ul className="Carousel__list">
+  //     <li><img src="./img/1.png" alt="1" /></li>
+  //     <li><img src="./img/1.png" alt="2" /></li>
+  //     <li><img src="./img/1.png" alt="3" /></li>
+  //     <li><img src="./img/1.png" alt="4" /></li>
+  //   </ul>
+  //
+  //   <button type="button">Prev</button>
+  //   <button type="button">Next</button>
+  // </div>
 );
 
 export default Carousel;
+
+Carousel.propTypes = {
+  images: PropTypes.arrayOf(PropTypes.string).isRequired,
+  width: PropTypes.number.isRequired,
+  count: PropTypes.number.isRequired,
+};
