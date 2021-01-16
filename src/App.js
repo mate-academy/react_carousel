9c0254793ebbfa153ecd074ea3084387152f0e5e
tamara ostapets <tamara.ostapec@gmail.com>
2021-01-16 16:59:45 +0200

    Carousel

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.scss';
 
-import Carousel from './components/Carousel';
+import { Carousel } from './components/Carousel';
 
 class App extends React.Component {
   state = {
@@ -27,7 +27,9 @@ class App extends React.Component {
         {/* eslint-disable-next-line */}
         <h1>Carousel with {images.length} images</h1>
 
-        <Carousel />
+        <Carousel
+          images={[...images]}
+        />
       </div>
     );
   }
--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,19 +1,76 @@
+import PropTypes, { string } from 'prop-types';
 import React from 'react';
-
 import './Carousel.scss';
 
-const Carousel = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+export class Carousel extends React.Component {
+  static propTypes = {
+    images: PropTypes.arrayOf(string).isRequired,
+  }
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+  state = {
+    frameSize: 3,
+    position: 0,
+    itemWidth: 130,
+  }
 
-export default Carousel;
+  getNumber = stringAttr => stringAttr.replace('/img/', '').split('.')[1]
+
+  shiftPrev = () => {
+    const ul = document.querySelector('.Carousel__list');
+    // eslint-disable-next-line
+    this.setState(prevState => (this.state.position === 0
+      // eslint-disable-next-line
+      ? { position: -(this.state.itemWidth * 7) }
+      // eslint-disable-next-line
+      : { position: Math.min(prevState.position + this.state.itemWidth * this.state.frameSize, 0) }));
+
+    ul.style.marginLeft = `${this.state.position}px`;
+  }
+
+  shiftNext = () => {
+    const ul = document.querySelector('.Carousel__list');
+
+    // eslint-disable-next-line
+    this.setState(prevState => (this.state.position <= -(this.state.itemWidth * 7)
+      ? { position: 0 }
+      // eslint-disable-next-line
+      : { position: Math.max(prevState.position - this.state.itemWidth * this.state.frameSize, -this.state.itemWidth * (this.props.images.length - this.state.frameSize)) }));
+
+    ul.style.marginLeft = `${this.state.position}px`;
+  }
+
+  render() {
+    const { images } = this.props;
+
+    return (
+      <div className="Carousel">
+        <ul className="Carousel__list">
+          {images.map(image => (
+            <li
+              className="Carousel__list-tiem"
+              key={this.getNumber(image)}
+            >
+              <img
+                className="Carousel__img"
+                src={image}
+                alt={this.getNumber(image)}
+              />
+            </li>
+          ))}
+        </ul>
+        <div className="Carousel__buttons">
+          <button
+            type="button"
+            className="Carousel__button Carousel__button--prev"
+            onClick={this.shiftPrev}
+          />
+          <button
+            type="button"
+            className="Carousel__button Carousel__button--next"
+            onClick={this.shiftNext}
+          />
+        </div>
+      </div>
+    );
+  }
+}
