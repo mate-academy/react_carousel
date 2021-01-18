14cb74799dbbc76ae4b85049bc61646a791ea1f9
Lora Levitska <larakad94@gmail.com>
2021-01-18 19:22:55 +0200

    fixed solution by recomendation

--- src/App.js
+++ src/App.js
@@ -17,32 +17,7 @@ class App extends React.Component {
       './img/9.png',
       './img/10.png',
     ],
-    itemWidth: 130,
-    frameSize: 3,
-    step: 3,
-    position: -390,
-    infinite: true,
   };
-
-  nextImage = () => {
-    this.setState(state => ({
-      position: state.infinite === true && state.position === -910 ? 0 : Math
-        .max(state.position - (state.itemWidth * state.frameSize),
-          -state.itemWidth * (document
-            .querySelectorAll('li').length - state.step)),
-    }));
-    document.querySelector('ul')
-      .style.marginLeft = `${this.state.position}px`;
-  }
-
-  prevImage = () => {
-    this.setState(state => ({
-      position: state.position === 0 ? -910 : Math.min(state.position
-        + (state.itemWidth * state.frameSize), 0),
-    }));
-    document.querySelector('ul')
-      .style.marginLeft = `${this.state.position}px`;
-  }
 
   render() {
     const { images } = this.state;
@@ -50,29 +25,14 @@ class App extends React.Component {
     return (
       <div className="App">
         <h1>{`Carousel with ${images.length} images`}</h1>
-        <div className="App__container">
-          <button
-            type="button"
-            className="button"
-            onClick={this.prevImage}
-          >
-            &#8656;
-          </button>
-
-          <div className="Carousel">
-            <ul className="Carousel__list">
-              <Carousel images={images} />
-            </ul>
-          </div>
-
-          <button
-            type="button"
-            className="button"
-            onClick={this.nextImage}
-          >
-            &#8658;
-          </button>
-        </div>
+        <Carousel
+          images={images}
+          step={3}
+          frameSize={3}
+          itemWidth={130}
+          animationDuration={1000}
+          infinite
+        />
       </div>
     );
   }
--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -2,36 +2,121 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Carousel.scss';
 
-const Carousel = ({ images }) => (
-  <>
-    {images.map((elem, index) => (
-      <li
-        key={elem + 1}
-        className="Carousel__list-element"
-      >
-        <img
-          src={elem}
-          alt={index + 1}
-          className="Carousel__list-picture"
-        />
+class Carousel extends React.Component {
+  state = {
+    itemWidth: this.props.itemWidth,
+    frameSize: this.props.frameSize,
+    step: this.props.step,
+    position: 0,
+    animationDuration: this.props.animationDuration,
+    infinite: this.props.infinite,
+    maxPosition: this.props.itemWidth * this.props.images.length
+      - this.props.itemWidth * this.props.frameSize,
+  }
 
-        <span style={{
-          position: 'absolute',
-          left: '0',
-          top: '0',
+  nextImage = () => {
+    this.setState(({ infinite,
+      position,
+      itemWidth,
+      frameSize,
+      step,
+      maxPosition }) => ({
+      position: infinite === true
+      && position === -maxPosition
+        ? 0
+        : Math.max((position - itemWidth * frameSize), -itemWidth
+          * (this.props.images.length - step)),
+    }));
+  }
+
+  prevImage = () => {
+    this.setState(({ infinite,
+      position,
+      itemWidth,
+      frameSize,
+      maxPosition }) => ({
+      position: infinite === true
+      && position === 0
+        ? -maxPosition
+        : Math.min(position + (itemWidth * frameSize), 0),
+    }));
+  }
+
+  render() {
+    const { frameSize, itemWidth, position, animationDuration } = this.state;
+    const { images } = this.props;
+
+    return (
+      <div
+        className="App__container"
+        style={{
+          width: `${frameSize * itemWidth + 65}px`,
         }}
+      >
+        <button
+          type="button"
+          className="button"
+          onClick={this.prevImage}
         >
-          {index + 1}
-        </span>
-      </li>
-    ))}
-  </>
-);
+          &#8656;
+        </button>
+
+        <div
+          className="Carousel"
+          style={{
+            width: `${frameSize * itemWidth}px`,
+          }}
+        >
+          <ul
+            className="Carousel__list"
+            style={{
+              marginLeft: position,
+              transitionDuration: `position ${animationDuration}s`,
+            }}
+          >
+            {images.map((elem, index) => (
+              <li
+                key={elem + 1}
+                className="Carousel__list-element"
+              >
+                <img
+                  src={elem}
+                  alt={index + 1}
+                  className="Carousel__list-picture"
+                />
+
+                <span style={{
+                  position: 'absolute',
+                  left: '0',
+                  top: '0',
+                }}
+                >
+                  {index + 1}
+                </span>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <button
+          type="button"
+          className="button"
+          onClick={this.nextImage}
+        >
+          &#8658;
+        </button>
+      </div>
+    );
+  }
+}
 
 export default Carousel;
 
 Carousel.propTypes = {
   images: PropTypes.arrayOf(PropTypes.string).isRequired,
-  // width: PropTypes.number.isRequired,
-  // count: PropTypes.number.isRequired,
+  itemWidth: PropTypes.number.isRequired,
+  step: PropTypes.number.isRequired,
+  frameSize: PropTypes.number.isRequired,
+  animationDuration: PropTypes.number.isRequired,
+  infinite: PropTypes.bool.isRequired,
 };
