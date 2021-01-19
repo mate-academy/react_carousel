021f6909fe18d9c27ecd12a5379bd752c7b8741d
MarchD <dubchmar@gmail.com>
2021-01-19 17:03:35 +0200

    added the attempt number one

--- src/App.js
+++ src/App.js
@@ -17,17 +17,39 @@ class App extends React.Component {
       './img/9.png',
       './img/10.png',
     ],
+    step: 3,
+    frameSize: 3,
+    itemWidth: 130,
+    animationDuration: 1000,
   };
 
   render() {
-    const { images } = this.state;
+    const {
+      images,
+      animationDuration,
+      frameSize,
+      itemWidth,
+      step,
+    } = this.state;
 
     return (
       <div className="App">
-        {/* eslint-disable-next-line */}
-        <h1>Carousel with {images.length} images</h1>
+        <h1>
+          Carousel with
+          {images.length}
+          {' '}
+          images
+        </h1>
+        <div className="container">
+          <Carousel
+            images={images}
+            step={step}
+            frameSize={frameSize}
+            itemWidth={itemWidth}
+            animationDuration={animationDuration}
+          />
+        </div>
 
-        <Carousel />
       </div>
     );
   }
--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,19 +1,95 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
 import './Carousel.scss';
 
-const Carousel = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+class Carousel extends React.Component {
+  state = {
+    transform: 0,
+  }
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+  toNext = () => {
+    const { itemWidth, step, images } = this.props;
+    const limit = -itemWidth * (images.length - step);
+
+    this.setState((state) => {
+      let widthOfOneTurn = state.transform - (itemWidth * step);
+
+      if (widthOfOneTurn < limit) {
+        widthOfOneTurn = limit;
+      }
+
+      return { transform: widthOfOneTurn };
+    });
+  }
+
+  toPrev = () => {
+    const { itemWidth, step } = this.props;
+
+    this.setState((state) => {
+      let widthOfOneTurn = state.transform + (itemWidth * step);
+
+      if (widthOfOneTurn > 0) {
+        widthOfOneTurn = 0;
+      }
+
+      return { transform: widthOfOneTurn };
+    });
+  }
+
+  render() {
+    const {
+      images,
+      animationDuration,
+      itemWidth,
+      frameSize,
+      step,
+    } = this.props;
+    const { transform } = this.state;
+    const limit = -itemWidth * (images.length - step);
+
+    return (
+      <div className="wrapper" style={{ width: `${itemWidth * frameSize}px` }}>
+        <ul
+          className="Carousel__list"
+          style={{
+            transform: `translateX(${this.state.transform}px)`,
+            transition: `transform ${animationDuration}ms`,
+          }}
+        >
+          {images.map(img => (
+            <li key={img}>
+              <img src={img} alt="1" style={{ width: `${itemWidth}px` }} />
+            </li>
+          ))}
+        </ul>
+
+        <button
+          type="button"
+          onClick={this.toPrev}
+          disabled={transform === 0}
+        >
+          Prev
+        </button>
+        <button
+          type="button"
+          onClick={this.toNext}
+          disabled={transform === limit}
+        >
+          Next
+        </button>
+      </div>
+    );
+  }
+}
 
 export default Carousel;
+
+Carousel.propTypes = {
+  images: PropTypes.arrayOf(
+    PropTypes.string.isRequired,
+  ).isRequired,
+  animationDuration: PropTypes.number.isRequired,
+  itemWidth: PropTypes.number.isRequired,
+  frameSize: PropTypes.number.isRequired,
+  step: PropTypes.number.isRequired,
+};
