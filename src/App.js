92f8ab1457b2c696817d858e761cd288cd5c3ad4
Olga Rubalska <olga.anikeieva@gmail.com>
2021-01-25 15:10:21 +0200

    Solution

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import React from 'react';
 import './App.scss';
 
@@ -17,17 +18,105 @@ class App extends React.Component {
       './img/9.png',
       './img/10.png',
     ],
+    step: 2,
+    frameSize: 3,
+    itemWidth: 100,
+    animationDuration: 1000,
   };
 
+  // eslint-disable-next-line class-methods-use-this
+  generateArray(arrayOfImages) {
+    const { length } = arrayOfImages;
+    const firstPartIMGArray = arrayOfImages.slice(0, length / 2);
+    const secondPartIMGArray = arrayOfImages.slice(length / 2);
+
+    const array = secondPartIMGArray.concat(arrayOfImages);
+
+    return array.concat(firstPartIMGArray);
+  }
+
   render() {
-    const { images } = this.state;
+    const
+      { images,
+        step,
+        frameSize,
+        itemWidth,
+        animationDuration } = this.state;
+
+    const newIMGarray = this.generateArray(images);
 
     return (
       <div className="App">
         {/* eslint-disable-next-line */}
         <h1>Carousel with {images.length} images</h1>
+        <div className="Labels">
+          <label>
+            Enter step:
+            {' '}
+            <input
+              type="number"
+              value={step}
+              onChange={(event) => {
+                const valueSet = (event.target.value > 2 || event.target.value <= 0)
+                  ? 2 : event.target.value;
 
-        <Carousel />
+                this.setState({ step: Number(valueSet) });
+              }}
+            />
+          </label>
+          <label>
+            Enter frame size:
+            {' '}
+            <input
+              type="number"
+              value={frameSize}
+              onChange={(event) => {
+                const valueSet = (event.target.value > 5 || event.target.value <= 0)
+                  ? 5 : event.target.value;
+
+                this.setState({ frameSize: Number(valueSet) });
+              }}
+            />
+          </label>
+          <label>
+            Enter frame width:
+            {' '}
+            <input
+              type="number"
+              value={itemWidth}
+              onChange={(event) => {
+                const valueSet = (event.target.value > 200 || event.target.value < 50)
+                  ? 130 : event.target.value;
+
+                this.setState({ itemWidth: Number(valueSet) });
+              }}
+              step="50"
+            />
+          </label>
+          <label>
+            Animation in ms:
+            {' '}
+            <input
+              type="number"
+              value={animationDuration}
+              onChange={(event) => {
+                const valueSet = (event.target.value < 200)
+                  ? 1000 : event.target.value;
+
+                this.setState({ animationDuration: Number(valueSet) });
+              }}
+              step="100"
+            />
+          </label>
+        </div>
+
+        <Carousel
+          images={newIMGarray}
+          step={step}
+          frameSize={frameSize}
+          itemWidth={itemWidth}
+          animationDuration={animationDuration}
+        />
       </div>
     );
   }
--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,19 +1,146 @@
+/* eslint-disable max-len */
 import React from 'react';
-
+import PropTypes from 'prop-types';
 import './Carousel.scss';
 
-const Carousel = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+// eslint-disable-next-line react/prefer-stateless-function
+class Carousel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      scrollWidth: -props.itemWidth * Math.floor(props.images.length / 4),
+      imagesState: props.images,
+      animationDuration: props.animationDuration / 1000,
+      containerSize: props.itemWidth * props.images.length,
+    };
+  }
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+  componentWillReceiveProps(props) {
+    document.querySelector('.Carousel').style.transition = '0s';
+    this.setState({
+      scrollWidth: -props.itemWidth * Math.floor(props.images.length / 4),
+      containerSize: props.itemWidth * props.images.length,
+      animationDuration: props.animationDuration / 1000,
+    });
+    setTimeout(() => {
+      document.querySelector('.Carousel').style.transition = `{this.state.animationDuration}s`;
+    }, 10);
+  }
+
+  setContainerSize = (imgWidth, arrayImages) => imgWidth * arrayImages.length;
+
+  scrollLeft = (e) => {
+    const buttonNext = e.target;
+    let deletedArrayItems;
+    const { scrollWidth, containerSize } = this.state;
+    const { itemWidth } = this.props;
+    const boundValue = (containerSize - (5 * itemWidth));
+
+    const container = document.querySelector('.Carousel');
+
+    this.setState((state, props) => {
+      buttonNext.disabled = 'true';
+
+      return {
+        scrollWidth: state.scrollWidth - props.itemWidth * props.step,
+      };
+    });
+
+    setTimeout(() => {
+      buttonNext.disabled = !buttonNext.disabled;
+    }, 200);
+
+    if (scrollWidth <= -boundValue) {
+      container.style.transition = '0s';
+      buttonNext.disabled = 'true';
+
+      this.setState((state, props) => {
+        deletedArrayItems = state.imagesState.splice(0, 10);
+
+        return {
+          imagesState: state.imagesState.concat(deletedArrayItems),
+          scrollWidth: -props.itemWidth * 5,
+        };
+      });
+
+      setTimeout(() => {
+        container.style.transition = `{this.state.animationDuration}s`;
+        buttonNext.disabled = !buttonNext.disabled;
+      }, 200);
+      setTimeout(() => {
+        buttonNext.disabled = !buttonNext.disabled;
+      }, 10);
+    }
+  };
+
+  render() {
+    const {
+      frameSize,
+      itemWidth,
+    } = this.props;
+
+    const
+      { scrollWidth,
+        imagesState,
+        animationDuration } = this.state;
+
+    return (
+      <>
+        <div
+          className="Carousel_container"
+          style={{
+            width: `${frameSize * itemWidth}px`,
+            height: `${itemWidth}px`,
+          }}
+        >
+          <div
+            className="Carousel"
+            style={{
+              transform: `translate(${scrollWidth}px)`,
+              transitionDuration: `${animationDuration}s`,
+            }}
+          >
+            {imagesState.map((image, index) => (
+              <div
+                // eslint-disable-next-line react/no-array-index-key
+                key={`${image} - ${index}`}
+              >
+                <img
+                  src={image}
+                  alt={index + 1}
+                  style={{
+                    width: `${itemWidth}px`,
+                    height: `${itemWidth}px`,
+                  }}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="Carousel_buttons">
+          <button
+            type="button"
+            onClick={(e) => {
+              this.scrollLeft(e);
+            }}
+          >
+            Next
+          </button>
+        </div>
+      </>
+    );
+  }
+}
+
+Carousel.propTypes = {
+  images: PropTypes.arrayOf(
+    PropTypes.string.isRequired,
+  ).isRequired,
+  step: PropTypes.number.isRequired,
+  frameSize: PropTypes.number.isRequired,
+  itemWidth: PropTypes.number.isRequired,
+  animationDuration: PropTypes.number.isRequired,
+
+};
 
 export default Carousel;
