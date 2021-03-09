806e871b296eecbd236aaddb17a8ee3fca6868e9
Bohdan Dobak <chudolipton@gmail.com>
2021-03-09 11:25:29 +0200

    made simple carousel in react

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,19 +1,101 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
-
+import PropTypes from 'prop-types';
 import './Carousel.scss';
 
-const Carousel = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+export class Carousel extends React.Component {
+  state = {
+    images: this.props.images,
+    itemWidth: 130,
+    frameSize: 3,
+    step: 3,
+    animationDuration: 1000,
+    translateX: 0,
+  }
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+  handlePrev = () => {
+    const {
+      itemWidth,
+      step,
+      translateX,
+    } = this.state;
 
-export default Carousel;
+    if (translateX >= 0) {
+      this.setState({
+        translateX: 0,
+      });
+    }
+
+    this.setState(prevState => ({
+      translateX: Math.min(prevState.translateX + itemWidth * step, 0),
+    }));
+  }
+
+  handleNext = () => {
+    const {
+      itemWidth,
+      images,
+      step,
+      frameSize,
+      translateX,
+    } = this.state;
+
+    const maxWidth = images.length * itemWidth - (frameSize * itemWidth);
+
+    if (translateX >= maxWidth) {
+      this.setState({
+        translateX: -maxWidth,
+      });
+    }
+
+    this.setState(prevState => ({
+      translateX: Math.max(
+        prevState.translateX - itemWidth * step,
+        -itemWidth * (images.length - step),
+      ),
+    }));
+  }
+
+  render() {
+    const {
+      images,
+      itemWidth,
+      frameSize,
+      animationDuration,
+      translateX,
+    } = this.state;
+
+    const style = {
+      transition: `transform ${animationDuration}ms`,
+      transform: `translateX(${translateX}px)`,
+    };
+
+    const width = {
+      /* stylelint-disable */
+      width: `${itemWidth * frameSize}px`,
+    };
+
+    return (
+      <div className="Carousel" style={width}>
+        <ul className="Carousel__list" style={style}>
+          {images.map((image, index) => (
+            <li key={image}>
+              <img
+                src={image}
+                alt={index}
+                style={{ width: `${itemWidth}px` }}
+              />
+            </li>
+          ))}
+        </ul>
+
+        <button type="button" onClick={this.handlePrev}>Prev</button>
+        <button type="button" onClick={this.handleNext}>Next</button>
+      </div>
+    );
+  }
+}
+
+Carousel.propTypes = {
+  images: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
