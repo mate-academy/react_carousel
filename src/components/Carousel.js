32cdabd86326f4b2399fa6bde6a24eb053dc7791
chri911 <delightsome@bigmir.net>
2021-03-09 20:41:40 +0200

    solution

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,19 +1,83 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
 import './Carousel.scss';
 
-const Carousel = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+export class Carousel extends React.Component {
+  state = {
+    transform: 0,
+  }
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+  showNext = () => {
+    const { images, step, itemWidth, frameSize } = this.props;
+    const scroll = itemWidth * step;
+    const maxLength = (images.length - frameSize) * itemWidth;
 
-export default Carousel;
+    this.setState(state => ({
+      transform: state.transform - scroll < -maxLength
+        ? -maxLength
+        : state.transform - scroll,
+    }));
+  }
+
+  showPrevious = () => {
+    const { step, itemWidth } = this.props;
+    const scroll = itemWidth * step;
+
+    this.setState(state => ({
+      transform: state.transform + scroll > 0 ? 0 : state.transform + scroll,
+    }));
+  }
+
+  render() {
+    const { images, itemWidth, frameSize, animationDuration } = this.props;
+
+    return (
+      <div
+        className="Carousel"
+        style={{ width: `${itemWidth * frameSize}px` }}
+      >
+        <ul
+          className="Carousel__list"
+          style={{
+            transform: `translateX(${this.state.transform}px)`,
+            transitionDuration: `${animationDuration}ms`,
+          }}
+        >
+          {
+            images.map((image, index) => (
+              <li
+                key={image}
+                style={{ width: `${itemWidth}px` }}
+              >
+                <img src={image} alt={index} />
+              </li>
+            ))}
+        </ul>
+        <div className="Carousel__buttons">
+          <button type="button" onClick={this.showPrevious}>
+            Prev
+          </button>
+          <button type="button" onClick={this.showNext}>
+            Next
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+Carousel.propTypes = {
+  images: PropTypes.arrayOf(
+    PropTypes.string.isRequired,
+  ).isRequired,
+  step: PropTypes.number,
+  itemWidth: PropTypes.number.isRequired,
+  frameSize: PropTypes.number,
+  animationDuration: PropTypes.number,
+};
+
+Carousel.defaultProps = {
+  step: 3,
+  frameSize: 3,
+  animationDuration: 1000,
+};
