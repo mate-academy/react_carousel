fc290c9a2c01187a650d5504feeaa3eba578156f
Taras <seman6745@gmail.com>
2021-03-10 19:53:03 +0200

    carusel

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,19 +1,82 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import './Carousel.scss';
 
-const Carousel = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+export class Carousel extends React.Component {
+  state = {
+    position: 0,
+  }
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+  goForward = () => {
+    const { images, step, itemWidth, framesize } = this.props;
 
-export default Carousel;
+    const scrollStep = itemWidth * step;
+    const maxLength = (images.length - framesize) * itemWidth;
+
+    this.setState(prevState => ({
+      position: (scrollStep - prevState.position > maxLength)
+        ? -maxLength
+        : prevState.position - scrollStep,
+    }));
+  }
+
+  goBack = () => {
+    const { step, itemWidth } = this.props;
+    const scrollStep = itemWidth * step;
+
+    this.setState(prevState => ({
+      position: (prevState.position + scrollStep > 0)
+        ? 0
+        : prevState.position + scrollStep,
+    }));
+  }
+
+  render() {
+    const { images, itemWidth, framesize, animationDuration } = this.props;
+
+    return (
+      <div
+        className="Carousel"
+        style={{ width: `${itemWidth * framesize}px` }}
+      >
+        <ul
+          className="Carousel__list"
+          style={{
+            transform: `translateX(${this.state.position}px)`,
+            transitionDuration: `${animationDuration}ms`,
+          }}
+        >
+          {images.map((image, index) => (
+            <li key={image}>
+              <img src={image} alt={index} />
+            </li>
+          ))}
+        </ul>
+
+        <button
+          type="button"
+          onClick={this.goBack}
+        >
+          Prev
+        </button>
+        <button
+          type="button"
+          onClick={this.goForward}
+        >
+          Next
+        </button>
+      </div>
+    );
+  }
+}
+
+Carousel.propTypes = {
+  images: PropTypes.arrayOf(
+    PropTypes.string.isRequired,
+  ).isRequired,
+  step: PropTypes.number.isRequired,
+  itemWidth: PropTypes.number.isRequired,
+  framesize: PropTypes.number.isRequired,
+  animationDuration: PropTypes.number.isRequired,
+};
