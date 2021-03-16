02fa9d9eca8033a6eb66c097bda5d86d191db5a4
Ruslan Shmadchenko <ruslanwpua@gmail.com>
2021-03-17 00:44:16 +0200

    solution

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,19 +1,95 @@
 import React from 'react';
+import propTypes from 'prop-types';
 
 import './Carousel.scss';
 
-const Carousel = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+export class Carousel extends React.Component {
+  state = {
+    position: 0,
+  }
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+  nextClickHandler = () => {
+    const { itemWidth, images, step } = this.props;
+    const maxWidth = itemWidth * images.length;
+    const picturesSectionWidth = step * itemWidth;
 
-export default Carousel;
+    this.setState((prevState) => {
+      if (maxWidth - picturesSectionWidth < prevState.position) {
+        return false;
+      }
+
+      return {
+        position: prevState.position + (step * itemWidth),
+      };
+    });
+  }
+
+  prevClickHandler = () => {
+    this.setState((prevState) => {
+      const { step, itemWidth } = this.props;
+
+      if (prevState.position <= 0) {
+        return false;
+      }
+
+      return {
+        position: prevState.position - (step * itemWidth),
+      };
+    });
+  }
+
+  render() {
+    const { images, itemWidth, frameSize } = this.props;
+    const { position } = this.state;
+
+    return (
+      <div
+        className="carousel"
+        style={{
+          maxWidth: `${itemWidth * frameSize}px`,
+        }}
+      >
+        <ul
+          className="carousel__list"
+          style={{
+            width: `${itemWidth * images.length}px`,
+            marginLeft: `${-position}px`,
+          }}
+        >
+          {images.map((image, i) => (
+            <li key={image} className="carousel__item">
+              <img
+                src={image}
+                width={`${itemWidth}px`}
+                alt={i + 1}
+              />
+            </li>
+          ))}
+        </ul>
+
+        <button
+          type="button"
+          onClick={this.prevClickHandler}
+        >
+          Prev
+        </button>
+        <button
+          type="button"
+          onClick={this.nextClickHandler}
+        >
+          Next
+        </button>
+      </div>
+    );
+  }
+}
+
+Carousel.propTypes = {
+  images: propTypes.arrayOf(
+    propTypes.string.isRequired,
+  ).isRequired,
+
+  step: propTypes.number.isRequired,
+  frameSize: propTypes.number.isRequired,
+  itemWidth: propTypes.number.isRequired,
+};
