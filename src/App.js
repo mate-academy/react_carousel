fab394de0aedc99ec38ff1783a15bf6ae6077559
Anton Kostylov <antoniok003@gmail.com>
2021-05-21 13:59:04 +0300

    Solution

--- src/App.js
+++ src/App.js
@@ -17,17 +17,134 @@ class App extends React.Component {
       './img/9.png',
       './img/10.png',
     ],
+    step: 3,
+    frameSize: 3,
+    itemWidth: 130,
+    animationDuration: 1000,
+    infinite: false,
+    translate: 0,
   };
 
   render() {
-    const { images } = this.state;
+    const { images, step, frameSize, itemWidth,
+      animationDuration, infinite, translate } = this.state;
+
+    const width = itemWidth * step;
+    const slidesCount = Math.ceil(10 / step);
+    const max = width * slidesCount - width;
+
+    const nextSlide = () => {
+      if (translate > -max) {
+        this.setState(state => ({
+          translate: state.translate - width,
+        }));
+      } else if (translate === -max && infinite) {
+        this.setState({
+          translate: 0,
+        });
+      }
+    };
+
+    const prevSlide = () => {
+      if (translate === 0 && infinite) {
+        this.setState({
+          translate: -max,
+        });
+      } else if (translate < 0) {
+        this.setState(state => ({
+          translate: state.translate + width,
+        }));
+      }
+    };
 
     return (
       <div className="App">
-        {/* eslint-disable-next-line */}
-        <h1>Carousel with {images.length} images</h1>
+        <h1 className="App__header">
+          {/* eslint-disable-next-line */}
+          Carousel with {images.length} images
+        </h1>
 
-        <Carousel />
+        <Carousel
+          images={images}
+          frameSize={frameSize}
+          itemWidth={itemWidth}
+          animationDuration={animationDuration}
+          translate={translate}
+          nextSlide={nextSlide}
+          prevSlide={prevSlide}
+        />
+        <div className="App__inputFields">
+          <label htmlFor="step">
+            Number of images scrolled per click
+          </label>
+          <input
+            type="number"
+            defaultValue={step}
+            name="step"
+            id="step"
+            onChange={({ target }) => {
+              this.setState({
+                step: target.value,
+              });
+            }}
+          />
+          <label htmlFor="frameSize">
+            Number of images displayed at the same time
+          </label>
+          <input
+            type="number"
+            defaultValue={frameSize}
+            name="frameSize"
+            id="frameSize"
+            onChange={({ target }) => {
+              this.setState({
+                frameSize: target.value,
+              });
+            }}
+          />
+          <label htmlFor="itemWidth">
+            Width of the one picture
+          </label>
+          <input
+            type="number"
+            defaultValue={itemWidth}
+            name="itemWidth"
+            id="itemWidth"
+            onChange={({ target }) => {
+              this.setState({
+                itemWidth: target.value,
+              });
+            }}
+          />
+          <label htmlFor="animationDuration">
+            Time in ms to show the new portion of images
+          </label>
+          <input
+            type="number"
+            defaultValue={animationDuration}
+            name="animationDuration"
+            id="animationDuration"
+            onChange={({ target }) => {
+              this.setState({
+                animationDuration: target.value,
+              });
+            }}
+          />
+          <label htmlFor="infinite">
+            To do the carousel cyclic
+          </label>
+          <input
+            type="checkbox"
+            name="infinite"
+            id="infinite"
+            defaultChecked={infinite && true}
+            onChange={({ target }) => {
+              this.setState({
+                infinite: target.checked,
+              });
+            }}
+          />
+        </div>
       </div>
     );
   }
