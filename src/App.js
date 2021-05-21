82d9d2db870a1d980768310fcf3c56f3f6718c36
Igor Stasiv <igorstsv@gmail.com>
2021-05-21 18:58:28 +0300

    Solution

--- src/App.js
+++ src/App.js
@@ -27,7 +27,14 @@ class App extends React.Component {
         {/* eslint-disable-next-line */}
         <h1>Carousel with {images.length} images</h1>
 
-        <Carousel />
+        <Carousel
+          images={images}
+          step={3}
+          frameSize={3}
+          itemWidth={130}
+          animationDuration={1000}
+          infinite
+        />
       </div>
     );
   }
