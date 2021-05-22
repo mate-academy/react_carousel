4757dc127ee96626a293b3e91cf59cf9baed948b
Ivan Grekov <grekovv76@gmail.com>
2021-05-22 14:28:25 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -3,6 +3,16 @@ import './App.scss';
 
 import Carousel from './components/Carousel';
 
+const defaultValues = {
+  defaultStep: 3,
+  defaultFrameSize: 3,
+  defaultItemWidth: 130,
+  defaultAnimationDuration: 1000,
+  defaultInfiniteValue: true,
+};
+
+// enterValues()
+
 class App extends React.Component {
   state = {
     images: [
@@ -22,12 +32,55 @@ class App extends React.Component {
   render() {
     const { images } = this.state;
 
+    const {
+      defaultStep,
+      defaultFrameSize,
+      defaultItemWidth,
+      defaultAnimationDuration,
+      defaultInfiniteValue,
+    } = defaultValues;
+
+    let step = defaultStep;
+    let frameSize = defaultFrameSize;
+    let itemWidth = defaultItemWidth;
+    let animationDuration = defaultAnimationDuration;
+    let infinite = defaultInfiniteValue;
+
+    /* eslint-disable */
+    (function setValues() {
+      if (confirm('Do you want set custom params of carousel?')) {
+        step = +prompt('Set scroll step') || defaultStep;
+        frameSize = +prompt('Set number of displayed images')
+          || defaultFrameSize;
+        itemWidth = +prompt('Set image width') || defaultItemWidth;
+        animationDuration = +prompt('Set scroll duration')
+          || defaultAnimationDuration;
+        infinite = confirm('Do you want infinite carousel?');
+      }
+    })();
+    /* eslint-enable */
+
     return (
       <div className="App">
-        {/* eslint-disable-next-line */}
-        <h1>Carousel with {images.length} images</h1>
+        <h1 className="title">
+          {`Carousel with ${images.length} images`}
+        </h1>
 
-        <Carousel />
+        <Carousel
+          images={images}
+          step={step}
+          frameSize={frameSize}
+          itemWidth={itemWidth}
+          animationDuration={animationDuration}
+          infinite={infinite}
+        />
+
+        <button
+          className="changing-button"
+          type="button"
+        >
+          Change Carousel
+        </button>
       </div>
     );
   }
