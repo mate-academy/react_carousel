1528a4615bae1833e56f2769920cd1593e6df841
Artem Dzhereleiko <dgerik2015@gmail.com>
2021-07-20 20:49:17 +0300

    solution

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,19 +1,201 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import './Carousel.scss';
 
-const Carousel = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+class Carousel extends React.Component {
+  state = {
+    animationDuration: this.props.animationDuration,
+    itemWidth: this.props.itemWidth,
+    frameSize: this.props.frameSize,
+    step: this.props.step,
+    infinite: this.props.infinite,
+    maxWidth: this.props.itemWidth * this.props.images.length,
+    scroll: 0,
+  }
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+  clickPrev = () => {
+    this.setState((prevState) => {
+      const maxScroll
+        = prevState.maxWidth - (prevState.itemWidth * prevState.frameSize);
+
+      if (prevState.infinite && prevState.scroll === 0) {
+        return { scroll: maxScroll };
+      }
+
+      if (prevState.scroll - prevState.itemWidth * prevState.step <= 0) {
+        return { scroll: 0 };
+      }
+
+      return { scroll: prevState.scroll
+        - prevState.itemWidth * prevState.step };
+    });
+  };
+
+  clickNext = () => {
+    this.setState((prevState) => {
+      const maxScroll
+        = prevState.maxWidth - (prevState.itemWidth * prevState.frameSize);
+
+      if (prevState.infinite && prevState.scroll === maxScroll) {
+        return { scroll: 0 };
+      }
+
+      if (prevState.scroll + prevState.itemWidth
+        * prevState.step >= maxScroll) {
+        return { scroll: maxScroll };
+      }
+
+      return { scroll: prevState.scroll
+        + prevState.itemWidth * prevState.step };
+    });
+  };
+
+  changeState = (event, atribute) => {
+    // console.log(atribute, event.target.value);
+    this.setState({
+      [atribute]: event.target.value,
+      scroll: 0,
+    });
+  };
+
+  changeStateItemWidth = (event, atribute) => {
+    this.setState({
+      [atribute]: event.target.value,
+      scroll: 0,
+      maxWidth: event.target.value * this.props.images.length,
+    });
+  };
+
+  changeStateInfinite = () => {
+    this.setState(prevState => ({ infinite: !prevState.infinite }));
+  };
+
+  render() {
+    const { images } = this.props;
+
+    const listStyle = {
+      width: `${this.state.frameSize * this.state.itemWidth}px`,
+    };
+
+    const itemStyle = {
+      transition: `transform ${this.state.animationDuration / 1000}s`,
+      transform: `translateX(-${this.state.scroll}px)`,
+    };
+
+    const imageStyle = {
+      width: `${this.state.itemWidth}px`,
+    };
+
+    return (
+      <>
+        <div className="Carousel">
+          <button
+            type="button"
+            className="button"
+            onClick={this.clickPrev}
+          >
+            {'<'}
+          </button>
+
+          <ul className="Carousel__list" style={listStyle}>
+            {images.map(image => (
+              <li key={image} className="Carousel__item" style={itemStyle}>
+                <img
+                  src={image}
+                  alt={image}
+                  className="Carousel__image"
+                  style={imageStyle}
+                />
+              </li>
+            ))}
+          </ul>
+
+          <button
+            type="button"
+            className="button"
+            onClick={this.clickNext}
+          >
+            {'>'}
+          </button>
+        </div>
+        <form className="form-input">
+          <label>
+            FrameSize:
+            <input
+              type="number"
+              defaultValue={this.state.frameSize}
+              min="1"
+              max="7"
+              onChange={event => (this.changeState(event, 'frameSize'))}
+              title="min = 1, max = 7"
+            />
+          </label>
+          <label>
+            Step:
+            <input
+              type="number"
+              defaultValue={this.state.step}
+              min="1"
+              max="5"
+              onChange={event => (this.changeState(event, 'step'))}
+              title="min = 1, max = 5"
+            />
+          </label>
+          <label>
+            Item width:
+            <input
+              type="number"
+              defaultValue={this.state.itemWidth}
+              min="100"
+              max="200"
+              step="10"
+              onChange={event => (
+                this.changeStateItemWidth(event, 'itemWidth')
+              )}
+              title="min = 100, max = 200"
+            />
+          </label>
+          <label>
+            Animation Duration:
+            <input
+              type="number"
+              defaultValue={this.state.animationDuration}
+              min="500"
+              max="5000"
+              step="100"
+              onChange={event => (this.changeState(event, 'animationDuration'))}
+              title="min = 500, max = 5000"
+            />
+          </label>
+          <label>
+            Infinite:
+            <input
+              type="checkbox"
+              onChange={this.changeStateInfinite}
+            />
+          </label>
+        </form>
+      </>
+    );
+  }
+}
+
+Carousel.propTypes = {
+  images: PropTypes.arrayOf(PropTypes.string).isRequired,
+  itemWidth: PropTypes.number,
+  step: PropTypes.number,
+  frameSize: PropTypes.number,
+  animationDuration: PropTypes.number,
+  infinite: PropTypes.bool,
+};
+
+Carousel.defaultProps = {
+  itemWidth: 130,
+  step: 3,
+  frameSize: 3,
+  animationDuration: 1000,
+  infinite: false,
+};
 
 export default Carousel;
