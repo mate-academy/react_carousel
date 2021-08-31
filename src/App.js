23662f475c0b2d221292fc9db8e2d5ef9ad050d6
VasylPerepichai <vperepichay@gmail.com>
2021-08-31 21:21:00 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -27,7 +27,25 @@ class App extends React.Component {
         {/* eslint-disable-next-line */}
         <h1>Carousel with {images.length} images</h1>
 
-        <Carousel />
+        <Carousel
+          images={[
+            './img/1.png',
+            './img/2.png',
+            './img/3.png',
+            './img/4.png',
+            './img/5.png',
+            './img/6.png',
+            './img/7.png',
+            './img/8.png',
+            './img/9.png',
+            './img/10.png',
+          ]}
+          step={1}
+          frameSize={1}
+          itemWidth={150}
+          animationDuration={1000}
+          infinite={false}
+        />
       </div>
     );
   }
--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,19 +1,77 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import './Carousel.scss';
 
-const Carousel = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+class Carousel extends React.Component {
+  carousel = document.querySelector('ul');
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+  state = {
+    counter: 0,
+  }
+
+  scrollCaroselLeft = (step, itemWidth) => {
+    document.querySelector('.Carousel').scrollLeft -= step * itemWidth;
+    this.setState(state => ({ counter: state.counter - 1 }));
+  }
+
+  scrollCaroselRight = (step, itemWidth) => {
+    document.querySelector('.Carousel').scrollLeft += step * itemWidth;
+    this.setState(state => ({ counter: state.counter + 1 }));
+  }
+
+  render() {
+    const { step, itemWidth, frameSize, images } = this.props;
+    const totalStepNumber = Math.floor(images.length / step);
+
+    return (
+      <div className="Container">
+        <button
+          type="button"
+          onClick={() => this.scrollCaroselLeft(step, itemWidth)}
+          disabled={this.state.counter <= 0}
+        >
+          ⇦
+        </button>
+        <div
+          className="Carousel"
+          style={{
+            width: `${itemWidth * frameSize}px`,
+          }}
+        >
+          <ul className="Carousel__list">
+            {images.map((url, index) => (
+              <li>
+                <img
+                  src={url}
+                  alt={index}
+                  key={url}
+                  style={{
+                    width: `${itemWidth}px`,
+                  }}
+                />
+              </li>
+            ))}
+          </ul>
+        </div>
+        <button
+          type="button"
+          onClick={() => this.scrollCaroselRight(step, itemWidth)}
+          disabled={this.state.counter >= totalStepNumber}
+        >
+          ⇨
+        </button>
+        {this.state.counter}
+      </div>
+    );
+  }
+}
 
 export default Carousel;
+
+Carousel.propTypes = {
+  step: PropTypes.number.isRequired,
+  images: PropTypes.arrayOf(PropTypes.string).isRequired,
+  itemWidth: PropTypes.number.isRequired,
+  frameSize: PropTypes.number.isRequired,
+};
