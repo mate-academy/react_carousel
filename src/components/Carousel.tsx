3c9b21d2cbbc41a4d6164ad57ca8fb47d73e44b0
Mariia Obraztsova <mariiaobraztsova99@gmail.com>
2021-11-09 22:45:12 +0200

    Solution

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,18 +1,91 @@
 import React from 'react';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+interface Props {
+  images: string[],
+  step: number,
+  frameSize: number,
+  itemWidth: number,
+  animationDuration: number,
+}
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+interface State {
+  scroll: number,
+  displayWidth: number,
+}
+
+class Carousel extends React.Component<Props, State> {
+  state = {
+    scroll: 0,
+    displayWidth: this.props.frameSize * this.props.itemWidth,
+  };
+
+  scrollNext = (step: number) => {
+    this.setState((prevState: State) => {
+      return {
+        scroll: prevState.scroll - step * this.props.itemWidth,
+      };
+    });
+  };
+
+  scrollPrev = (step: number) => {
+    this.setState((prevState: State) => {
+      return {
+        scroll: prevState.scroll + step * this.props.itemWidth,
+      };
+    });
+  };
+
+  render() {
+    const { images, step, animationDuration } = this.props;
+    const { scroll, displayWidth } = this.state;
+    const isPrevDisabled = (scroll === 0);
+    const maxNextClicks = -displayWidth * Math.floor(images.length / step);
+    const isNextDisabled = (scroll === maxNextClicks);
+
+    return (
+      <div className="Carousel">
+        <div
+          className="imagesContainer"
+          style={{ maxWidth: `${displayWidth}px` }}
+        >
+          <ul
+            className="Carousel__list"
+            style={
+              {
+                transform: `translateX(${scroll}px)`,
+                transition: `transform ${animationDuration}ms`,
+              }
+            }
+          >
+            {images.map(image => (
+              <li><img src={image} alt="smile" /></li>
+            ))}
+          </ul>
+        </div>
+
+        <div className="Carousel__buttons">
+          <button
+            type="button"
+            className="Carousel__button"
+            disabled={isPrevDisabled}
+            onClick={() => this.scrollPrev(step)}
+          >
+            Prev
+          </button>
+
+          <button
+            type="button"
+            className="Carousel__button"
+            disabled={isNextDisabled}
+            onClick={() => this.scrollNext(step)}
+          >
+            Next
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Carousel;
