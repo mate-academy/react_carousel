fb9bb6201c1f884cb56c9852858513bbbdd0a57d
Dariia Romanova <dariia.romanova@gmail.com>
2021-12-11 14:27:12 +0200

    Carousel

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,18 +1,74 @@
 import React from 'react';
+import { CarouselType } from '../types/CarouselType';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+type Props = CarouselType;
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+const Carousel: React.FC<Props> = ({
+  images, step, itemWidth, frameSize, animationDuration,
+}) => {
+  const carousel = React.useRef<HTMLUListElement>(null);
+
+  const scrollNext = () => {
+    if (carousel.current) {
+      if (carousel.current.scrollLeft <= (images.length - frameSize - 1) * itemWidth) {
+        carousel.current.scrollLeft += (step * itemWidth)
+          - (carousel.current.scrollLeft % itemWidth);
+      } else {
+        carousel.current.scrollLeft = 0;
+      }
+    }
+  };
+
+  const scrollPrev = () => {
+    if (carousel.current) {
+      if (carousel.current.scrollLeft !== 0) {
+        carousel.current.scrollLeft -= step * itemWidth
+          + (carousel.current.scrollLeft % itemWidth);
+      } else {
+        carousel.current.scrollLeft = (images.length - frameSize) * itemWidth;
+      }
+    }
+  };
+
+  React.useEffect(() => {
+    if (carousel.current) {
+      carousel.current.style.width = `${frameSize * itemWidth}px`;
+    }
+
+    setInterval(() => {
+      scrollNext();
+    }, animationDuration);
+  });
+
+  return (
+    <div className="Carousel">
+      <button
+        type="button"
+        onClick={scrollPrev}
+        className="Carousel__button Carousel__button--prev"
+      >
+        -
+      </button>
+      <ul
+        className="Carousel__list"
+        ref={carousel}
+      >
+        {
+          images.map((image, index) => (
+            <li key={image} className="Carousel__image"><img src={image} alt={String(index)} /></li>
+          ))
+        }
+      </ul>
+      <button
+        type="button"
+        onClick={scrollNext}
+        className="Carousel__button Carousel__button--next"
+      >
+        -
+      </button>
+    </div>
+  );
+};
 
 export default Carousel;
