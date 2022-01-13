bde686016ae5086262bc5eaa292ff5b23addc87d
Serhii Dvorets <dvorets.sergey@gmail.com>
2022-01-13 22:57:40 +0200

    1

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,12 @@ import './App.scss';
 import Carousel from './components/Carousel';
 
 interface State {
-  images: string[];
+  images: string[],
+  step: number,
+  frameSize: number,
+  itemWidth: number,
+  animationDuration: number,
+  // infinite: boolean,
 }
 
 class App extends React.Component<{}, State> {
@@ -20,17 +25,86 @@ class App extends React.Component<{}, State> {
       './img/9.png',
       './img/10.png',
     ],
+    step: 3,
+    frameSize: 3,
+    itemWidth: 130,
+    animationDuration: 1000,
+    // infinite: false,
   };
 
   render() {
-    const { images } = this.state;
+    const {
+      step,
+      images,
+      frameSize,
+      itemWidth,
+      animationDuration,
+    } = this.state;
 
     return (
       <div className="App">
         {/* eslint-disable-next-line */}
-        <h1>Carousel with {images.length} images</h1>
+        <h1 className='Carousel__title'>Carousel</h1>
 
-        <Carousel />
+        <Carousel
+          step={step}
+          imagesList={images}
+          frameSize={frameSize}
+          itemWidth={itemWidth}
+          animationDuration={animationDuration}
+        />
+        <div className="input__container">
+          <div className="input__label">
+            Choose the number of scrolled images
+          </div>
+          <label htmlFor="step">
+            <input
+              placeholder="3"
+              min={1}
+              max={10}
+              id="step"
+              className="input input__step"
+              type="number"
+              onChange={(event) => {
+                this.setState({ step: +event.target.value });
+              }}
+            />
+          </label>
+        </div>
+        <div className="input__container">
+          <div className="input__label">
+            Set the number of displayed pictures
+          </div>
+          <label htmlFor="frameSize">
+            <input
+              placeholder="3"
+              min={1}
+              max={10}
+              id="frameSize"
+              className="input input__frameSize"
+              type="number"
+              onChange={(event) => {
+                this.setState({ frameSize: +event.target.value });
+              }}
+            />
+          </label>
+        </div>
+        <div className="input__container">
+          <div className="input__label">Set the size of the picture</div>
+          <label htmlFor="frameSize">
+            <input
+              placeholder="130"
+              min={30}
+              max={500}
+              id="frameSize"
+              className="input input__frameSize"
+              type="number"
+              onChange={(event) => {
+                this.setState({ itemWidth: +event.target.value });
+              }}
+            />
+          </label>
+        </div>
       </div>
     );
   }
--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,17 +1,72 @@
 import React from 'react';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+type Props = {
+  step: number,
+  imagesList: string[];
+  frameSize: number;
+  itemWidth: number;
+  animationDuration: number,
+};
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
+const Carousel: React.FC<Props> = (props) => (
+  <div className="container">
+    <button
+      className="button button__prev"
+      type="button"
+      onClick={() => {
+        const imagesContainer: HTMLElement | null = document.querySelector('.Carousel__list');
+
+        if (imagesContainer) {
+          imagesContainer.style.transform += `translateX(${props.itemWidth * props.step}px)`;
+          /* eslint-disable-next-line */
+          console.log(imagesContainer.style.left);
+        }
+      }}
+    >
+      press for
+      <br />
+      <br />
+      <strong>PREV</strong>
+    </button>
+    <div
+      className="Carousel"
+      style={{
+        width: props.frameSize * props.itemWidth,
+      }}
+    >
+      <ul className="Carousel__list" style={{ transition: `transform ${props.animationDuration}ms` }}>
+        {props.imagesList.map((item, index) => (
+          <>
+            <li>
+              <img
+                src={item}
+                alt={index.toString()}
+                className="Carousel__item"
+                style={{ width: props.itemWidth }}
+              />
+            </li>
+          </>
+        ))}
+      </ul>
+    </div>
+    <button
+      className="button button__prev"
+      type="button"
+      onClick={() => {
+        const imagesContainer: HTMLElement | null = document.querySelector('.Carousel__list');
+
+        if (imagesContainer) {
+          imagesContainer.style.transform += `translateX(${-props.itemWidth * props.step}px)`;
+          /* eslint-disable-next-line */
+        }
+      }}
+    >
+      press for
+      <br />
+      <br />
+      <strong>NEXT</strong>
+    </button>
   </div>
 );
 
