0fa1fd1b7288718355bce35ea1fa7f73e4bcc272
Mykhailo Ianush <yanushmisha@gmail.com>
2022-02-16 21:04:33 +0200

    solution

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,18 +1,122 @@
 import React from 'react';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+type Props = {
+  images: string[],
+  step: number,
+  frameSize: number,
+  itemWidth: number,
+  animationDuration: number,
+  infinite: boolean,
+};
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+type State = {
+  currentIndex: number,
+  maxIndex: number,
+};
+
+class Carousel extends React.Component<Props, State> {
+  state: State = {
+    currentIndex: 0,
+    maxIndex: Math.floor(this.props.images.length / this.props.step),
+  };
+
+  next() {
+    const { currentIndex, maxIndex } = this.state;
+
+    if (currentIndex === maxIndex) {
+      return 0;
+    }
+
+    return currentIndex + 1;
+  }
+
+  prev() {
+    const { currentIndex, maxIndex } = this.state;
+
+    if (currentIndex === 0) {
+      return maxIndex;
+    }
+
+    return currentIndex - 1;
+  }
+
+  translate() {
+    const { images, frameSize, step } = this.props;
+    const maxTransform = (-100 * (images.length - frameSize))
+      / frameSize;
+    const currentTransform = (-100 / frameSize) * this.state.currentIndex
+      * step;
+
+    if (currentTransform < maxTransform) {
+      return `translateX(${maxTransform}%)`;
+    }
+
+    return `translateX(${currentTransform}%)`;
+  }
+
+  renderNext() {
+    if (!this.props.infinite) {
+      return this.state.currentIndex !== this.state.maxIndex;
+    }
+
+    return true;
+  }
+
+  renderPrev() {
+    if (!this.props.infinite) {
+      return this.state.currentIndex !== 0;
+    }
+
+    return true;
+  }
+
+  render() {
+    const { itemWidth, frameSize, images } = this.props;
+
+    return (
+      <div
+        className="Carousel"
+        style={{ width: `${itemWidth * frameSize}px` }}
+      >
+        <ul
+          className="Carousel__list"
+          style={{ transform: this.translate() }}
+        >
+          {images.map(imageUrl => (
+            <li key={imageUrl} className="Carousel__item">
+              <img
+                src={imageUrl}
+                alt={imageUrl}
+                width={itemWidth}
+                className="Carousel__img"
+              />
+            </li>
+          ))}
+        </ul>
+
+        {this.renderPrev()
+          && (
+            <button
+              type="button"
+              onClick={() => this.setState({ currentIndex: this.prev() })}
+            >
+              Prev
+            </button>
+          )}
+        {this.renderNext()
+          && (
+            <button
+              type="button"
+              onClick={() => this.setState({ currentIndex: this.next() })}
+            >
+              Next
+            </button>
+          )}
+
+      </div>
+    );
+  }
+}
 
 export default Carousel;
