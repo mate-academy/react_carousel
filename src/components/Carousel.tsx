432de98546e8f362e2b8873a6871ad4fd392798f
trtskvalerie <trtsk.tea.valerie@gmail.com>
2022-03-13 17:32:54 +0200

    scrollLeft implementation

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,18 +1,107 @@
-import React from 'react';
+/* eslint-disable no-console */
+import React, { useRef, useState } from 'react';
+
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+type Props = {
+  images: string[],
+  itemWidth?: number,
+  frameSize?: number,
+  step?: number,
+};
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+const Carousel: React.FC<Props> = ({
+  images, itemWidth = 130, frameSize = 3, step = 3,
+}) => {
+  const [nextIsDisabled, setNextIsDisabled] = useState(false);
+  const [prevIsDisabled, setPrevIsDisabled] = useState(true);
+
+  const stepPixels = itemWidth * step;
+  const frameWidth = itemWidth * frameSize;
+  const scrollMin = 0;
+  const scrollMax = (itemWidth * images.length) - frameWidth;
+
+  const containerRef = useRef(document.createElement('div'));
+
+  const onPrevClick = () => {
+    const scrollTo = Math.round(containerRef.current.scrollLeft - stepPixels);
+
+    if (nextIsDisabled) {
+      setNextIsDisabled(false);
+    }
+
+    if (scrollTo <= scrollMin) {
+      setPrevIsDisabled(true);
+    }
+
+    containerRef.current.scrollLeft = scrollTo;
+  };
+
+  const onNextClick = () => {
+    const scrollTo = Math.round(containerRef.current.scrollLeft + stepPixels);
+
+    if (prevIsDisabled) {
+      setPrevIsDisabled(false);
+    }
+
+    if (scrollTo >= scrollMax) {
+      setNextIsDisabled(true);
+    }
+
+    containerRef.current.scrollLeft = scrollTo;
+  };
+
+  return (
+    <div className="container">
+      <button
+        type="button"
+        className="button"
+        disabled={prevIsDisabled}
+        onClick={onPrevClick}
+      >
+        ←
+      </button>
+
+      <div
+        className="img-container"
+        ref={containerRef}
+        style={{ width: `${frameWidth}px` }}
+      >
+        <div className="Carousel">
+          <ul className="Carousel__list">
+            {images.map(img => {
+              const key = img.match(/\d+/g) as string[];
+
+              return (
+                <li key={key[0]} className="Carousel__item">
+                  <img
+                    src={img}
+                    alt={key[0]}
+                    style={{ width: `${itemWidth}px` }}
+                  />
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+
+      <button
+        type="button"
+        className="button"
+        disabled={nextIsDisabled}
+        onClick={onNextClick}
+      >
+        →
+      </button>
+    </div>
+  );
+};
+
+Carousel.defaultProps = {
+  itemWidth: 130,
+  frameSize: 3,
+  step: 3,
+};
 
 export default Carousel;
