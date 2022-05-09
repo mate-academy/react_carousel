02444a09308f9967fe98e832c7db7fd07097698f
Max Nikiforov <maxxnikiforov@gmail.com>
2022-05-09 14:55:39 +0700

    Caruse solution

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,18 +1,119 @@
 import React from 'react';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+type Props = {
+  images: string[];
+  frameSize: number;
+  itemWidth: number;
+  animationDuration: number;
+  step: number;
+  infinite: boolean;
+};
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+type State = {
+  position: number;
+};
 
-export default Carousel;
+export class Carousel extends React.Component<Props, State> {
+  state = {
+    position: 0,
+  };
+
+  scrollNext = () => {
+    const {
+      frameSize,
+      itemWidth,
+      step,
+      images,
+      infinite,
+    } = this.props;
+    const { position } = this.state;
+    let way = 0;
+
+    way = position + itemWidth * step;
+
+    if (position + step * itemWidth >= images.length * itemWidth - itemWidth
+      || infinite) {
+      way = images.length * itemWidth - frameSize * itemWidth;
+    }
+
+    if (position + frameSize * itemWidth >= images.length * itemWidth
+       - itemWidth) {
+      way = position + itemWidth;
+    }
+
+    if (position + frameSize * itemWidth >= images.length * itemWidth) {
+      way = position;
+    }
+
+    this.setState({
+      position: way,
+    });
+  };
+
+  scrollPrev = () => {
+    const {
+      itemWidth,
+      step,
+      infinite,
+    } = this.props;
+    const { position } = this.state;
+    let way = position - itemWidth * step;
+
+    if (position - itemWidth * step < 0 || infinite === true) {
+      way = 0;
+    }
+
+    this.setState({
+      position: way,
+    });
+  };
+
+  render() {
+    const { position } = this.state;
+    const {
+      images,
+      frameSize,
+      itemWidth,
+      animationDuration,
+    } = this.props;
+
+    return (
+      <div className="Carousel">
+        <ul
+          className="Carousel__list"
+          style={{
+            transform: `translateX(${-position}px)`,
+            transitionDuration: `${animationDuration}ms`,
+          }}
+        >
+          {images.map((image) => (
+            <li key={image}>
+              <img
+                src={image}
+                alt={image}
+                style={{ width: itemWidth }}
+              />
+            </li>
+          ))}
+        </ul>
+        <div className="Carousel__nav" style={{ width: itemWidth * frameSize }}>
+          <button
+            className="Carousel__prev"
+            type="button"
+            onClick={this.scrollPrev}
+          >
+            &apos;
+          </button>
+          <button
+            className="Carousel__next"
+            type="button"
+            onClick={this.scrollNext}
+          >
+            &apos;
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
