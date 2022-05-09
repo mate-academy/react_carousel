20189ab04ade41417c97b499789093e95a8588a4
Yegor Kochetkov <kegor.fx@gmail.com>
2022-05-10 00:11:31 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,21 @@ import React from 'react';
 import './App.scss';
 import Carousel from './components/Carousel';
 
-interface State {
+type Settings = {
+  frameSize: number;
+  itemWidth: number;
+  step: number;
+  animationDuration: number;
+  infinite: boolean;
+};
+
+type State = {
   images: string[];
-}
+  settings: Settings;
+};
 
 class App extends React.Component<{}, State> {
-  state = {
+  state: State = {
     images: [
       './img/1.png',
       './img/2.png',
@@ -20,17 +29,125 @@ class App extends React.Component<{}, State> {
       './img/9.png',
       './img/10.png',
     ],
+    settings: {
+      step: 3,
+      frameSize: 3,
+      itemWidth: 130,
+      animationDuration: 1000,
+      infinite: false,
+    },
   };
+
+  updateSettings
+  <T extends keyof Settings>(parameter: T, newValue: Settings[T]) {
+    this.setState(({ settings }) => ({
+      settings: {
+        ...settings,
+        [parameter]: newValue,
+      },
+    }));
+  }
 
   render() {
     const { images } = this.state;
+    const {
+      step,
+      frameSize,
+      itemWidth,
+      animationDuration,
+      infinite,
+    } = this.state.settings;
 
     return (
-      <div className="App">
+      <div className="app">
         {/* eslint-disable-next-line */}
         <h1>Carousel with {images.length} images</h1>
 
-        <Carousel />
+        <Carousel
+          images={images}
+          step={step}
+          frameSize={frameSize}
+          itemWidth={itemWidth}
+          animationDuration={animationDuration}
+          infinite={infinite}
+        />
+
+        <fieldset className="app__settings">
+          <label className="app__setting">
+            Step:
+            <input
+              className="app__step"
+              type="number"
+              min="1"
+              max={images.length}
+              defaultValue={step}
+              onChange={
+                (({ target }) => this
+                  .updateSettings('step', Number(target.value)))
+              }
+            />
+          </label>
+
+          <label className="app__setting">
+            Frame size:
+            <input
+              className="app__frame-size"
+              type="number"
+              min="1"
+              max={images.length}
+              defaultValue={frameSize}
+              onChange={
+                (({ target }) => this
+                  .updateSettings('frameSize', Number(target.value)))
+              }
+            />
+          </label>
+
+          <label className="app__setting">
+            Item width:
+            <input
+              className="app__item-width"
+              type="number"
+              min="80"
+              max="180"
+              step="10"
+              defaultValue={itemWidth}
+              onChange={
+                (({ target }) => this
+                  .updateSettings('itemWidth', Number(target.value)))
+              }
+            />
+          </label>
+
+          <label className="app__setting">
+            Anitmation duration:
+            <input
+              className="app__animation-duration"
+              type="number"
+              min="0"
+              max="2000"
+              step="500"
+              defaultValue={animationDuration}
+              onChange={
+                (({ target }) => this
+                  .updateSettings('animationDuration', Number(target.value)))
+              }
+            />
+          </label>
+
+          <label className="app__setting">
+            Infinite:
+            <input
+              className="app__infinite"
+              type="checkbox"
+              defaultChecked={infinite}
+              onChange={
+                (({ target }) => this
+                  .updateSettings('infinite', target.checked))
+              }
+            />
+          </label>
+        </fieldset>
       </div>
     );
   }
--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,18 +1,115 @@
 import React from 'react';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+interface Props {
+  images: string[],
+  step: number,
+  frameSize: number,
+  itemWidth: number,
+  animationDuration: number,
+  // infinite: boolean,
+}
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+type State = {
+  shift: number,
+};
+
+class Carousel extends React.Component<Props, State> {
+  state: State = {
+    shift: 0,
+  };
+
+  scrollLeft = () => {
+    const shift = this.props.itemWidth * this.props.step;
+
+    this.setState((prevState) => {
+      let addShift = prevState.shift + shift;
+
+      if (addShift
+      >= this.props.images.length * this.props.itemWidth) {
+        addShift = this.props.images.length * this.props.itemWidth
+        - this.props.frameSize * this.props.itemWidth;
+      }
+
+      return ({
+        shift: addShift,
+      });
+    });
+  };
+
+  scrollRight = () => {
+    const shift = this.props.itemWidth * this.props.step;
+
+    this.setState((prevState) => {
+      let addShift = prevState.shift - shift;
+
+      if (addShift
+      <= 0) {
+        addShift = 0;
+      }
+
+      return ({
+        shift: addShift,
+      });
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <div
+          className="carousel"
+          style={{
+            width: `${this.props.itemWidth * this.props.frameSize}px`,
+            height: `${this.props.itemWidth + 4}px`,
+          }}
+        >
+          <div
+            className="carousel__container"
+            style={{
+              transform: `translateX(-${this.state.shift}px)`,
+              transition: `transform ${this.props.animationDuration}ms`,
+            }}
+          >
+            {this.props.images.map((image) => (
+              <img
+                key={image}
+                src={image}
+                alt={image}
+                style={{
+                  width: `${this.props.itemWidth}px`,
+                }}
+              />
+            ))}
+          </div>
+        </div>
+        <div className="carousel__buttons">
+          <button
+            type="button"
+            className="carousel__prev-button"
+            onClick={this.scrollRight}
+            disabled={
+              this.state.shift === 0
+            }
+          >
+            {'<'}
+          </button>
+          <button
+            type="button"
+            className="carousel__next-button"
+            onClick={this.scrollLeft}
+            disabled={
+              this.state.shift
+                >= (this.props.images.length - this.props.frameSize)
+                * this.props.itemWidth
+            }
+          >
+            {'>'}
+          </button>
+        </div>
+      </>
+    );
+  }
+}
 
 export default Carousel;
