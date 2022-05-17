961294ea35a2eb902faa480dc304650ba6e012d7
unknown <avicostov2@gmail.com>
2022-05-17 11:33:53 +0200

    solut

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.scss';
-import Carousel from './components/Carousel';
+import { Carousel } from './components/Carousel';
 
 interface State {
-  images: string[];
+  images: string[],
+  itemWidth: number
+  step: number,
+  frameSize: number,
+  delay: number
 }
 
 class App extends React.Component<{}, State> {
@@ -20,17 +24,79 @@ class App extends React.Component<{}, State> {
       './img/9.png',
       './img/10.png',
     ],
+    itemWidth: 160,
+    step: 3,
+    frameSize: 3,
+    delay: 1000,
+  };
+
+  maxValueOfTrans = () => {
+    const { images, frameSize } = this.state;
+
+    return -(images.length - frameSize) * 100;
   };
 
   render() {
-    const { images } = this.state;
+    const {
+      images, itemWidth, frameSize, step, delay,
+    } = this.state;
 
     return (
       <div className="App">
         {/* eslint-disable-next-line */}
         <h1>Carousel with {images.length} images</h1>
+        W/H:
+        {'  '}
+        <input
+          type="number"
+          defaultValue={130}
+          onChange={({ target }) => (
+              this.setState({ itemWidth: +(target.value) })
+            )
+          }
+        />
+        <br />
+        Framesize:
+        {'  '}
+        <input
+          type="number"
+          defaultValue={3}
+          onChange={({ target }) => (
+              this.setState({ frameSize: +(target.value) })
+            )
+          }
+        />
+        <br />
+        Step:
+        {'  '}
+        <input
+          type="number"
+          defaultValue={3}
+          onChange={({ target }) => (
+              this.setState({ step: +(target.value) })
+            )
+          }
+        />
+        <br />
+        Delay:
+        {'  '}
+        <input
+          type="number"
+          defaultValue={1000}
+          onChange={({ target }) => (
+              this.setState({ delay: +(target.value) })
+            )
+          }
+        />
 
-        <Carousel />
+        <Carousel
+          itemWidth={itemWidth}
+          frameSize={frameSize}
+          step={step}
+          images={images}
+          delay={delay}
+          maxValue={this.maxValueOfTrans()}
+        />
       </div>
     );
   }
