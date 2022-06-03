42e535eef7d92a218d2daf77e525bdf41f0ff63a
Swazi Kunene <kuneneswazi18@gmail.com>
2022-06-03 14:00:48 +0200

    React Carousel Solution

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,10 @@ import Carousel from './components/Carousel';
 
 interface State {
   images: string[];
+  step: number;
+  frameSize: number;
+  itemWidth: number;
+  animationDuration: number;
 }
 
 class App extends React.Component<{}, State> {
@@ -20,17 +24,107 @@ class App extends React.Component<{}, State> {
       './img/9.png',
       './img/10.png',
     ],
+    step: 3,
+    frameSize: 3,
+    itemWidth: 130,
+    animationDuration: 1000,
+  };
+
+  changeStepCount = (value: number) => {
+    this.setState({ step: value });
+  };
+
+  changeframeSize = (value: number) => {
+    this.setState({ frameSize: value });
+  };
+
+  changeItemWidth = (value: number) => {
+    this.setState({ itemWidth: value });
+  };
+
+  changeAnimationDuration = (value: number) => {
+    this.setState({ animationDuration: value });
   };
 
   render() {
-    const { images } = this.state;
+    const {
+      images,
+      step,
+      frameSize,
+      itemWidth,
+      animationDuration,
+    } = this.state;
 
     return (
       <div className="App">
         {/* eslint-disable-next-line */}
-        <h1>Carousel with {images.length} images</h1>
+        <h1 data-cy="title">Carousel with {images.length} images</h1>
+        <div className="controls">
+          <div className="controls__item">
+            <span>Step:</span>
+            {' '}
+            <input
+              type="number"
+              min="1"
+              max={frameSize}
+              value={step}
+              onChange={({ target }) => {
+                this.changeStepCount(+target.value);
+              }}
+            />
+          </div>
 
-        <Carousel />
+          <div className="controls__item">
+            <span>Frame size:</span>
+            {' '}
+            <input
+              type="number"
+              min="3"
+              max={1300 / itemWidth}
+              value={frameSize}
+              onChange={({ target }) => {
+                this.changeframeSize(+target.value);
+              }}
+            />
+          </div>
+
+          <div className="controls__item">
+            <span>Item width:</span>
+            {' '}
+            <input
+              type="number"
+              min="130"
+              max={1300 / frameSize}
+              value={itemWidth}
+              step="10"
+              onChange={({ target }) => {
+                this.changeItemWidth(+target.value);
+              }}
+            />
+          </div>
+
+          <div className="controls__item">
+            <span>Animation duration:</span>
+            {' '}
+            <input
+              type="number"
+              min="400"
+              value={animationDuration}
+              step="100"
+              onChange={({ target }) => {
+                this.changeAnimationDuration(+target.value);
+              }}
+            />
+          </div>
+        </div>
+
+        <Carousel
+          images={images}
+          step={step}
+          frameSize={frameSize}
+          itemWidth={itemWidth}
+          animationDuration={animationDuration}
+        />
       </div>
     );
   }
