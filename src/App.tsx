315e0dc4523d68c35d9c6defd9bf921ede1266a9
Andrii Nychaiuk <anichajuk@gmail.com>
2022-07-09 16:13:49 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,11 @@ import Carousel from './components/Carousel';
 
 interface State {
   images: string[];
+  itemWidth: number;
+  frameSize: number;
+  step: number;
+  animationDuration: number;
+  isInfinite: boolean;
 }
 
 class App extends React.Component<{}, State> {
@@ -20,17 +25,134 @@ class App extends React.Component<{}, State> {
       './img/9.png',
       './img/10.png',
     ],
+    itemWidth: 130,
+    frameSize: 3,
+    step: 3,
+    animationDuration: 1000,
+    isInfinite: false,
+  };
+
+  changeItemWidth = (value: number) => {
+    this.setState({ itemWidth: value });
+  };
+
+  changeFrameSize = (value: number) => {
+    this.setState({ frameSize: value });
+  };
+
+  changeStep = (value: number) => {
+    this.setState({ step: value });
+  };
+
+  changeAnimationDuration = (value: number) => {
+    this.setState({ animationDuration: value });
+  };
+
+  disableInfinite = () => {
+    return this.state.isInfinite
+      ? this.setState({ isInfinite: false })
+      : this.setState({ isInfinite: true });
   };
 
   render() {
-    const { images } = this.state;
+    const {
+      images,
+      frameSize,
+      step,
+      itemWidth,
+      animationDuration,
+    } = this.state;
 
     return (
       <div className="App">
         {/* eslint-disable-next-line */}
         <h1>Carousel with {images.length} images</h1>
 
-        <Carousel />
+        <Carousel
+          images={images}
+          itemWidth={itemWidth}
+          frameSize={frameSize}
+          step={step}
+          animationDuration={animationDuration}
+          infinite={this.state.isInfinite}
+        />
+
+        <div className="App__container">
+          <div className="App__input-box">
+            <span className="App__input-text">Emoji size</span>
+            <input
+              className="App__input"
+              type="number"
+              name="itemWidth"
+              min="50"
+              max="300"
+              step="10"
+              defaultValue={itemWidth}
+              onChange={({ target }) => {
+                this.changeItemWidth(+target.value);
+              }}
+            />
+          </div>
+
+          <div className="App__input-box">
+            <span className="App__input-text">Carousel width</span>
+            <input
+              className="App__input"
+              type="number"
+              name="frameSize"
+              min="1"
+              max="10"
+              defaultValue={frameSize}
+              onChange={({ target }) => {
+                this.changeFrameSize(+target.value);
+              }}
+            />
+          </div>
+
+          <div className="App__input-box">
+            <span className="App__input-text">Step</span>
+            <input
+              className="App__input"
+              type="number"
+              name="step"
+              min="1"
+              max="9"
+              defaultValue={step}
+              onChange={({ target }) => {
+                this.changeStep(+target.value);
+              }}
+            />
+          </div>
+
+          <div className="App__input-box">
+            <span className="App__input-text">Scrolling duration</span>
+            <input
+              className="App__input"
+              type="number"
+              name="animationDuration"
+              min="0"
+              max="10000"
+              step="100"
+              defaultValue={animationDuration}
+              onChange={({ target }) => {
+                this.changeAnimationDuration(+target.value);
+              }}
+            />
+          </div>
+
+          <div className="App__input-box">
+            <label htmlFor="disabled" className="App__input-text">
+              Infinite scroll
+            </label>
+            <input
+              className="App__input"
+              type="checkbox"
+              name="disabled"
+              onClick={this.disableInfinite}
+              defaultChecked={this.state.isInfinite}
+            />
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,18 +1,154 @@
 import React from 'react';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+interface Props {
+  images: string[],
+  step: number,
+  frameSize: number,
+  itemWidth: number,
+  animationDuration: number,
+  infinite: boolean,
+}
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+interface State {
+  disabledForward: boolean,
+  disabledBack: boolean,
+  index: number,
+}
+
+class Carousel extends React.Component<Props, State> {
+  state = {
+    disabledForward: false,
+    disabledBack: true,
+    index: 0,
+  };
+
+  scrollForward = () => {
+    const {
+      images,
+      frameSize,
+      step,
+      infinite,
+    } = this.props;
+
+    const { index } = this.state;
+
+    const lastStep = images.length - frameSize;
+
+    if (index < images.length - step) {
+      this.setState((prev) => ({
+        index: prev.index + step,
+      }));
+    }
+
+    if (!infinite && index + step >= lastStep) {
+      this.setState({ index: lastStep });
+      this.setState({ disabledForward: true });
+    }
+
+    if (infinite) {
+      if (index + step > lastStep
+        && index !== lastStep) {
+        this.setState({ index: lastStep });
+      }
+
+      if (index === lastStep) {
+        this.setState({ index: 0 });
+      }
+    }
+
+    this.setState({ disabledBack: false });
+  };
+
+  scrollBack = () => {
+    const {
+      images,
+      frameSize,
+      step,
+      infinite,
+    } = this.props;
+
+    const { index } = this.state;
+
+    if (index > step) {
+      this.setState((prev) => ({
+        index: prev.index - step,
+      }));
+    }
+
+    if (index <= step && index !== 0) {
+      this.setState({ index: 0 });
+
+      if (!infinite) {
+        this.setState({ disabledBack: true });
+      }
+    }
+
+    if (infinite && index === 0) {
+      this.setState({ index: images.length - frameSize });
+    }
+
+    this.setState({ disabledForward: false });
+  };
+
+  render() {
+    const {
+      images,
+      frameSize,
+      itemWidth,
+      animationDuration,
+    } = this.props;
+
+    return (
+      <div className="Carousel">
+        <ul
+          className="Carousel__list"
+          style={{
+            width: `${frameSize * itemWidth}px`,
+          }}
+        >
+          {images.map((image, i) => {
+            const counter = i;
+
+            return (
+              <li
+                key={`${image}__${counter}`}
+                style={{
+                  transform: `translateX(-${this.state.index * 100}%)`,
+                  transition: `${animationDuration}ms`,
+                }}
+              >
+                <img
+                  src={image}
+                  alt={`${i}`}
+                  style={{ width: `${itemWidth}px` }}
+                />
+              </li>
+            );
+          })}
+        </ul>
+
+        <div className="Carousel__button-box">
+          <button
+            className="Carousel__button"
+            type="button"
+            onClick={this.scrollBack}
+            disabled={this.state.disabledBack}
+          >
+            Prev
+          </button>
+          <button
+            className="Carousel__button"
+            type="button"
+            onClick={this.scrollForward}
+            disabled={this.state.disabledForward}
+          >
+            Next
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Carousel;
