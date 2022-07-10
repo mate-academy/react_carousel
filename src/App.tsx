0769a388420a9cd0f67797db1f2e34aed8b0d164
Stanislav Topikha <stanislav.topikha@gmail.com>
2022-07-10 15:37:21 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,14 @@ import './App.scss';
 import Carousel from './components/Carousel';
 
 interface State {
-  images: string[];
+  images: string[],
+  userNumInputs: {
+    step: number,
+    frameSize: number,
+    itemWidth: number,
+    animationDuration: number,
+  },
+  infinite: boolean,
 }
 
 class App extends React.Component<{}, State> {
@@ -20,17 +27,88 @@ class App extends React.Component<{}, State> {
       './img/9.png',
       './img/10.png',
     ],
+    userNumInputs: {
+      step: 3,
+      frameSize: 3,
+      itemWidth: 130,
+      animationDuration: 1000,
+    },
+    infinite: false,
   };
 
+  numInputHandler(value: string, key: string) {
+    return this.setState((prev) => ({
+      userNumInputs: {
+        ...prev.userNumInputs,
+        [key]: +value,
+      },
+    }));
+  }
+
+  checkboxHandler() {
+    return this.setState(prev => ({ infinite: !prev.infinite }));
+  }
+
   render() {
-    const { images } = this.state;
+    const {
+      images,
+      userNumInputs,
+      infinite,
+    } = this.state;
+
+    function chooseStep(key: string): number {
+      switch (key) {
+        case 'itemWidth':
+          return 10;
+        case 'animationDuration':
+          return 100;
+        default:
+          return 1;
+      }
+    }
 
     return (
-      <div className="App">
+      <div className="app">
         {/* eslint-disable-next-line */}
-        <h1>Carousel with {images.length} images</h1>
+        <h1 className="app__title" data-cy="title">Carousel with {images.length} images</h1>
 
-        <Carousel />
+        <Carousel
+          images={images}
+          step={userNumInputs.step}
+          frameSize={userNumInputs.frameSize}
+          itemWidth={userNumInputs.itemWidth}
+          animationDuration={userNumInputs.animationDuration}
+          infinite={infinite}
+        />
+
+        <form className="app__form">
+          {(Object.keys(userNumInputs) as (keyof typeof userNumInputs)[])
+            .map(key => (
+              <label key={key}>
+                {`${key.split(/(?=[A-Z])/).join(' ').toUpperCase()}: `}
+                <input
+                  name="key"
+                  type="number"
+                  defaultValue={userNumInputs[key]}
+                  onChange={
+                    event => this.numInputHandler(event.target.value, key)
+                  }
+                  min={0}
+                  step={chooseStep(key)}
+                />
+              </label>
+            ))}
+
+          <label>
+            Infinite
+            <input
+              name="Infinite"
+              type="checkbox"
+              onChange={() => (this.checkboxHandler())}
+            />
+          </label>
+        </form>
+
       </div>
     );
   }
--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,18 +1,225 @@
 import React from 'react';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+type Props = {
+  images: string[],
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+  step: number,
+  frameSize: number,
+  itemWidth: number,
+  animationDuration: number,
+  infinite: boolean,
+};
+
+type State = {
+  listShift: number,
+
+  step: number,
+  frameSize: number,
+  itemWidth: number,
+  animationDuration: number,
+  infinite: boolean,
+
+  isPrevDisabled: boolean,
+  isNextDisabled: boolean,
+};
+
+class Carousel extends React.Component<Props, State> {
+  state = {
+    listShift: 0,
+
+    step: this.props.step,
+    frameSize: this.props.frameSize,
+    itemWidth: this.props.itemWidth,
+    animationDuration: this.props.animationDuration,
+    infinite: this.props.infinite,
+
+    isPrevDisabled: false,
+    isNextDisabled: false,
+  };
+
+  componentDidMount() {
+    this.buttonsSwitch();
+  }
+
+  componentDidUpdate() {
+    this.propsToStateUpdate();
+
+    if (this.state.infinite) {
+      if (this.state.isNextDisabled || this.state.isPrevDisabled) {
+        this.setState({ isPrevDisabled: false, isNextDisabled: false });
+      }
+
+      return;
+    }
+
+    this.buttonsSwitch();
+  }
+
+  getShiftLimit() {
+    return (this.props.images.length - this.state.frameSize)
+       * this.state.itemWidth;
+  }
+
+  getStepShift() {
+    return this.state.step * this.state.itemWidth;
+  }
+
+  propsToStateUpdate() {
+    if (this.state.step !== this.props.step) {
+      this.setState({ step: this.props.step });
+
+      return;
+    }
+
+    if (this.state.frameSize !== this.props.frameSize) {
+      this.setState({ frameSize: this.props.frameSize });
+
+      return;
+    }
+
+    if (this.state.itemWidth !== this.props.itemWidth) {
+      this.setState({ itemWidth: this.props.itemWidth });
+
+      return;
+    }
+
+    if (this.state.animationDuration !== this.props.animationDuration) {
+      this.setState({ animationDuration: this.props.animationDuration });
+
+      return;
+    }
+
+    if (this.state.infinite !== this.props.infinite) {
+      this.setState({ infinite: this.props.infinite });
+    }
+  }
+
+  nextBtnHandler() {
+    const shiftLimit = this.getShiftLimit();
+
+    if (this.state.listShift < shiftLimit) {
+      let stepShift = this.getStepShift();
+
+      if ((this.state.listShift + stepShift) > shiftLimit) {
+        stepShift = shiftLimit - this.state.listShift;
+      }
+
+      this.setState(prev => ({
+        listShift: prev.listShift + stepShift,
+      }));
+
+      return;
+    }
+
+    if (this.state.infinite && this.state.listShift === shiftLimit) {
+      this.setState({ listShift: 0 });
+    }
+  }
+
+  prevBtnHandler() {
+    if (this.state.listShift > 0) {
+      let stepShift = this.getStepShift();
+
+      if ((this.state.listShift - stepShift) < 0) {
+        stepShift = this.state.listShift;
+      }
+
+      this.setState(prev => ({
+        listShift: prev.listShift - stepShift,
+      }));
+
+      return;
+    }
+
+    if (this.state.infinite && this.state.listShift === 0) {
+      this.setState({ listShift: this.getShiftLimit() });
+    }
+  }
+
+  buttonsSwitch() {
+    if (this.state.isPrevDisabled && this.state.listShift > 0) {
+      this.setState({ isPrevDisabled: false });
+
+      return;
+    }
+
+    if (this.state.listShift <= 0 && !this.state.isPrevDisabled) {
+      this.setState({ isPrevDisabled: true });
+
+      return;
+    }
+
+    const shiftLimit = this.getShiftLimit();
+
+    if (this.state.listShift >= shiftLimit && !this.state.isNextDisabled) {
+      this.setState({ isNextDisabled: true });
+
+      return;
+    }
+
+    if (this.state.isNextDisabled && this.state.listShift < shiftLimit) {
+      this.setState({ isNextDisabled: false });
+    }
+  }
+
+  render(): React.ReactNode {
+    const listStyles: React.CSSProperties = {
+      transition: `transform ${this.state.animationDuration}ms`,
+      transform: `translateX(-${this.state.listShift}px)`,
+    };
+
+    const imgStyles: React.CSSProperties = {
+      width: `${this.state.itemWidth}px`,
+    };
+
+    const wrapperStyles: React.CSSProperties = {
+      width: `${this.state.frameSize * this.state.itemWidth}px`,
+    };
+
+    return (
+      <div className="Carousel">
+        <button
+          type="button"
+          className="Carousel__button"
+          onClick={() => (this.prevBtnHandler())}
+          disabled={this.state.isPrevDisabled}
+        >
+          ⬅️
+        </button>
+
+        <div
+          className="Carousel__wrapper"
+          style={wrapperStyles}
+        >
+          <ul
+            className="Carousel__list"
+            style={listStyles}
+          >
+            {this.props.images.map((imgSrc, index) => (
+              <li key={imgSrc}>
+                <img
+                  src={imgSrc}
+                  alt={String(index)}
+                  style={imgStyles}
+                />
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <button
+          type="button"
+          className="Carousel__button"
+          onClick={() => (this.nextBtnHandler())}
+          disabled={this.state.isNextDisabled}
+          data-cy="next"
+        >
+          ➡️
+        </button>
+      </div>
+    );
+  }
+}
 
 export default Carousel;
