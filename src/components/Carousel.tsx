7ef9ec1d48d0bb0b11204cc74565ea280d212ca2
Oleksii Budnik <budnikoleksii@gmail.com>
2022-08-01 20:46:28 +0300

    Solution

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,18 +1,189 @@
-import React from 'react';
+import { Component } from 'react';
 import './Carousel.scss';
+import { Frame } from '../types/Frame';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+interface Props {
+  images: Frame[];
+}
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+interface State {
+  itemWidth: number;
+  frameSize: number;
+  step: number;
+  animationDuration: number;
+  scrollWidth: number,
+  prevDisabled: boolean,
+  nextDisabled: boolean,
+}
 
-export default Carousel;
+export class Carousel extends Component<Props, State> {
+  state = {
+    itemWidth: 130,
+    frameSize: 3,
+    step: 3,
+    animationDuration: 1000,
+    scrollWidth: 0,
+    prevDisabled: true,
+    nextDisabled: false,
+  };
+
+  moveSlide = (index: number) => {
+    const { images } = this.props;
+    const {
+      step,
+      frameSize,
+      itemWidth,
+    } = this.state;
+
+    this.setState((prevState) => {
+      const maxScroll = (images.length - frameSize) * itemWidth;
+      let scroll = prevState.scrollWidth + (step * itemWidth * index);
+      let next = false;
+      let prev = false;
+
+      if (scroll >= maxScroll) {
+        scroll = maxScroll;
+        next = true;
+      }
+
+      if (scroll <= 0) {
+        scroll = 0;
+        prev = true;
+      }
+
+      return {
+        scrollWidth: scroll,
+        nextDisabled: next,
+        prevDisabled: prev,
+      };
+    });
+  };
+
+  nextSlide = () => {
+    this.moveSlide(1);
+  };
+
+  prevSlide = () => {
+    this.moveSlide(-1);
+  };
+
+  render() {
+    const { images } = this.props;
+    const {
+      itemWidth,
+      frameSize,
+      step,
+      animationDuration,
+      scrollWidth,
+      prevDisabled,
+      nextDisabled,
+    } = this.state;
+
+    return (
+      <div
+        className="Carousel"
+        style={{
+          width: `${itemWidth * frameSize}px`,
+        }}
+      >
+        <div className="Carousel__nav">
+          <label>
+            Step:
+            <input
+              type="number"
+              min="1"
+              max={frameSize}
+              value={step}
+              onChange={({ target }) => this.setState({ step: +target.value })}
+            />
+          </label>
+
+          <label>
+            Frame width:
+            <input
+              type="number"
+              min="3"
+              max={1300 / itemWidth}
+              value={frameSize}
+              onChange={({ target }) => (
+                this.setState({ frameSize: +target.value })
+              )}
+            />
+          </label>
+
+          <label>
+            Item width:
+            <input
+              type="number"
+              min="130"
+              max="300"
+              value={itemWidth}
+              step="10"
+              onChange={({ target }) => (
+                this.setState({ itemWidth: +target.value })
+              )}
+            />
+          </label>
+
+          <label>
+            Animation duration:
+            <input
+              type="number"
+              min="400"
+              max="3000"
+              value={animationDuration}
+              step="100"
+              onChange={({ target }) => (
+                this.setState({ animationDuration: +target.value })
+              )}
+            />
+          </label>
+        </div>
+
+        <ul
+          className="Carousel__list"
+          style={{
+            transform: `translateX(${-scrollWidth}px)`,
+            transition: `transform ${animationDuration}ms ease`,
+          }}
+        >
+          {images.map(({ id, image }) => (
+            <li
+              key={id}
+              className="Carousel__item"
+              style={{
+                width: `${itemWidth}px`,
+              }}
+            >
+              <img
+                src={image}
+                alt={image}
+                style={{
+                  width: `${itemWidth}px`,
+                }}
+              />
+            </li>
+          ))}
+        </ul>
+
+        <button
+          type="button"
+          className="carousel__button"
+          disabled={prevDisabled}
+          onClick={this.prevSlide}
+        >
+          Prev
+        </button>
+        <button
+          type="button"
+          className="carousel__button"
+          disabled={nextDisabled}
+          onClick={this.nextSlide}
+          data-cy="next"
+        >
+          Next
+        </button>
+      </div>
+    );
+  }
+}
