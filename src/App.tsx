5a1bf04b83064386e4b2384723f00fa982912560
silchyn <ivan.silchin@gmail.com>
2022-08-09 18:11:40 +0300

    added carousel

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.scss';
-import Carousel from './components/Carousel';
+import { Carousel } from './components/Carousel';
 
 interface State {
   images: string[];
+  itemWidth: number;
+  frameSize: number;
+  step: number;
+  animationDuration: number;
+  infinite: boolean;
 }
 
 class App extends React.Component<{}, State> {
@@ -20,17 +25,146 @@ class App extends React.Component<{}, State> {
       './img/9.png',
       './img/10.png',
     ],
+    itemWidth: 130,
+    frameSize: 3,
+    step: 3,
+    animationDuration: 1000,
+    infinite: false,
+  };
+
+  componentDidMount() {
+    document.querySelectorAll('.form__input')
+      .forEach(inputElement => inputElement
+        .addEventListener('input', this.input));
+  }
+
+  componentWillUnmount() {
+    document.querySelectorAll('.form__input')
+      .forEach(inputElement => inputElement
+        .removeEventListener('input', this.input));
+  }
+
+  input = (inputEvent: Event) => {
+    const { target } = inputEvent;
+    const imagesLength = this.state.images.length;
+
+    if (target && target instanceof HTMLInputElement) {
+      if (target.classList.contains('form__itemWidth')) {
+        this.setState({
+          itemWidth: Math.min(Math.max(+target.value, 80), 200),
+        });
+
+        target.value = this.state.itemWidth.toString();
+      } else if (target.classList.contains('form__frameSize')) {
+        this.setState({
+          frameSize: Math.min(Math.max(+target.value, 1), imagesLength),
+        });
+
+        target.value = this.state.frameSize.toString();
+      } else if (target.classList.contains('form__step')) {
+        this.setState({ step: Math.max(+target.value, 1) });
+
+        target.value = this.state.step.toString();
+      } else if (target.classList.contains('form__animationDuration')) {
+        this.setState({ animationDuration: Math.max(+target.value, 0) });
+
+        target.value = this.state.animationDuration.toString();
+      } else if (target.classList.contains('form__infinite')) {
+        this.setState({ infinite: target.checked });
+      }
+    }
   };
 
   render() {
-    const { images } = this.state;
+    const {
+      images,
+      itemWidth,
+      frameSize,
+      step,
+      animationDuration,
+      infinite,
+    } = this.state;
 
     return (
-      <div className="App">
+      <div className="App level">
         {/* eslint-disable-next-line */}
-        <h1>Carousel with {images.length} images</h1>
+        <h1 data-cy="title" className="title level-item">Carousel with {images.length} images</h1>
 
-        <Carousel />
+        <form
+          action="/"
+          method="post"
+          className="form level-item"
+        >
+          <div className="form__columns">
+            <div className="form__column">
+              <label className="form__label">
+                Item width
+
+                <input
+                  type="number"
+                  className="form__itemWidth input form__input"
+                  defaultValue={itemWidth}
+                  min={80}
+                  max={200}
+                />
+              </label>
+
+              <label className="form__label">
+                Frame size
+
+                <input
+                  type="number"
+                  className="form__frameSize input form__input"
+                  defaultValue={frameSize}
+                  min={1}
+                  max={images.length}
+                />
+              </label>
+            </div>
+            <div className="form__column">
+              <label className="form__label">
+                Step
+
+                <input
+                  type="number"
+                  className="form__step input form__input"
+                  defaultValue={step}
+                  min={1}
+                />
+              </label>
+
+              <label className="form__label">
+                Animation duration
+
+                <input
+                  type="number"
+                  className="form__animationDuration input form__input"
+                  defaultValue={animationDuration}
+                  min={0}
+                />
+              </label>
+            </div>
+          </div>
+
+          <label className="form__label">
+            Infinite
+
+            <input
+              type="checkbox"
+              className="form__infinite checkbox form__input"
+              defaultChecked={infinite}
+            />
+          </label>
+        </form>
+
+        <Carousel
+          images={images}
+          itemWidth={itemWidth}
+          frameSize={frameSize}
+          step={step}
+          animationDuration={animationDuration}
+          infinite={infinite}
+        />
       </div>
     );
   }
--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,18 +1,114 @@
-import React from 'react';
+import { Component } from 'react';
 import './Carousel.scss';
+import { v4 as uuid } from 'uuid';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+interface Props {
+  images: string[];
+  itemWidth: number;
+  frameSize: number;
+  step: number;
+  animationDuration: number;
+  infinite: boolean;
+}
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+interface State {
+  position: number;
+}
 
-export default Carousel;
+// eslint-disable-next-line react/prefer-stateless-function
+export class Carousel extends Component<Props, State> {
+  state = {
+    position: 0,
+  };
+
+  componentDidMount() {
+    const buttons = document.querySelector('.Carousel__buttons');
+
+    if (buttons) {
+      buttons.children[0].addEventListener('click', () => {
+        this.setState((state, props) => {
+          if (props.infinite && !state.position) {
+            return { position: props.images.length - props.frameSize };
+          }
+
+          return { position: Math.max(0, state.position - props.step) };
+        });
+      });
+      buttons.children[1].addEventListener('click', () => {
+        this.setState((state, props) => {
+          if (props.infinite && state.position
+            === props.images.length - props.frameSize) {
+            return { position: 0 };
+          }
+
+          return {
+            position: Math.min(props.images.length - props.frameSize,
+              state.position + props.step),
+          };
+        });
+      });
+    }
+  }
+
+  render() {
+    const {
+      images,
+      itemWidth,
+      frameSize,
+      animationDuration,
+      infinite,
+    } = this.props;
+    const { position } = this.state;
+
+    return (
+      <div
+        className="Carousel"
+        style={{ width: itemWidth * frameSize }}
+      >
+        <ul
+          className="Carousel__list"
+          style={{
+            transform: `translateX(-${itemWidth * position}px)`,
+            width: itemWidth * images.length,
+            transition: `transform ${animationDuration}ms`,
+          }}
+        >
+          {
+            images
+              .map((image, i) => (
+                <li key={uuid()}>
+                  <img
+                    src={image}
+                    alt={(i + 1).toString()}
+                    width={itemWidth}
+                    height={itemWidth}
+                  />
+                </li>
+              ))
+          }
+        </ul>
+
+        <div
+          className="Carousel__buttons"
+          style={{ width: itemWidth * frameSize }}
+        >
+          <button
+            type="button"
+            disabled={!infinite && position === 0}
+            className="button"
+          >
+            ↶
+          </button>
+          <button
+            type="button"
+            disabled={!infinite && position === images.length - frameSize}
+            data-cy="next"
+            className="button"
+          >
+            ↷
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
