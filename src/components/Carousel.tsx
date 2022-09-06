6687a33a06de38f6ab3187f3c006619d584c71ca
Kostya Konev <kosgoh.214@gmail.com>
2022-09-06 16:04:15 +0300

    help

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,18 +1,63 @@
-import React from 'react';
+/* eslint-disable */
+import { useState } from 'react';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+interface Props {
+  images: string[],
+  step: number,
+  frameSize: number,
+  itemWidth: number,
+  animationDuration: number,
+  infinite: boolean,
+}
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+const Carousel: React.FC<Props> = ( props ) => {
+  const [scrollWidth, setScrollWidth] = useState(0);
+
+  return (
+    <div className="Carousel">
+      <div
+        className="container"
+        style={{ width: props.itemWidth * props.frameSize }}
+      >
+        <ul
+          className="Carousel__list"
+          style={{ transition: `transform ${props.animationDuration}ms`,
+          transform: `translateX(${-scrollWidth}px)`}}
+        >
+          {props.images.map((image, i) => {
+            return (
+              <li key={i}>
+                <img
+                  src={image}
+                  alt={`${i}`}
+                  style={{ width: props.itemWidth, transition: `width ${props.animationDuration}ms` }}
+                />
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <div className='swipe-buttons'>
+        <button 
+          type="button"
+          // onClick={() => setScrolled(0)}
+        >
+          Prev
+        </button>
+        <button
+          type="button"
+          data-qa="next"
+          onClick={() => setScrollWidth(prev => {
+            return prev + (props.itemWidth * props.step);
+          })}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Carousel;
