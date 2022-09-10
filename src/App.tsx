c50ebda41ae7c860a91090b2f6adbf0df700f134
Aleksandr Kovalenko <dias.koval@gmail.com>
2022-09-10 21:31:47 +0300

    develop

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.scss';
-import Carousel from './components/Carousel';
+import { Carousel } from './components/Carousel';
 
 interface State {
   images: string[];
+  step: number,
+  frameSize: number,
+  itemWidth: number,
+  animationDuration: number,
 }
 
 class App extends React.Component<{}, State> {
@@ -20,18 +24,103 @@ class App extends React.Component<{}, State> {
       './img/9.png',
       './img/10.png',
     ],
+    step: 3,
+    frameSize: 3,
+    itemWidth: 130,
+    animationDuration: 1000,
+  };
+
+  changeStep = (value: number) => {
+    this.setState({ step: value });
+  };
+
+  changeFrameSize = (value: number) => {
+    this.setState({ frameSize: value });
+  };
+
+  changeItemWidth = (value: number) => {
+    this.setState({ itemWidth: value });
+  };
+
+  changeAnimationDuration = (value: number) => {
+    this.setState({ animationDuration: value });
   };
 
   render() {
-    const { images } = this.state;
+    const {
+      images,
+      step,
+      frameSize,
+      itemWidth,
+      animationDuration,
+    } = this.state;
 
     return (
       <div className="App">
         {/* eslint-disable-next-line */}
-        <h1>Carousel with {images.length} images</h1>
-
-        <Carousel />
+        <h1 className='App__carousel'>Carousel with {frameSize} images</h1>
+        <div className="App__name">
+          <span className="App__title">Step:</span>
+          <input
+            className="App__input"
+            type="number"
+            min="1"
+            max={frameSize}
+            value={step}
+            onChange={({ target }) => {
+              this.changeStep(+target.value);
+            }}
+          />
+        </div>
+        <div className="App__name">
+          <span className="App__title">Frame size:</span>
+          <input
+            className="App__input"
+            type="number"
+            min="3"
+            max="10"
+            value={frameSize}
+            onChange={({ target }) => {
+              this.changeFrameSize(+target.value);
+            }}
+          />
+        </div>
+        <div className="App__name">
+          <span className="App__title">Item width:</span>
+          <input
+            className="App__input"
+            type="number"
+            min="130"
+            max={1300 / frameSize}
+            value={itemWidth}
+            step="10"
+            onChange={({ target }) => {
+              this.changeItemWidth(+target.value);
+            }}
+          />
+        </div>
+        <div className="App__name">
+          <span className="App__title">Animation duration:</span>
+          <input
+            className="App__input"
+            type="number"
+            min="400"
+            value={animationDuration}
+            step="100"
+            onChange={({ target }) => {
+              this.changeAnimationDuration(+target.value);
+            }}
+          />
+        </div>
+        <Carousel
+          images={images}
+          step={step}
+          frameSize={frameSize}
+          itemWidth={itemWidth}
+          animationDuration={animationDuration}
+        />
       </div>
+
     );
   }
 }
--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,18 +1,115 @@
 import React from 'react';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+type Props = {
+  images: string[],
+  step: number,
+  frameSize: number,
+  itemWidth: number,
+  animationDuration: number,
+};
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+type State = {
+  scroll: number,
+};
 
-export default Carousel;
+export class Carousel extends React.Component<Props, State> {
+  state = {
+    scroll: 0,
+  };
+
+  buttonPrev = () => {
+    const { step, itemWidth } = this.props;
+
+    this.setState((state) => {
+      const scrollprev = state.scroll - (step * itemWidth);
+
+      return (scrollprev >= 0)
+        ? { scroll: scrollprev }
+        : { scroll: 0 };
+    });
+  };
+
+  buttonNext = () => {
+    const {
+      step,
+      itemWidth,
+      frameSize,
+      images,
+    } = this.props;
+
+    this.setState((state) => {
+      const scrollnext = state.scroll + (step * itemWidth);
+      const maxScroll = (images.length - frameSize) * itemWidth;
+
+      return (scrollnext <= maxScroll)
+        ? { scroll: scrollnext }
+        : { scroll: maxScroll };
+    });
+  };
+
+  render() {
+    const {
+      scroll,
+    } = this.state;
+
+    const {
+      images,
+      frameSize,
+      itemWidth,
+      animationDuration,
+    } = this.props;
+
+    return (
+
+      <div
+        className="Carousel"
+        style={{
+          width: `${itemWidth * frameSize}px`,
+        }}
+      >
+
+        <ul
+          className="Carousel__list"
+          style={{
+            transform: `translateX(${-scroll}px)`,
+            transition: `${animationDuration}ms`,
+          }}
+        >
+
+          {images.map((image) => (
+            <li
+              key={image}
+              className="carousel__item"
+              style={{
+              }}
+            >
+              <img
+                src={image}
+                alt="img"
+                style={{
+                  width: `${itemWidth}px`,
+                }}
+              />
+            </li>
+          ))}
+        </ul>
+
+        <button
+          className="button"
+          type="button"
+          onClick={this.buttonPrev}
+        >
+          Prev
+        </button>
+        <button
+          className="button"
+          type="button"
+          onClick={this.buttonNext}
+        >
+          Next
+        </button>
+      </div>
+    );
+  }
+}
