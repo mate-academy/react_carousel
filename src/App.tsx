7f5c6f34045358f54922ab7a789e2e79bd823858
NataliyaAn <otchet333@gmail.com>
2022-09-14 13:36:59 +0300

    solution without onChange

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.scss';
-import Carousel from './components/Carousel';
+import { Carousel } from './components/Carousel';
 
 interface State {
-  images: string[];
+  images: string[] | string;
+  step: number;
+  frameSize: number;
+  itemWidth?: number;
+  animationDuration: number;
 }
 
 class App extends React.Component<{}, State> {
@@ -20,17 +24,99 @@ class App extends React.Component<{}, State> {
       './img/9.png',
       './img/10.png',
     ],
+    itemWidth: 390,
+    frameSize: 130,
+    animationDuration: 500,
+    step: 3,
   };
 
+  // handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+  //   const newValueVariable = Number(event.target.value);
+  //   const name = event.target.name;
+
+  //   this.setState([name]: newValueVariable);
+  // }
+
   render() {
-    const { images } = this.state;
+    const {
+      images,
+      itemWidth,
+      frameSize,
+      step,
+      animationDuration,
+    } = this.state;
+    // const inputs:string[] = Object.keys(this.state)
+    //   .filter(el => el !== 'images');
 
     return (
       <div className="App">
         {/* eslint-disable-next-line */}
-        <h1>Carousel with {images.length} images</h1>
+        <h1 data-cy="title"
+          className="title"
+        >
+          {`Carousel with ${images.length} images`}
+        </h1>
+        <label className="variables">
+          Item width:
+          <input
+            className="variables__input"
+            type="number"
+            name="itemWidth"
+            value={itemWidth}
+            // onChange={this.handleChange}
+          />
+        </label>
 
-        <Carousel />
+        <label className="variables">
+          Frame size:
+          <input
+            className="variables__input"
+            type="number"
+            name="frameSize"
+            value={frameSize}
+            // onChange={this.handleChange}
+          />
+        </label>
+
+        <label className="variables">
+          Step:
+          <input
+            className="variables__input"
+            type="number"
+            name="step"
+            value={step}
+            // onChange={this.handleChange}
+          />
+        </label>
+
+        <label className="variables">
+          Animation duration:
+          <input
+            className="variables__input"
+            type="number"
+            name="animationDuration"
+            value={animationDuration}
+            // onChange={this.handleChange}
+          />
+        </label>
+        <Carousel
+          images={images}
+          itemWidth={itemWidth}
+          frameSize={frameSize}
+          animationDuration={animationDuration}
+          step={step}
+        />
+        {/* {inputs.map(variable => (
+          <label className="variables" key={variable}>
+            {`${variable[0].toLocaleUpperCase() + variable.slice(1)} :`}
+            <input
+              type="number"
+              name={`${variable}`}
+              value={this.state.step}
+              onChange={this.handleChange}
+            />
+          </label>
+        ))} */}
       </div>
     );
   }
--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,18 +1,107 @@
+// import { transform } from 'cypress/types/lodash';
 import React from 'react';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+type Props = {
+  images: string[];
+  step: number;
+  frameSize: number;
+  itemWidth: number;
+  animationDuration: number;
+};
+type State = {
+  moveX: number,
+};
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+export class Carousel extends React.Component<Props, State> {
+  state: Readonly<State> = {
+    moveX: 0,
+  };
 
-export default Carousel;
+  maxMove = this.props.frameSize * (10 - this.props.step);
+
+  distinationMove = this.props.step * this.props.frameSize;
+
+  handleClickMoveRight = () => {
+    const prevmoveX = this.state.moveX;
+
+    this.setState({
+      moveX: ((prevmoveX - this.distinationMove) < (-this.maxMove))
+        ? -this.maxMove
+        : (prevmoveX - this.distinationMove),
+    });
+  };
+
+  handleClickMoveLeft = () => {
+    const prevmoveX = this.state.moveX;
+
+    this.setState({
+      moveX: ((prevmoveX + this.distinationMove) > 0)
+        ? 0
+        : (prevmoveX + this.distinationMove),
+    });
+  };
+
+  render() {
+    const {
+      itemWidth,
+      frameSize,
+      animationDuration,
+      step,
+    } = this.props;
+
+    const {
+      moveX,
+    } = this.state;
+
+    return (
+      <div
+        className="Carousel"
+        style={{
+          width: itemWidth,
+        }}
+      >
+        <ul
+          className="Carousel__list"
+          style={{
+            transform: `translateX(${moveX}px)`,
+            transition: `transform  ${animationDuration}ms`,
+          }}
+        >
+          {this.props.images.map((img, ind) => (
+            <li className="oneImg" key={img}>
+              <img
+                src={img}
+                style={{
+                  width: frameSize,
+                }}
+                alt={String(ind + 1)}
+              />
+            </li>
+          ))}
+        </ul>
+        <div className="buttonBlock">
+          <button
+            type="button"
+            className="buttonBlock__button"
+            onClick={this.handleClickMoveLeft}
+            disabled={moveX === 0}
+          >
+            Prev
+          </button>
+          <button
+            type="button"
+            className="buttonBlock__button"
+            data-cy="next"
+            onClick={this.handleClickMoveRight}
+            disabled={-moveX === frameSize * (10 - step)}
+          >
+            Next
+          </button>
+
+        </div>
+      </div>
+    );
+  }
+}
+
