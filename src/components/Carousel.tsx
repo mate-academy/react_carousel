bf645c9f535b1421ab5c8147185b8e0e94455767
Ihor Andrianov <andrianoia@gmail.com>
2022-10-07 21:45:58 +0300

    solution

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,18 +1,106 @@
-import React from 'react';
-import './Carousel.scss';
+import React, { useEffect, useRef, useState } from 'react';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+type Images = {
+  images: string[];
+  size: number;
+  frameSize: number;
+  step: number;
+  animationDuration: number;
+};
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+const buttonStyle = `h-full
+            p-5 text-2xl
+            hover:bg-accent-focus/20
+            transition-all
+            ease-in duration-75`;
+
+export function usePrevious<T>(value: T) {
+  const ref = useRef<T>();
+
+  useEffect(() => {
+    ref.current = value;
+  });
+
+  return ref.current;
+}
+
+const Carousel: React.FC<Images> = ({
+  images,
+  size,
+  frameSize,
+  step,
+  animationDuration,
+}) => {
+  const [scrollTo, setScrollTo] = useState(0);
+  const prevSize = usePrevious<number>(size);
+
+  useEffect(() => {
+    if (prevSize) {
+      setScrollTo((scrollTo / prevSize) * size);
+    }
+  }, [size]);
+
+  const scrollLeft = () => {
+    // we need 8 to not show empty box in slider on last element;
+    if (scrollTo > (size * 8 * -1)) {
+      setScrollTo(Math
+        .max(
+          scrollTo - size * step,
+          (size * (images.length - frameSize) * -1),
+        ));
+    }
+  };
+
+  const scrollRight = () => {
+    if (scrollTo < 0) {
+      setScrollTo(Math.min(scrollTo + size * step, 0));
+    }
+  };
+
+  return (
+    <div className="Carousel">
+      <div className="flex m-10 border-gray-300 border shadow-xl rounded-md">
+        <button
+          style={{ height: `${size}px` }}
+          className={buttonStyle}
+          type="button"
+          onClick={scrollRight}
+        >
+          {'<'}
+        </button>
+        <div
+          className="overflow-hidden"
+          style={{
+            width: Math
+              .min((size * frameSize), window.innerWidth - 140),
+          }}
+        >
+          <ul
+            className="Carousel__list w-full flex flex-nowrap"
+            style={{ translate: scrollTo, transitionDuration: `${animationDuration}ms` }}
+          >
+            {images.map((image) => (
+              <li
+                key={image}
+                className="flex-shrink-0"
+              >
+                <img src={image} alt="emoji" style={{ width: `${size}px`, height: `${size}px` }} />
+              </li>
+            ))}
+          </ul>
+        </div>
+        <button
+          style={{ height: `${size}px` }}
+          className={buttonStyle}
+          type="button"
+          onClick={scrollLeft}
+        >
+          {'>'}
+        </button>
+      </div>
+
+    </div>
+  );
+};
 
 export default Carousel;
