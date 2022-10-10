8ff262eb14742a48fe930800fee906fa66f06906
naveed-shoukat <shoukat.naveed@gmail.com>
2022-10-11 00:27:41 +0300

    solution

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,18 +1,115 @@
 import React from 'react';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+type Props = {
+  images: string[];
+  step:number,
+  frameSize:number,
+  itemWidth:number,
+  animationDuration:number,
+  infinite:boolean,
+};
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+const Carousel: React.FC<Props> = ({
+  images,
+  step = 3,
+  frameSize = 3,
+  itemWidth = 130,
+  animationDuration = 1000,
+  infinite = false,
+}) => {
+  const [pointer, setPointer] = React.useState(0);
+  const totalImages = images.length;
+  const maxWidthOfWrapper = itemWidth * frameSize;
+
+  React.useEffect(() => {
+    setPointer(0);
+  }, [frameSize, step]);
+
+  const handlePrev = () => {
+    setPointer(prevPointer => {
+      const AbsPointer = Math.abs(prevPointer);
+
+      if (AbsPointer < totalImages && AbsPointer > step) {
+        return prevPointer + step;
+      }
+
+      return 0;
+    });
+  };
+
+  const handleNext = () => {
+    setPointer(prevPointer => {
+      const AbsPointer = Math.abs(prevPointer) + 1;
+
+      if (AbsPointer < totalImages - step - (frameSize - step)) {
+        return prevPointer - step - (frameSize - step);
+      }
+
+      return -1 * images.length + step + (frameSize - step);
+    });
+  };
+
+  return (
+    <div className="Wrapper">
+      <div
+        className="Carousel Wrapper__Carousel"
+        style={{ width: maxWidthOfWrapper }}
+      >
+        <ul
+          className="Carousel__list"
+          style={
+            {
+              transform: `translateX(${itemWidth * pointer}px)`,
+              transition: `transform ${animationDuration}ms ease`,
+              animationIterationCount: `${infinite}`,
+            }
+          }
+        >
+          {images.map((image, index) => {
+            return (
+              <li
+                key={`${image}`}
+                className="Carousel__item"
+              >
+                <img
+                  src={image}
+                  alt={`${index + 1}`}
+                  style={{ width: itemWidth }}
+                />
+              </li>
+            );
+          })}
+        </ul>
+
+        <div className="Carousel__btn-wrapper">
+          <button
+            type="button"
+            className={
+              pointer !== 0
+                ? 'Carousel__btn'
+                : 'Carousel__btn Carousel__btn--disable'
+            }
+            onClick={handlePrev}
+          >
+            &larr;
+          </button>
+          <button
+            data-cy="next"
+            type="button"
+            className={
+              pointer !== -1 * images.length + step + (frameSize - step)
+                ? 'Carousel__btn'
+                : 'Carousel__btn Carousel__btn--disable'
+            }
+            onClick={handleNext}
+          >
+            &rarr;
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Carousel;
