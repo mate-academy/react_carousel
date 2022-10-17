cc7fcc3fbf14af19c962365623b4d1ef94b98f2d
Andriy Andrieiev <proph7000@gmail.com>
2022-10-17 20:15:13 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.scss';
-import Carousel from './components/Carousel';
+import { Carousel } from './components';
 
 interface State {
   images: string[];
+  step: number;
+  frameSize: number;
+  itemWidth: number;
+  animationDuration: number;
+  infinite: boolean;
 }
 
 class App extends React.Component<{}, State> {
@@ -20,17 +25,132 @@ class App extends React.Component<{}, State> {
       './img/9.png',
       './img/10.png',
     ],
+    step: 3,
+    frameSize: 3,
+    itemWidth: 130,
+    animationDuration: 1000,
+    infinite: false,
   };
 
   render() {
-    const { images } = this.state;
+    const {
+      images,
+      step,
+      frameSize,
+      itemWidth,
+      animationDuration,
+      infinite,
+    } = this.state;
 
     return (
       <div className="App">
         {/* eslint-disable-next-line */}
-        <h1>Carousel with {images.length} images</h1>
+        <h1 data-cy="title">Carousel with {images.length} images</h1>
 
-        <Carousel />
+        <div className="parameters-of-carousel">
+          <label className="parameters-of-carousel__label">
+            Choose item width in px:
+            <input
+              className="parameters-of-carousel__input"
+              name="itemWidth"
+              type="number"
+              value={itemWidth}
+              onChange={(event) => {
+                if (+event.target.value >= 50 && +event.target.value < 300) {
+                  this.setState({ itemWidth: +event.target.value });
+                }
+              }}
+            />
+          </label>
+
+          <label className="parameters-of-carousel__label">
+            Choose frame size:
+            <input
+              className="parameters-of-carousel__input"
+              name="frameSize"
+              type="number"
+              value={frameSize}
+              onChange={(event) => {
+                if (
+                  +event.target.value >= 1
+                    && +event.target.value <= images.length
+                ) {
+                  this.setState({ frameSize: +event.target.value });
+                }
+              }}
+            />
+          </label>
+
+          <label className="parameters-of-carousel__label">
+            Choose step:
+            <input
+              className="parameters-of-carousel__input"
+              name="step"
+              type="number"
+              value={step}
+              onChange={(event) => {
+                if (
+                  +event.target.value >= 1
+                    && +event.target.value <= images.length
+                ) {
+                  this.setState({ step: +event.target.value });
+                }
+              }}
+            />
+          </label>
+
+          <label className="parameters-of-carousel__label">
+            Choose animation duration in ms:
+            <input
+              className="parameters-of-carousel__input"
+              name="animationDuration"
+              type="number"
+              value={animationDuration}
+              onChange={(event) => {
+                this.setState({ animationDuration: +event.target.value });
+              }}
+            />
+          </label>
+
+          <div className="parameters-of-carousel__isInfinite">
+            Is carousel infinite:
+            <div className="parameters-of-carousel__container">
+              <div className="parameters-of-carousel__input">
+                <input
+                  name="infinite"
+                  type="radio"
+                  value="y"
+                  onClick={() => {
+                    this.setState({ infinite: true });
+                  }}
+                />
+                Yes
+              </div>
+
+              <div className="parameters-of-carousel__input">
+                <input
+                  name="infinite"
+                  type="radio"
+                  value="n"
+                  checked={!infinite}
+                  onClick={() => {
+                    this.setState({ infinite: false });
+                  }}
+                />
+                No
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <Carousel
+          images={images}
+          step={step}
+          frameSize={frameSize}
+          itemWidth={itemWidth}
+          animationDuration={animationDuration}
+          infinite={infinite}
+        />
       </div>
     );
   }
