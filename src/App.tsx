ddda3eddc56270e7a9798c6b2bc5bbc3385162c6
Denys Kleimenov <denyskleimenovit@gmail.com>
2022-11-09 21:06:04 +0200

    Carousel was implemented

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,10 @@ import Carousel from './components/Carousel';
 
 interface State {
   images: string[];
+  itemWidth: number;
+  frameSize: number;
+  step: number;
+  animationDuration: number;
 }
 
 class App extends React.Component<{}, State> {
@@ -20,17 +24,84 @@ class App extends React.Component<{}, State> {
       './img/9.png',
       './img/10.png',
     ],
+    itemWidth: 130,
+    frameSize: 3,
+    step: 3,
+    animationDuration: 1000,
   };
 
   render() {
-    const { images } = this.state;
+    const {
+      images, itemWidth, frameSize, step, animationDuration,
+    } = this.state;
+
+    const inputWidth = { width: 300 };
 
     return (
       <div className="App">
         {/* eslint-disable-next-line */}
-        <h1>Carousel with {images.length} images</h1>
+        <h1 data-cy='title'>Carousel with {images.length} images</h1>
 
-        <Carousel />
+        <Carousel
+          images={images}
+          itemWidth={itemWidth}
+          frameSize={frameSize}
+          step={step}
+          animationDuration={animationDuration}
+        />
+
+        {`Emoji Width (${itemWidth})`}
+        <input
+          type="range"
+          name="itemWidth"
+          style={inputWidth}
+          value={itemWidth}
+          step={5}
+          min={60}
+          max={200}
+          onChange={event => {
+            this.setState({ itemWidth: +event.target.value });
+          }}
+        />
+        {`Frame Size (${frameSize})`}
+        <input
+          type="range"
+          name="frameSize"
+          style={inputWidth}
+          value={frameSize}
+          step={1}
+          min={1}
+          max={5}
+          onChange={event => {
+            this.setState({ frameSize: +event.target.value });
+          }}
+        />
+        {`Step (${step})`}
+        <input
+          type="range"
+          name="step"
+          style={inputWidth}
+          value={step}
+          step={1}
+          min={1}
+          max={5}
+          onChange={event => {
+            this.setState({ step: +event.target.value });
+          }}
+        />
+        {`Animation Duration (${animationDuration})`}
+        <input
+          type="range"
+          name="animationDuration"
+          style={inputWidth}
+          value={animationDuration}
+          step={100}
+          min={100}
+          max={3000}
+          onChange={event => {
+            this.setState({ animationDuration: +event.target.value });
+          }}
+        />
       </div>
     );
   }
--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,18 +1,93 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+type Props = {
+  images: string[],
+  itemWidth: number,
+  frameSize: number,
+  step: number,
+  animationDuration: number,
+};
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+const Carousel: React.FC<Props> = ({
+  images, itemWidth, frameSize, step, animationDuration,
+}) => {
+  const [movePx, setMovePx] = useState(0);
+  const totalWidth = -(itemWidth * 10);
+  const maxWidth = totalWidth + itemWidth * frameSize;
+
+  const handleNextEmojiClick = () => {
+    setMovePx((currentMovePx) => {
+      if (currentMovePx - itemWidth * step < (maxWidth)) {
+        for (let i = itemWidth * step; i !== 0; i -= itemWidth) {
+          if (currentMovePx - i === maxWidth) {
+            return currentMovePx - i;
+          }
+        }
+      }
+
+      return currentMovePx - itemWidth * step;
+    });
+  };
+
+  const handlePrevEmojiClick = () => {
+    setMovePx((currentMovePx) => {
+      if (currentMovePx + itemWidth * step > 0) {
+        for (let i = itemWidth; i !== 0; i += itemWidth) {
+          if (currentMovePx + i === 0) {
+            return currentMovePx + i;
+          }
+        }
+      }
+
+      return currentMovePx + itemWidth * step;
+    });
+  };
+
+  return (
+    <div className="Carousel">
+      <div className="container" style={{ width: frameSize * itemWidth }}>
+        <div
+          className="container2"
+          style={{
+            transform: `translateX(${movePx}px)`,
+            transition: `transform ${animationDuration}ms`,
+          }}
+        >
+          <ul className="Carousel__list">
+            {images.map((image, index) => (
+              <li key={image}>
+                <img
+                  src={image}
+                  alt={`emoji ${index + 1}`}
+                  width={itemWidth}
+                />
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      <div className="Carousel__buttons">
+        <button
+          type="button"
+          className="Carousel__buttons-button"
+          onClick={handlePrevEmojiClick}
+          disabled={movePx === 0}
+        >
+          {'Prev <<'}
+        </button>
+        <button // style buttons so that they look like arrows
+          type="button"
+          className="Carousel__buttons-button" // add a disabled class and style for this class
+          onClick={handleNextEmojiClick}
+          disabled={movePx === maxWidth}
+          data-cy="next"
+        >
+          {'Next >>'}
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Carousel;
