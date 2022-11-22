b7a65633d903842aec6c1ff224e81fadceb4dba6
Alina <alina@Vovas-MacBook-Pro.local>
2022-11-22 22:48:34 +0200

    slider done

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,11 @@ import './App.scss';
 import Carousel from './components/Carousel';
 
 interface State {
-  images: string[];
+  images?: string[];
+  itemWidth?: number;
+  frameSize?: number;
+  step?: number;
+  animationDuration?: number;
 }
 
 class App extends React.Component<{}, State> {
@@ -20,17 +24,107 @@ class App extends React.Component<{}, State> {
       './img/9.png',
       './img/10.png',
     ],
+    itemWidth: 130,
+    frameSize: 3,
+    step: 3,
+    animationDuration: 1000,
+    infinite: true,
   };
 
+  updateInputValue(value: string, className: string) {
+    const newState = { [className]: +value } as Pick<State, keyof State>;
+
+    this.setState(newState);
+  }
+
   render() {
-    const { images } = this.state;
+    const {
+      images,
+      itemWidth,
+      frameSize,
+      step,
+      animationDuration,
+      infinite,
+    } = this.state;
 
     return (
       <div className="App">
         {/* eslint-disable-next-line */}
-        <h1>Carousel with {images.length} images</h1>
+        <h1 data-cy={'title'}>Carousel with {images.length} images</h1>
+        <div className="input">
+          <label className="input-block" htmlFor="itemId">
+            Item width
+            <input
+              value={this.state.itemWidth || 130}
+              onChange={(event) => {
+                return this.updateInputValue(event.target.value, 'itemWidth');
+              }}
+              type="number"
+              min="100"
+              max="500"
+              step="10"
+              className="itemId"
+              placeholder="Item width"
+            />
+          </label>
+          <label className="input-block" htmlFor="frameId">
+            Frame size
+            <input
+              value={this.state.frameSize || 3}
+              onChange={(event) => {
+                return this.updateInputValue(event.target.value, 'frameSize');
+              }}
+              type="number"
+              min="1"
+              max={images.length}
+              className="frameId"
+              placeholder="Frame size"
+            />
+          </label>
 
-        <Carousel />
+          <label className="input-block" htmlFor="stepId">
+            Step
+            <input
+              value={this.state.step || 3}
+              onChange={(event) => {
+                return this.updateInputValue(event.target.value, 'step');
+              }}
+              type="number"
+              min="1"
+              max={images.length}
+              className="stepId"
+              placeholder="Step"
+            />
+          </label>
+
+          <label className="input-block" htmlFor="animationDuration">
+            Animation duration
+            <input
+              value={this.state.animationDuration || 1000}
+              onChange={(event) => {
+                return (
+                  this.updateInputValue(event.target.value, 'animationDuration')
+                );
+              }}
+              type="number"
+              min="200"
+              max="5000"
+              step="100"
+              className="animationDuration"
+              placeholder="Animation duration"
+            />
+          </label>
+
+        </div>
+
+        <Carousel
+          images={images}
+          frameSize={frameSize}
+          step={step}
+          itemWidth={itemWidth}
+          animationDuration={animationDuration}
+          infinite={infinite}
+        />
       </div>
     );
   }
