0c5386d8425906e340bd625b6f1b08ec32781be5
Yevhen Pavlovskyi <pavlovskiy07@gmail.com>
2022-11-29 11:41:39 +0200

    working

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,18 +1,141 @@
 import React from 'react';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+type State = {
+  position: number;
+};
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+type Props = {
+  images: string[];
+  step: number;
+  frameSize: number;
+  itemWidth: number;
+  animationDuration: number;
+  infinite: boolean;
+};
+
+class Carousel extends React.Component < Props, State > {
+  state = {
+    position: 0,
+  };
+
+  calcStepWidth = () => {
+    const { itemWidth, step } = this.props;
+
+    return itemWidth * step;
+  };
+
+  calcMaxRight = () => {
+    const { images, itemWidth, frameSize } = this.props;
+
+    return (images.length * -itemWidth) + (itemWidth * frameSize);
+  };
+
+  clickNext = () => {
+    const { position } = this.state;
+    const { infinite } = this.props;
+
+    const maxRight = this.calcMaxRight();
+    const stepWidth = this.calcStepWidth();
+    const currentLeft = ((position - stepWidth) < maxRight)
+      ? maxRight
+      : position - stepWidth;
+
+    this.setState({
+      position: (position === maxRight && infinite)
+        ? 0
+        : currentLeft,
+    });
+  };
+
+  clickPrev = () => {
+    const { position } = this.state;
+    const { infinite } = this.props;
+
+    const maxRight = this.calcMaxRight();
+    const stepWidth = this.calcStepWidth();
+    const currentLeft = ((position + stepWidth) > 0)
+      ? 0
+      : position + stepWidth;
+
+    this.setState({
+      position: (position === 0 && infinite)
+        ? maxRight
+        : currentLeft,
+    });
+  };
+
+  render() {
+    const { position } = this.state;
+
+    const {
+      images,
+      frameSize,
+      itemWidth,
+      animationDuration,
+      infinite,
+    } = this.props;
+
+    return (
+      <div className="carousel">
+        <div
+          className="carousel__wraper"
+          style={{
+            width: `${frameSize * itemWidth}px`,
+            height: `${itemWidth}px`,
+          }}
+        >
+          <ul
+            className="carousel__list"
+            style={{
+              left: `${position}px`,
+              transition: `${animationDuration}ms`,
+            }}
+          >
+            {
+              images.map((image, i) => {
+                return (
+                  <li
+                    key={image}
+                    className="carousel__item"
+                    style={{
+                      width: `${itemWidth}px`,
+                      height: `${itemWidth}px`,
+                    }}
+                  >
+                    <img
+                      src={image}
+                      alt={`${i + 1}`}
+                    />
+                  </li>
+                );
+              })
+            }
+          </ul>
+        </div>
+
+        <div className="carousel__buttons">
+          <button
+            className="carousel__button"
+            type="button"
+            onClick={this.clickPrev}
+            disabled={(!infinite && position === 0) && true}
+          >
+            &#8592;
+          </button>
+
+          <button
+            className="carousel__button"
+            type="button"
+            onClick={this.clickNext}
+            disabled={(!infinite && position === this.calcMaxRight()) && true}
+          >
+            &#8594;
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Carousel;
