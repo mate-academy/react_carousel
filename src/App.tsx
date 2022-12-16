50a619a000846f253070c3a7a8d7b1dbadbd48e5
Oleksandr Filo <filo.sanya@gmail.com>
2022-12-16 18:08:03 +0200

    carousel was created

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.scss';
-import Carousel from './components/Carousel';
+import { Carousel } from './components/Carousel';
 
 interface State {
   images: string[];
+  itemWidth: number,
+  frameSize: number,
+  step: number,
+  animationDuration: number,
 }
 
 class App extends React.Component<{}, State> {
@@ -20,17 +24,113 @@ class App extends React.Component<{}, State> {
       './img/9.png',
       './img/10.png',
     ],
+    itemWidth: 130,
+    frameSize: 3,
+    step: 3,
+    animationDuration: 1000,
   };
 
+  handleChangeItemWidth = (event: React.ChangeEvent<HTMLInputElement>) => (
+    this.setState({
+      itemWidth: +event.target.value,
+    })
+  );
+
+  handleChangeFrameSize = (event: React.ChangeEvent<HTMLInputElement>) => (
+    this.setState({
+      frameSize: +event.target.value,
+    })
+  );
+
+  handleChangeStepValue = (event: React.ChangeEvent<HTMLInputElement>) => (
+    this.setState({
+      step: +event.target.value,
+    })
+  );
+
+  handleChangeAnimDuration = (event: React.ChangeEvent<HTMLInputElement>) => (
+    this.setState({
+      animationDuration: +event.target.value,
+    })
+  );
+
   render() {
-    const { images } = this.state;
+    const {
+      images,
+      itemWidth,
+      frameSize,
+      step,
+      animationDuration,
+    } = this.state;
 
     return (
       <div className="App">
         {/* eslint-disable-next-line */}
-        <h1>Carousel with {images.length} images</h1>
+        <h1 data-cy="title">Carousel with {images.length} images</h1>
 
-        <Carousel />
+        <form action="#" className="App__parameters">
+          <label htmlFor="itemId">
+            {'ItemWidth: '}
+            <input
+              id="itemId"
+              type="number"
+              name="itemWidth"
+              className="App__input"
+              min="1"
+              defaultValue={itemWidth}
+              onChange={this.handleChangeItemWidth}
+            />
+          </label>
+
+          <label htmlFor="frameId">
+            {'FrameSize: '}
+            <input
+              id="frameId"
+              type="number"
+              name="frameSize"
+              className="App__input"
+              min="1"
+              max="10"
+              defaultValue={frameSize}
+              onChange={this.handleChangeFrameSize}
+            />
+          </label>
+
+          <label htmlFor="stepId">
+            {'Step: '}
+            <input
+              id="stepId"
+              type="number"
+              name="step"
+              className="App__input"
+              min="1"
+              max="10"
+              defaultValue={step}
+              onChange={this.handleChangeStepValue}
+            />
+          </label>
+
+          <label>
+            {'AnimationDuration: '}
+            <input
+              type="number"
+              name="animationDuration"
+              className="App__input"
+              min="1"
+              max="10000"
+              defaultValue={animationDuration}
+              onChange={this.handleChangeAnimDuration}
+            />
+          </label>
+        </form>
+
+        <Carousel
+          images={images}
+          itemSize={itemWidth}
+          frameSize={frameSize}
+          step={step}
+          animationDuration={animationDuration}
+        />
       </div>
     );
   }
