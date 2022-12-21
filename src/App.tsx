d97f587deb7ba9ff6b483d91ca367b68237ca07d
July Terekhova <sky206380@gmail.com>
2022-12-21 17:18:02 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import './App.scss';
-import Carousel from './components/Carousel';
+import './styles/App.scss';
+import { Carousel } from './components/Carousel';
+import { CarouselState } from './types/CarouselState';
 
-interface State {
-  images: string[];
-}
+type State = CarouselState;
 
-class App extends React.Component<{}, State> {
+export class App extends React.Component<{}, State> {
   state = {
     images: [
       './img/1.png',
@@ -20,20 +19,100 @@ class App extends React.Component<{}, State> {
       './img/9.png',
       './img/10.png',
     ],
+    itemWidth: 130,
+    frameSize: 3,
+    step: 3,
+    animationDuration: 1000,
+    infinity: false,
   };
 
   render() {
-    const { images } = this.state;
+    const {
+      images,
+      itemWidth,
+      frameSize,
+      step,
+      animationDuration,
+      infinity,
+    } = this.state;
 
     return (
       <div className="App">
         {/* eslint-disable-next-line */}
-        <h1>Carousel with {images.length} images</h1>
+        <h1 className='App__title' data-cy='title'>Carousel with {images.length} images</h1>
+        <form
+          className="App__form"
+          onSubmit={(event) => {
+            event.preventDefault();
+          }}
+        >
+          <label>
+            Item width:
+            <input
+              name="itemWidth"
+              type="number"
+              value={itemWidth}
+              onChange={(event) => {
+                this.setState({ itemWidth: +event.target.value });
+              }}
+            />
+          </label>
+          <label>
+            Frame Size:
+            <input
+              name="frameSize"
+              type="number"
+              value={frameSize}
+              onChange={(event) => {
+                this.setState({ frameSize: +event.target.value });
+              }}
+            />
+          </label>
+          <label>
+            Step:
+            <input
+              name="step"
+              type="number"
+              value={step}
+              onChange={(event) => {
+                this.setState({ step: +event.target.value });
+              }}
+            />
+          </label>
+          <label>
+            Animation duration:
+            <input
+              name="animationDuration"
+              type="number"
+              value={animationDuration}
+              onChange={(event) => {
+                this.setState({ animationDuration: +event.target.value });
+              }}
+            />
+          </label>
+          <label>
+            Infinity:
+            <input
+              name="infinit"
+              type="checkbox"
+              checked={infinity}
+              onChange={(event) => {
+                this.setState({ infinity: event.target.checked });
+              }}
+            />
+          </label>
 
-        <Carousel />
+        </form>
+
+        <Carousel
+          images={images}
+          itemWidth={itemWidth}
+          frameSize={frameSize}
+          step={step}
+          animationDuration={animationDuration}
+          infinity={infinity}
+        />
       </div>
     );
   }
 }
-
-export default App;
--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,18 +1,133 @@
-import React from 'react';
+import { Component } from 'react';
+import { CarouselState } from '../types/CarouselState';
+
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+type Props = CarouselState;
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+type State = {
+  shift: number,
+};
 
-export default Carousel;
+export class Carousel extends Component<Props, State> {
+  state = {
+    shift: 0,
+  };
+
+  image = this.props.images;
+
+  maxShift = 1;
+
+  translateNext = () => {
+    const { itemWidth, step } = this.props;
+    const { shift } = this.state;
+    const shiftNext = itemWidth * step + shift;
+
+    const currentMaxShift = itemWidth * this.image.length
+    - step * itemWidth
+    + ((step - this.props.frameSize) * itemWidth);
+
+    this.setState({
+      shift: shiftNext > currentMaxShift ? currentMaxShift : shiftNext,
+    });
+    this.maxShift = currentMaxShift;
+
+    if (this.props.infinity) {
+      if (shiftNext > currentMaxShift && shift !== currentMaxShift) {
+        this.setState({
+          shift: currentMaxShift,
+        });
+      } else if (shift === currentMaxShift) {
+        this.setState({
+          shift: 0,
+        });
+      } else {
+        this.setState({
+          shift: shiftNext,
+        });
+      }
+    }
+  };
+
+  translatePrev = () => {
+    const { itemWidth, step } = this.props;
+    const { shift } = this.state;
+    const shiftPrev = shift - itemWidth * step;
+
+    const currentMaxShift = itemWidth * this.image.length
+    - step * itemWidth
+    + ((step - this.props.frameSize) * itemWidth);
+
+    this.setState({
+      shift: shiftPrev > 0 ? shiftPrev : 0,
+    });
+
+    if (this.props.infinity) {
+      if (shiftPrev > 0 && shift !== 0) {
+        this.setState({
+          shift: shiftPrev,
+        });
+      } else if (shift === 0) {
+        this.setState({
+          shift: currentMaxShift,
+        });
+      } else {
+        this.setState({
+          shift: 0,
+        });
+      }
+    }
+  };
+
+  render() {
+    const {
+      images,
+      frameSize,
+      itemWidth,
+      animationDuration,
+    } = this.props;
+    const { shift } = this.state;
+
+    return (
+      <div className="Carousel">
+        <button
+          type="button"
+          className="Carousel__button Carousel__button--prev"
+          onClick={() => {
+            this.translatePrev();
+          }}
+          disabled={(shift === 0 && !this.props.infinity)
+            || frameSize === images.length}
+        >
+          {' '}
+        </button>
+        <ul className="Carousel__list" style={{ width: `${frameSize * itemWidth}px` }}>
+          <div className="Carousel__container" style={{ transform: `translateX(-${shift}px)`, transition: `transform ${animationDuration / 1000}s` }}>
+            {images.map((image, index) => (
+              <li key={image}>
+                <img
+                  src={image}
+                  alt={index.toString()}
+                  className="Carousel__image"
+                  style={{ width: itemWidth, height: itemWidth }}
+                />
+              </li>
+            ))}
+          </div>
+        </ul>
+        <button
+          type="button"
+          className="Carousel__button Carousel__button--next"
+          data-cy="next"
+          onClick={() => {
+            this.translateNext();
+          }}
+          disabled={(shift === this.maxShift && !this.props.infinity)
+            || frameSize === images.length}
+        >
+          {' '}
+        </button>
+      </div>
+    );
+  }
+}
