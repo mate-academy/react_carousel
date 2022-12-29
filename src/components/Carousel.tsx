cb2f8aabb853f136795c322063cf6951895ab00d
mr-valentos <nerpavlad@gmail.com>
2022-12-29 22:18:31 +0200

    Carousel

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,18 +1,121 @@
-import React from 'react';
+import { Component } from 'react';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+type Props = {
+  images: string[],
+  step: number,
+  animationDuration: number,
+  frameSize: number,
+  itemWidth: number,
+};
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+type State = {
+  transform: string;
+};
+
+class Carousel extends Component<Props, State > {
+  state = {
+    transform: 'translateX(0px)',
+  };
+
+  x = 0;
+
+  gap = 10;
+
+  first = true;
+
+  last = false;
+
+  handlePrevBtn = (step: number, itemWidth: number) => {
+    this.x += step * itemWidth + (step * this.gap);
+    this.last = false;
+    if (this.x >= 0) {
+      this.first = true;
+      this.x = 0;
+    }
+
+    this.setState({ transform: `translateX(${this.x}px)` });
+  };
+
+  handleNextBtn = (step: number, itemWidth: number, length: number) => {
+    this.x -= step * itemWidth + (step * this.gap);
+    this.first = false;
+    const max = -((itemWidth + this.gap) * length);
+
+    if (this.x <= max) {
+      this.last = true;
+      this.x = max;
+    }
+
+    this.setState({ transform: `translateX(${this.x}px)` });
+  };
+
+  render() {
+    const {
+      images,
+      step,
+      animationDuration,
+      itemWidth,
+      frameSize,
+    } = this.props;
+    const { transform } = this.state;
+
+    const width = frameSize * itemWidth + (this.gap * (frameSize - 1));
+
+    return (
+      <div
+        className="Carousel"
+        style={{ width: `${width}px` }}
+      >
+        <ul
+          className="Carousel__list"
+          style={{
+            transform: `${transform}`,
+            transition: `transform ${animationDuration}ms`,
+            gap: `${this.gap}px`,
+          }}
+        >
+          {images.map((el, i) => (
+            <li key={el}>
+              <img
+                src={el}
+                alt={`${i}`}
+                height={itemWidth}
+                width={itemWidth}
+              />
+            </li>
+          ))}
+        </ul>
+
+        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+        <button
+          type="button"
+          className={`Carousel__btn prev-btn ${this.first && 'disabled'}`}
+          style={{
+            background: 'url("./img/arrow-left.svg")0 0/ cover no-repeat',
+          }}
+          onClick={() => this.handlePrevBtn(
+            step,
+            itemWidth,
+          )}
+        />
+        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+        <button
+          type="button"
+          data-cy="next"
+          className={`Carousel__btn next-btn ${this.last && 'disabled'}`}
+          style={{
+            background: 'url("./img/arrow-right.svg")0 0/ cover no-repeat',
+          }}
+          onClick={() => this.handleNextBtn(
+            step,
+            itemWidth,
+            (images.length - frameSize),
+          )}
+        />
+      </div>
+    );
+  }
+}
 
 export default Carousel;
