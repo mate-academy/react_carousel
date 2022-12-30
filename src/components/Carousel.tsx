ac1e98980f934128280e96ef82f838ce2fdb9951
Volodymir Tymtsias <vovanuch2007@gmail.com>
2022-12-30 18:30:52 +0200

    Solution React Carousel

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,18 +1,101 @@
 import React from 'react';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+type Props = {
+  images: string[];
+  step: number;
+  frameSize: number;
+  itemWidth: number;
+  animationDuration: number;
+  infinite: boolean;
+};
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+type State = {
+  position: number;
+};
 
-export default Carousel;
+export class Carousel extends React.Component<Props, State> {
+  state: Readonly<State> = {
+    position: 0,
+  };
+
+  render() {
+    const {
+      images,
+      step,
+      frameSize,
+      itemWidth,
+      animationDuration,
+      infinite,
+    } = this.props;
+    let { position } = this.state;
+    const width = itemWidth * frameSize + 10 * (frameSize - 1);
+    const endPosition = (-itemWidth - 10) * (images.length - frameSize);
+    const shift = (itemWidth + 10) * step;
+
+    return (
+      <div className="Carousel" style={{ width: `${width}px` }}>
+        <ul
+          className="Carousel__list"
+          style={{
+            transition: `margin-left ${animationDuration}ms`,
+            marginLeft: `${position}px`,
+          }}
+        >
+          {images.map((image) => {
+            const nameImage = image.match(/[^\\/]+(?=\.\w+$)/) || '';
+
+            return (
+              <li key={images.indexOf(image)}>
+                <img
+                  src={image}
+                  alt={nameImage[0]}
+                  style={{
+                    width: `${itemWidth}px`,
+                    height: `${itemWidth}px`,
+                  }}
+                />
+              </li>
+            );
+          })}
+        </ul>
+
+        <div className="Carousel__buttons">
+          <button
+            type="button"
+            onClick={() => {
+              if (position === 0 && infinite) {
+                position = endPosition;
+              } else {
+                position += shift;
+                position = Math.min(position, 0);
+              }
+
+              this.setState({ position });
+            }}
+            disabled={position === 0 && !infinite}
+          >
+            ⇦
+          </button>
+          <button
+            type="button"
+            data-cy="next"
+            onClick={() => {
+              if (position === endPosition && infinite) {
+                position = 0;
+              } else {
+                position -= shift;
+                position = Math.max(position, endPosition);
+              }
+
+              this.setState({ position });
+            }}
+            disabled={position === endPosition && !infinite}
+          >
+            ⇨
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
