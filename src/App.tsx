6cc3884ddb4a41d0b7051db0eee9b68420f07ca9
Artem Hirzhev <a.hirzhev@gmail.com>
2022-12-31 17:10:29 +0200

    Initail commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.scss';
-import Carousel from './components/Carousel';
+import { Carousel } from './components/Carousel';
 
 interface State {
   images: string[];
+  step: number;
+  frameSize: number;
+  itemWidth: number;
+  animationDuration: number;
+  infinite: boolean;
 }
 
 class App extends React.Component<{}, State> {
@@ -20,17 +25,129 @@ class App extends React.Component<{}, State> {
       './img/9.png',
       './img/10.png',
     ],
+    step: 3,
+    frameSize: 3,
+    itemWidth: 130,
+    animationDuration: 1000,
+    infinite: false,
+  };
+
+  handleEvent = (id: string) => {
+    const element = document.querySelector(`#${id}`) as HTMLInputElement;
+
+    if (!element.value) {
+      this.setState(state => ({
+        step: state.step,
+      }));
+    } else {
+      switch (id) {
+        case 'step':
+          this.setState({
+            step: +element.value,
+          });
+          break;
+
+        case 'frameSize':
+          this.setState({
+            frameSize: +element.value,
+          });
+          break;
+
+        case 'itemWidth':
+          this.setState({
+            itemWidth: +element.value,
+          });
+          break;
+
+        case 'animationDuration':
+          this.setState({
+            animationDuration: +element.value,
+          });
+          break;
+
+        default:
+          throw new Error('Wrong id');
+      }
+    }
   };
 
   render() {
-    const { images } = this.state;
+    const {
+      images,
+      step,
+      frameSize,
+      itemWidth,
+      animationDuration,
+      infinite,
+    } = this.state;
 
     return (
       <div className="App">
         {/* eslint-disable-next-line */}
-        <h1>Carousel with {images.length} images</h1>
+        <h1 data-cy="title">Carousel with {images.length} images</h1>
 
-        <Carousel />
+        <form className="form">
+          <label className="form__field" htmlFor="step">Step</label>
+
+          <input
+            id="step"
+            className="form__input"
+            type="number"
+            min="0"
+            onBlur={() => (this.handleEvent('step'))}
+            placeholder={`${step}`}
+          />
+
+          <label className="form__field" htmlFor="frameSize">Frame Size</label>
+
+          <input
+            id="frameSize"
+            className="form__input"
+            type="number"
+            min="0"
+            max="10"
+            onBlur={() => (this.handleEvent('frameSize'))}
+            placeholder={`${frameSize}`}
+          />
+
+          <label className="form__field" htmlFor="itemWidth">Item Width</label>
+
+          <input
+            id="itemWidth"
+            className="form__input"
+            type="number"
+            min="130"
+            max="1300"
+            onBlur={() => (this.handleEvent('itemWidth'))}
+            placeholder={`${itemWidth}`}
+          />
+
+          <label
+            className="form__field"
+            htmlFor="animationDuration"
+          >
+            Animation Duration
+          </label>
+
+          <input
+            id="animationDuration"
+            className="form__input"
+            type="number"
+            min="0"
+            max="10000"
+            onBlur={() => (this.handleEvent('animationDuration'))}
+            placeholder={`${animationDuration}`}
+          />
+        </form>
+
+        <Carousel
+          images={images}
+          step={step}
+          frameSize={frameSize}
+          itemWidth={itemWidth}
+          animationDuration={animationDuration}
+          infinite={infinite}
+        />
       </div>
     );
   }
