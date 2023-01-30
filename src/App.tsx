1978dafe672e2bc38c2fccf0b2f70e75820210fe
Ihor Bondarenko <garryrocksmanjs@gmail.com>
2023-01-30 20:18:01 +0200

    React Carousel - completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.scss';
-import Carousel from './components/Carousel';
+import { Carousel } from './components/Carousel';
+import 'bulma/css/bulma.min.css';
 
 interface State {
   images: string[];
+  ItemWidth: number;
+  FrameSize: number;
+  Step: number;
+  AnimationDuration: number;
 }
 
 class App extends React.Component<{}, State> {
@@ -20,17 +25,130 @@ class App extends React.Component<{}, State> {
       './img/9.png',
       './img/10.png',
     ],
+    ItemWidth: 130,
+    FrameSize: 3,
+    Step: 3,
+    AnimationDuration: 1000,
   };
 
   render() {
-    const { images } = this.state;
+    const {
+      images,
+      ItemWidth,
+      FrameSize,
+      Step,
+      AnimationDuration,
+    } = this.state;
 
     return (
       <div className="App">
         {/* eslint-disable-next-line */}
-        <h1>Carousel with {images.length} images</h1>
+        <h1 className="App__title title is-1" data-cy="title">Carousel with {images.length} images</h1>
 
-        <Carousel />
+        <Carousel
+          images={images}
+          step={Step}
+          frameSize={FrameSize}
+          itemWidth={ItemWidth}
+          animationDuration={AnimationDuration}
+          infinite={false}
+        />
+
+        <form
+          action="/"
+          method="POST"
+          onSubmit={(event) => {
+            event.preventDefault();
+          }}
+        >
+          <div className="field">
+            <label
+              htmlFor="itemWidth"
+              className="label"
+            >
+              Item Width:
+            </label>
+            <input
+              id="itemWidth"
+              className="input"
+              type="number"
+              name="ItemWidth"
+              placeholder="Enter an item Width"
+              value={ItemWidth}
+              onChange={(event) => {
+                this.setState({
+                  ItemWidth: +event.target.value,
+                });
+              }}
+            />
+          </div>
+
+          <div className="field">
+            <label
+              htmlFor="frameSize"
+              className="label"
+            >
+              Frame Size
+            </label>
+            <input
+              id="frameSize"
+              className="input"
+              type="number"
+              name="FrameSize"
+              placeholder="Enter a Frame Size"
+              value={FrameSize}
+              onChange={event => {
+                this.setState({
+                  FrameSize: +event.target.value,
+                });
+              }}
+            />
+          </div>
+
+          <div className="field">
+            <label
+              htmlFor="step"
+              className="label"
+            >
+              Step:
+            </label>
+            <input
+              id="step"
+              className="input"
+              type="number"
+              name="Step"
+              placeholder="Enter Step"
+              value={Step}
+              onChange={event => {
+                this.setState({
+                  Step: +event.target.value,
+                });
+              }}
+            />
+          </div>
+
+          <div className="field">
+            <label
+              htmlFor="animationDuration"
+              className="label"
+            >
+              AnimationDuration:
+            </label>
+            <input
+              id="animationDuration"
+              className="input"
+              type="number"
+              name="AnimationDuration"
+              placeholder="Enter an Animation Duration"
+              value={AnimationDuration}
+              onChange={event => {
+                this.setState({
+                  AnimationDuration: +event.target.value,
+                });
+              }}
+            />
+          </div>
+        </form>
       </div>
     );
   }
--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,18 +1,104 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+type Props = {
+  images: string[];
+  step: number;
+  frameSize: number;
+  itemWidth: number;
+  animationDuration: number;
+  infinite: boolean;
+};
+export const Carousel: React.FC<Props> = (props) => {
+  const {
+    images,
+    step,
+    frameSize,
+    itemWidth,
+    animationDuration,
+    infinite,
+  } = props;
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+  const [move, setMove] = useState(0);
+  const [itemLength, setItemWidth] = useState(itemWidth);
+  const carouselWidth = images.length * itemLength;
 
-export default Carousel;
+  useEffect(() => {
+    if (itemWidth !== itemLength) {
+      if (move !== 0) {
+        setMove(prevState => prevState - frameSize);
+      }
+
+      setItemWidth(itemWidth);
+    }
+  });
+  const handlerNext = () => {
+    if (move < -carouselWidth + step * itemLength) {
+      if (infinite) {
+        setMove(0);
+      }
+    } else {
+      setMove(prevPosition => prevPosition - step * itemLength);
+    }
+  };
+
+  const handlerPrev = () => {
+    if (move < 0) {
+      setMove(prevPosition => prevPosition + step * itemLength);
+    }
+  };
+
+  return (
+    <div
+      className="Carousel"
+      style={{
+        width: `${frameSize * itemLength}px`,
+      }}
+    >
+      <ul
+        className="Carousel__list"
+        style={{
+          transform: `translateX(${move}px)`,
+          transitionDuration: `${animationDuration}ms`,
+        }}
+      >
+        {images.map((image, index) => (
+          <li
+            key={Math.random()}
+            style={{ width: `${itemLength}px` }}
+          >
+            <figure
+              style={{ width: `${itemLength}px` }}
+            >
+              <img
+                className="Carousel__picture"
+                src={image}
+                alt={(index + 1).toString()}
+                style={{ width: `${itemLength}px` }}
+              />
+            </figure>
+          </li>
+        ))}
+      </ul>
+      <button
+        className="Carousel__button button is-success"
+        type="button"
+        onClick={() => {
+          handlerPrev();
+        }}
+      >
+        Prev
+      </button>
+      <button
+        className="Carousel__button button is-warning"
+        data-cy="next"
+        type="button"
+        onClick={() => {
+          handlerNext();
+        }}
+      >
+        Next
+      </button>
+    </div>
+  );
+};
