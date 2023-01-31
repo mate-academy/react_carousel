376e3833f77a9185fd27555bc816f43fb26579c5
Pavel Maistrenko <maystrenko.p@gmail.com>
2023-01-31 13:10:45 +0100

    solution

---
+++ src/ImagesList.tsx
@@ -0,0 +1,12 @@
+export const imagesFromServ = [
+  { id: 1, path: './img/1.png' },
+  { id: 2, path: './img/2.png' },
+  { id: 3, path: './img/3.png' },
+  { id: 4, path: './img/4.png' },
+  { id: 5, path: './img/5.png' },
+  { id: 6, path: './img/6.png' },
+  { id: 7, path: './img/7.png' },
+  { id: 8, path: './img/8.png' },
+  { id: 9, path: './img/9.png' },
+  { id: 10, path: './img/10.png' },
+];
