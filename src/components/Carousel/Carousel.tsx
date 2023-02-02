48036cfbcba5d313efdccb1d7fbb9a9d642f6b21
Oselskyi Oleksandr <oselskyi3@gmail.com>
2023-02-02 14:58:36 +0200

    Solution

---
+++ src/components/Carousel/Carousel.tsx
@@ -0,0 +1,124 @@
+import React from 'react';
+import './Carousel.scss';
+
+type Props = {
+  images: string[];
+  step: number;
+  frameSize: number;
+  itemWidth: number;
+  animationDuration: number;
+  infinite: boolean;
+};
+
+type State = {
+  transform: number;
+};
+
+export class Carousel extends React.Component<Props, State> {
+  state = {
+    transform: 0,
+  };
+
+  handlePrevClick = () => {
+    const {
+      images,
+      step,
+      frameSize,
+      itemWidth,
+      infinite,
+    } = this.props;
+
+    const hiddenImagesSize = (images.length - frameSize) * itemWidth;
+
+    this.setState((state) => ({
+      transform: Math.min(state.transform + itemWidth * step, 0),
+    }));
+
+    if (this.state.transform === 0 && infinite) {
+      this.setState({ transform: -hiddenImagesSize });
+    }
+  };
+
+  handleNextClick = () => {
+    const {
+      images,
+      step,
+      frameSize,
+      itemWidth,
+      infinite,
+    } = this.props;
+
+    const hiddenImagesSize = (images.length - frameSize) * itemWidth;
+
+    this.setState((state) => ({
+      transform: Math.max(state.transform - (itemWidth * step),
+        -hiddenImagesSize),
+    }));
+
+    if (this.state.transform === -hiddenImagesSize && infinite) {
+      this.setState({ transform: 0 });
+    }
+  };
+
+  render() {
+    const {
+      images,
+      frameSize,
+      itemWidth,
+      infinite,
+      animationDuration,
+    } = this.props;
+
+    const { transform } = this.state;
+
+    return (
+      <div className="Carousel">
+        <ul
+          className="Carousel__list"
+          style={{ width: `${frameSize * itemWidth}px` }}
+        >
+          {images.map((image, index) => (
+            <li
+              key={image}
+              style={{
+                transform: `translateX(${transform}px)`,
+                transition: `${animationDuration}ms`,
+              }}
+            >
+              <img
+                src={image}
+                alt={`${index + 1}`}
+                style={{ width: `${itemWidth}px` }}
+              />
+            </li>
+          ))}
+        </ul>
+
+        <div className="Carousel__buttons">
+          <button
+            className="Carousel__button"
+            type="button"
+            onClick={this.handlePrevClick}
+            disabled={transform === 0 && !infinite}
+            data-cy="Next"
+          >
+            &#8249;
+          </button>
+
+          <button
+            data-cy="next"
+            className="Carousel__button"
+            type="button"
+            onClick={this.handleNextClick}
+            disabled={
+              transform === -(images.length - frameSize) * itemWidth
+              && !infinite
+            }
+          >
+            &#8250;
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import './Form.scss';
+
+type Props = {
+  itemWidth: number;
+  handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  frameSize: number;
+  step: number;
+  animationDuration: number;
+  images: string[];
+  infinite: boolean;
+  handleInfinite: () => void;
+};
+
+export const Form:React.FC<Props> = ({
+  itemWidth,
+  handleChange,
+  frameSize,
+  step,
+  animationDuration,
+  images,
+  infinite,
+  handleInfinite,
+}) => (
+  <form className="Form">
+    <label>
+      Item width:
+      <input
+        className="Form__input"
+        type="number"
+        name="itemWidth"
+        value={itemWidth}
+        min="100"
+        max="200"
+        onChange={handleChange}
+      />
+    </label>
+
+    <label>
+      Frame size:
+      <input
+        className="Form__input"
+        type="number"
+        name="frameSize"
+        value={frameSize}
+        min="1"
+        max={images.length}
+        onChange={handleChange}
+      />
+    </label>
+
+    <label>
+      Step:
+      <input
+        className="Form__input"
+        type="number"
+        name="step"
+        value={step}
+        min="1"
+        max={images.length - frameSize}
+        onChange={handleChange}
+      />
+    </label>
+
+    <label>
+      Animation duration:
+      <input
+        className="Form__input"
+        type="number"
+        name="animationDuration"
+        value={animationDuration}
+        min="100"
+        max="5000"
+        step="100"
+        onChange={handleChange}
+      />
+    </label>
+
+    <label>
+      Infinite:
+      <input
+        className="Form__input"
+        type="checkbox"
+        name="infinite"
+        checked={infinite}
+        onChange={handleInfinite}
+      />
+    </label>
+  </form>
+);
