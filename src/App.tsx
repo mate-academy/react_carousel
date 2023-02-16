6663a90a8fec80199c8a3f676bbeec08fcbc2365
Tetiana Koval <tetiana.koval.bit@gmail.com>
2023-02-16 13:53:15 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,11 @@ import Carousel from './components/Carousel';
 
 interface State {
   images: string[];
+  step: number;
+  frameSize: number;
+  itemWidth: number;
+  animationDuration: number;
+  infinite: boolean;
 }
 
 class App extends React.Component<{}, State> {
@@ -20,6 +25,11 @@ class App extends React.Component<{}, State> {
       './img/9.png',
       './img/10.png',
     ],
+    step: 3,
+    frameSize: 3,
+    itemWidth: 130,
+    animationDuration: 1000,
+    infinite: false,
   };
 
   render() {
@@ -28,9 +38,62 @@ class App extends React.Component<{}, State> {
     return (
       <div className="App">
         {/* eslint-disable-next-line */}
-        <h1>Carousel with {images.length} images</h1>
+        <h1 data-cy="title">Carousel with {images.length} images</h1>
+        <label htmlFor="step">
+          Step:
+          <input
+            type="number"
+            min="1"
+            max="5"
+            onBlur={(e) => this.setState({ step: +e.target.value })}
+          />
+        </label>
+        <label htmlFor="frameSize">
+          Frame Size:
+          <input
+            type="number"
+            min="1"
+            max="5"
+            onBlur={(e) => this.setState({ frameSize: +e.target.value })}
+          />
+        </label>
+        <label htmlFor="itemWidth">
+          Item Width:
+          <input
+            type="number"
+            min="50"
+            max="1000"
+            onBlur={(e) => this.setState({ itemWidth: +e.target.value })}
+          />
+        </label>
+        <label htmlFor="animationDuration">
+          Animation Duration:
+          <input
+            type="number"
+            min="100"
+            max="5000"
+            onBlur={(e) => this
+              .setState({ animationDuration: +e.target.value })}
+          />
+        </label>
+        <label htmlFor="infinite">
+          Infinite:
+          <input
+            type="checkbox"
+            // onChange={(e) => console.log(e.target.checked)}
+            onChange={(e:React.ChangeEvent<HTMLInputElement>) => this
+              .setState({ infinite: e.target.checked })}
+          />
+        </label>
 
-        <Carousel />
+        <Carousel
+          images={images}
+          step={this.state.step}
+          frameSize={this.state.frameSize}
+          itemWidth={this.state.itemWidth}
+          animationDuration={this.state.animationDuration}
+          infinite={this.state.infinite}
+        />
       </div>
     );
   }
--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,18 +1,123 @@
 import React from 'react';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+type State = {
+  position: number,
+};
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+type Props = {
+  images: string[],
+  step: number,
+  frameSize: number,
+  itemWidth: number,
+  animationDuration: number,
+  infinite: boolean,
+};
+
+class Carousel extends React.Component<Props, State> {
+  state = {
+    position: 0,
+  };
+
+  moveSlider = (buttonName: string) => {
+    const {
+      images,
+      step,
+      frameSize,
+      itemWidth,
+      infinite,
+    } = this.props;
+    const imagesLength = itemWidth * images.length;
+    const lastSlidePosition = imagesLength - (frameSize * itemWidth);
+    const lastSlideWidth = (images.length % frameSize) * itemWidth;
+
+    this.setState((prevState) => {
+      const prevPosition = prevState.position;
+      const slideWidth = step * itemWidth;
+
+      if (buttonName === 'Next') {
+        if (prevPosition === lastSlidePosition && infinite) {
+          return ({ position: 0 });
+        }
+
+        if (prevPosition + slideWidth > lastSlidePosition) {
+          return ({ position: lastSlidePosition });
+        }
+
+        return ({ position: prevPosition + slideWidth });
+      }
+
+      if (buttonName === 'Prev') {
+        if (prevPosition === 0 && infinite) {
+          return ({ position: lastSlidePosition });
+        }
+
+        if (prevPosition === lastSlideWidth || prevPosition < slideWidth) {
+          return ({ position: 0 });
+        }
+
+        return ({ position: prevPosition - slideWidth });
+      }
+
+      return ({ position: 0 });
+    });
+  };
+
+  render() {
+    const {
+      images,
+      frameSize,
+      itemWidth,
+      animationDuration,
+    } = this.props;
+
+    return (
+      <div
+        className="Carousel"
+        style={{
+          width: `${frameSize * itemWidth}px`,
+        }}
+      >
+        <ul
+          className="Carousel__list"
+          style={{
+            transform: `translate(-${this.state.position}px)`,
+            transition: `transform ${animationDuration}ms ease`,
+          }}
+        >
+          {images.map((image, index) => (
+            <li
+              key={image}
+              style={{
+                width: `${itemWidth}px`,
+                height: `${itemWidth}px`,
+              }}
+            >
+              <img src={image} alt={`${index}`} />
+            </li>
+          ))}
+        </ul>
+
+        <button
+          type="button"
+          onClick={(event) => {
+            this.moveSlider(event.currentTarget.innerText);
+          }}
+        >
+          Prev
+        </button>
+        <button
+          type="button"
+          data-cy="next"
+          onClick={(event) => {
+            this.moveSlider(event.currentTarget.innerText);
+          }}
+        >
+          Next
+        </button>
+      </div>
+    );
+  }
+}
 
 export default Carousel;
