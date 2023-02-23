f203f0d04957f00f30cdd0cd6bf4999083f36261
Skuratovskyi Vladyslav <vladskur0369@gmail.com>
2023-02-24 01:43:51 +0200

    Solution v10.1: key is work

--- src/components/ImagesInfo.tsx
+++ src/components/ImagesInfo.tsx
@@ -1,17 +1,14 @@
 export const ImagesInfo: React.FC <{
   image: string;
   pass: { itemWidth: number };
-  key: React.Key;
-}> = ({ image, pass, key }) => (
-  <>
-    <li key={key}>
-      <img
-        src={image}
-        alt={`${image}`}
-        style={{
-          width: `${pass.itemWidth}px`,
-        }}
-      />
-    </li>
-  </>
+}> = ({ image, pass }) => (
+  <li>
+    <img
+      src={image}
+      alt={`${image}`}
+      style={{
+        width: `${pass.itemWidth}px`,
+      }}
+    />
+  </li>
 );
--- src/components/ImagesList.tsx
+++ src/components/ImagesList.tsx
@@ -1,13 +1,13 @@
 import { ImagesInfo } from './ImagesInfo';
 
-export const ImagesList: React.FC <{
+export const ImagesList: React.FC<{
   images: string[];
   pass: { itemWidth: number };
 }> = ({ images, pass }) => (
   <>
     {
       images.map((image) => (
-        <ImagesInfo image={image} pass={pass} key={image} />
+        <ImagesInfo key={image} image={image} pass={pass} />
       ))
     }
   </>
