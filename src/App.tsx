2e1ac23d3912d05b0b59dab3c2b82905b04d4c62
Vadym Prydatok <vadymprydatok@gmail.com>
2023-03-04 20:30:28 +0200

    carousel

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,11 @@ import Carousel from './components/Carousel';
 
 interface State {
   images: string[];
+  withOfItem: number;
+  frameSize: number;
+  rollingStep: number;
+  animation: number;
+  infiniteRolling: boolean;
 }
 
 class App extends React.Component<{}, State> {
@@ -20,17 +25,105 @@ class App extends React.Component<{}, State> {
       './img/9.png',
       './img/10.png',
     ],
+
+    withOfItem: 130,
+    frameSize: 3,
+    rollingStep: 3,
+    animation: 1000,
+    infiniteRolling: false,
   };
 
   render() {
-    const { images } = this.state;
+    const {
+      images,
+      withOfItem,
+      frameSize,
+      rollingStep,
+      animation,
+      infiniteRolling,
+    } = this.state;
 
     return (
       <div className="App">
-        {/* eslint-disable-next-line */}
-        <h1>Carousel with {images.length} images</h1>
+        <h1 data-cy="title">{`Carousel with ${images.length} images`}</h1>
+        <form className="Options">
+          <label className="label" htmlFor="itemWidth">
+            Item width:
+            <input
+              type="number"
+              className="input"
+              value={withOfItem}
+              id="itemWidth"
+              onChange={(event) => {
+                this.setState({ withOfItem: +event.target.value });
+              }}
+            />
+          </label>
 
-        <Carousel />
+          <label className="label" htmlFor="frameSize">
+            Frame size:
+            <input
+              type="number"
+              min={0}
+              max={10}
+              className="input"
+              value={frameSize}
+              id="frameSize"
+              onChange={(event) => {
+                this.setState({ frameSize: +event.target.value });
+              }}
+            />
+          </label>
+
+          <label className="label" htmlFor="step">
+            Step:
+            <input
+              type="number"
+              min={0}
+              max={10}
+              className="input"
+              value={rollingStep}
+              id="step"
+              onChange={(event) => {
+                this.setState({ rollingStep: +event.target.value });
+              }}
+            />
+          </label>
+
+          <label className="label" htmlFor="animationDuration">
+            Animation Duration:
+            <input
+              type="number"
+              className="input"
+              value={animation}
+              id="animationDuration"
+              onChange={(event) => {
+                this.setState({ animation: +event.target.value });
+              }}
+            />
+          </label>
+
+          <label className="label" htmlFor="infinity">
+            Infinite:
+            <input
+              type="checkbox"
+              className="input"
+              id="infinite"
+              checked={infiniteRolling}
+              onChange={() => {
+                this.setState({ infiniteRolling: !infiniteRolling });
+              }}
+            />
+          </label>
+        </form>
+        <Carousel
+          images={images}
+          step={rollingStep}
+          frameSize={frameSize}
+          itemWidth={withOfItem}
+          animationDuration={animation}
+          infinite={infiniteRolling}
+        />
       </div>
     );
   }
--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,18 +1,135 @@
+/* eslint-disable prefer-const */
+import classNames from 'classnames';
 import React from 'react';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+type Props = {
+  images: string[]
+  step: number
+  frameSize: number
+  itemWidth: number
+  animationDuration: number
+  infinite: boolean
+};
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+type State = {
+  scroll: number;
+};
+
+class Carousel extends React.Component<Props, State> {
+  state = {
+    scroll: 0,
+  };
+
+  render() {
+    let {
+      images,
+      itemWidth,
+      frameSize,
+      step,
+      animationDuration,
+      infinite,
+    } = this.props;
+
+    let { scroll } = this.state;
+    const scrollLength = (itemWidth / 2) * (images.length - frameSize);
+
+    if (frameSize > images.length) {
+      frameSize = images.length;
+    }
+
+    if (step > images.length) {
+      step = images.length;
+    }
+
+    if (scroll > scrollLength) {
+      scroll = scrollLength;
+    }
+
+    if (scroll < 0) {
+      scroll = 0;
+    }
+
+    const styleForItem = {
+      width: itemWidth,
+      transform: `translateX(-${scroll}px)`,
+      transition: `all ${animationDuration}ms`,
+    };
+    const widthForView = {
+      width: itemWidth * frameSize,
+    };
+
+    return (
+      <div className="Carousel">
+        <ul
+          style={widthForView}
+          className={classNames(
+            'Carousel__list',
+            {
+              'Carousel__list--left': scroll === 0 && !infinite,
+              'Carousel__list--right': scroll === scrollLength && !infinite,
+            },
+          )}
+        >
+          {images.map((image, index) => (
+            <li
+              key={image}
+              style={styleForItem}
+              className="Carousel__item"
+            >
+              <img
+                style={styleForItem}
+                className="Carousel__image"
+                src={image}
+                alt={`smille№${index + 1}`}
+              />
+            </li>
+          ))}
+        </ul>
+
+        <div className="Button__container">
+          <button
+            data-cy="prev"
+            type="button"
+            disabled={scroll === 0 && infinite === false}
+            className="Button"
+            onClick={() => {
+              if (scroll === 0) {
+                this.setState({ scroll: scrollLength });
+
+                return;
+              }
+
+              this.setState((state) => ({
+                scroll: state.scroll - ((itemWidth * step) / 2),
+              }));
+            }}
+          >
+            Prev
+          </button>
+          <button
+            data-cy="next"
+            type="button"
+            disabled={scroll === scrollLength && infinite === false}
+            className="Button"
+            onClick={() => {
+              if (scroll === scrollLength) {
+                this.setState({ scroll: 0 });
+
+                return;
+              }
+
+              this.setState((state) => ({
+                scroll: state.scroll + ((itemWidth * step) / 2),
+              }));
+            }}
+          >
+            Next
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Carousel;
