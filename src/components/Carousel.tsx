e8109203b7b36816f4994d88cf427672d2db79f6
Janos Lukacs <pszichae@gmail.com>
2023-04-01 16:14:24 +0200

    added images and the ability to scroll

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,18 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+type Props = {
+  images: string[],
+};
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+const Carousel: React.FC<Props> = ({ images }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [imageSize] = useState(130);
+  const [visibleImages] = useState(3);
+  const [step] = useState(3);
+
+  const containerWidth = (currentIndex + visibleImages) * imageSize;
+
+  const handleNextClick = () => {
+    const nextIndex = currentIndex + step;
+
+    if (nextIndex < images.length) {
+      setCurrentIndex(nextIndex);
+    }
+  };
+
+  const handlePrevClick = () => {
+    const prevIndex = currentIndex - step;
+
+    if (prevIndex >= 0) {
+      setCurrentIndex(prevIndex);
+    }
+  };
+
+  const translateValue = `translateX(-${currentIndex * imageSize}px)`;
+
+  return (
+    <>
+      <div className="Carousel">
+        <div
+          className="Container"
+          style={{ width: `${containerWidth}px`, transform: translateValue }}
+        >
+          <ul className="Carousel__list">
+            {images.map((img, index) => (
+              <li
+                key={img}
+                className="Carousel__list--item"
+                style={{ width: `${imageSize}` }}
+              >
+                <img src={img} alt={`${index + 1}`} />
+              </li>
+            ))}
+          </ul>
+        </div>
+        <button
+          type="button"
+          onClick={handlePrevClick}
+        >
+          Prev
+        </button>
+        <button
+          type="button"
+          onClick={handleNextClick}
+          data-cy="next"
+        >
+          Next
+        </button>
+      </div>
+    </>
+  );
+};
 
 export default Carousel;
