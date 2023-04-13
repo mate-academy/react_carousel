3036133c85a968c66265e00967da16bcf0c08955
yana-kotsulym <yanak7170@gmail.com>
2023-04-13 21:32:52 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import './App.scss';
-import Carousel from './components/Carousel';
+import { Carousel } from './components/Carousel';
 
 interface State {
-  images: string[];
+  images: string[],
+  step: number,
+  frameSize: number,
+  itemWidth: number,
+  animationDuration: number,
+  infinite: boolean,
 }
 
 class App extends React.Component<{}, State> {
-  state = {
+  state: Readonly<State> = {
     images: [
       './img/1.png',
       './img/2.png',
@@ -20,17 +25,92 @@ class App extends React.Component<{}, State> {
       './img/9.png',
       './img/10.png',
     ],
+    step: 2,
+    frameSize: 3,
+    itemWidth: 130,
+    animationDuration: 1000,
+    infinite: true,
+  };
+
+  setUsersData = (value: number, stateItem: string) => {
+    this.setState({
+      [stateItem]: value,
+    } as unknown as Pick<State, keyof State>);
   };
 
   render() {
-    const { images } = this.state;
+    const {
+      images,
+      step,
+      frameSize,
+      itemWidth,
+      animationDuration,
+      infinite,
+    } = this.state;
 
     return (
       <div className="App">
         {/* eslint-disable-next-line */}
-        <h1>Carousel with {images.length} images</h1>
+        <h1 data-cy="title">
+          {`Carousel with
+          ${images.length}
+          images`}
+        </h1>
+        <form action="#">
+          <input
+            type="number"
+            className="ItemWidth"
+            placeholder="Enter the width of the image"
+            onKeyDown={(event) => {
+              this.setUsersData(
+                +event.currentTarget.value, 'itemWidth',
+              );
+            }}
+          />
 
-        <Carousel />
+          <input
+            type="number"
+            className="FrameSize"
+            placeholder="Enter the number of the visible image"
+            onKeyDown={(event) => {
+              this.setUsersData(
+                +event.currentTarget.value, 'frameSize',
+              );
+            }}
+          />
+
+          <input
+            type="number"
+            className="Step"
+            placeholder="Enter the number of images scrolled per click"
+            onKeyDown={(event) => {
+              this.setUsersData(
+                +event.currentTarget.value, 'step',
+              );
+            }}
+          />
+
+          <input
+            type="number"
+            className="AnimationDuration"
+            placeholder="Enter the time in ms to
+            show the new portion of images"
+            onKeyDown={(event) => {
+              this.setUsersData(
+                +event.currentTarget.value, 'animationDuration',
+              );
+            }}
+          />
+        </form>
+
+        <Carousel
+          images={images}
+          step={step}
+          frameSize={frameSize}
+          itemWidth={itemWidth}
+          animationDuration={animationDuration}
+          infinite={infinite}
+        />
       </div>
     );
   }
--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,18 +1,160 @@
 import React from 'react';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+interface Props {
+  images: string[],
+  step: number,
+  frameSize: number,
+  itemWidth: number,
+  animationDuration: number,
+  infinite: boolean,
+}
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+type State = {
+  transform: number,
+  isMaxLeft: boolean,
+  isMaxRight: boolean,
+};
 
-export default Carousel;
+export class Carousel extends React.Component<Props, State> {
+  state: Readonly<State> = {
+    transform: 0,
+    isMaxRight: false,
+    isMaxLeft: !this.props.infinite,
+  };
+
+  showPrev = () => {
+    const {
+      images,
+      step,
+      itemWidth,
+      frameSize,
+      infinite,
+    } = this.props;
+
+    this.setState(((state: Readonly<State>) => {
+      const offset = state.transform + (itemWidth * step);
+      let maxOffset = 0;
+
+      if (infinite && offset >= maxOffset) {
+        maxOffset = (itemWidth * images.length)
+        - (itemWidth * frameSize);
+
+        return {
+          transform: -maxOffset,
+          isMaxLeft: false,
+        };
+      }
+
+      if (offset >= maxOffset) {
+        return {
+          transform: maxOffset,
+          isMaxLeft: true,
+        };
+      }
+
+      return {
+        transform: offset,
+        isMaxLeft: false,
+        isMaxRight: false,
+      } as Pick<State, keyof State>;
+    }));
+  };
+
+  showNext = () => {
+    const {
+      images,
+      step,
+      frameSize,
+      itemWidth,
+      infinite,
+    } = this.props;
+
+    this.setState(((state: Readonly<State>) => {
+      const offset = state.transform - (itemWidth * step);
+      let maxOffset = (itemWidth * images.length)
+        - (itemWidth * frameSize);
+
+      if (infinite && offset <= (-maxOffset)) {
+        maxOffset = 0;
+
+        return {
+          transform: 0,
+          isMaxRight: false,
+        } as Pick<State, keyof State>;
+      }
+
+      if (offset <= (-maxOffset)) {
+        return {
+          transform: (-maxOffset),
+          isMaxRight: true,
+        } as Pick<State, keyof State>;
+      }
+
+      return {
+        transform: offset,
+        isMaxRight: false,
+        isMaxLeft: false,
+      };
+    }));
+  };
+
+  render() {
+    const {
+      images,
+      frameSize,
+      itemWidth,
+      animationDuration,
+    } = this.props;
+
+    const {
+      transform,
+      isMaxLeft,
+      isMaxRight,
+    } = this.state;
+
+    const carouselContainerStyle = {
+      width: `${frameSize * itemWidth}px`,
+    };
+
+    const carouselListStyle = {
+      width: `${images.length * itemWidth}px`,
+      transform: `translateX(${(transform)}px)`,
+      transition: `all ${animationDuration}ms ease`,
+    };
+
+    const carouselItemStyle = {
+      width: `${itemWidth}px`,
+    };
+
+    return (
+      <div className="Carousel" style={carouselContainerStyle}>
+        <ul className="Carousel__list" style={carouselListStyle}>
+          {images.map((image, index) => (
+            <li key={image} style={carouselItemStyle}>
+              <img src={image} alt={(index + 1).toString()} />
+            </li>
+          ))}
+        </ul>
+
+        <button
+          type="button"
+          onClick={this.showPrev}
+          className="button__prev button"
+          disabled={isMaxLeft}
+        >
+          &#8249;
+        </button>
+        <button
+          type="button"
+          onClick={this.showNext}
+          data-cy="next"
+          disabled={isMaxRight}
+          className="button__next button"
+        >
+          &#8250;
+        </button>
+      </div>
+    );
+  }
+}
