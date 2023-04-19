55a91e3a9d3315afd9189dee1e8fc885119c8034
Oleksandr <98667890az@gmail.com>
2023-04-19 16:11:51 +0300

    react_carousel

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,18 +1,116 @@
-import React from 'react';
+import { Component } from 'react';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+type Props = {
+  images: string[],
+  step: number,
+  frameSize: number,
+  itemWidth: number,
+  animationDuration: number
+  infinite: boolean,
+};
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+type State = {
+  position: number;
+};
+
+class Carousel extends Component<Props, State> {
+  state = {
+    position: 0,
+  };
+
+  render() {
+    const {
+      images,
+      step,
+      frameSize,
+      itemWidth,
+      animationDuration = 1000,
+      infinite = false,
+    } = this.props;
+
+    const { position } = this.state;
+    const maxPosition = -itemWidth * (images.length - frameSize);
+    const widthStep = step * itemWidth;
+
+    return (
+      <div className="Carousel">
+        <div
+          className="container"
+          style={{
+            width: frameSize * itemWidth,
+          }}
+        >
+          <ul
+            className="Carousel__list"
+            style={{
+              transform: `translateX(${position}px)`,
+              transition: `transform ${animationDuration}ms ease`,
+            }}
+          >
+            {images.map((img) => (
+              <li
+                className="item"
+                key={img}
+                style={{
+                  width: itemWidth,
+                }}
+              >
+                <img src={img} alt="" />
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <button
+          type="button"
+          onClick={() => {
+            if (position + widthStep > 0) {
+              this.setState(
+                { position: 0 },
+              );
+            } else {
+              this.setState(
+                { position: position + widthStep },
+              );
+            }
+
+            if (position === 0 && infinite) {
+              this.setState(
+                { position: maxPosition },
+              );
+            }
+          }}
+        >
+          Prev
+        </button>
+
+        <button
+          type="button"
+          data-cy="next"
+          onClick={() => {
+            if (position - widthStep <= maxPosition) {
+              this.setState(
+                { position: maxPosition },
+              );
+            } else {
+              this.setState(
+                { position: position - widthStep },
+              );
+            }
+
+            if (position === maxPosition && infinite) {
+              this.setState(
+                { position: 0 },
+              );
+            }
+          }}
+        >
+          Next
+        </button>
+      </div>
+    );
+  }
+}
 
 export default Carousel;
