c84ba4ad2e9fa99baf53b2eeb4396f5a499b01cc
Olha Kissa <olhakissa121r@gmail.com>
2023-04-26 18:10:31 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.scss';
-import Carousel from './components/Carousel';
+import { Carousel } from './components/Carousel';
 
 interface State {
   images: string[];
+  step: number;
+  frameSize: number;
+  itemWidth: number;
+  animationDuration: number;
 }
 
 class App extends React.Component<{}, State> {
@@ -20,17 +24,88 @@ class App extends React.Component<{}, State> {
       './img/9.png',
       './img/10.png',
     ],
+    step: 3,
+    frameSize: 3,
+    itemWidth: 130,
+    animationDuration: 1000,
+  };
+
+  handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+
+    this.setState(current => ({
+      ...current,
+      [name]: +value,
+    }));
   };
 
   render() {
-    const { images } = this.state;
+    const {
+      images,
+      step,
+      frameSize,
+      itemWidth,
+      animationDuration,
+    } = this.state;
 
     return (
       <div className="App">
         {/* eslint-disable-next-line */}
-        <h1>Carousel with {images.length} images</h1>
+        <h1 data-cy="title">Carousel with {images.length} images</h1>
 
-        <Carousel />
+        <form>
+          <label htmlFor="stepInputId">
+            Step:
+          </label>
+          <input
+            id="stepInputId"
+            type="text"
+            name="step"
+            value={this.state.step}
+            onChange={this.handleChange}
+          />
+
+          <label htmlFor="frameSizeInputId">
+            Frame size:
+          </label>
+          <input
+            id="frameSizeInputId"
+            type="text"
+            name="frameSize"
+            value={this.state.frameSize}
+            onChange={this.handleChange}
+          />
+
+          <label htmlFor="itemWidthInputId">
+            Input width:
+          </label>
+          <input
+            id="itemWidthInputId"
+            type="text"
+            name="itemWidth"
+            value={this.state.itemWidth}
+            onChange={this.handleChange}
+          />
+
+          <label htmlFor="animationDurationInputId">
+            Animation duration:
+          </label>
+          <input
+            id="animationDurationInputId"
+            type="text"
+            name="animationDuration"
+            value={this.state.animationDuration}
+            onChange={this.handleChange}
+          />
+        </form>
+
+        <Carousel
+          images={images}
+          step={step}
+          frameSize={frameSize}
+          itemWidth={itemWidth}
+          animationDuration={animationDuration}
+        />
       </div>
     );
   }
