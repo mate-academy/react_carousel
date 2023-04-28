7fd8949878db558c963f702413d07cef670f5eab
DanielSorokowski <sorokowskidaniel0@gmail.com>
2023-04-28 12:01:28 +0200

    2 failed tests

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,11 @@ import Carousel from './components/Carousel';
 
 interface State {
   images: string[];
+  step: number,
+  frameSize: number,
+  itemWidth: number,
+  animationDuration: number,
+  infinite: boolean,
 }
 
 class App extends React.Component<{}, State> {
@@ -20,17 +25,118 @@ class App extends React.Component<{}, State> {
       './img/9.png',
       './img/10.png',
     ],
+    step: 3,
+    frameSize: 3,
+    itemWidth: 130,
+    animationDuration: 1000,
+    infinite: false,
+  };
+
+  handleStepChange = (value: number) => {
+    this.setState({ step: value });
+  };
+
+  handleFrameSizeChange = (value: number) => {
+    this.setState({ frameSize: value });
+  };
+
+  handleItemWidthChange = (value: number) => {
+    this.setState({ itemWidth: value });
+  };
+
+  handleAnimationDurationChange = (value: number) => {
+    this.setState({ animationDuration: value });
+  };
+
+  handleInfiniteChange = (value: boolean) => {
+    this.setState({ infinite: value });
   };
 
   render() {
-    const { images } = this.state;
+    const {
+      images,
+      step,
+      frameSize,
+      itemWidth,
+      animationDuration,
+      infinite,
+    } = this.state;
 
     return (
       <div className="App">
         {/* eslint-disable-next-line */}
-        <h1>Carousel with {images.length} images</h1>
+        <h1 data-cy="title">Carousel with {images.length} images</h1>
 
-        <Carousel />
+        <Carousel
+          images={images}
+          step={step}
+          frameSize={frameSize}
+          itemWidth={itemWidth}
+          animationDuration={animationDuration}
+          infinite={infinite}
+        />
+
+        <form className="form" action="">
+          <label htmlFor="stepId">
+            Step:
+          </label>
+          <input
+            type="number"
+            name="stepId"
+            id="stepId"
+            value={step}
+            onChange={(event) => (
+              this.handleStepChange(Number(event.target.value))
+            )}
+          />
+          <label htmlFor="frameId">
+            Frame Size:
+          </label>
+          <input
+            type="number"
+            name="frameId"
+            id="frameId"
+            value={frameSize}
+            onChange={(event) => (
+              this.handleFrameSizeChange(Number(event.target.value))
+            )}
+          />
+          <label htmlFor="itemId">
+            Item Width:
+          </label>
+          <input
+            type="number"
+            name="itemId"
+            id="itemId"
+            value={itemWidth}
+            onChange={(event) => (
+              this.handleItemWidthChange(Number(event.target.value))
+            )}
+          />
+          <label htmlFor="animationDuration">
+            Animation Duration:
+          </label>
+          <input
+            type="number"
+            name="animationDuration"
+            id="animationDuration"
+            value={animationDuration}
+            onChange={(event) => (
+              this.handleAnimationDurationChange(Number(event.target.value))
+            )}
+          />
+          <label htmlFor="infinite">
+            Infinite:
+          </label>
+          <input
+            type="checkbox"
+            name="infinite"
+            id="infinite"
+            onChange={() => (
+              this.handleInfiniteChange(!infinite)
+            )}
+          />
+        </form>
       </div>
     );
   }
