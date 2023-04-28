24c1b6fb178daad04be8d61026750027ea65f602
Peti51 <galpeter31@gmail.com>
2023-04-28 17:46:36 +0200

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,10 @@ type Props = {};
 interface State {
   images: string[];
   currentIndex: number;
-  containerWidth: number;
+  frameSize: number;
+  itemWidth: number,
+  title: number,
+  infinite: boolean;
 }
 
 class App extends React.Component<Props, State> {
@@ -27,19 +30,60 @@ class App extends React.Component<Props, State> {
 
     currentIndex: 0,
 
-    containerWidth: 390,
+    title: 0,
+
+    frameSize: 390,
+
+    itemWidth: 130,
+
+    infinite: false,
   };
+
+  timerID = 0;
 
   componentDidMount(): void {
     this.setState((state) => ({
-      currentIndex: state.containerWidth / 130,
+      currentIndex: state.frameSize / state.itemWidth,
     }));
+
+    this.setState((state) => ({
+      title: state.frameSize / state.itemWidth,
+    }));
+
+    this.timerID = window.setInterval(() => {
+      this.setState((state) => ({
+        infinite: !state.infinite,
+      }));
+    }, 10000);
+  }
+
+  componentDidUpdate(prevProps: Props, prevState: State) {
+    if (this.state.infinite === true
+      && this.state.infinite !== prevState.infinite
+      && this.state.currentIndex < this.state.images.length) {
+      this.buttonNext();
+    }
+
+    if (this.state.infinite === true
+      && this.state.infinite !== prevState.infinite
+      && this.state.currentIndex >= this.state.images.length) {
+      this.setState((state) => ({
+        currentIndex: state.frameSize / state.itemWidth,
+      }));
+    }
+
+    return prevProps;
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
   }
 
   buttonPrev = () => {
-    if (this.state.currentIndex > this.state.containerWidth / 130) {
+    if (this.state.currentIndex > this.state.frameSize / this.state.itemWidth) {
       this.setState((state) => ({
-        currentIndex: state.currentIndex - (state.containerWidth / 130),
+        currentIndex: state.currentIndex
+        - (state.frameSize / state.itemWidth),
       }));
     }
   };
@@ -47,47 +91,55 @@ class App extends React.Component<Props, State> {
   buttonNext = () => {
     if (this.state.currentIndex < 10) {
       this.setState((state) => ({
-        currentIndex: state.currentIndex + (state.containerWidth / 130),
+        currentIndex: state.currentIndex + (state.frameSize / state.itemWidth),
       }));
     }
   };
 
   render() {
     let modifiedArr = this.state.images.slice(
-      this.state.currentIndex - (this.state.containerWidth / 130),
+      this.state.currentIndex - (this.state.frameSize / this.state.itemWidth),
       this.state.currentIndex,
     );
 
-    if (modifiedArr.length !== this.state.containerWidth / 130) {
+    if (modifiedArr.length !== this.state.frameSize / this.state.itemWidth) {
       modifiedArr = this.state.images.slice(7, 10);
     }
 
     return (
       <div className="App">
         {/* eslint-disable-next-line */}
-        <h1>Carousel with {this.state.images.length} images</h1>
-        <div style={{ width: `${this.state.containerWidth}px` }}>
-          <Carousel arrOfCarousel={modifiedArr} />
-        </div>
+        <h1>Carousel with {this.state.title} images</h1>
+        <Carousel
+          arrOfCarousel={modifiedArr}
+          frameSize={this.state.frameSize}
+          itemWidth={this.state.itemWidth}
+        />
 
         <button
           type="button"
+          className="Carousel__button Carousel__button1"
           onClick={() => {
             this.buttonPrev();
           }}
+          disabled={this.state.currentIndex
+            <= this.state.frameSize / this.state.itemWidth}
         >
           Prev
 
         </button>
         <button
           type="button"
+          className="Carousel__button"
           onClick={() => {
             this.buttonNext();
           }}
+          disabled={this.state.currentIndex >= 10}
         >
           Next
 
         </button>
+        {this.state.currentIndex}
       </div>
     );
   }
--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -3,24 +3,25 @@ import './Carousel.scss';
 
 type Props = {
   arrOfCarousel: string[],
+  frameSize: number,
+  itemWidth: number,
 };
 
-const Carousel: React.FC<Props> = ({ arrOfCarousel = [] }) => (
-  <ul className="Carousel__list">
-    {arrOfCarousel.map((image) => {
-      return (
-        <li>
+const Carousel: React.FC<Props>
+  = ({ arrOfCarousel = [], frameSize = 390, itemWidth = 130 }) => (
+    <ul className="Carousel__list" style={{ width: `${frameSize}px` }}>
+      {arrOfCarousel.map((image) => (
+        <li key={image}>
           <img
             src={image}
             alt="1"
-            className="Carousel__ image"
+            className="Carousel__image"
+            style={{ width: `${itemWidth}px`, height: `${itemWidth}px` }}
+            key={image}
           />
-
         </li>
-      );
-    })}
-  </ul>
-
-);
+      ))}
+    </ul>
+  );
 
 export default Carousel;
