9b7960f54a2e4169b92fa4cb9466efc3268b4171
Alex Nastychenko <nastychenko1993@gmail.com>
2023-05-04 06:07:31 +0300

    solution

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,18 +1,152 @@
-import React from 'react';
+/* eslint-disable react/no-unused-prop-types */
+import React, { createRef } from 'react';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+interface State {
+  currentSlide: number;
+}
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+interface Props {
+  images: string[];
+  step: number,
+  frameSize: number,
+  itemWidth: number,
+  animationDuration: number,
+  infinite: boolean,
+}
 
-export default Carousel;
+export class Carousel extends React.Component<Props, State> {
+  private listRef = createRef<HTMLUListElement>(); // create ref for carousel__list
+
+  state = {
+    currentSlide: 0,
+  };
+
+  handlePrev = () => {
+    const { currentSlide } = this.state;
+
+    const {
+      images,
+      step,
+      frameSize,
+      itemWidth,
+      infinite,
+      animationDuration,
+    } = this.props;
+
+    const maxSlide = images.length * itemWidth - frameSize * itemWidth;
+
+    if (currentSlide < 0) {
+      if (currentSlide + itemWidth * step > 0) {
+        this.setState({ currentSlide: 0 });
+      } else {
+        this.setState({ currentSlide: currentSlide + itemWidth * step });
+      }
+    } else if (infinite && !currentSlide) {
+      this.setState({ currentSlide: -maxSlide });
+    } else {
+      this.setState({ currentSlide: currentSlide - itemWidth * step });
+    }
+
+    const list = this.listRef.current;
+
+    if (list) {
+      list.style.transition = `transform ${animationDuration}ms ease-in-out`;
+      list.style.transform = `translateX(${this.state.currentSlide}px)`;
+    }
+  };
+
+  handleNext = () => {
+    const { currentSlide } = this.state;
+    const {
+      images,
+      step,
+      frameSize,
+      itemWidth,
+      animationDuration,
+      infinite,
+    } = this.props;
+
+    const maxSlide = images.length * itemWidth - frameSize * itemWidth;
+
+    if (currentSlide > -maxSlide) {
+      if (currentSlide - itemWidth * step < -maxSlide) {
+        this.setState({ currentSlide: -maxSlide });
+      } else {
+        this.setState({ currentSlide: currentSlide - itemWidth * step });
+      }
+    } else if (infinite && currentSlide === -maxSlide) {
+      this.setState({ currentSlide: 0 });
+    }
+
+    // Add sliding animation to the carousel list
+    const list = this.listRef.current;
+
+    if (list) {
+      list.style.transition = `transform ${animationDuration}ms ease-in-out`;
+      list.style.transform = `translateX(${this.state.currentSlide}px)`;
+    }
+  };
+
+  render() {
+    const {
+      images,
+      frameSize,
+      itemWidth,
+      animationDuration,
+      infinite,
+    } = this.props;
+
+    const { currentSlide } = this.state;
+
+    const isAtBoundaryNext = infinite
+      ? false
+      : currentSlide === (
+        images.length * itemWidth - frameSize * itemWidth
+      ) * -1;
+
+    const isAtBoundaryLast = infinite ? false : !currentSlide;
+
+    return (
+      <div className="carousel" style={{ width: frameSize * itemWidth }}>
+        <ul
+          ref={this.listRef}
+          className="carousel__list"
+          style={{
+            transform: `translateX(${currentSlide}px)`,
+            transition: `all ${animationDuration}ms ease-in-out`,
+            width: `${itemWidth * images.length}px`,
+          }}
+        >
+          {images.map(img => (
+            <li key={images.indexOf(img)}>
+              <img
+                src={`${img}`}
+                alt="Emojy"
+                style={{ width: itemWidth }}
+              />
+            </li>
+          ))}
+        </ul>
+
+        <div className="buttons">
+          <button
+            type="button"
+            onClick={this.handlePrev}
+            disabled={isAtBoundaryLast}
+          >
+            &lt;
+          </button>
+          <button
+            data-cy="next"
+            onClick={this.handleNext}
+            type="button"
+            disabled={isAtBoundaryNext}
+          >
+            &gt;
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
