6b7969567f38a42dc507615c4936efdf85373adf
Natali <swetswet1234@gmail.com>
2023-05-16 17:26:48 +0300

    develop

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.scss';
-import Carousel from './components/Carousel';
+import { Carousel } from './components/Carousel';
 
 interface State {
   images: string[];
+  step: number;
+  frameSize: number;
+  itemWidth: number;
+  animationDuration: number;
+  infinite: boolean,
 }
 
 class App extends React.Component<{}, State> {
@@ -20,17 +25,110 @@ class App extends React.Component<{}, State> {
       './img/9.png',
       './img/10.png',
     ],
+    step: 3,
+    frameSize: 3,
+    itemWidth: 130,
+    animationDuration: 1000,
+    infinite: false,
+  };
+
+  stepSet = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ step: +event.currentTarget.value });
+  };
+
+  frameSizeSet = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ frameSize: +event.currentTarget.value });
+  };
+
+  itemWidthSet = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ itemWidth: +event.currentTarget.value });
+  };
+
+  animationDurationSet = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ animationDuration: +event.currentTarget.value });
+  };
+
+  infiniteSet = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ infinite: event.currentTarget.checked });
   };
 
   render() {
-    const { images } = this.state;
+    const {
+      images,
+      step,
+      frameSize,
+      itemWidth,
+      animationDuration,
+      infinite,
+    } = this.state;
 
     return (
       <div className="App">
-        {/* eslint-disable-next-line */}
-        <h1>Carousel with {images.length} images</h1>
+        <h1 data-cy="title">
+          {'Carousel with '}
+          {images.length}
+          {' images'}
+        </h1>
 
-        <Carousel />
+        <Carousel
+          images={images}
+          step={step}
+          frameSize={frameSize}
+          itemWidth={itemWidth}
+          animationDuration={animationDuration}
+          infinite={infinite}
+        />
+
+        <form className="form" action="#">
+          <label className="label" htmlFor="stepName">Step:</label>
+          <input
+            type="number"
+            id="step_id"
+            className="step__input"
+            defaultValue={step}
+            onChange={this.stepSet}
+            min="1"
+            max={images.length}
+          />
+
+          <label className="label" htmlFor="frameSizeName">FrameSize:</label>
+          <input
+            type="number"
+            id="size_id"
+            className="size__input"
+            defaultValue={frameSize}
+            onChange={this.frameSizeSet}
+            min="1"
+            max={images.length}
+          />
+
+          <label className="label" htmlFor="itemWidthName">Width:</label>
+          <input
+            type="number"
+            id="width_id"
+            className="width__input"
+            defaultValue={itemWidth}
+            onChange={this.itemWidthSet}
+            min="130"
+          />
+
+          <label className="label" htmlFor="animationName">Animation:</label>
+          <input
+            type="number"
+            id="animation_id"
+            className="animation__input"
+            defaultValue={animationDuration}
+            onChange={this.animationDurationSet}
+            min="1000"
+          />
+
+          <input
+            type="checkbox"
+            id="infinite_id"
+            className="infinite__input"
+            onChange={this.infiniteSet}
+          />
+        </form>
       </div>
     );
   }
