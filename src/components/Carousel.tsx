9f1c955af00f2d5d692970c37a44d98ea084461a
Oleksandr Plias <pls777@ukr.net>
2023-05-19 15:30:28 +0300

    Solution

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,18 +1,131 @@
-import React from 'react';
+import { Component } from 'react';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+interface Props {
+  images: string[];
+  step: number,
+  frameSize: number,
+  itemWidth: number,
+  animationDuration: number,
+  infinite: boolean,
+}
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+type State = {
+  position: number,
+};
 
-export default Carousel;
+export class Carousel extends Component<Props, State> {
+  state = {
+    position: 0,
+  };
+
+  slideRight = () => {
+    const { position } = this.state;
+
+    const {
+      step,
+      infinite,
+      frameSize,
+      images,
+    } = this.props;
+
+  const maxPosition = -(images.length - frameSize);
+
+  this.setState({
+    position: position - step > maxPosition ? position - step : maxPosition,
+  });
+  
+  if (position === maxPosition && infinite) {
+    this.setState({
+      position: 0,
+    });
+  };
+}
+  slideLeft = () => {
+    const { position } = this.state;
+  
+    const {
+      step, 
+      frameSize, 
+      images, 
+      infinite,
+    } = this.props;
+  
+    const minPosition = (images.length - frameSize);
+  
+    this.setState({
+      position: position + step < 0 ? position + step : 0,
+    });
+  
+    if (position === 0 && infinite) {
+      this.setState({
+        position: -minPosition,
+      });
+    }
+  };
+
+  render() {
+    const {
+      images,
+      frameSize,
+      itemWidth,
+      animationDuration,
+      infinite,
+    } = this.props;
+
+    const { position } = this.state;
+
+    const maxPosition = -(images.length - frameSize);
+    const leftDisabled = position === 0 && !infinite;
+    const righttDisabled = position === maxPosition && !infinite;
+
+    return (
+      <div
+        className="Carousel"
+        style={{
+          width: `${itemWidth * frameSize}px`,
+          transition: `${animationDuration}ms`,
+        }}
+      >
+        <ul className="Carousel__list">
+          {images.map((image) => (
+            <li
+              key={image}
+              style={{
+                transform: `translateX(${position * itemWidth}px)`,
+                transition: `${animationDuration}ms`,
+              }}
+            >
+              <img
+                src={image}
+                alt={`${images.indexOf(image) + 1}`}
+                width={itemWidth}
+              />
+            </li>
+          ))}
+        </ul>
+
+        <div className="Carousel__buttons">
+          <button
+            data-cy="prev"
+            type="button"
+            className="Carousel__button"
+            disabled={leftDisabled}
+            onClick={this.slideLeft}
+          >
+            &#8678;
+          </button>
+          <button
+            data-cy="next"
+            type="button"
+            className="Carousel__button"
+            disabled={righttDisabled}
+            onClick={this.slideRight}
+          >
+            &#8680;
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
