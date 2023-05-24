14331ef9be9d9bb3393632ac877292c61f7ed854
Ostap Sokolov <thefallcon2001@gmail.com>
2023-05-23 13:05:03 -1200

    carousel

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,14 @@ import React from 'react';
 import './App.scss';
 import Carousel from './components/Carousel';
 
-interface State {
-  images: string[];
-}
+type State = {
+  images: string[],
+  step: number,
+  frameSize: number,
+  itemWidth: number,
+  animationDuration: number,
+  infinite: boolean,
+};
 
 class App extends React.Component<{}, State> {
   state = {
@@ -20,17 +25,155 @@ class App extends React.Component<{}, State> {
       './img/9.png',
       './img/10.png',
     ],
+    step: 3,
+    frameSize: 3,
+    itemWidth: 130,
+    animationDuration: 1000,
+    infinite: false,
+  };
+
+  changeStep = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ step: Number(event.currentTarget.value) });
+  };
+
+  changeFrameSize = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ frameSize: Number(event.currentTarget.value) });
+  };
+
+  changeItemWidth = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ itemWidth: Number(event.currentTarget.value) });
+  };
+
+  changeAnimationDuration = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ animationDuration: Number(event.currentTarget.value) });
+  };
+
+  changeInfinite = () => {
+    this.setState(prevState => (
+      { infinite: !prevState.infinite }
+    ));
   };
 
   render() {
-    const { images } = this.state;
+    const {
+      images,
+      step,
+      frameSize,
+      itemWidth,
+      animationDuration,
+      infinite,
+    } = this.state;
 
     return (
       <div className="App">
-        {/* eslint-disable-next-line */}
-        <h1>Carousel with {images.length} images</h1>
+        <h1 className="title" data-cy="title">
+          Carousel with
+          {' '}
+          <span className="title__num">{images.length}</span>
+          {' '}
+          images
+        </h1>
+        <div className="form">
+          <Carousel
+            images={images}
+            step={step}
+            frameSize={frameSize}
+            itemWidth={itemWidth}
+            animationDuration={animationDuration}
+            infinite={infinite}
+          />
+          <div className="form__field">
+            <div className="form__content">
+              <label className="form__content--name" htmlFor="step">
+                Step :
+                {' '}
+              </label>
+              <input
+                className="form__content--input"
+                type="number"
+                id="step"
+                name="step"
+                defaultValue={step}
+                min={1}
+                max={5}
+                onChange={this.changeStep}
+              />
 
-        <Carousel />
+            </div>
+
+            <div className="form__content">
+              <label className="form__content--name" htmlFor="frameSize">
+                Frame size :
+                {' '}
+              </label>
+              <input
+                className="form__content--input"
+                type="number"
+                name="frameSize"
+                id="frameSize"
+                defaultValue={frameSize}
+                min={1}
+                max={10}
+                onChange={this.changeFrameSize}
+              />
+            </div>
+
+            <div className="form__content">
+              <label className="form__content--name" htmlFor="itemWidth">
+                Item width :
+                {' '}
+              </label>
+              <input
+                className="form__content--input"
+                type="number"
+                name="itemWidth"
+                id="itemWidth"
+                defaultValue={itemWidth}
+                min={50}
+                max={300}
+                onChange={this.changeItemWidth}
+              />
+            </div>
+
+            <div className="form__content">
+              <label
+                className="form__content--name"
+                htmlFor="animationDuration"
+              >
+                Animtion duration :
+                {' '}
+              </label>
+              <input
+                className="form__content--input"
+                type="number"
+                name="animationDuration"
+                id="animationDuration"
+                defaultValue={animationDuration}
+                min={0}
+                max={5000}
+                step={100}
+                onChange={this.changeAnimationDuration}
+              />
+            </div>
+
+            <div className="form__content">
+              <label
+                className="form__content--name"
+                htmlFor="infinite"
+              >
+                Infinite :
+                {' '}
+              </label>
+              <input
+                className="form__content--checkbox"
+                type="checkbox"
+                name="infinite"
+                id="infinite"
+                onChange={this.changeInfinite}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
