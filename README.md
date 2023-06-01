1e6b97c03f01b4a23a67f259997cbfb7831a98d6
Pavlo Petrashevskyi <dragonthunderaddsdp@gmail.com>
2023-06-01 19:03:19 +0300

    Carousel was created

--- README.md
+++ README.md
@@ -4,12 +4,12 @@
 
 1. implement a carousel described [here](https://github.com/mate-academy/dom_carousel) as a React component
     ```jsx harmony
-    <Carousel images={['url1', 'url2']} />
+    <Carousel images={['url1', 'url2']} /> +
     ```
-2. add an ability to customize `itemWidth` with default value of `130px`
-3. add `frameSize` - number of images displayed at the same time with the default of `3`
-4. add `step` (default 3) - number of images scrolled per click
-5. add `animationDuration` (default `1000`) - time in ms to show the new portion of images
+2. add an ability to customize `itemWidth` with default value of `130px` +
+3. add `frameSize` - number of images displayed at the same time with the default of `3` 4
+4. add `step` (default 3) - number of images scrolled per click +
+5. add `animationDuration` (default `1000`) - time in ms to show the new portion of images +
 6. (*) add an `infinite` prop (`false` by default) - to do the carousel cyclic
     ```jsx harmony
     <Carousel
@@ -21,10 +21,10 @@
       infinite={false}
     />
     ```
-    
+
 ## REQUIREMENTS:
 
-1. The title of the page should contain "Carousel"
+1. The title of the page should contain "Carousel" +
 2. The page should continue the input fieds for:
    - `ItemWidth`
    - `FrameSize`
@@ -32,7 +32,7 @@
    - `AnimationDuration`
 3. Add data-cy attributes:
    - `title` inside h1 tag
-   - `next` to the `"Next" button`
+   - `next` to the `"Next" button` +
 
 
 ## Instructions
@@ -40,4 +40,4 @@
 - Implement a solution following the [React task guideline](https://github.com/mate-academy/react_task-guideline#react-tasks-guideline).
 - Use the [React TypeScript cheat sheet](https://mate-academy.github.io/fe-program/js/extra/react-typescript).
 - Open one more terminal and run tests with `npm test` to ensure your solution is correct.
-- Replace `<your_account>` with your Github username in the [DEMO LINK](https://<your_account>.github.io/react_carousel/) and add it to the PR description.
+- Replace `<your_account>` with your Github username in the [DEMO LINK](https://Pavlo-Petrashevskyi.github.io/react_carousel/) and add it to the PR description.
--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,11 @@ import Carousel from './components/Carousel';
 
 interface State {
   images: string[];
+  step: number,
+  frameSize: number,
+  itemWidth: number,
+  animationDuration: number,
+  infinite: boolean;
 }
 
 class App extends React.Component<{}, State> {
@@ -20,17 +25,143 @@ class App extends React.Component<{}, State> {
       './img/9.png',
       './img/10.png',
     ],
+    step: 3,
+    frameSize: 3,
+    itemWidth: 130,
+    animationDuration: 1000,
+    infinite: false,
   };
 
   render() {
-    const { images } = this.state;
+    const {
+      images,
+      step,
+      frameSize,
+      itemWidth,
+      animationDuration,
+      infinite,
+    } = this.state;
 
     return (
       <div className="App">
-        {/* eslint-disable-next-line */}
-        <h1>Carousel with {images.length} images</h1>
+        <h1
+          data-cy="title"
+          className="App__title"
+        >
+          {`Carousel with ${images.length} images`}
+        </h1>
 
-        <Carousel />
+        <Carousel
+          images={images}
+          step={step}
+          frameSize={frameSize}
+          itemWidth={itemWidth}
+          animationDuration={animationDuration}
+          infinite={infinite}
+        />
+
+        <form
+          className="App__form"
+          method="get"
+        >
+          <label
+            htmlFor="itemWidth"
+            className="App__form-label"
+          >
+            Item width:
+
+            <input
+              type="number"
+              id="itemWidth"
+              name="itemWidth"
+              className="App__form-item-input input"
+              defaultValue="130"
+              min="0"
+              onChange={(e) => {
+                this.setState({ itemWidth: +e.target.value });
+              }}
+            />
+          </label>
+
+          <label
+            htmlFor="frameSize"
+            className="App__form-label"
+          >
+            Frame Size:
+
+            <input
+              type="number"
+              id="frameSize"
+              name="frameSize"
+              className="App__form-frame-input input"
+              defaultValue="3"
+              min="1"
+              max="10"
+              onChange={(e) => {
+                this.setState({ frameSize: +e.target.value });
+              }}
+            />
+          </label>
+
+          <label
+            htmlFor="step"
+            className="App__form-label"
+          >
+            Step:
+
+            <input
+              type="number"
+              id="step"
+              name="step"
+              className="App__form-step-input input"
+              defaultValue="3"
+              min="1"
+              max="7"
+              onChange={(e) => {
+                this.setState({ step: +e.target.value });
+              }}
+            />
+          </label>
+
+          <label
+            htmlFor="animationDuration"
+            className="App__form-label"
+          >
+            Animation duration:
+
+            <input
+              type="number"
+              id="animationDuration"
+              name="animationDuration"
+              className="App__form-animation-input input"
+              defaultValue="1000"
+              onChange={(e) => {
+                this.setState({ animationDuration: +e.target.value });
+              }}
+            />
+          </label>
+
+          <label
+            htmlFor="infinite"
+            className="App__form-label"
+          >
+            infinite:
+
+            <input
+              type="checkbox"
+              id="infinite"
+              name="infinite"
+              className="App__form-infinite-input input"
+              defaultValue="false"
+              onChange={() => {
+                return infinite
+                  ? this.setState({ infinite: false })
+                  : this.setState({ infinite: true });
+              }}
+            />
+          </label>
+        </form>
+
       </div>
     );
   }
