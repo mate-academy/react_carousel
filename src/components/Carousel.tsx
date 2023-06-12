fcef4ca688ecb22aee8401c2502ecc9679bd57c4
Mykhailo Orlov <mihalikmihalik85@gmail.com>
2023-06-12 18:24:31 +0300

    finish

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,18 +1,167 @@
 import React from 'react';
 import './Carousel.scss';
+import { Input } from './FormFields';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+type Props = {
+  images: string[],
+};
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+type State = {
+  itemWidth: number,
+  frameSize: number,
+  step: number,
+  animationDuration: number,
+  translate: number,
+};
+
+class Carousel extends React.Component<Props, State> {
+  state = {
+    itemWidth: 130,
+    frameSize: 3,
+    step: 3,
+    animationDuration: 1000,
+    translate: 0,
+  };
+
+  stepPrev = () => {
+    const { step, itemWidth, translate } = this.state;
+
+    if (translate + step * itemWidth < 0) {
+      this.setState(prevState => ({
+        translate: prevState.translate + step * itemWidth,
+      }));
+    } else {
+      this.setState({ translate: 0 });
+    }
+  };
+
+  stepNext = () => {
+    const {
+      step, itemWidth, translate, frameSize,
+    } = this.state;
+
+    const hiddenScroll = -(
+      itemWidth * this.props.images.length - frameSize * itemWidth);
+
+    if (translate - step * itemWidth < hiddenScroll) {
+      this.setState({ translate: hiddenScroll });
+    } else {
+      this.setState((prevState) => ({
+        translate: prevState.translate - step * itemWidth,
+      }));
+    }
+  };
+
+  widthChanger = (value: number) => {
+    this.setState({ itemWidth: value });
+  };
+
+  stepChanger = (value: number) => {
+    this.setState({ step: value });
+  };
+
+  AnimationDurationChanger = (value: number) => {
+    this.setState({ animationDuration: value });
+  };
+
+  FrameSizeChanger = (value: number) => {
+    this.setState({ frameSize: value });
+  };
+
+  render() {
+    const { images } = this.props;
+    const {
+      itemWidth,
+      frameSize,
+      translate,
+      animationDuration,
+    } = this.state;
+
+    return (
+      <div className="Carousel">
+        <div
+          className="Carousel__container"
+          style={{ width: `${itemWidth * frameSize}px` }}
+        >
+
+          <ul
+            className="Carousel__list"
+            style={{
+              transform: `translateX(${translate}px)`,
+              transition: `transform ${animationDuration}ms`,
+            }}
+          >
+            {images.map((image, i) => (
+              <li
+                key={image}
+              >
+                <img
+                  src={image}
+                  alt={`${i}`}
+                  width={itemWidth}
+                />
+              </li>
+            ))}
+          </ul>
+
+          <div
+            className="Carousel__buttons"
+          >
+            <button
+              type="button"
+              onClick={this.stepPrev}
+            >
+              &larr;
+            </button>
+            <button
+              data-cy="next"
+              type="button"
+              onClick={this.stepNext}
+            >
+              &rarr;
+            </button>
+          </div>
+        </div>
+        <div className="Inputs">
+          <Input
+            name="ItemWidth"
+            defaultValue={130}
+            min={50}
+            max={140}
+            step={10}
+            onChange={this.widthChanger}
+          />
+
+          <Input
+            name="Step"
+            defaultValue={3}
+            min={1}
+            max={10}
+            step={1}
+            onChange={this.stepChanger}
+          />
+
+          <Input
+            name="AnimationDuration"
+            defaultValue={1000}
+            min={100}
+            max={3000}
+            step={100}
+            onChange={this.AnimationDurationChanger}
+          />
+
+          <Input
+            name="Frame-size"
+            defaultValue={3}
+            min={1}
+            max={10}
+            step={1}
+            onChange={this.FrameSizeChanger}
+          />
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Carousel;
---
+++ src/components/FormFields.tsx
@@ -0,0 +1,34 @@
+type Props = {
+  name: string,
+  min: number,
+  max: number,
+  step: number,
+  defaultValue: number,
+  onChange: (value: number) => void,
+};
+
+export const Input: React.FC<Props> = ({
+  name,
+  min,
+  max,
+  step,
+  defaultValue,
+  onChange,
+}) => {
+  return (
+    <label htmlFor={name}>
+      {`${name}`}
+      <input
+        id={name}
+        type="range"
+        defaultValue={defaultValue}
+        step={step}
+        min={min}
+        max={max}
+        onChange={(event) => {
+          onChange(+event.target.value);
+        }}
+      />
+    </label>
+  );
+};
