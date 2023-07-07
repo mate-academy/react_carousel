2dba45a6d31619cb084e201e341e7fa9a8da6aee
alex <cebanoleksandr@gmail.com>
2023-07-07 22:41:15 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,106 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.scss';
-import Carousel from './components/Carousel';
+import { Carousel } from './components/Carousel';
 
-interface State {
-  images: string[];
-}
+const images = [
+  './img/1.png',
+  './img/2.png',
+  './img/3.png',
+  './img/4.png',
+  './img/5.png',
+  './img/6.png',
+  './img/7.png',
+  './img/8.png',
+  './img/9.png',
+  './img/10.png',
+];
 
-class App extends React.Component<{}, State> {
-  state = {
-    images: [
-      './img/1.png',
-      './img/2.png',
-      './img/3.png',
-      './img/4.png',
-      './img/5.png',
-      './img/6.png',
-      './img/7.png',
-      './img/8.png',
-      './img/9.png',
-      './img/10.png',
-    ],
+const App: React.FC = () => {
+  const [step, setStep] = useState(3);
+  const [frameSize, setFrameSize] = useState(3);
+  const [itemWidth, setItemWidth] = useState(130);
+  const [animationDuration, setAnimationDuration] = useState(1000);
+  const [isPrevBtnActive, setIsPrevBtnActive] = useState(false);
+  const [isNextBtnActive, setIsNextBtnActive] = useState(true);
+  const [position, setPosition] = useState(0);
+
+  const handlePrevBtnClick = () => {
+    setPosition(currentPosition => (position - step >= 0
+      ? currentPosition - step
+      : 0));
   };
 
-  render() {
-    const { images } = this.state;
+  const handleNextBtnClick = () => {
+    setPosition(currentPosition => (
+      position + step <= images.length - frameSize
+        ? currentPosition + step
+        : images.length - frameSize
+    ));
+  };
 
-    return (
-      <div className="App">
-        {/* eslint-disable-next-line */}
-        <h1>Carousel with {images.length} images</h1>
+  useEffect(() => {
+    setIsPrevBtnActive(position > 0);
+    setIsNextBtnActive(position < images.length - frameSize);
+  }, [position]);
 
-        <Carousel />
+  return (
+    <div className="App">
+      {/* eslint-disable-next-line */}
+      <h1 className="App__title">
+        Carousel with {images.length} images
+      </h1>
+
+      <div className="form">
+        <div className="form__control">
+          <label>Step: </label>
+          <input 
+            type="number"
+            value={step}
+            onChange={(e) => setStep(+e.target.value)}
+          />
+        </div>
+
+        <div className="form__control">
+          <label>Count images: </label>
+          <input 
+            type="number" 
+            value={frameSize}
+            onChange={(e) => setFrameSize(+e.target.value)}
+          />
+        </div>
+
+        <div className="form__control">
+          <label>Image width: </label>
+          <input
+            type="number" 
+            value={itemWidth}
+            onChange={(e) => setItemWidth(+e.target.value)}
+          />
+        </div>
+
+        <div className="form__control">
+          <label>Duration: </label>
+          <input
+            type="number" 
+            value={animationDuration}
+            onChange={(e) => setAnimationDuration(+e.target.value)}
+          />
+        </div>
       </div>
-    );
-  }
+
+      <Carousel
+        images={images}
+        position={position}
+        animationDuration={animationDuration}
+        frameSize={frameSize}
+        itemWidth={itemWidth}
+        isPrevBtnActive={isPrevBtnActive}
+        handlePrevBtnClick={handlePrevBtnClick}
+        isNextBtnActive={isNextBtnActive}
+        handleNextBtnClick={handleNextBtnClick}
+      />
+    </div>
+  );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import ReactDOM from 'react-dom';
 import App from './App';
+import './App.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
