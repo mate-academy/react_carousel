f32cda8c8d9fcf8d6384cd88d212ae1678a5696f
Volodymyr Kuzmin <vlkzmn@gmail.com>
2023-07-08 11:54:13 +0300

    solution

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,18 +1,97 @@
 import React from 'react';
+import cn from 'classnames';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+type Props = {
+  images: string[];
+  step: number;
+  frameSize: number;
+  itemWidth: number;
+  animationDuration: number;
+};
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+const Carousel: React.FC<Props> = ({
+  images,
+  step,
+  frameSize,
+  itemWidth,
+  animationDuration,
+}) => {
+  const [shift, setShift] = React.useState(0);
+
+  const maxShift = -(images.length - frameSize);
+
+  const slideLeft = () => {
+    const shiftLeft = shift - step;
+
+    return shift > maxShift
+      && setShift(shiftLeft < maxShift ? maxShift : shiftLeft);
+  };
+
+  const slideRight = () => shift < 0
+    && setShift((shift + step) < 0 ? shift + step : 0);
+
+  const transform = () => (shift < maxShift ? maxShift : shift) * itemWidth;
+
+  return (
+    <>
+      <div
+        className="Carousel"
+        style={{
+          width: `${frameSize * itemWidth}px`,
+          transition: `width ${animationDuration}ms`,
+        }}
+      >
+        <ul
+          className="Carousel__list"
+          style={{
+            width: `${images.length * itemWidth}px`,
+            transform: `translateX(${transform()}px)`,
+            transition: `transform ${animationDuration}ms, width ${animationDuration}ms`,
+          }}
+        >
+          {images.map((item, i) => (
+            <li key={item}>
+              <img
+                src={item}
+                className="Carousel__image"
+                alt={`${i}`}
+                style={{
+                  width: `${itemWidth}px`,
+                  height: `${itemWidth}px`,
+                  transition: `width ${animationDuration}ms, height ${animationDuration}ms`,
+                }}
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="Carousel__buttons">
+        <button
+          type="button"
+          className={cn(
+            'Carousel__button',
+            { 'Carousel__button--disabled': shift === maxShift },
+          )}
+          onClick={slideLeft}
+        >
+          &#8678;
+        </button>
+        <button
+          type="button"
+          className={cn(
+            'Carousel__button',
+            { 'Carousel__button--disabled': shift === 0 },
+          )}
+          data-cy="next"
+          onClick={slideRight}
+        >
+          &#8680;
+        </button>
+      </div>
+    </>
+  );
+};
 
 export default Carousel;
