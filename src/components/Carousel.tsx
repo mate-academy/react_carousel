b649a430cdd8bdaf2c05be31f8364fd0b0d7e014
IvanVaverchak <ivanvaverchak@2000gmail.com>
2023-07-09 16:08:54 +0300

    add task solution

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,18 +1,123 @@
 import React from 'react';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+type Props = {
+  images: string[],
+  step: number,
+  frameSize: number,
+  itemWidth: number,
+  animationDuration: number,
+  infinite: boolean,
+};
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+type State = {
+  curentIndex: number,
+};
 
-export default Carousel;
+export class Carousel extends React.Component<Props, State> {
+  state: State = {
+    curentIndex: 0,
+  };
+
+  nextButton = () => {
+    const {
+      images,
+      step,
+      frameSize,
+      infinite,
+    } = this.props;
+
+    const { curentIndex } = this.state;
+    const lastIndex = images.length - frameSize;
+
+    this.setState({
+      curentIndex: curentIndex + step > lastIndex
+        ? lastIndex
+        : curentIndex + step,
+    });
+
+    if (curentIndex === lastIndex && infinite) {
+      this.setState({ curentIndex: 0 });
+    }
+  };
+
+  prevButton = () => {
+    const {
+      images,
+      step,
+      frameSize,
+      infinite,
+    } = this.props;
+
+    const { curentIndex } = this.state;
+    const lastIndex = images.length - frameSize;
+
+    this.setState({
+      curentIndex: curentIndex - step > 0
+        ? curentIndex - step
+        : 0,
+    });
+
+    if (curentIndex === 0 && infinite) {
+      this.setState({ curentIndex: lastIndex });
+    }
+  };
+
+  render() {
+    const { curentIndex } = this.state;
+    const {
+      images,
+      itemWidth,
+      frameSize,
+      animationDuration,
+      infinite,
+    } = this.props;
+
+    const prevDisable = curentIndex === 0 && !infinite;
+    const nextDisable = curentIndex === images.length - frameSize && !infinite;
+
+    return (
+      <div
+        className="Carousel"
+        style={{
+          width: `${itemWidth * frameSize}px`,
+        }}
+      >
+        <ul className="Carousel__list">
+          {images.map(image => (
+            <li
+              className="Carousel__list-item"
+              key={`${images.indexOf(image)}`}
+              style={{
+                transform: `translateX(-${curentIndex * itemWidth}px)`,
+                transition: `${animationDuration}ms`,
+              }}
+            >
+              <img
+                src={image}
+                alt={`${images.indexOf(image)} emoji`}
+                width={itemWidth}
+              />
+            </li>
+          ))}
+        </ul>
+
+        <button
+          type="button"
+          disabled={prevDisable}
+          onClick={this.prevButton}
+        >
+          Prev
+        </button>
+        <button
+          type="button"
+          disabled={nextDisable}
+          onClick={this.nextButton}
+          data-cy="next"
+        >
+          Next
+        </button>
+      </div>
+    );
+  }
+}
