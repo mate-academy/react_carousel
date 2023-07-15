8cd1ad05f9d54118af97867bbb27f73318c42671
Eugene Kulish <genekulish13@gmail.com>
2023-07-15 15:25:28 +0300

    add Carousel

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import './App.scss';
 import Carousel from './components/Carousel';
 
@@ -24,13 +25,29 @@ class App extends React.Component<{}, State> {
 
   render() {
     const { images } = this.state;
+    const frameSize = 3;
+    const step = 3;
+    const itemWidth = 130;
+    const animationDuration = 1000;
+    // const infinite = false;
 
     return (
       <div className="App">
-        {/* eslint-disable-next-line */}
-        <h1>Carousel with {images.length} images</h1>
+        <h1
+          className="title"
+          data-cy="title"
+        >
+          {`Carousel with ${images.length} images`}
+        </h1>
 
-        <Carousel />
+        <Carousel
+          images={images}
+          step={step}
+          frameSize={frameSize}
+          itemWidth={itemWidth}
+          animationDuration={animationDuration}
+          // infinite={infinite}
+        />
       </div>
     );
   }
--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,18 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
+import cn from 'classnames';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+type Props = {
+  images: string[],
+  step: number,
+  frameSize: number,
+  itemWidth: number,
+  animationDuration: number,
+  // infinite: boolean,
+};
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+const Carousel: React.FC<Props> = ({
+  images,
+  step,
+  frameSize,
+  itemWidth,
+  animationDuration,
+  // infinite,
+}) => {
+  const [marginLeft, setMarginLeft] = useState(0);
+
+  const leftSide = -itemWidth * (images.length - frameSize);
+  const rightSide = 0;
+
+  return (
+    <div
+      className="Carousel"
+      style={{ width: frameSize * itemWidth }}
+    >
+      <ul
+        className="Carousel__list"
+        style={{
+          marginLeft,
+          transition: `marginLeft ${animationDuration}ms`,
+        }}
+      >
+        {images.map((image, index) => (
+          <li
+            key={image}
+          >
+            <img
+              src={image}
+              alt={`${index + 1}`}
+              style={{ width: itemWidth }}
+            />
+          </li>
+        ))}
+      </ul>
+
+      <div className="button-container">
+        <button
+          type="button"
+          className={cn('button', marginLeft === rightSide && 'disabled')}
+          onClick={() => {
+            setMarginLeft(Math.min(marginLeft + step * itemWidth, rightSide));
+          }}
+        >
+          {' '}
+        </button>
+
+        <button
+          data-cy="next"
+          type="button"
+          className={cn('button', marginLeft === leftSide && 'disabled')}
+          onClick={() => {
+            setMarginLeft(Math.max(marginLeft - step * itemWidth, leftSide));
+          }}
+        >
+          {' '}
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Carousel;
