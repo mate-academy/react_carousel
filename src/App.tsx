3e386410be83114140d35285a1e39e29b036090b
Maksym Avramenko <nonste@outlook.com>
2023-07-16 22:10:27 +0300

    develop

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
-import Carousel from './components/Carousel';
+import { Carousel } from './components/Carousel';
 
-interface State {
-  images: string[];
-}
+export const App: React.FC = () => {
+  const [images] = useState([
+    './img/1.png',
+    './img/2.png',
+    './img/3.png',
+    './img/4.png',
+    './img/5.png',
+    './img/6.png',
+    './img/7.png',
+    './img/8.png',
+    './img/9.png',
+    './img/10.png',
+  ]);
 
-class App extends React.Component<{}, State> {
-  state = {
-    images: [
-      './img/1.png',
-      './img/2.png',
-      './img/3.png',
-      './img/4.png',
-      './img/5.png',
-      './img/6.png',
-      './img/7.png',
-      './img/8.png',
-      './img/9.png',
-      './img/10.png',
-    ],
-  };
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [frameSize, setFrameSize] = useState(3);
+  const [step, setStep] = useState(3);
+  const [itemWidth, setItemWidth] = useState(130);
+  const [animationDuration, setAnimationDuration] = useState(1);
+  const [infinite, setInfinite] = useState(false);
 
-  render() {
-    const { images } = this.state;
+  return (
+    <div className="app">
+      <h1 className="title" data-cy="title">{`Carousel with ${images.length} images`}</h1>
 
-    return (
-      <div className="App">
-        {/* eslint-disable-next-line */}
-        <h1>Carousel with {images.length} images</h1>
-
-        <Carousel />
-      </div>
-    );
-  }
-}
-
-export default App;
+      <Carousel
+        images={images}
+        frameSize={frameSize}
+        updateFrameSize={(size) => setFrameSize(size)}
+        step={step}
+        updateStep={(steps) => setStep(steps)}
+        itemWidth={itemWidth}
+        updateItemWidth={(width) => setItemWidth(width)}
+        animationDuration={animationDuration}
+        updateAnimationDuration={(duration) => setAnimationDuration(duration)}
+        currentIndex={currentIndex}
+        updateCurrentIndex={(index) => setCurrentIndex(index)}
+        infinite={infinite}
+        updateInfinite={(state) => setInfinite(state)}
+      />
+    </div>
+  );
+};
--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,18 +1,153 @@
 import React from 'react';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+type Props = {
+  images: string[];
+  frameSize: number;
+  updateFrameSize: (newFrameSize: number) => void;
+  step: number;
+  updateStep: (newStep: number) => void;
+  itemWidth: number;
+  updateItemWidth: (newItemWidth: number) => void;
+  animationDuration: number;
+  updateAnimationDuration: (newAnimationDuration: number) => void;
+  currentIndex: number;
+  updateCurrentIndex: (index: number) => void;
+  infinite: boolean;
+  updateInfinite: (state: boolean) => void;
+};
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+export const Carousel: React.FC<Props> = ({
+  images,
+  frameSize,
+  updateFrameSize,
+  step,
+  updateStep,
+  itemWidth,
+  updateItemWidth,
+  animationDuration,
+  updateAnimationDuration,
+  currentIndex,
+  updateCurrentIndex: setCurrentIndex,
+  infinite,
+  updateInfinite,
+}) => {
+  const handleNext = () => {
+    let newIndex = currentIndex + step;
 
-export default Carousel;
+    if (newIndex >= images.length - frameSize) {
+      if (infinite) {
+        newIndex = newIndex + frameSize - images.length;
+      } else {
+        newIndex = images.length - frameSize;
+      }
+    }
+
+    setCurrentIndex(newIndex);
+  };
+
+  const handlePrev = () => {
+    let newIndex = currentIndex - step;
+
+    if (newIndex < 0) {
+      if (infinite) {
+        newIndex = images.length - step;
+      } else {
+        newIndex = 0;
+      }
+    }
+
+    setCurrentIndex(newIndex);
+  };
+
+  const transformValue = -currentIndex * itemWidth;
+
+  return (
+    <div className="carousel">
+      <div className="carousel__wrapper">
+        <button
+          className="carousel__button"
+          type="button"
+          onClick={handlePrev}
+        >
+          Prev
+        </button>
+        <div
+          className="carousel__list__wrapper"
+          style={{ width: `${itemWidth * frameSize}px` }}
+        >
+          <ul
+            className="carousel__list"
+            style={{
+              transform: `translateX(${transformValue}px)`,
+              transition: `transform ${animationDuration}s ease`,
+            }}
+          >
+            {images.map((image: string, index: number) => (
+              <li key={image}>
+                <img
+                  src={image}
+                  alt={`${index}`}
+                  className="carousel__img"
+                  style={{ width: `${itemWidth}px` }}
+                />
+              </li>
+            ))}
+          </ul>
+        </div>
+        <button
+          className="carousel__button"
+          type="button"
+          data-cy="next"
+          onClick={handleNext}
+        >
+          Next
+        </button>
+      </div>
+      <label>
+        {`Frame size is ${frameSize} `}
+        <input
+          type="range"
+          min="2"
+          max="5"
+          value={frameSize}
+          onChange={(event) => updateFrameSize(+event.target.value)}
+        />
+      </label>
+      <label>
+        {`Step number is ${step} `}
+        <input
+          type="range"
+          min="2"
+          max="5"
+          value={step}
+          onChange={(event) => updateStep(+event.target.value)}
+        />
+      </label>
+      <label>
+        {`Itemwidth is ${itemWidth} `}
+        <input
+          type="range"
+          min="130"
+          max="200"
+          value={itemWidth}
+          onChange={(event) => updateItemWidth(+event.target.value)}
+        />
+      </label>
+      <label>
+        {`Animation duration ${animationDuration}s `}
+        <input
+          type="range"
+          min="1"
+          max="3"
+          value={animationDuration}
+          onChange={(event) => updateAnimationDuration(+event.target.value)}
+        />
+      </label>
+      <label>
+        {'Infinite loop '}
+        <input type="checkbox" onChange={() => updateInfinite(!infinite)} />
+      </label>
+    </div>
+  );
+};
