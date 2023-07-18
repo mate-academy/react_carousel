b4e164c031762eb587860c8564a0108732bf63b6
Mykola Plekan <zikzar@gmail.com>
2023-07-18 13:55:47 +0300

    solution

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,18 +1,91 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+interface Props {
+  images: string[];
+  step: number;
+  frameSize: number;
+  itemWidth: number;
+  animationDuration: number;
+  infinite: boolean;
+}
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+const Carousel: React.FC<Props> = ({
+  images,
+  step,
+  frameSize,
+  itemWidth,
+  animationDuration,
+  infinite,
+}) => {
+  const [firstImg, setFirstImage] = useState(0);
+
+  function carouselNext() {
+    if (firstImg === (images.length - frameSize) && infinite) {
+      return setFirstImage(0);
+    }
+
+    return setFirstImage((num) => (
+      (num + step) <= (images.length - frameSize)
+        ? num + step
+        : images.length - frameSize
+    ));
+  }
+
+  function carouselPrev() {
+    if (firstImg === 0 && infinite) {
+      return setFirstImage(images.length - frameSize);
+    }
+
+    return setFirstImage((num) => (
+      (num - step) >= 0
+        ? num - step
+        : frameSize - frameSize
+    ));
+  }
+
+  return (
+    <div
+      className="Carousel"
+      style={{ width: `${frameSize * itemWidth}px` }}
+    >
+      <ul className="Carousel__list">
+        {images.map((image, index) => (
+          <li
+            key={`${index + 1}`}
+            style={{
+              transform: `translateX(${-(itemWidth * firstImg)}px)`,
+              transition: `transform ${animationDuration}ms`,
+            }}
+          >
+            <img
+              style={{ width: `${itemWidth}px` }}
+              src={image}
+              alt={`${index + 1}`}
+            />
+          </li>
+        ))}
+      </ul>
+
+      <div className="container">
+        <button
+          disabled={firstImg - step < -step + 1 && !infinite}
+          type="button"
+          onClick={() => carouselPrev()}
+        >
+          Prev
+        </button>
+        <button
+          data-cy="next"
+          disabled={firstImg === images.length - step && !infinite}
+          type="button"
+          onClick={() => carouselNext()}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Carousel;
