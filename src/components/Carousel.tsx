284abc5045fd97f97da95642fece45a48e0540b5
Diana Tuz <dianatuz12@gmail.com>
2023-07-20 14:13:17 +0300

    solution

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,18 +1,87 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+type Props = {
+  images: string[];
+  step: number;
+  frameSize: number;
+  itemWidth: number;
+  animationDuration: number;
+  infinite: boolean;
+};
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+export const Carousel: React.FC<Props> = ({
+  images,
+  step,
+  frameSize,
+  itemWidth,
+  animationDuration,
+  infinite,
+}) => {
+  const [startImage, setStartImage] = useState(0);
+  const [endImage, setEndImage] = useState(frameSize);
 
-export default Carousel;
+  useEffect(() => {
+    setEndImage(startImage + frameSize);
+  }, [frameSize, startImage]);
+
+  const handlePrev = () => {
+    if (startImage > 0) {
+      setStartImage(startImage - step);
+      setEndImage(endImage - step);
+    }
+  };
+
+  const handleNext = () => {
+    if (endImage < images.length) {
+      setEndImage(endImage + step);
+      setStartImage(startImage + step);
+    }
+
+    if (infinite && endImage === images.length - 1) {
+      setStartImage(0);
+      setEndImage(frameSize);
+    }
+  };
+
+  return (
+    <div className="Carousel">
+      <ul
+        className="Carousel__list"
+      >
+        {images.slice(startImage, endImage).map((image: string) => (
+          <li
+            key={image}
+            className="Carousel__image"
+            style={{ animation: `ease ${animationDuration}ms` }}
+
+          >
+            <img
+              src={image}
+              alt={image}
+              style={{ width: `${itemWidth}px` }}
+            />
+          </li>
+        ))}
+      </ul>
+      <div className="Carousel__buttons">
+        <button
+          className="Carousel__bnt"
+          type="button"
+          onClick={handlePrev}
+          disabled={startImage === 0}
+        >
+          Prev
+        </button>
+        <button
+          className="Carousel__bnt"
+          type="button"
+          data-cy="next"
+          onClick={handleNext}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+};
