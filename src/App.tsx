5f276349b0262c1bdffa90fdab97cf65a1efa87d
zipic <zpc23041988@gmail.com>
2023-07-20 17:51:06 +0300

    test_carousel

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,91 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import Carousel from './components/Carousel';
 
-interface State {
-  images: string[];
-}
+const DEFAULT_FRAME_SIZE = 3;
+const DEFAULT_ITEM_WIDTH = 130;
+const STEP = 3;
+const DEFAULT_DURATION = 1000;
 
-class App extends React.Component<{}, State> {
-  state = {
-    images: [
-      './img/1.png',
-      './img/2.png',
-      './img/3.png',
-      './img/4.png',
-      './img/5.png',
-      './img/6.png',
-      './img/7.png',
-      './img/8.png',
-      './img/9.png',
-      './img/10.png',
-    ],
+export const App: React.FC = () => {
+  const [frameSize, setFrameSize] = useState(DEFAULT_FRAME_SIZE);
+  const [itemWidth, setItemWidth] = useState(DEFAULT_ITEM_WIDTH);
+  const [step, setStep] = useState(STEP);
+  const [animationDuration, setAnimationDuration] = useState(DEFAULT_DURATION);
+
+  const handleItems = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = +event.target.value;
+
+    setFrameSize(value || DEFAULT_FRAME_SIZE);
   };
 
-  render() {
-    const { images } = this.state;
+  const handleWidth = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = +event.target.value;
 
-    return (
-      <div className="App">
-        {/* eslint-disable-next-line */}
-        <h1>Carousel with {images.length} images</h1>
+    setItemWidth(value || DEFAULT_ITEM_WIDTH);
+  };
 
-        <Carousel />
+  const handeStep = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = +event.target.value;
+
+    setStep(value || STEP);
+  };
+
+  const handleDuration = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = +event.target.value;
+
+    setAnimationDuration(value || DEFAULT_DURATION);
+  };
+
+  const images = [
+    './img/1.png',
+    './img/2.png',
+    './img/3.png',
+    './img/4.png',
+    './img/5.png',
+    './img/6.png',
+    './img/7.png',
+    './img/8.png',
+    './img/9.png',
+    './img/10.png',
+  ];
+
+  return (
+    <div className="App">
+      {/* eslint-disable-next-line */}
+      <h1 data-cy="title">Carousel with {images.length} images</h1>
+      <div className="Carousel_menu">
+        <input
+          placeholder="itemWidth"
+          type="text"
+          onChange={handleWidth}
+        />
+        <input
+          placeholder="frameSize"
+          type="text"
+          onChange={handleItems}
+        />
+        <input
+          placeholder="step"
+          type="text"
+          onChange={handeStep}
+        />
+        <input
+          placeholder="animationDuration"
+          type="text"
+          onChange={handleDuration}
+        />
       </div>
-    );
-  }
-}
+
+      <Carousel
+        images={images}
+        frameSize={frameSize}
+        itemWidth={itemWidth}
+        step={step}
+        animationDuration={animationDuration}
+      />
+    </div>
+  );
+};
 
 export default App;
--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,18 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+interface Props {
+  images: string[];
+  frameSize: number;
+  itemWidth: number;
+  step: number;
+  animationDuration: number;
+}
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+const Carousel: React.FC<Props> = ({
+  images,
+  frameSize,
+  itemWidth,
+  step,
+  animationDuration,
+}) => {
+  const [startIndex, setStartIndex] = useState(0);
+
+  const handleNext = () => {
+    setStartIndex((prevIndex) => Math.min(
+      prevIndex + step, images.length - frameSize,
+    ));
+  };
+
+  const handlePrev = () => {
+    setStartIndex((prevIndex) => Math.max(prevIndex - step, 0));
+  };
+
+  const visibleImages = images.slice(startIndex, startIndex + frameSize);
+
+  return (
+    <div className="Carousel">
+      <ul className="Carousel__list" style={{ transition: `${animationDuration}ms ease` }}>
+        {visibleImages.map((img) => (
+          <li key={img}>
+            <img src={img} alt={img} style={{ width: `${itemWidth}px` }} />
+          </li>
+        ))}
+      </ul>
+
+      <div className="Container">
+        <button
+          type="button"
+          disabled={startIndex === 0}
+          onClick={handlePrev}
+          data-cy="prev"
+        >
+          Prev
+        </button>
+
+        <button
+          type="button"
+          disabled={startIndex === images.length}
+          onClick={handleNext}
+          data-cy="next"
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Carousel;
