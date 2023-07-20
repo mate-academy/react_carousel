8fe59ebea06a1e8337ed9020951ff678e7c512a3
Yaroslav Dudar <dudar.komod@gmail.com>
2023-07-20 17:06:46 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,161 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import Carousel from './components/Carousel';
 
-interface State {
-  images: string[];
-}
+export const App: React.FC = () => {
+  const imagesData: string[] = [
+    './img/1.png',
+    './img/2.png',
+    './img/3.png',
+    './img/4.png',
+    './img/5.png',
+    './img/6.png',
+    './img/7.png',
+    './img/8.png',
+    './img/9.png',
+    './img/10.png',
+  ];
 
-class App extends React.Component<{}, State> {
-  state = {
-    images: [
-      './img/1.png',
-      './img/2.png',
-      './img/3.png',
-      './img/4.png',
-      './img/5.png',
-      './img/6.png',
-      './img/7.png',
-      './img/8.png',
-      './img/9.png',
-      './img/10.png',
-    ],
+  const [itemWidth, setItemWidth] = useState(130);
+  const [frameSize, setFrameSize] = useState(3);
+  const [step, setStep] = useState(3);
+  const [animationDuration, setAnimationDuration] = useState(1000);
+  const [infinite, setInfinite] = useState(false);
+  const [translate, setTranslate] = useState(0);
+
+  const getNewItemWidth = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+
+    if (itemWidth !== +value) {
+      setItemWidth(+value);
+
+      const newTranslate = translate + (step * itemWidth - translate);
+
+      setTranslate(newTranslate);
+    }
   };
 
-  render() {
-    const { images } = this.state;
+  const getNewFrameSize = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
 
-    return (
-      <div className="App">
-        {/* eslint-disable-next-line */}
-        <h1>Carousel with {images.length} images</h1>
+    if (frameSize !== +value) {
+      setFrameSize(+value);
+    }
+  };
 
-        <Carousel />
+  const getNewStep = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+
+    if (step !== +value) {
+      setStep(+value);
+    }
+  };
+
+  const getNewAD = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+
+    if (animationDuration !== +value) {
+      setAnimationDuration(+value);
+    }
+  };
+
+  const getNewInfinte = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { checked } = event.target;
+
+    if (checked) {
+      setInfinite(true);
+    } else {
+      setInfinite(false);
+    }
+  };
+
+  return (
+    <div className="App" data-cy="title">
+      <h1>
+        Carousel with
+        {' '}
+        {imagesData.length}
+        {' '}
+        images
+      </h1>
+      <Carousel
+        images={imagesData}
+        step={step}
+        frameSize={frameSize}
+        itemWidth={itemWidth}
+        animationDuration={animationDuration}
+        infinite={infinite}
+        translate={translate}
+        onTranslate={setTranslate}
+      />
+
+      <div className="App__setting">
+        <label htmlFor="itemWidth" className="App__label">
+          Item width:
+          <input
+            className="App__input"
+            id="itemWidth"
+            type="number"
+            min={30}
+            max={400}
+            value={itemWidth}
+            onChange={getNewItemWidth}
+          />
+        </label>
+
+        <label htmlFor="frameSize" className="App__label">
+          Frame size:
+          <input
+            className="App__input"
+            id="frameSize"
+            type="number"
+            min={1}
+            max={10}
+            value={frameSize}
+            onChange={getNewFrameSize}
+          />
+        </label>
+
+        <label htmlFor="step" className="App__label">
+          Step:
+          <input
+            className="App__input"
+            id="step"
+            type="number"
+            min={1}
+            max={10}
+            value={step}
+            onChange={getNewStep}
+          />
+        </label>
+
+        <label htmlFor="animationDuration" className="App__label">
+          Animation duration:
+          <input
+            className="App__input"
+            id="animationDuration"
+            type="number"
+            min={250}
+            max={5000}
+            step={250}
+            value={animationDuration}
+            onChange={getNewAD}
+          />
+        </label>
+
+        <label htmlFor="infinite" className="App__label">
+          Infinite:
+          <input
+            className="App__input  App__input--checkbox"
+            id="infinite"
+            type="checkbox"
+            checked={infinite}
+            onChange={getNewInfinte}
+          />
+        </label>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,18 +1,118 @@
 import React from 'react';
+import cn from 'classnames';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+type Props = {
+  images: string[];
+  step: number;
+  frameSize: number;
+  itemWidth: number;
+  animationDuration: number;
+  infinite: boolean;
+  translate: number;
+  onTranslate: React.Dispatch<React.SetStateAction<number>>;
+};
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+const Carousel: React.FC<Props> = ({
+  images,
+  step,
+  frameSize,
+  itemWidth,
+  animationDuration,
+  infinite,
+  translate,
+  onTranslate,
+}) => {
+  const maxTranslate = (images.length - frameSize) * itemWidth;
+  const minTranslate = 0;
+  const disableForward = translate === maxTranslate && !infinite;
+  const disableBack = translate === minTranslate && !infinite;
+
+  const moveForward = () => {
+    if (infinite && translate === maxTranslate) {
+      onTranslate(minTranslate);
+
+      return;
+    }
+
+    const newTranslate = translate + (step * itemWidth) > maxTranslate
+      ? maxTranslate
+      : translate + (step * itemWidth);
+
+    onTranslate(newTranslate);
+  };
+
+  const moveBack = () => {
+    if (infinite && translate === minTranslate) {
+      onTranslate(maxTranslate);
+
+      return;
+    }
+
+    const newTranslate = translate - (step * itemWidth) <= minTranslate
+      ? minTranslate
+      : translate - (step * itemWidth);
+
+    onTranslate(newTranslate);
+  };
+
+  return (
+    <div
+      className="Carousel"
+      data-qa={infinite}
+      style={{ width: `${frameSize * itemWidth}px`, transition: `${animationDuration}ms all` }}
+    >
+      <div className="Carousel__container">
+        <ul
+          className="Carousel__list"
+          style={{ transition: `${animationDuration}ms all`, transform: `translateX(-${translate}px)` }}
+        >
+          {images.map((image, index) => {
+            return (
+              <li key={image}>
+                <img
+                  src={image}
+                  alt={(index + 1).toString()}
+                  style={{ width: `${itemWidth}px` }}
+                  data-qa={step}
+                />
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <div className="Carousel__buttons">
+        <button
+          style={{ transition: `${animationDuration}ms all` }}
+          disabled={disableBack}
+          className={cn(
+            'Carousel__button',
+            { 'Carousel__button--disabled': disableBack },
+          )}
+          type="button"
+          onClick={moveBack}
+        >
+          Prev
+
+        </button>
+
+        <button
+          data-cy="next"
+          style={{ transition: `${animationDuration}ms all` }}
+          disabled={disableForward}
+          className={cn(
+            'Carousel__button',
+            { 'Carousel__button--disabled': disableForward },
+          )}
+          type="button"
+          onClick={moveForward}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Carousel;
