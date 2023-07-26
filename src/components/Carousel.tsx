26a06bd00794673689363ec202f7df4ae11c7e60
Kateryna Stemblivska <ekaterina_09@ukr.net>
2023-07-26 16:43:12 +0300

    Solution

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,18 +1,87 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+type Props = {
+  images: string[],
+  step: number,
+  frameSize: number,
+  itemWidth: number,
+  animationDuration: number,
+  infinite: boolean,
+};
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+const Carousel: React.FC<Props> = ({
+  images,
+  step,
+  frameSize,
+  itemWidth,
+  animationDuration,
+  infinite,
+}) => {
+  const [currentShift, setCurrentShift] = useState(0);
+
+  const widthOfListBox = itemWidth * frameSize;
+  const hiddenImagesLength = (images.length - frameSize) * itemWidth;
+
+  const getPrevImage = () => {
+    setCurrentShift((shift) => Math.min(shift + itemWidth * step, 0));
+  };
+
+  const getNextImage = () => {
+    if (currentShift === -hiddenImagesLength && infinite) {
+      setCurrentShift(0);
+    }
+
+    setCurrentShift((shift) => Math.max(
+      shift - itemWidth * step,
+      -hiddenImagesLength,
+    ));
+  };
+
+  return (
+    <div className="Carousel">
+      <ul
+        className="Carousel__list"
+        style={{ width: `${widthOfListBox}px` }}
+      >
+        {images.map((img, ind) => (
+          <li
+            key={img}
+            style={{
+              transform: `translateX(${currentShift}px)`,
+              transition: `${animationDuration}ms`,
+            }}
+          >
+            <img
+              src={img}
+              alt={`${ind + 1}`}
+              style={{ width: `${itemWidth}px` }}
+            />
+          </li>
+        ))}
+      </ul>
+
+      <div className="Carousel__buttons-wrapper">
+        <button
+          className="Carousel__button"
+          type="button"
+          onClick={getPrevImage}
+          disabled={currentShift === 0}
+        >
+          &#9664;
+        </button>
+        <button
+          className="Carousel__button"
+          data-cy="next"
+          type="button"
+          onClick={getNextImage}
+          disabled={currentShift === -hiddenImagesLength && !infinite}
+        >
+          &#9654;
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Carousel;
