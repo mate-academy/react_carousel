b7ea89ad59baa675822b486ed6344763e21e5646
david.shamiiev <david.shamiiev@rad4m.com>
2023-07-28 14:00:01 +0300

    minor changes

--- src/components/PopUp/PopUp.tsx
+++ src/components/PopUp/PopUp.tsx
@@ -3,7 +3,7 @@ import './PopUp.scss';
 
 export const PopUp: React.FC = () => (
   <div className="pop-up">
-    I have issues with "infinite". I tried a lot.
+    I have issues with infinite. I tried a lot.
     But for now i dont have any solution.
     I created another option with reordering index. But I cant animate it(
     If you have suggestions how I can make it cyclic - please help!
