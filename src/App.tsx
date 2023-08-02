4cd0b43bfbe3700d0e36bef92a283955f3bcde61
Dima Astapenko <bonum_taurus@ukr.net>
2023-08-02 23:28:06 +0300

    task solution

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,11 @@ import Carousel from './components/Carousel';
 
 interface State {
   images: string[];
+  frameSize: number;
+  itemWidth: number;
+  step: number,
+  animationDuration: number,
+  infinite: boolean,
 }
 
 class App extends React.Component<{}, State> {
@@ -20,17 +25,78 @@ class App extends React.Component<{}, State> {
       './img/9.png',
       './img/10.png',
     ],
+    frameSize: 3,
+    itemWidth: 130,
+    step: 3,
+    animationDuration: 1000,
+    infinite: false,
+  };
+
+  changeItemWidth = (width: number) => {
+    this.setState({ itemWidth: width });
+  };
+
+  changeFrameSize = (size: number) => {
+    this.setState({ frameSize: size });
+  };
+
+  changeStepLong = (long: number) => {
+    this.setState({ step: long });
+  };
+
+  changeDuration = (time: number) => {
+    this.setState({ animationDuration: time });
   };
 
   render() {
-    const { images } = this.state;
+    const {
+      images,
+      frameSize,
+      itemWidth,
+      step,
+      animationDuration,
+      infinite,
+    } = this.state;
 
     return (
       <div className="App">
         {/* eslint-disable-next-line */}
-        <h1>Carousel with {images.length} images</h1>
+        <h1 data-cy="title">Carousel with {images.length} images</h1>
 
-        <Carousel />
+        <input
+          type="text"
+          className="field"
+          placeholder="item width"
+          onChange={(event) => this.changeItemWidth(+event.currentTarget.value)}
+        />
+        <input
+          type="text"
+          className="field"
+          placeholder="frame size"
+          onChange={(event) => this.changeFrameSize(+event.currentTarget.value)}
+        />
+        <input
+          type="text"
+          className="field"
+          placeholder="step"
+          onChange={(event) => this.changeStepLong(+event.currentTarget.value)}
+        />
+
+        <input
+          type="text"
+          className="field"
+          placeholder="animation duration"
+          onChange={(event) => this.changeDuration(+event.currentTarget.value)}
+        />
+
+        <Carousel
+          images={images}
+          frameSize={frameSize}
+          itemWidth={itemWidth}
+          step={step}
+          animationDuration={animationDuration}
+          infinite={infinite}
+        />
       </div>
     );
   }
--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,18 +1,98 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+type Props = {
+  images: string[],
+  frameSize: number,
+  itemWidth: number,
+  step: number,
+  animationDuration: number,
+  infinite: boolean,
+};
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+const Carousel: React.FC<Props> = ({
+  images,
+  frameSize,
+  itemWidth,
+  step,
+  animationDuration,
+  infinite,
+}) => {
+  const [shift, setShift] = useState(0);
+
+  useEffect(() => {
+    setShift(0);
+  }, [frameSize, itemWidth, step]);
+
+  const widthPortion = itemWidth * frameSize;
+  const widthCarousel = itemWidth * images.length;
+
+  return (
+    <div
+      className="Carousel"
+      style={{ width: widthPortion }}
+    >
+      <div
+        className="Carousel__list"
+        style={{
+          width: `${widthCarousel}px`,
+          transform: `translateX(${shift}px)`,
+          transition: `transform ${animationDuration}ms`,
+        }}
+      >
+        {images.map(image => (
+          <img
+            style={{ width: itemWidth }}
+            key={image}
+            src={image}
+            alt="emoji"
+          />
+        ))}
+      </div>
+
+      <button
+        onClick={() => {
+          setShift((currentShift) => {
+            if (currentShift === 0 && infinite) {
+              return -(widthCarousel - itemWidth * frameSize);
+            }
+
+            if (currentShift > -(itemWidth * step)) {
+              return 0;
+            }
+
+            return currentShift + itemWidth * step;
+          });
+        }}
+        type="button"
+        disabled={shift >= 0 && !infinite && true}
+      >
+        Prev
+      </button>
+
+      <button
+        onClick={() => {
+          setShift((currentShift) => {
+            if (currentShift <= -(widthCarousel - widthPortion) && infinite) {
+              return 0;
+            }
+
+            if (currentShift <= -(widthCarousel - widthPortion)
+              + itemWidth * step) {
+              return -(widthCarousel - widthPortion);
+            }
+
+            return currentShift - itemWidth * step;
+          });
+        }}
+        data-cy="Next"
+        type="button"
+        disabled={shift <= -(widthCarousel - widthPortion) && !infinite && true}
+      >
+        Next
+      </button>
+    </div>
+  );
+};
 
 export default Carousel;
