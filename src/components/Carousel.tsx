996b9c32b70d094ab8b4b059af0e0f66972bcc3f
Gabriella Ivanjuk <gabriella.ivanjuk@gmail.com>
2023-08-03 19:01:09 +0300

    solution

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,18 +1,132 @@
-import React from 'react';
+import React, { useState } from 'react';
+import classNames from 'classnames';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+interface Props {
+  images: string[];
+}
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+const Carousel: React.FC<Props> = ({ images }) => {
+  const [translate, setTranslate] = useState(0);
+  const [step, setStep] = useState(3);
+  const [frameSize, setFrameSize] = useState(3);
+  const [itemWidth, setItemWidth] = useState(130);
+  const [animationDuration, setAnimationDuration] = useState(1000);
+
+  const styleVisibleCarousel = {
+    transform: `translateX(${translate}px)`,
+    transition: `transform ${animationDuration}ms`,
+  };
+
+  const styleCarousel = {
+    width: `${itemWidth + frameSize}px`,
+  };
+
+  const styleCarouselImage = {
+    width: `${itemWidth}px`,
+  };
+
+  const carouselLength = images.length * itemWidth;
+  const maxTranslateRight = -carouselLength + (itemWidth * frameSize);
+
+  function handleNextImage() {
+    const newTranslate = translate - (itemWidth * step);
+
+    if (newTranslate > maxTranslateRight) {
+      setTranslate(newTranslate);
+    } else {
+      setTranslate(maxTranslateRight);
+    }
+  }
+
+  function handlePrevImage() {
+    const newTranslate = translate + (itemWidth * step);
+
+    if (newTranslate <= 0) {
+      setTranslate(newTranslate);
+    } else {
+      setTranslate(0);
+    }
+  }
+
+  return (
+    <div className="Carousel">
+      <div className="Carousel__inputs">
+        <input
+          type="text"
+          onChange={(event) => setStep(+event.currentTarget.value)}
+          placeholder="Write a number of step here..."
+        />
+
+        <input
+          type="text"
+          onChange={(event) => setFrameSize(+event.currentTarget.value)}
+          placeholder="Write a size of frame here..."
+        />
+
+        <input
+          type="text"
+          onChange={(event) => setItemWidth(+event.currentTarget.value)}
+          placeholder="Write a size of image here..."
+        />
+
+        <input
+          type="text"
+          onChange={(event) => setAnimationDuration(+event.currentTarget.value)}
+          placeholder="Write a duration of animation here..."
+        />
+      </div>
+
+      <ul
+        className="Carousel__list"
+        style={styleCarousel}
+      >
+        <div
+          className="Carousel__visibleList"
+          style={styleVisibleCarousel}
+        >
+          {images.map((image: string, index: number) => (
+            <li key={image}>
+              <img
+                src={image}
+                alt={(index + 1).toString()}
+                style={styleCarouselImage}
+              />
+            </li>
+          ))}
+        </div>
+      </ul>
+
+      <div className="Carousel__buttons">
+        <button
+          type="button"
+          onClick={() => handlePrevImage()}
+          className={classNames(
+            'Carousel__button',
+            {
+              'Carousel__button--disabled': translate === 0,
+            },
+          )}
+        >
+          Prev
+        </button>
+
+        <button
+          type="button"
+          onClick={() => handleNextImage()}
+          className={classNames(
+            'Carousel__button',
+            {
+              'Carousel__button--disabled': translate === maxTranslateRight,
+            },
+          )}
+          data-cy="next"
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Carousel;
