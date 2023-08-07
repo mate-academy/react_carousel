dee89dd76d55cde413b5cf7e07d765eb65213879
Kate <ekaterina2002aleks@gmail.com>
2023-08-07 22:00:14 +0300

    Solution

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,19 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Carousel.scss';
-
-// const Carousel: React.FC = () => (
-//   <div className="Carousel">
-//     <ul className="Carousel__list">
-//       <li><img src="./img/1.png" alt="1" /></li>
-//       <li><img src="./img/1.png" alt="2" /></li>
-//       <li><img src="./img/1.png" alt="3" /></li>
-//       <li><img src="./img/1.png" alt="4" /></li>
-//     </ul>
-
-//     <button type="button">Prev</button>
-//     <button type="button">Next</button>
-//   </div>
-// );
 
 type Props = {
   images: string[],
@@ -24,67 +10,57 @@ type Props = {
   infinite: boolean,
 };
 
-type State = {
-  current: number,
-};
-
-export class Carousel extends React.Component<Props, State> {
-  state = {
-    current: 0,
+export const Carousel: React.FC<Props> = ({
+  images,
+  step = 3,
+  frameSize = 3,
+  itemWidth = 130,
+  animationDuration = 1000,
+  infinite = false,
+}) => {
+  const [current, setCurrent] = useState(0);
+  const imageStyle = {
+    width: `${itemWidth}px`,
+    height: `${itemWidth}px`,
+  };
+  const listStyle = {
+    width: `${frameSize * itemWidth}px`,
+    transform: '',
+    transitionDuration: `${animationDuration}s`,
+  };
+  const handlePrev = () => {
+    if (!infinite) {
+      if (current > 3) {
+        listStyle.transform = `translate(${current * itemWidth - step * itemWidth}px, 0)`;
+        setCurrent(old => old - 3);
+      } else {
+        listStyle.transform = 'translate(0, 0)';
+        setCurrent(0);
+      }
+    }
   };
 
-  render() {
-    const {
-      images,
-      step = 3,
-      frameSize = 3,
-      itemWidth = 130,
-      animationDuration = 1000,
-      infinite = false,
-    } = this.props;
-    const { current } = this.state;
-    const imageStyle = {
-      width: `${itemWidth}px`,
-      height: `${itemWidth}px`,
-    };
-    const listStyle = {
-      width: `${frameSize * itemWidth}px`,
-      transform: '',
-      transitionDuration: `${animationDuration}s`,
-    };
-    const handlePrev = () => {
-      if (!infinite) {
-        if (current > 3) {
-          listStyle.transform = `translate(${current * itemWidth - step * itemWidth}px, 0)`;
-        } else {
-          listStyle.transform = 'translate(0, 0)';
-        }
+  const handleNext = () => {
+    if (!infinite) {
+      if (current < images.length - 3) {
+        listStyle.transform = `translate(${current * itemWidth + step * itemWidth}px, 0)`;
+        setCurrent(old => old + 3);
       }
-    };
+    }
+  };
 
-    const handleNext = () => {
-      if (!infinite) {
-        if (current < images.length - 3) {
-          listStyle.transform = `translate(${current * itemWidth + step * itemWidth}px, 0)`;
-        }
-      }
-    };
+  return (
+    <div className="Carousel">
+      <ul className="Carousel__list" style={listStyle}>
+        {images.map(image => (
+          <li key={image}>
+            <img className="image" style={imageStyle} src={image} alt={`${images.indexOf(image)}`} />
+          </li>
+        ))}
+      </ul>
 
-    return (
-      <div className="Carousel">
-        <ul className="Carousel__list" style={listStyle}>
-          {images.map(image => (
-            <li key={image}>
-              <img className="image" style={imageStyle} src={image} alt={`${images.indexOf(image)}`} />
-            </li>
-          ))}
-        </ul>
-
-        <button type="button" onClick={handlePrev}>Prev</button>
-        <button type="button" onClick={handleNext}>Next</button>
-      </div>
-    );
-  }
-}
-
-// export default Carousel;
+      <button type="button" onClick={handlePrev}>Prev</button>
+      <button type="button" onClick={handleNext}>Next</button>
+    </div>
+  );
+};
