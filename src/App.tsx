10fc497d0c8b72f6785ef44f90f219e8c685c729
Anna Pastushenko <annpastushenko23@gmail.com>
2023-08-08 16:25:51 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,104 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.scss';
-import Carousel from './components/Carousel';
 
-interface State {
-  images: string[];
-}
+import Carousel from './components/Carousel/Carousel';
+import Settings from './components/Settings/Settings';
 
-class App extends React.Component<{}, State> {
-  state = {
-    images: [
-      './img/1.png',
-      './img/2.png',
-      './img/3.png',
-      './img/4.png',
-      './img/5.png',
-      './img/6.png',
-      './img/7.png',
-      './img/8.png',
-      './img/9.png',
-      './img/10.png',
-    ],
+const images = [
+  './img/1.png',
+  './img/2.png',
+  './img/3.png',
+  './img/4.png',
+  './img/5.png',
+  './img/6.png',
+  './img/7.png',
+  './img/8.png',
+  './img/9.png',
+  './img/10.png',
+];
+
+const imageCount = images.length;
+
+const getImageId = (url: string) => {
+  return url.match(/(\d+)\.png$/)?.[1] || '';
+};
+
+const App: React.FC = () => {
+  const [itemWidth, setItemWidth] = useState(130);
+  const [frameSize, setFrameSize] = useState(3);
+  const [step, setStep] = useState(3);
+  const [animationDuration, setAnimationDuration] = useState(1000);
+  const [position, setPosition] = useState(0);
+  const [isPrevButtontnActive, setIsPrevButtontnActive] = useState(false);
+  const [isNextButtonActive, setIsNextButtonActive] = useState(true);
+
+  const maxCarouselWidth = frameSize * itemWidth;
+  const actualImageWidth = maxCarouselWidth / frameSize;
+
+  const FIRST_POSITION = 0;
+  const LAST_POSITION = imageCount - frameSize;
+
+  const handlePrevButtonClick = () => {
+    setPosition(currentPosition => (position - step >= FIRST_POSITION
+      ? currentPosition - step
+      : FIRST_POSITION));
   };
 
-  render() {
-    const { images } = this.state;
+  const handleNextButtonClick = () => {
+    setPosition(currentPosition => (position + step <= LAST_POSITION
+      ? currentPosition + step
+      : LAST_POSITION));
+  };
 
-    return (
-      <div className="App">
-        {/* eslint-disable-next-line */}
-        <h1>Carousel with {images.length} images</h1>
+  useEffect(() => {
+    setIsPrevButtontnActive(position > FIRST_POSITION);
+    setIsNextButtonActive(position < LAST_POSITION);
+  }, [position]);
 
-        <Carousel />
-      </div>
-    );
-  }
-}
+  useEffect(() => { }, [
+    itemWidth,
+    frameSize,
+    step,
+    animationDuration,
+  ]);
+
+  return (
+    <div className="App">
+      <h1
+        data-cy="title"
+        className="App__title"
+      >
+        Carousel with
+        {imageCount}
+        images
+      </h1>
+
+      <Settings
+        itemWidth={itemWidth}
+        setItemWidth={setItemWidth}
+        imageCount={imageCount}
+        frameSize={frameSize}
+        step={step}
+        animationDuration={animationDuration}
+        setFrameSize={setFrameSize}
+        setStep={setStep}
+        setAnimationDuration={setAnimationDuration}
+      />
+
+      <Carousel
+        images={images}
+        position={position}
+        animationDuration={animationDuration}
+        frameSize={frameSize}
+        actualImageWidth={actualImageWidth}
+        isPrevButtonActive={isPrevButtontnActive}
+        isNextButtonActive={isNextButtonActive}
+        hadlePrevButtonClick={handlePrevButtonClick}
+        hadleNextButtonClick={handleNextButtonClick}
+        getImageId={getImageId}
+      />
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/Carousel/Carousel.tsx
@@ -0,0 +1,97 @@
+import React from 'react';
+import cn from 'classnames';
+
+import './Carousel.scss';
+
+interface Props {
+  images: string[];
+  position: number,
+  animationDuration: number,
+  frameSize: number,
+  actualImageWidth: number,
+  isPrevButtonActive: boolean,
+  isNextButtonActive: boolean,
+  hadlePrevButtonClick: () => void
+  hadleNextButtonClick: () => void
+  getImageId: (url: string) => string;
+}
+
+const Carousel: React.FC<Props> = ({
+  images,
+  position,
+  animationDuration,
+  frameSize,
+  actualImageWidth,
+  isPrevButtonActive,
+  isNextButtonActive,
+  hadlePrevButtonClick,
+  hadleNextButtonClick,
+  getImageId,
+}) => {
+  const carouselWidth = frameSize * actualImageWidth;
+  const translateCoords = position * -actualImageWidth;
+
+  return (
+    <>
+      <div className="Carousel">
+        <div
+          className="Carousel__content"
+          style={{ maxWidth: `${carouselWidth}px` }}
+        >
+          <ul
+            className="Carousel__list"
+            style={{
+              transform: `translateX(${translateCoords}px)`,
+              transition: `transform ${animationDuration}ms linear`,
+            }}
+          >
+            {images.map((image: string) => (
+              <li
+                key={getImageId(image)}
+                className="Carousel__item"
+                style={{
+                  flexBasis: `${actualImageWidth}px`,
+                }}
+              >
+                <img
+                  src={image}
+                  alt={`${getImageId(image)}`}
+                  style={{
+                    flexBasis: `${actualImageWidth}px`,
+                  }}
+                  className="Carousel__image"
+                />
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className="Carousel__buttons">
+          <button
+            type="button"
+            disabled={!isPrevButtonActive}
+            className={cn('Carousel__button__prev', {
+              'is-disabled': !isPrevButtonActive,
+            })}
+            onClick={hadlePrevButtonClick}
+          >
+            Back
+          </button>
+          <button
+            type="button"
+            disabled={!isNextButtonActive}
+            className={cn('Carousel__button__next', {
+              'is-disabled': !isNextButtonActive,
+            })}
+            onClick={hadleNextButtonClick}
+            data-cy="next"
+          >
+            Go!
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Carousel;
