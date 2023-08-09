a5983a361c616273bb6e0689a5d943522a50bec8
Vitalii Fedusov <vitalii.fedusov@gmail.com>
2023-08-09 23:40:49 +0300

    almost done

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,11 @@ import Carousel from './components/Carousel';
 
 interface State {
   images: string[];
+  step: number;
+  frameSize: number;
+  itemWidth: number;
+  animationDuration: number;
+  // infinite: boolean;
 }
 
 class App extends React.Component<{}, State> {
@@ -20,10 +25,38 @@ class App extends React.Component<{}, State> {
       './img/9.png',
       './img/10.png',
     ],
+    step: 3,
+    frameSize: 3,
+    itemWidth: 130,
+    animationDuration: 1000,
+    // infinite: false,
   };
 
   render() {
-    const { images } = this.state;
+    const {
+      images, step, frameSize, itemWidth, animationDuration,
+    } = this.state;
+
+    const handleStep
+      = (event: React.ChangeEvent<HTMLInputElement>) => this.setState({
+        step: +event.target.value,
+      });
+    const handleFrameSize
+      = (event: React.ChangeEvent<HTMLInputElement>) => this.setState({
+        frameSize: +event.target.value,
+      });
+    const handleItemWidth
+      = (event: React.ChangeEvent<HTMLInputElement>) => this.setState({
+        itemWidth: +event.target.value,
+      });
+    const handleAnimationDuration
+      = (event: React.ChangeEvent<HTMLInputElement>) => this.setState({
+        animationDuration: +event.target.value,
+      });
+    // const handleInfinite
+    //   = () => this.setState({
+    //     infinite: true,
+    //   });
 
     return (
       <div className="App">
@@ -32,7 +65,37 @@ class App extends React.Component<{}, State> {
 
         <Carousel
           images={this.state.images}
+          step={this.state.step}
+          frameSize={this.state.frameSize}
+          itemWidth={this.state.itemWidth}
+          animationDuration={this.state.animationDuration}
+          // infinite={this.state.infinite}
         />
+        <div className="input-container">
+          <h1>
+            item - width
+            <input type="text" onChange={handleItemWidth} value={itemWidth} />
+          </h1>
+          <h1>
+            frame-size
+            <input type="text" onChange={handleFrameSize} value={frameSize} />
+          </h1>
+          <h1>
+            step
+            <input type="text" onChange={handleStep} value={step} />
+          </h1>
+          <h1>
+            animation-duration
+            <input
+              type="text"
+              onChange={handleAnimationDuration}
+              value={animationDuration}
+            />
+          </h1>
+          {/* <button type="button" onClick={handleInfinite}>
+            Infinite
+          </button> */}
+        </div>
       </div>
     );
   }
--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,59 +1,53 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import './Carousel.scss';
 import cn from 'classnames';
 
 interface Props {
   images: string[],
+  step: number,
+  frameSize: number,
+  itemWidth: number,
+  animationDuration: number,
+  // infinite: boolean,
 }
 
-// interface PreparedImages {
-//   array: number[],
-//   value: number,
-//   itemWidth: number,
-//   frameSize: number,
-// }
-
-// const numbersOfImages = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-// function getPreparedImages(array: number[], value: number, itemWidth: number, frameSize: number) {
-//   const startIndex = Math.abs(value) / itemWidth;
-//   const endIndex = startIndex + frameSize / itemWidth;
-//   const preparedImages = [...array];
-
-//   return preparedImages.slice(startIndex, endIndex);
-// }
-
-const Carousel: React.FC<Props> = ({ images }) => {
-  const imageAmount = 3;
-  const itemWidth = 130;
+const Carousel: React.FC<Props> = ({
+  images,
+  step,
+  frameSize,
+  itemWidth,
+  animationDuration,
+  // infinite,
+}) => {
   const gap = 20;
-  const frameSize = imageAmount * itemWidth + (gap * (imageAmount - 1));
-  const containerWidth
-    = (itemWidth * (10 - imageAmount)) + (gap * (10 - imageAmount));
+  const totalGapValue = gap * (frameSize - 1);
+  const visibleImagesWidth = itemWidth * frameSize + totalGapValue;
 
   const [translateValue, setTranslateValue] = useState(0);
-  // const visibleImages
-  //   = getPreparedImages(numbersOfImages, translateValue, itemWidth, frameSize);
 
-  // console.log(translateValue, 'translate value');
-  // console.log(visibleImages, 'array');
+  const newTranslateValue = step * (itemWidth + gap);
+
+  const allImagesWidth = (10 * itemWidth) + (9 * gap);
+  const extremeTranslatePoint = allImagesWidth - visibleImagesWidth;
 
   const handleeClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     const currentButton = event.currentTarget;
     const currentButtonClasslist = currentButton.classList;
+    let value = 0;
 
     if (currentButtonClasslist.contains('button-prev')) {
-      const newValuePrev = translateValue + frameSize + gap;
-      const valueToSetPrev = Math.min(newValuePrev, 0);
-
-      setTranslateValue(valueToSetPrev);
+      value = Math.min(translateValue + newTranslateValue, 0);
     } else if (currentButtonClasslist.contains('button-next')) {
-      const newValueNext = translateValue - frameSize - gap;
-      const valueToSetNext = Math.max(newValueNext, (-containerWidth));
-
-      setTranslateValue(valueToSetNext);
+      value = Math.max(
+        translateValue - newTranslateValue, (-extremeTranslatePoint),
+      );
     }
+
+    setTranslateValue(value);
   };
+  console.log(translateValue, 'transl value');
+  console.log(newTranslateValue, 'new value');
+  console.log(visibleImagesWidth, 'visible');
 
   return (
     <div className="Carousel">
@@ -67,20 +61,28 @@ const Carousel: React.FC<Props> = ({ images }) => {
         >
           <div className="button-prev__text">&lt;</div>
         </button>
-        <ul className="Carousel__list" style={{ width: `${frameSize}px` }}>
+        <ul className="Carousel__list" style={{ width: `${visibleImagesWidth}px` }}>
           <div
             className="Carousel__item-wrapper"
             style={{
               transform: `translateX(${translateValue}px)`,
+              transition: `transform ${animationDuration}ms ease`,
               gap: `${gap}px`,
             }}
           >
-            {/* <li><img src="./img/1.png" alt="1" /></li> */}
             {images.map(image => (
-              <li className="Carousel__item" key={image}>
+              <li
+                className="Carousel__item"
+                key={image}
+                style={{ height: `${itemWidth}px` }}
+              >
                 <img
                   src={image}
                   alt={`${images.indexOf(image) + 1}`}
+                  style={{
+                    width: `${itemWidth}px`,
+                    height: `${itemWidth}px`,
+                  }}
                 />
               </li>
             ))}
@@ -88,7 +90,7 @@ const Carousel: React.FC<Props> = ({ images }) => {
         </ul>
         <button
           className={cn('button-next', {
-            'button-next__disabled': translateValue === -containerWidth,
+            'button-next__disabled': translateValue === -extremeTranslatePoint,
           })}
           type="button"
           data-cy="next"
@@ -96,25 +98,6 @@ const Carousel: React.FC<Props> = ({ images }) => {
         >
           <div className="button-next__text">&gt;</div>
         </button>
-      </div>
-
-      <div className="input-container">
-        <h1>
-          item - width
-          <input type="text" />
-        </h1>
-        <h1>
-          frame-size
-          <input type="text" />
-        </h1>
-        <h1>
-          step
-          <input type="text" />
-        </h1>
-        <h1>
-          animation-duration
-          <input type="text" />
-        </h1>
       </div>
     </div>
   );
