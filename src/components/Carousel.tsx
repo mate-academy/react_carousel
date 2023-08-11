0795ffa337230eef2a788d5c987bd5f5090236fb
Nadiia Romanskaya <97957968+NadiiaR@users.noreply.github.com>
2023-08-11 21:05:54 +0300

    add task solution

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,18 +1,107 @@
-import React from 'react';
+import React, { useState } from 'react';
+import cn from 'classnames';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+type Props = {
+  images: string[],
+  step: number,
+  frameSize: number,
+  itemWidth: number,
+  animationDuration: number,
+  infinite: boolean,
+};
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+const Carousel: React.FC<Props> = ({
+  images,
+  step,
+  frameSize,
+  itemWidth,
+  animationDuration,
+  infinite,
+}) => {
+  const [currentImage, setCurrentImage] = useState(1);
+  const imageRigth = (currentImage - 1) * itemWidth;
+  const imageLeft = images.length - currentImage;
+
+  const widthCarousel = frameSize * itemWidth;
+  const isEnd = currentImage === imageLeft;
+  const isStart = currentImage === 1;
+
+  const handleNextImage = () => {
+    if (!isEnd) {
+      const nextImage = currentImage + step;
+
+      setCurrentImage(nextImage > imageLeft
+        ? imageLeft
+        : nextImage);
+    } else if (infinite) {
+      setCurrentImage(1);
+    }
+  };
+
+  const handlePrevImage = () => {
+    if (!isStart) {
+      const nextImage = currentImage - step;
+
+      setCurrentImage(nextImage < 1
+        ? 1
+        : nextImage);
+    } else if (infinite) {
+      setCurrentImage(imageLeft);
+    }
+  };
+
+  return (
+    <div className="Carousel">
+      <ul
+        className="Carousel__list"
+        style={{ width: widthCarousel }}
+      >
+        {images.map((img) => (
+          <li
+            key={img}
+            className="Carousel__list--link"
+            style={{
+              transform: `translateX(-${imageRigth}px)`,
+              transition: `${animationDuration}ms`,
+            }}
+          >
+            <img
+              src={img}
+              alt={img}
+              style={{
+                width: `${itemWidth}px`,
+              }}
+            />
+          </li>
+        ))}
+      </ul>
+
+      <div className="buttons">
+        <button
+          type="button"
+          className={cn(
+            'button',
+            { 'button--disabled': (isStart && !infinite) },
+          )}
+          onClick={() => handlePrevImage()}
+        >
+          {'<<'}
+        </button>
+
+        <button
+          className={cn(
+            'button',
+            { 'button--disabled': (isEnd && !infinite) },
+          )}
+          type="button"
+          onClick={() => handleNextImage()}
+        >
+          {'>>'}
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Carousel;
