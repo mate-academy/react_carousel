c22082152acd8b1422f9eb003009524b271ac02f
JulyaPetrovskaya <119932772+JulyaPetrovskaya@users.noreply.github.com>
2023-08-17 16:34:53 +0300

    solution

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,18 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+interface Props {
+  images: string[]; // массив URL-адресов изображений
+  itemWidth: number; // ширина каждого элемента карусели, значение по умолчанию будет установлено позже
+  frameSize: number; // количество изображений, отображаемых одновременно
+  step: number; // количество изображений, прокручиваемых за один клик
+  animationDuration: number;
+  infinite: boolean; // бесконечная карусель
+}
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+const Carousel: React.FC<Props> = ({
+  images,
+  itemWidth = 130,
+  frameSize = 3,
+  step = 1,
+  animationDuration = 300,
+  infinite = false,
+}) => {
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
+  const carouselWidth = itemWidth * frameSize;
+
+  const carouselStyle = {
+    width: `${carouselWidth}px`,
+    overflow: 'hidden',
+  };
+
+  const handleNext = () => {
+    setCurrentIndex((currentI) => Math.min(currentI + step,
+      images.length - frameSize));
+  };
+
+  const handlePrev = () => {
+    setCurrentIndex((currentI) => Math.max(currentI- step, 0));
+  };
+
+  let visibleImages;
+
+  if (infinite) {
+    visibleImages = [
+      ...images.slice(-step),
+      ...images,
+      ...images.slice(0, step),
+    ];
+  } else {
+    visibleImages = images;
+  }
+
+  return (
+    <div className="Carousel" style={carouselStyle}>
+      <ul
+        className="Carousel__list"
+        style={{
+          transform: `translateX(-${
+            (currentIndex + (infinite ? step : 0)) * itemWidth
+          }px)`,
+          transition: `transform ${animationDuration}ms ease-out`,
+        }}
+      >
+        {visibleImages.map((img) => (
+          <li key={img} style={{ width: `${itemWidth}px` }}>
+            <img src={img} alt="Carousel item" style={{ width: '100%' }} />
+          </li>
+        ))}
+      </ul>
+
+      <button type="button" onClick={handlePrev}>
+        Prev
+      </button>
+
+      <button type="button" onClick={handleNext}>
+        Next
+      </button>
+    </div>
+  );
+};
 
 export default Carousel;
