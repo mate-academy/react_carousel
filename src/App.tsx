6b3b89ed696a6b76f3b08bd0c610d5c449d98f3f
Constantin <Constantin.Andreevich.Zhevzhik@gmail.com>
2023-08-24 12:43:35 +0300

    made changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
-import Carousel from './components/Carousel';
+import Carousel from './components/Carousel/Carousel';
+import Control from './components/Control/Control';
+import { Setup } from './types/setup';
 
-interface State {
-  images: string[];
-  itemWidth: number;
-  frameSize: number;
-  step: number;
-  animationDuration: number;
-}
-
-class App extends React.Component<{}, State> {
-  state = {
+const App: React.FC = () => {
+  const [setup, setSetup] = useState<Setup>({
     images: [
       './img/1.png',
       './img/2.png',
@@ -28,121 +22,51 @@ class App extends React.Component<{}, State> {
     frameSize: 3,
     step: 3,
     animationDuration: 1000,
-  };
+  });
 
-  handleInputChange = (key: keyof State) => (
-    e: React.ChangeEvent<HTMLInputElement>,
-  ) => {
-    const value = Number(e.target.value);
+  const handleInputChange = (
+    key: keyof Setup,
+  ) => (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = key === 'images'
+      ? Array.from(e.target.files || []).map(file => URL.createObjectURL(file))
+      : Number(e.target.value);
 
-    this.setState(prevState => ({
-      ...prevState,
+    setSetup(prevSetup => ({
+      ...prevSetup,
       [key]: value,
     }));
   };
 
-  render() {
-    const {
-      images, itemWidth, frameSize, step, animationDuration,
-    } = this.state;
+  const {
+    images, itemWidth, frameSize, step, animationDuration,
+  } = setup;
 
-    return (
-      <div className="App">
-        {/* eslint-disable-next-line */}
-        <h1 data-cy="title" className="title">Carousel with {images.length} images</h1>
+  return (
+    <div className="App">
+      <h1
+        data-cy="title"
+        className="title"
+      >
+        {`Carousel with ${images.length} images`}
+      </h1>
 
-        <Carousel
-          images={images}
-          itemWidth={itemWidth}
-          frameSize={frameSize}
-          step={step}
-          animationDuration={animationDuration}
-        />
+      <Carousel
+        images={images}
+        itemWidth={itemWidth}
+        frameSize={frameSize}
+        step={step}
+        animationDuration={animationDuration}
+      />
 
-        <div className="Control">
-          <h2 className="Control__title">Control panel</h2>
-          <div className="Control__container">
-            <div className="Control__item">
-              <label
-                htmlFor="itemWidth"
-                className="Control__label"
-              >
-                Item Width:
-              </label>
-              <input
-                type="number"
-                id="itemWidth"
-                className="Control__input"
-                name="itemWidth"
-                min="130"
-                max="260"
-                step="10"
-                value={itemWidth}
-                onChange={this.handleInputChange('itemWidth')}
-              />
-            </div>
-            <div className="Control__item">
-              <label
-                htmlFor="frameSize"
-                className="Control__label"
-              >
-                Frame Size:
-              </label>
-              <input
-                type="number"
-                id="frameSize"
-                className="Control__input"
-                name="frameSize"
-                min="1"
-                max="10"
-                step="1"
-                value={frameSize}
-                onChange={this.handleInputChange('frameSize')}
-              />
-            </div>
-            <div className="Control__item">
-              <label
-                htmlFor="step"
-                className="Control__label"
-              >
-                Step:
-              </label>
-              <input
-                type="number"
-                id="step"
-                className="Control__input"
-                name="step"
-                min="1"
-                max="10"
-                step="1"
-                value={step}
-                onChange={this.handleInputChange('step')}
-              />
-            </div>
-            <div className="Control__item">
-              <label
-                htmlFor="animationDuration"
-                className="Control__label"
-              >
-                Duration:
-              </label>
-              <input
-                type="number"
-                id="animationDuration"
-                className="Control__input"
-                name="animationDuration"
-                min="500"
-                max="5000"
-                step="500"
-                value={animationDuration}
-                onChange={this.handleInputChange('animationDuration')}
-              />
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
+      <Control
+        itemWidth={setup.itemWidth}
+        frameSize={setup.frameSize}
+        step={setup.step}
+        animationDuration={setup.animationDuration}
+        handleInputChange={handleInputChange}
+      />
+    </div>
+  );
+};
 
 export default App;
