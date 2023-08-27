acb0e0629fc07bdac535692efceb7c29a8ae5af5
Dmytro <dmytro2105@gmail.com>
2023-08-27 16:43:54 +0300

    Carousel

--- src/App.tsx
+++ src/App.tsx
@@ -3,34 +3,111 @@ import './App.scss';
 import Carousel from './components/Carousel';
 
 interface State {
-  images: string[];
+  itemWidth: number,
+  frameSize: number,
+  step: number,
+  animationDuration: number,
 }
+
+const images: string[] = [
+  './img/1.png',
+  './img/2.png',
+  './img/3.png',
+  './img/4.png',
+  './img/5.png',
+  './img/6.png',
+  './img/7.png',
+  './img/8.png',
+  './img/9.png',
+  './img/10.png',
+];
 
 class App extends React.Component<{}, State> {
   state = {
-    images: [
-      './img/1.png',
-      './img/2.png',
-      './img/3.png',
-      './img/4.png',
-      './img/5.png',
-      './img/6.png',
-      './img/7.png',
-      './img/8.png',
-      './img/9.png',
-      './img/10.png',
-    ],
+    itemWidth: 130,
+    frameSize: 3,
+    step: 3,
+    animationDuration: 1000,
   };
 
   render() {
-    const { images } = this.state;
+    const {
+      itemWidth, frameSize, step, animationDuration,
+    } = this.state;
 
     return (
       <div className="App">
         {/* eslint-disable-next-line */}
-        <h1>Carousel with {images.length} images</h1>
+        <h1 data-cy="title">Carousel with {images.length} images</h1>
 
-        <Carousel />
+        <div className="parametr-editor">
+          <label htmlFor="itemId">
+            Item width:
+
+            <input
+              id="widthId"
+              type="number"
+              value={itemWidth}
+              min={130}
+              max={1300}
+              onChange={ev => {
+                this.setState({ itemWidth: +ev.target.value });
+              }}
+            />
+          </label>
+
+          <label htmlFor="frameId">
+            Displayed images number
+
+            <input
+              id="frameSize"
+              type="number"
+              value={frameSize}
+              min={1}
+              max={10}
+              onChange={ev => {
+                this.setState({ frameSize: +ev.target.value });
+              }}
+            />
+          </label>
+
+          <label htmlFor="stepId">
+            Step:
+
+            <input
+              id="step"
+              type="number"
+              value={step}
+              min={1}
+              max={10}
+              onChange={ev => {
+                this.setState({ step: +ev.target.value });
+              }}
+            />
+          </label>
+
+          <label htmlFor="itemWidth">
+            Animation duration:
+
+            <input
+              id="widthId"
+              type="number"
+              value={animationDuration}
+              min={500}
+              onChange={ev => {
+                this.setState({ animationDuration: +ev.target.value });
+              }}
+            />
+          </label>
+        </div>
+
+        <Carousel
+          images={images}
+          step={step}
+          frameSize={frameSize}
+          itemWidth={itemWidth}
+          animationDuration={animationDuration}
+        />
       </div>
     );
   }
--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,18 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+type Props = {
+  images: string[],
+  itemWidth: number,
+  frameSize: number,
+  step: number,
+  animationDuration: number,
+};
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+type Direction = 'forward' | 'backward';
+
+const Carousel: React.FC<Props> = ({
+  images, itemWidth, frameSize, step, animationDuration,
+}) => {
+  const [startIndex, setStartIndex] = useState(0);
+
+  const handleMove = (direction: Direction) => {
+    const stepShift = direction === 'forward' ? step : -step;
+
+    const lastImageIndex = startIndex + stepShift;
+
+    if (lastImageIndex >= images.length - frameSize) {
+      setStartIndex(images.length - frameSize);
+    } else if (lastImageIndex <= 0) {
+      setStartIndex(0);
+    } else {
+      setStartIndex(lastImageIndex);
+    }
+  };
+
+  const styleImages = {
+    transitionDuration: `${animationDuration}ms`,
+    transform: `translateX(-${startIndex * itemWidth}px)`,
+  };
+
+  return (
+    <div
+      className="Carousel"
+      style={{ width: `${itemWidth * frameSize}px` }}
+    >
+      <ul className="Carousel__list">
+        {images.map(image => (
+          <li
+            key={image}
+            className="Carousel__emoji"
+            style={styleImages}
+          >
+            <img
+              src={image}
+              width={itemWidth}
+              alt={image
+                .substring(image.lastIndexOf('/') + 1, image.lastIndexOf('.'))}
+            />
+          </li>
+        ))}
+      </ul>
+
+      <button
+        type="button"
+        onClick={() => handleMove('backward')}
+        disabled={startIndex === 0}
+      >
+        Prev
+      </button>
+
+      <button
+        type="button"
+        data-cy="next"
+        onClick={() => handleMove('forward')}
+        disabled={startIndex === images.length - frameSize}
+      >
+        Next
+      </button>
+    </div>
+  );
+};
 
 export default Carousel;
