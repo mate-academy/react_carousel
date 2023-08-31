004863c996d7e0a89c3180b812e4e5bb13bbd09b
Andrey Bondar <andrey_bondar@outlook.com>
2023-08-31 16:23:13 +0300

    carousel develop

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,11 @@ import Carousel from './components/Carousel';
 
 interface State {
   images: string[];
+  width: number;
+  frameSize: number;
+  step: number;
+  animationDuration: number;
+  infinite: boolean;
 }
 
 class App extends React.Component<{}, State> {
@@ -20,17 +25,110 @@ class App extends React.Component<{}, State> {
       './img/9.png',
       './img/10.png',
     ],
+    width: 130,
+    frameSize: 3,
+    step: 3,
+    animationDuration: 1000,
+    infinite: false,
+  };
+
+  handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      name,
+      value,
+      type,
+      checked,
+    } = event.target;
+    const inputValue = type === 'checkbox' ? checked : Number(value);
+
+    this.setState({
+      [name]: inputValue,
+    } as unknown as Pick<State, keyof State>);
   };
 
   render() {
-    const { images } = this.state;
+    const {
+      images,
+      width,
+      frameSize,
+      step,
+      animationDuration,
+      infinite,
+    } = this.state;
+
+    const mainPageBoxWidth = width * frameSize;
 
     return (
-      <div className="App">
+      <div className="App" style={{ width: mainPageBoxWidth }}>
         {/* eslint-disable-next-line */}
-        <h1>Carousel with {images.length} images</h1>
-
-        <Carousel />
+        <h1 data-cy="title">Carousel with {images.length} images</h1>
+        <div className="inputContainer">
+          <label htmlFor="width" className="input-wrapp">
+            Image size (px):
+            <input
+              type="number"
+              name="width"
+              id="width"
+              value={width}
+              onChange={this.handleInputChange}
+              min={130}
+              max={230}
+            />
+          </label>
+          <label htmlFor="frameSize" className="input-wrapp">
+            Frame size (px):
+            <input
+              type="number"
+              name="frameSize"
+              id="frameSize"
+              value={frameSize}
+              onChange={this.handleInputChange}
+              min={1}
+              max={10}
+            />
+          </label>
+          <label htmlFor="step" className="input-wrapp">
+            Step:
+            <input
+              type="number"
+              name="step"
+              id="step"
+              onChange={this.handleInputChange}
+              value={step}
+              min={1}
+              max={10}
+            />
+          </label>
+          <label htmlFor="animationDuration" className="input-wrapp">
+            Animation duration (ms):
+            <input
+              type="number"
+              name="animationDuration"
+              id="animationDuration"
+              value={animationDuration}
+              step="100"
+              onChange={this.handleInputChange}
+            />
+          </label>
+          <label htmlFor="infinite" className="input-wrapp">
+            Infinite:
+            <input
+              type="checkbox"
+              id="infinite"
+              name="infinite"
+              checked={infinite}
+              onChange={this.handleInputChange}
+            />
+          </label>
+        </div>
+        <Carousel
+          images={images}
+          imgWidth={width}
+          frameWidth={frameSize}
+          scrollStep={step}
+          animation={animationDuration}
+          infinite={infinite}
+        />
       </div>
     );
   }
--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,18 +1,146 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+type Props = {
+  images: string[];
+  imgWidth: number;
+  frameWidth: number;
+  scrollStep: number;
+  animation: number;
+  infinite: boolean;
+};
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+type CheckPosition = (
+  direction: 'right' | 'left',
+  chain: boolean,
+) => void;
+
+type ActiveButton = (
+  position: number,
+  chain: boolean,
+) => boolean;
+
+const Carousel: React.FC<Props> = ({
+  images,
+  imgWidth,
+  frameWidth,
+  scrollStep,
+  animation,
+  infinite,
+}) => {
+  const [scrollImage, setScrollImage] = useState(0);
+  const frameWidthValue = frameWidth * imgWidth;
+  const finalPosition = -130 * (10 - frameWidth);
+
+  const checkPosition: CheckPosition = (direction, chain) => {
+    if (direction === 'right') {
+      const scrollRight = scrollImage + (0 - scrollStep * imgWidth);
+
+      if (chain && scrollImage === finalPosition) {
+        setScrollImage(0);
+
+        return;
+      }
+
+      if (scrollRight < finalPosition) {
+        setScrollImage(finalPosition);
+      } else {
+        setScrollImage(scrollRight);
+      }
+    }
+
+    if (direction === 'left') {
+      const scrollLeft = scrollImage + scrollStep * imgWidth;
+
+      if (chain && scrollImage === 0) {
+        setScrollImage(finalPosition);
+
+        return;
+      }
+
+      if (scrollLeft > 0) {
+        setScrollImage(0);
+      } else {
+        setScrollImage(scrollLeft);
+      }
+    }
+  };
+
+  const isActiveLeftButton: ActiveButton = (position, chain) => {
+    if (chain) {
+      return false;
+    }
+
+    if (position === 0) {
+      return true;
+    }
+
+    return false;
+  };
+
+  const isActiveRightButton: ActiveButton = (position, chain) => {
+    if (chain) {
+      return false;
+    }
+
+    if (position === finalPosition) {
+      return true;
+    }
+
+    return false;
+  };
+
+  const styles = {
+    carouselListContainer: {
+      width: `${frameWidthValue}px`,
+    },
+    carouselList: {
+      transition: `transform ${animation}ms ease-in-out`,
+      transform: `translateX(${scrollImage}px)`,
+    },
+  };
+
+  return (
+    <div className="Carousel">
+      <div
+        className="Carousel__list-container"
+        style={styles.carouselListContainer}
+      >
+        <ul
+          className="Carousel__list"
+          style={styles.carouselList}
+        >
+          {images.map((img, imgIndex) => (
+            <li key={img}>
+              <img
+                style={{ width: imgWidth }}
+                src={img}
+                alt={String(imgIndex + 1)}
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="button-container">
+        <button
+          type="button"
+          onClick={() => checkPosition('left', infinite)}
+          disabled={isActiveLeftButton(scrollImage, infinite)}
+        >
+          Prev
+        </button>
+        <button
+          data-cy="next"
+          type="button"
+          onClick={() => checkPosition('right', infinite)}
+          disabled={isActiveRightButton(scrollImage, infinite)}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Carousel;
