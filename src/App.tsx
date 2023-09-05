0be861ef241ee61855486d44cbb3ec2080b822d4
Oleksandr Chernikov <pjatochkin2013@gmail.com>
2023-09-05 15:30:21 +0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,71 +1,45 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.scss';
 import Carousel from './components/Carousel';
 
-const App: React.FC = () => {
-  const [images] = useState([
-    './img/1.png',
-    './img/2.png',
-    './img/3.png',
-    './img/4.png',
-    './img/5.png',
-    './img/6.png',
-    './img/7.png',
-    './img/8.png',
-    './img/9.png',
-    './img/10.png',
-  ]);
+interface State {
+  images: string[];
+}
 
-  const [stepChange, setStepChange] = useState(3);
-  const [frameSizeChange, setFrameSizeChange] = useState(3);
-  const [itemWidthChange, setItemWidthChange] = useState(130);
-  const [duration, setDuration] = useState(1000);
+class App extends React.Component<{}, State> {
+  state = {
+    images: [
+      './img/1.png',
+      './img/2.png',
+      './img/3.png',
+      './img/4.png',
+      './img/5.png',
+      './img/6.png',
+      './img/7.png',
+      './img/8.png',
+      './img/9.png',
+      './img/10.png',
+    ],
+  };
 
-  return (
-    <div className="App">
-      <h1 className="App__title">{`Carousel with ${images.length} images`}</h1>
+  render() {
+    const { images } = this.state;
 
-      <label htmlFor="step">Choose a step:</label>
-      <select
-        id="step"
-        onChange={(e) => setStepChange(parseInt(e.target.value, 10))}
-      >
-        <option value="3">3</option>
-        <option value="2">2</option>
-      </select>
-      <label htmlFor="frameSize">Choose a size frame:</label>
-      <select
-        id="frameSize"
-        onChange={(e) => setFrameSizeChange(parseInt(e.target.value, 10))}
-      >
-        <option value="3">3</option>
-        <option value="2">2</option>
-      </select>
-      <label htmlFor="frameSize">Choose a width:</label>
-      <select
-        id="frameSize"
-        onChange={(e) => setItemWidthChange(parseInt(e.target.value, 10))}
-      >
-        <option value="130">130</option>
-      </select>
-      <label htmlFor="frameSize">Choose a duration:</label>
-      <select
-        id="frameSize"
-        onChange={(e) => setDuration(parseInt(e.target.value, 10))}
-      >
-        <option value="1000">1000</option>
-        <option value="1200">1200</option>
-      </select>
+    return (
+      <div className="App">
+        <h1 className="App__title">{`Carousel with ${images.length} images`}</h1>
 
-      <Carousel
-        images={images}
-        step={stepChange}
-        frameSize={frameSizeChange}
-        itemWidth={itemWidthChange}
-        animationDuration={duration}
-      />
-    </div>
-  );
-};
+        <Carousel
+          images={images}
+          step={3}
+          frameSize={3}
+          itemWidth={130}
+          animationDuration={1000}
+          infinite={false}
+        />
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -7,6 +7,7 @@ interface Props {
   frameSize: number;
   itemWidth: number;
   animationDuration: number;
+  infinite: boolean;
 }
 
 const Carousel: React.FC<Props> = ({
@@ -15,84 +16,148 @@ const Carousel: React.FC<Props> = ({
   frameSize,
   itemWidth,
   animationDuration,
+  infinite,
 }) => {
+  const [stepChange, setStepChange] = useState(step);
+  const [frameSizeChange, setFrameSizeChange] = useState(frameSize);
+  const [itemWidthChange, setItemWidthChange] = useState(itemWidth);
+  const [duration, setDuration] = useState(animationDuration);
+  const [infiniteImg, setInfiniteImg] = useState(infinite);
   const [visibleIndex, setVisibleIndex] = useState(0);
-  const [translateX, setTranslateX] = useState(0);
-  const visiblePhoto = images.slice(visibleIndex, visibleIndex + step);
-  let sizeTranslate = 0;
-
-  if (step === 2 || frameSize === 2) {
-    sizeTranslate = itemWidth * frameSize;
-  } else {
-    sizeTranslate = itemWidth * frameSize;
-  }
 
   const handleNext = () => {
-    if (visibleIndex + step < images.length) {
-      setVisibleIndex(visibleIndex + step);
-      setTranslateX(translateX + sizeTranslate);
-    }
+    const newIndex = (visibleIndex + stepChange) % images.length;
 
-    if (visibleIndex + step * 2 === images.length + 2) {
-      setVisibleIndex((visibleIndex - 2) + (step));
-      setTranslateX(translateX + 130);
+    setVisibleIndex(newIndex);
+
+    if (images.length - (visibleIndex + frameSizeChange) < stepChange) {
+      setVisibleIndex((images.length - stepChange));
     }
   };
 
   const handlePrev = () => {
-    if (visibleIndex - step >= 0) {
-      setVisibleIndex(visibleIndex - step);
-      setTranslateX(translateX - sizeTranslate);
-    }
+    const newIndex = (visibleIndex - stepChange);
 
-    if (visibleIndex - step === -2) {
-      setVisibleIndex((visibleIndex + 2) - (step));
-      setTranslateX(translateX - 130);
+    setVisibleIndex(newIndex);
+
+    if ((visibleIndex - frameSizeChange) < 0) {
+      setVisibleIndex((0));
     }
   };
 
+  const handleFrameSizeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newFrameSize = parseInt(e.target.value, 10);
+
+    setFrameSizeChange(newFrameSize);
+    setVisibleIndex(0);
+  };
+
   useEffect(() => {
-    const wrapElement = document.querySelector('.Wrap') as HTMLElement;
-    const imgElem = document.querySelector('.Carousel__img') as HTMLElement;
+    const wrapElement = document.querySelector('.Carousel') as HTMLElement;
+    const imgElements = document.querySelectorAll(
+      '.Carousel__img',
+    ) as NodeListOf<HTMLElement>;
 
-    imgElem.style.width = `${itemWidth}px`;
-    imgElem.style.height = `${itemWidth}px`;
+    imgElements.forEach((imgElem) => {
+      const newElem = imgElem;
 
-    if (step === 2 || frameSize === 2) {
-      wrapElement.style.width = '260px';
-    } else {
-      wrapElement.style.width = '390px';
-    }
+      newElem.style.width = `${itemWidthChange}px`;
+      newElem.style.height = `${itemWidthChange}px`;
+    });
 
     if (wrapElement) {
-      wrapElement.style.transform = `translateX(${translateX}px)`;
-      wrapElement.style.transition = `all ${animationDuration}ms ease-in-out`;
+      wrapElement.style.transition = `all ${duration}ms ease-in-out`;
     }
-  }, [translateX]);
+  }, [itemWidthChange, duration]);
+
+  const visiblePhoto = images.slice(
+    visibleIndex, visibleIndex + frameSizeChange,
+
+  );
 
   return (
     <div className="MainWrap">
-      <>
-        <button type="button" className={`Carousel__button button--left ${visibleIndex === 0 && 'disabled'}`} onClick={handlePrev}>Prev</button>
-        <div className="Carousel">
-          <div className="Wrap">
-            <ul className="Carousel__list">
-              {visiblePhoto.map((elem, index) => (
-                <li key={elem}>
-                  <img src={elem} alt={`${index + 1}`} className="Carousel__img" />
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
+      <div className="Carousel">
+        <ul className="Carousel__list">
+          {visiblePhoto.map((elem, index) => (
+            <li key={elem}>
+              <img key={elem} src={elem} alt={`${index + 1}`} className="Carousel__img" />
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="Carousel__buttons">
         <button
           type="button"
-          className={`Carousel__button button--right ${visibleIndex === (images.length - step) && 'disabled'}`}
-          onClick={handleNext}
+          className="Carousel__button"
+          onClick={handlePrev}
+          disabled={visibleIndex === 0 && !infiniteImg}
         >
-          Next
+          {'< Prev'}
         </button>
-      </>
+        <button
+          type="button"
+          className="Carousel__button"
+          onClick={handleNext}
+          disabled={visibleIndex >= images.length - stepChange && !infiniteImg}
+        >
+          {'Next >'}
+        </button>
+      </div>
+      <div className="Carousel__interactionForms">
+        <form>
+          <label htmlFor="step">Choose a step:</label>
+          <input
+            id="step"
+            min={2}
+            max={4}
+            type="number"
+            onChange={(e) => {
+              setStepChange(parseInt(e.target.value, 10));
+              setVisibleIndex(0);
+            }}
+            value={stepChange}
+          />
+          <label htmlFor="frameSize">Choose a size frame:</label>
+          <input
+            id="frameSize"
+            min={2}
+            max={5}
+            type="number"
+            onChange={handleFrameSizeChange}
+            value={frameSizeChange}
+          />
+          <label htmlFor="itemWidth">Choose a width item:</label>
+          <input
+            id="itemWidth"
+            min={130}
+            max={150}
+            type="number"
+            onChange={(e) => setItemWidthChange(parseInt(e.target.value, 10))}
+            value={itemWidthChange}
+          />
+          <label htmlFor="duration">Choose a duration animation:</label>
+          <input
+            id="duration"
+            type="number"
+            min={1000}
+            max={5000}
+            step={50}
+            onChange={(e) => setDuration(parseInt(e.target.value, 10))}
+            value={duration}
+          />
+          <label htmlFor="checkbox">infinite</label>
+          <input
+            id="checkbox"
+            type="checkbox"
+            onChange={(e) => {
+              setInfiniteImg(e.target.checked);
+              setVisibleIndex(0);
+            }}
+            checked={infiniteImg}
+          />
+        </form>
+      </div>
     </div>
   );
 };
