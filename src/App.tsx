008a980232669dd73928bbecd4d5094f93df38d7
Konrad <konrad.domalewski@gmail.com>
2023-09-10 13:28:36 +0200

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,14 @@ import Carousel from './components/Carousel';
 
 interface State {
   images: string[];
+  itemWidth: number;
+  frameSize: number;
+  step: number;
+  animationDuration: number;
 }
 
 class App extends React.Component<{}, State> {
-  state = {
+  state: State = {
     images: [
       './img/1.png',
       './img/2.png',
@@ -20,17 +24,77 @@ class App extends React.Component<{}, State> {
       './img/9.png',
       './img/10.png',
     ],
+    itemWidth: 130,
+    frameSize: 3,
+    step: 3,
+    animationDuration: 1000,
   };
 
+  handleInputChange(name: keyof State, value: number | unknown) {
+    this.setState({ [name]: value } as Pick<State, keyof State>);
+  }
+
   render() {
-    const { images } = this.state;
+    const {
+      images, itemWidth, frameSize, step, animationDuration,
+    } = this.state;
 
     return (
       <div className="App">
         {/* eslint-disable-next-line */}
-        <h1>Carousel with {images.length} images</h1>
+        <h1 data-cy="title">Carousel with {images.length} images</h1>
 
-        <Carousel />
+        <div>
+          <label>
+            Item Width:
+            <input
+              type="number"
+              name="itemWidth"
+              value={itemWidth}
+              // eslint-disable-next-line max-len
+              onChange={(e) => this.handleInputChange('itemWidth', parseInt(e.target.value, 10))}
+            />
+          </label>
+          <label>
+            Frame Size:
+            <input
+              type="number"
+              name="frameSize"
+              value={frameSize}
+              // eslint-disable-next-line max-len
+              onChange={(e) => this.handleInputChange('frameSize', parseInt(e.target.value, 10))}
+            />
+          </label>
+          <label>
+            Step:
+            <input
+              type="number"
+              name="step"
+              value={step}
+              // eslint-disable-next-line max-len
+              onChange={(e) => this.handleInputChange('step', parseInt(e.target.value, 10))}
+            />
+          </label>
+          <label>
+            Animation Duration (ms):
+            <input
+              type="number"
+              name="animationDuration"
+              value={animationDuration}
+              // eslint-disable-next-line max-len
+              onChange={(e) => this.handleInputChange('animationDuration', parseInt(e.target.value, 10))}
+            />
+          </label>
+        </div>
+
+        <Carousel
+          images={images}
+          itemWidth={itemWidth}
+          frameSize={frameSize}
+          step={step}
+          animationDuration={animationDuration}
+          infinite={false}
+        />
       </div>
     );
   }
