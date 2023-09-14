ea9e632e9673b3a9b3607fdb6b0ffda5b7101693
andrew gidrovich <mr.androvich55555@gmail.com>
2023-09-14 20:45:02 +0300

    add task solution

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,18 +1,68 @@
-import React from 'react';
+/* eslint-disable react/require-default-props */
+import React, { useState, useEffect } from 'react';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+interface CarouselProps {
+  images: string[];
+  itemWidth?: number;
+  frameSize?: number;
+  step?: number;
+  animationDuration?: number;
+  infinite?: boolean;
+}
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+const Carousel: React.FC<CarouselProps> = ({
+  images,
+  itemWidth = 130,
+  frameSize = 3,
+  step = 3,
+  animationDuration = 1000,
+  infinite = false,
+}) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleNextClick = () => {
+    if (currentIndex + step < images.length) {
+      setCurrentIndex(currentIndex + step);
+    } else if (infinite) {
+      setCurrentIndex(0);
+    }
+  };
+
+  const handlePrevClick = () => {
+    if (currentIndex - step >= 0) {
+      setCurrentIndex(currentIndex - step);
+    } else if (infinite) {
+      setCurrentIndex(images.length - frameSize);
+    }
+  };
+
+  useEffect(() => {
+    const interval = setInterval(handleNextClick, animationDuration);
+
+    return () => clearInterval(interval);
+  }, [currentIndex, handleNextClick, animationDuration]);
+
+  const visibleImages = images.slice(currentIndex, currentIndex + frameSize);
+
+  return (
+    <div className="Carousel">
+      <ul className="Carousel__list">
+        {visibleImages.map((image) => (
+          <li key={image}>
+            <img src={image} alt={`Зображення ${image}`} style={{ width: itemWidth }} />
+          </li>
+        ))}
+      </ul>
+
+      <button type="button" onClick={handlePrevClick} data-cy="prevButton">
+        prev
+      </button>
+      <button type="button" onClick={handleNextClick} data-cy="nextButton">
+        next
+      </button>
+    </div>
+  );
+};
 
 export default Carousel;
