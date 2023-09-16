0c7b5f3d81f8ec705c8f4925ef9775dc6444c833
OlegRak86 <rakolegit@gmail.com>
2023-09-16 15:18:04 +0300

    react_carousel

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import './App.scss';
 import Carousel from './components/Carousel';
-
-interface State {
-  images: string[];
-}
+import { State } from './interface';
 
 class App extends React.Component<{}, State> {
   state = {
@@ -20,17 +17,123 @@ class App extends React.Component<{}, State> {
       './img/9.png',
       './img/10.png',
     ],
+    frameSize: 3,
+    itemWidth: 130,
+    step: 3,
+    animationDuration: 1000,
+    infinite: false,
   };
 
   render() {
-    const { images } = this.state;
+    const {
+      images,
+      frameSize,
+      itemWidth,
+      step,
+      animationDuration,
+      infinite,
+    } = this.state;
 
     return (
       <div className="App">
-        {/* eslint-disable-next-line */}
-        <h1>Carousel with {images.length} images</h1>
 
-        <Carousel />
+        <Carousel
+          images={images}
+          frameSize={frameSize}
+          itemWidth={itemWidth}
+          step={step}
+          animationDuration={animationDuration}
+          infinite={infinite}
+        />
+        <div className="input_container">
+          <label
+            htmlFor="frame"
+          >
+            Frame Size
+            <input
+              id="frame"
+              type="number"
+              min={1}
+              max={10}
+              value={frameSize}
+              onChange={(event) => {
+                this.setState({
+                  frameSize: +event.target.value,
+                });
+              }}
+            />
+          </label>
+          <label
+            htmlFor="width"
+          >
+            Item Width
+            <input
+              id="width"
+              type="number"
+              min={130}
+              max={260}
+              step={1}
+              value={itemWidth}
+              onChange={(event) => {
+                this.setState({
+                  itemWidth: +event.target.value,
+                });
+              }}
+            />
+          </label>
+
+          <label
+            htmlFor="step"
+          >
+            Step
+            <input
+              id="step"
+              type="number"
+              min={1}
+              max={10}
+              step={1}
+              value={step}
+              onChange={(event) => {
+                this.setState({
+                  step: +event.target.value,
+                });
+              }}
+            />
+          </label>
+
+          <label
+            htmlFor="delay"
+          >
+            Delay
+            <input
+              id="delay"
+              type="number"
+              min={1}
+              step={10}
+              value={animationDuration}
+              onChange={(event) => {
+                this.setState({
+                  animationDuration: +event.target.value,
+                });
+              }}
+            />
+          </label>
+
+          <label
+            htmlFor="infinite"
+          >
+            Infinite
+            <input
+              type="checkbox"
+              id="infinite"
+              onChange={(event) => {
+                this.setState({
+                  infinite: event.target.checked,
+                });
+              }}
+            />
+          </label>
+        </div>
       </div>
     );
   }
