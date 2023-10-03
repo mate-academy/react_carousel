53d1ef195f4c14d4bf14cdd460f6f57259cb498c
stunnerboyone <anatolii.polishchuk@belkins.io>
2023-10-03 23:51:16 +0300

    carousel

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,18 +1,91 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+type Props = {
+  images: string[];
+  itemWidth: number;
+  frameSize: number;
+  step: number;
+  animationDuration: number;
+  infinite: boolean;
+};
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+const Carousel: React.FC<Props> = ({
+  images,
+  itemWidth,
+  frameSize,
+  step,
+  animationDuration,
+  infinite,
+}) => {
+  const [itemsOnPage, setitemsOnPage] = useState(0);
+
+  const maxItems = (images.length - frameSize) * itemWidth;
+
+  const getPrevImage = () => {
+    setitemsOnPage(Math.min(itemsOnPage + itemWidth * step, 0));
+
+    if (itemsOnPage === 0) {
+      setitemsOnPage(maxItems);
+    }
+  };
+
+  const getNextImage = () => {
+    setitemsOnPage(Math.max(itemsOnPage - itemWidth * step, -maxItems));
+
+    if (itemsOnPage === -maxItems && infinite) {
+      setitemsOnPage(0);
+    }
+  };
+
+  const frameWith = frameSize * itemWidth;
+
+  return (
+    <div className="carousel">
+      <ul
+        className="carousel__list"
+        style={{ width: `${frameWith}px` }}
+      >
+        {images.map(image => (
+          <li
+            className="carousel_item"
+            key={image}
+            style={{
+              transform: `translateX(${itemsOnPage}px)`,
+              transition: `${animationDuration}ms`,
+            }}
+          >
+            <img
+              src={image}
+              alt={`${image}`}
+              style={{ width: `${itemWidth}px` }}
+            />
+          </li>
+        ))}
+      </ul>
+
+      <div className="carousel__buttons">
+        <button
+          type="button"
+          className="carousel__button"
+          onClick={getPrevImage}
+          disabled={!itemsOnPage}
+        >
+          Prev
+        </button>
+
+        <button
+          type="button"
+          className="carousel__button"
+          onClick={getNextImage}
+          data-cy="next"
+          disabled={itemsOnPage === -maxItems && !infinite}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Carousel;
