cafe9ccc4cf8a82cc18a6faf9aecc1b7623b0ed7
Anton Bilous <toxanteloxran@gmail.com>
2023-10-06 15:31:57 +0300

    add

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,19 @@ import './App.scss';
 import Carousel from './components/Carousel';
 
 interface State {
+  itemWidth: number;
+  frameSize: number;
+  step: number;
+  animationDuration: number;
   images: string[];
 }
 
 class App extends React.Component<{}, State> {
   state = {
+    itemWidth: 130,
+    frameSize: 3,
+    step: 3,
+    animationDuration: 1000,
     images: [
       './img/1.png',
       './img/2.png',
@@ -22,15 +30,70 @@ class App extends React.Component<{}, State> {
     ],
   };
 
+  itemWidth = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ itemWidth: +event.target.value });
+  };
+
+  frameSize = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ frameSize: +event.target.value });
+  };
+
+  step = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ step: +event.target.value });
+  };
+
+  animationDuration = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ animationDuration: +event.target.value });
+  };
+
+  chooseInput = (data: string, event: React.ChangeEvent<HTMLInputElement>) => {
+    switch (data) {
+      case 'itemWidth':
+        return this.itemWidth(event);
+      case 'frameSize':
+        return this.frameSize(event);
+      case 'step':
+        return this.step(event);
+      default:
+        return this.animationDuration(event);
+    }
+  };
+
   render() {
     const { images } = this.state;
+    const dataInput = ['itemWidth', 'frameSize', 'step', 'animationDuration'];
 
     return (
       <div className="App">
         {/* eslint-disable-next-line */}
-        <h1>Carousel with {images.length} images</h1>
+        <h1 data-cy="title">Carousel with {images.length} images</h1>
 
-        <Carousel />
+        <ul>
+          {dataInput.map(data => (
+            <li>
+              <label htmlFor={data}>
+                {`${data}: `}
+              </label>
+
+              <input
+                type="text"
+                id={data}
+                placeholder={`Enter a ${data}`}
+                value={this.state[data as keyof State]}
+                onChange={event => this.chooseInput(data, event)}
+              />
+            </li>
+          ))}
+        </ul>
+
+        <Carousel
+          images={this.state.images}
+          step={this.state.step}
+          frameSize={this.state.frameSize}
+          itemWidth={this.state.itemWidth}
+          animationDuration={this.state.animationDuration}
+          // infinite={false}
+        />
       </div>
     );
   }
--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,18 +1,99 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+type CarouselProps = {
+  images: string[];
+  step: number;
+  frameSize: number;
+  itemWidth: number;
+  animationDuration: number;
+  // infinite: boolean;
+};
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+const Carousel: React.FC<CarouselProps> = ({
+  images,
+  step,
+  frameSize,
+  itemWidth,
+  animationDuration,
+  // infinite,
+}) => {
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  const scrollPositionLeft = () => {
+    let newPositoin = scrollPosition + itemWidth * step;
+
+    if (newPositoin > 0) {
+      newPositoin = 0;
+    }
+
+    setScrollPosition(newPositoin);
+  };
+
+  const scrollPositionRight = () => {
+    let newPositoin = scrollPosition - itemWidth * step;
+
+    if ((newPositoin - step * itemWidth) < -(itemWidth * images.length)) {
+      newPositoin = itemWidth * -(images.length - step);
+    }
+
+    setScrollPosition(newPositoin);
+  };
+
+  return (
+    <div
+      className="Container"
+      style={{
+        width: `${frameSize * itemWidth}px`,
+        position: 'relative',
+      }}
+    >
+      <div
+        className="Carousel"
+        style={{
+          width: `${frameSize * itemWidth}px`,
+        }}
+      >
+        <ul
+          className="Carousel__list"
+          style={{
+            transform: `translateX(${scrollPosition}px)`,
+            transition: `transform ${animationDuration}ms ease`,
+          }}
+        >
+          {images.map(image => (
+            <li>
+              <img
+                src={image}
+                alt="Smile"
+                style={{
+                  width: `${itemWidth}px`,
+                }}
+              />
+            </li>
+          ))}
+        </ul>
+
+      </div>
+      <button
+        type="button"
+        onClick={scrollPositionLeft}
+      >
+        &#8592;
+      </button>
+      <button
+        type="button"
+        data-cy="next"
+        onClick={scrollPositionRight}
+        style={{
+          position: 'absolute',
+          right: '0',
+        }}
+      >
+        &#8594;
+      </button>
+    </div>
+  );
+};
 
 export default Carousel;
