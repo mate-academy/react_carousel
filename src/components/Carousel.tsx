48732e85fa3d8800a62749e7bf93d70775c87027
Sergio47ua <serhiivoitykappleid@gmail.com>
2023-10-10 15:35:00 +0200

    add task solution

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,18 +1,89 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+interface CarouselProps {
+  images: string[];
+  step: number;
+  frameSize: number;
+  itemWidth: number;
+  animationDuration: number;
+  infinite: boolean;
+}
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+const Carousel: React.FC<CarouselProps> = ({
+  images,
+  step,
+  frameSize,
+  itemWidth,
+  animationDuration,
+  infinite,
+}) => {
+  const [visibleItems, setVisibleItems] = useState(0);
+
+  const maxItems = (images.length - frameSize) * itemWidth;
+
+  const handlePrevButton = () => {
+    setVisibleItems(Math.min(visibleItems + itemWidth * step, 0));
+
+    if (visibleItems === 0) {
+      setVisibleItems(maxItems);
+    }
+  };
+
+  const handleNextButton = () => {
+    setVisibleItems(Math.max(visibleItems - itemWidth * step, -maxItems));
+
+    if (visibleItems === -maxItems && infinite) {
+      setVisibleItems(0);
+    }
+  };
+
+  const frameWith = frameSize * itemWidth;
+
+  return (
+    <div className="Carousel">
+      <ul
+        className="Carousel__list"
+        style={{ width: `${frameWith}px` }}
+      >
+        {images.map((image) => (
+          <li
+            key={image}
+            style={{
+              transform: `translateX(${visibleItems}px)`,
+              transition: `${animationDuration}ms`,
+            }}
+          >
+            <img
+              src={image}
+              alt={`${image}`}
+              width={itemWidth}
+            />
+          </li>
+        ))}
+      </ul>
+
+      <div className="button-container">
+        <button
+          type="button"
+          className="button"
+          onClick={handlePrevButton}
+          disabled={!visibleItems}
+        >
+          Prev
+        </button>
+        <button
+          type="button"
+          data-cy="next"
+          className="button"
+          onClick={handleNextButton}
+          disabled={visibleItems === -maxItems && !infinite}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Carousel;
