684603f68a4fec075a246745a6b31081b7bbb3ee
Vitaliy Zviriuk <zvirvitalik1991@gmail.com>
2023-10-11 20:52:09 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,11 @@ import Carousel from './components/Carousel';
 
 interface State {
   images: string[];
+  step: number;
+  itemWidth: number;
+  frameSize: number;
+  animationDuration: number;
+  infinite:boolean;
 }
 
 class App extends React.Component<{}, State> {
@@ -20,17 +25,51 @@ class App extends React.Component<{}, State> {
       './img/9.png',
       './img/10.png',
     ],
+    frameSize: 3,
+    step: 3,
+    itemWidth: 130,
+    animationDuration: 1000,
+    infinite: false,
   };
 
   render() {
-    const { images } = this.state;
+    const {
+      images,
+      frameSize,
+      step,
+      itemWidth,
+      animationDuration,
+      infinite,
+    } = this.state;
 
     return (
       <div className="App">
         {/* eslint-disable-next-line */}
         <h1>Carousel with {images.length} images</h1>
 
-        <Carousel />
+        <Carousel
+          images={images}
+          itemWidth={itemWidth}
+          getWidth={(value: number) => {
+            this.setState({ itemWidth: value });
+          }}
+          getFrame={(value: number) => {
+            this.setState({ frameSize: value });
+          }}
+          getStep={(value: number) => {
+            this.setState({ step: value });
+          }}
+          getAnimationDuration={(value:number) => {
+            this.setState({ animationDuration: value });
+          }}
+          makeInfinite={(value:boolean) => {
+            this.setState({ infinite: value });
+          }}
+          step={step}
+          frameSize={frameSize}
+          animationDuration={animationDuration}
+          infinite={infinite}
+        />
       </div>
     );
   }
