6cf3b8403ddcace456b8c1e58aad0252af9461dd
4xmplme <andreykav2002@gmail.com>
2023-10-22 23:17:41 +0300

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,157 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
-import Carousel from './components/Carousel';
+import { Carousel } from './components/Carousel';
 
-interface State {
-  images: string[];
-}
+const images = [
+  './img/1.png',
+  './img/2.png',
+  './img/3.png',
+  './img/4.png',
+  './img/5.png',
+  './img/6.png',
+  './img/7.png',
+  './img/8.png',
+  './img/9.png',
+  './img/10.png',
+];
 
-class App extends React.Component<{}, State> {
-  state = {
-    images: [
-      './img/1.png',
-      './img/2.png',
-      './img/3.png',
-      './img/4.png',
-      './img/5.png',
-      './img/6.png',
-      './img/7.png',
-      './img/8.png',
-      './img/9.png',
-      './img/10.png',
-    ],
+const App: React.FC = () => {
+  const [corouselSettings, setCorouselSettings] = useState({
+    itemWidth: 130,
+    frameSize: 3,
+    step: 3,
+    animationDuration: 1000,
+    infinite: false,
+    currentImage: 0,
+  });
+
+  const {
+    itemWidth,
+    frameSize,
+    step,
+    animationDuration,
+    infinite,
+    currentImage,
+  } = corouselSettings;
+
+  const setCurrentImage = (value: number) => {
+    return setCorouselSettings(prevState => ({
+      ...prevState,
+      currentImage: value,
+    }));
   };
 
-  render() {
-    const { images } = this.state;
+  return (
+    <div className="App">
+      <h1 data-cy="title" className="App__title">{`Carousel with ${images.length} images`}</h1>
 
-    return (
-      <div className="App">
-        {/* eslint-disable-next-line */}
-        <h1>Carousel with {images.length} images</h1>
+      <div className="App__settings">
+        <label
+          htmlFor="itemId"
+          className="App__label"
+        >
+          Item width:
 
-        <Carousel />
+          <input
+            type="number"
+            min="70"
+            max="260"
+            step="5"
+            value={itemWidth}
+            id="itemId"
+            className="App__input"
+            onChange={event => setCorouselSettings(prevState => ({
+              ...prevState,
+              itemWidth: +event.target.value,
+            }))}
+          />
+        </label>
+
+        <label
+          htmlFor="frameId"
+          className="App__label"
+        >
+          Frame size:
+
+          <input
+            type="number"
+            min="1"
+            max="10"
+            step="1"
+            value={frameSize}
+            id="frameId"
+            className="App__input"
+            onChange={event => setCorouselSettings(prevState => ({
+              ...prevState,
+              frameSize: +event.target.value,
+            }))}
+          />
+        </label>
+
+        <label
+          htmlFor="stepId"
+          className="App__label"
+        >
+          Step:
+
+          <input
+            type="number"
+            min="1"
+            max="3"
+            step="1"
+            value={step}
+            id="stepId"
+            className="App__input"
+            onChange={event => setCorouselSettings(prevState => ({
+              ...prevState,
+              step: +event.target.value,
+            }))}
+          />
+        </label>
+
+        <label className="App__label">
+          Animation duration:
+
+          <input
+            type="number"
+            min="0"
+            max="5000"
+            step="500"
+            value={animationDuration}
+            className="App__input"
+            onChange={event => setCorouselSettings(prevState => ({
+              ...prevState,
+              animationDuration: +event.target.value,
+            }))}
+          />
+        </label>
+
+        <label className="App__label">
+          Infinite:
+
+          <input
+            type="checkbox"
+            className="App__switch"
+            onChange={() => setCorouselSettings(prevState => ({
+              ...prevState,
+              infinite: !prevState.infinite,
+            }))}
+          />
+        </label>
       </div>
-    );
-  }
-}
+
+      <Carousel
+        images={images}
+        itemWidth={itemWidth}
+        frameSize={frameSize}
+        step={step}
+        animationDuration={animationDuration}
+        infinite={infinite}
+        currentImage={currentImage}
+        setCurrentImage={setCurrentImage}
+      />
+    </div>
+  );
+};
 
 export default App;
