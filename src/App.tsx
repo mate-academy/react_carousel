531782c82decab349f9d80f3960a34434f67a553
Rodionsav <rodionsav@mate.academy>
2023-11-01 11:59:48 +0100

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,112 @@
-import React from 'react';
+import { useState } from 'react';
 import './App.scss';
 import Carousel from './components/Carousel';
 
-interface State {
-  images: string[];
-}
+const images = [
+  './img/1.png',
+  './img/2.png',
+  './img/3.png',
+  './img/4.png',
+  './img/5.png',
+  './img/6.png',
+  './img/7.png',
+  './img/8.png',
+  './img/9.png',
+  './img/10.png',
+];
 
-class App extends React.Component<{}, State> {
-  state = {
-    images: [
-      './img/1.png',
-      './img/2.png',
-      './img/3.png',
-      './img/4.png',
-      './img/5.png',
-      './img/6.png',
-      './img/7.png',
-      './img/8.png',
-      './img/9.png',
-      './img/10.png',
-    ],
+export const App: React.FC = () => {
+  const [itemWidth, setItemWidth] = useState<number>(130);
+  const [frameWidth, setFrameWidth] = useState(1);
+  const [step, setStep] = useState(1);
+  const [animationDuration, setAnimationDuration] = useState(1000);
+  const [infinite, setInfinite] = useState(false);
+
+  const handleItemChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setItemWidth(+event.target.value);
   };
 
-  render() {
-    const { images } = this.state;
+  const handleFrameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFrameWidth(+event.target.value);
+  };
 
-    return (
-      <div className="App">
-        {/* eslint-disable-next-line */}
-        <h1>Carousel with {images.length} images</h1>
+  const handleStepChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setStep(+event.target.value);
+  };
 
-        <Carousel />
-      </div>
-    );
-  }
-}
+  const handleAnimationChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    setAnimationDuration(+event.target.value);
+  };
 
-export default App;
+  return (
+    <div className="App">
+      {/* eslint-disable-next-line */}
+
+      <h1>
+        Carousel with
+        {images.length}
+        {' '}
+        images
+      </h1>
+      <label htmlFor="itemWidth">itemWidth</label>
+      <input
+        id="itemWidth"
+        type="number"
+        min={130}
+        max={260}
+        value={itemWidth}
+        // onChange={() => setItemWidth()}
+        onChange={handleItemChange}
+      />
+
+      <label htmlFor="frameWidth">frameWidth</label>
+      <input
+        id="frameWidth"
+        type="number"
+        min={1}
+        max={10}
+        value={frameWidth}
+        onChange={handleFrameChange}
+      />
+
+      <label htmlFor="step">Step</label>
+      <input
+        id="step"
+        type="number"
+        min={1}
+        max={10}
+        value={step}
+        onChange={handleStepChange}
+      />
+      <label htmlFor="animation">Animation duration</label>
+      <input
+        id="animation"
+        type="number"
+        min={500}
+        max={3000}
+        value={animationDuration}
+        onChange={handleAnimationChange}
+      />
+
+      <label htmlFor="infinite">Infinite</label>
+      <input
+        id="infinite"
+        type="checkbox"
+        checked={infinite}
+        onChange={() => setInfinite(!infinite)}
+      />
+
+      <Carousel
+        images={images}
+        itemWidth={itemWidth}
+        frameWidth={frameWidth}
+        step={step}
+        animationDuration={animationDuration}
+        infinite={infinite}
+      />
+
+    </div>
+  );
+};
