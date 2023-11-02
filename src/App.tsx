1adb4da0689e04922d41b5cbff2ba3baba06b44f
Vadym Bavorovskyi <vadymbavorovskiy@gmail.com>
2023-11-02 10:15:25 +0200

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import './App.scss';
 import Carousel from './components/Carousel';
+import { Images } from './types/Images';
 
 interface State {
-  images: string[];
+  images: Images;
+  steps: number[];
+  selectedStep: number;
+  framesSize: number[];
+  selectedFrameSize: number;
+  itemsWidth: number[];
+  selectedItemWidth: number;
+  animationsDuration: number[];
+  selectedAnimationDuration: number;
 }
 
 class App extends React.Component<{}, State> {
-  state = {
+  state: State = {
     images: [
       './img/1.png',
       './img/2.png',
@@ -20,17 +29,192 @@ class App extends React.Component<{}, State> {
       './img/9.png',
       './img/10.png',
     ],
+
+    steps: [
+      1,
+      2,
+      3,
+      4,
+      5,
+    ],
+
+    selectedStep: 3,
+
+    framesSize: [
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+    ],
+
+    selectedFrameSize: 3,
+
+    itemsWidth: [
+      130,
+      140,
+      150,
+      160,
+      170,
+      180,
+      190,
+      200,
+    ],
+
+    selectedItemWidth: 130,
+
+    animationsDuration: [
+      500,
+      1000,
+      1500,
+      2000,
+      1500,
+      2500,
+      3000,
+      3500,
+    ],
+
+    selectedAnimationDuration: 1000,
+  };
+
+  handleStepChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    this.setState({ selectedStep: +event.target.value });
+  };
+
+  handleFrameSizeChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    this.setState({ selectedFrameSize: +event.target.value });
+  };
+
+  handleItemWidthChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    this.setState({ selectedItemWidth: +event.target.value });
+  };
+
+  handleAnimationDurationChange =
+  (event: React.ChangeEvent<HTMLSelectElement>) => {
+    this.setState({ selectedAnimationDuration: +event.target.value });
   };
 
   render() {
-    const { images } = this.state;
+    const {
+      images,
+      steps,
+      selectedStep,
+      framesSize,
+      selectedFrameSize,
+      itemsWidth,
+      selectedItemWidth,
+      animationsDuration,
+      selectedAnimationDuration,
+    } = this.state;
 
     return (
       <div className="App">
-        {/* eslint-disable-next-line */}
-        <h1>Carousel with {images.length} images</h1>
+        <h1 data-cy="title">{`Carousel with ${images.length} images`}</h1>
 
-        <Carousel />
+        <section className="Control-panel">
+          <div className="Control-panel__select-steps">
+            <label htmlFor="label-step">
+              Enter the number of steps
+            </label>
+
+            <select
+              name="label-step"
+              onChange={this.handleStepChange}
+              value={selectedStep}
+            >
+              {
+                steps.map((item, index) => (
+                  <option
+                    value={item}
+                    key={`${index + 1}`}
+                  >
+                    {item}
+                  </option>
+                ))
+              }
+            </select>
+          </div>
+
+          <div className="Control-panel__select-frameSize">
+            <label htmlFor="label-frameSize">
+              Enter the number of frameSize
+            </label>
+
+            <select
+              name="label-frameSize"
+              onChange={this.handleFrameSizeChange}
+              value={selectedFrameSize}
+            >
+              {
+                framesSize.map((item, index) => (
+                  <option
+                    value={item}
+                    key={`${index + 1}`}
+                  >
+                    {item}
+                  </option>
+                ))
+              }
+            </select>
+          </div>
+
+          <div className="Control-panel__select-itemsWidth">
+            <label htmlFor="itemsWidthSelect">
+              Select image width
+            </label>
+
+            <select
+              name="itemsWidthSelect"
+              onChange={this.handleItemWidthChange}
+              value={selectedItemWidth}
+            >
+              {
+                itemsWidth.map((item, index) => (
+                  <option
+                    value={item}
+                    key={`${index + 1}`}
+                  >
+                    {item}
+                  </option>
+                ))
+              }
+            </select>
+          </div>
+
+          <div className="Control-panel__select-animationsDuration">
+            <label htmlFor="itemsWidthSelect">
+              Select the duration of the animation
+            </label>
+
+            <select
+              name="itemsWidthSelect"
+              onChange={this.handleAnimationDurationChange}
+              value={selectedAnimationDuration}
+            >
+              {
+                animationsDuration.map((item, index) => (
+                  <option
+                    value={item}
+                    key={`${index + 1}`}
+                  >
+                    {item}
+                  </option>
+                ))
+              }
+            </select>
+          </div>
+        </section>
+
+        <Carousel
+          images={images}
+          step={selectedStep}
+          frameSize={selectedFrameSize}
+          itemWidth={selectedItemWidth}
+          animationDuration={selectedAnimationDuration}
+        />
       </div>
     );
   }
--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,18 +1,113 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Carousel.scss';
+import { Images } from '../types/Images';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+type Props = {
+  images: Images;
+  step: number;
+  frameSize: number;
+  itemWidth: number;
+  animationDuration: number;
+};
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+const Carousel: React.FC<Props> = ({
+  images,
+  step,
+  frameSize,
+  itemWidth,
+  animationDuration,
+}) => {
+  const [index, setIndex] = useState(frameSize);
+  const [offsetX, setOffsetX] = useState(0);
+
+  useEffect(() => {
+    setIndex(frameSize);
+    setOffsetX(0);
+  }, [step, frameSize, itemWidth]);
+
+  const updateSlide = (newOffsetX: number, newIndex: number) => {
+    setOffsetX(newOffsetX);
+
+    setIndex(newIndex);
+  };
+
+  const handlePrevSlide = () => {
+    if (index > frameSize) {
+      const currentStep = step < index - frameSize
+        ? step
+        : index - frameSize;
+
+      updateSlide(
+        offsetX + (currentStep * (itemWidth + 20)),
+        index - currentStep,
+      );
+    }
+  };
+
+  const handleNextSlide = () => {
+    if (index < images.length) {
+      const currentStep = step < images.length - index
+        ? step
+        : images.length - index;
+
+      updateSlide(
+        offsetX - (currentStep * (itemWidth + 20)),
+        index + currentStep,
+      );
+    }
+  };
+
+  return (
+    <div className="container">
+      <button onClick={handlePrevSlide} type="button">
+        <img
+          src="./img/arrow.jpg"
+          alt="arrow"
+          className="Carousel__btn-img"
+        />
+      </button>
+
+      <div
+        className="Carousel"
+        style={{ width: `${frameSize * itemWidth + (frameSize - 1) * 20}px ` }}
+      >
+        <ul
+          className="Carousel__list"
+          style={
+            {
+              transform: `translateX(${offsetX}px)`,
+              transition: `transform ${animationDuration}ms`,
+            }
+          }
+        >
+          {
+            images.map(image => (
+              <li key={image}>
+                <img
+                  className="Carousel__img"
+                  style={{ width: `${itemWidth}px` }}
+                  src={image}
+                  alt={image}
+                />
+              </li>
+            ))
+          }
+        </ul>
+      </div>
+
+      <button
+        onClick={handleNextSlide}
+        type="button"
+        data-cy="next"
+      >
+        <img
+          src="./img/arrow.jpg"
+          alt="arrow"
+          className="Carousel__btn-img Carousel__btn-img--transform-scaleX"
+        />
+      </button>
+    </div>
+  );
+};
 
 export default Carousel;
