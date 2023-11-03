eeb44ce1d5635df3b14ea7e8df565680cb48bc76
Vadym Bavorovskyi <vadymbavorovskiy@gmail.com>
2023-11-03 11:18:42 +0200

    add task solution

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,18 +1,116 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Carousel.scss';
+import { Images } from '../types/Images';
+import { getUniqueId } from '../utils/getUniqueId';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+type Props = {
+  images: Images;
+  step: number;
+  frameSize: number;
+  itemWidth: number;
+  animationDuration: number;
+};
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+const Carousel: React.FC<Props> = ({
+  images,
+  step,
+  frameSize,
+  itemWidth,
+  animationDuration,
+}) => {
+  const [index, setIndex] = useState(frameSize);
+  const [offsetX, setOffsetX] = useState(0);
+
+  useEffect(() => {
+    setIndex(frameSize);
+    setOffsetX(0);
+  }, [step, frameSize, itemWidth]);
+
+  const updateSlide = (newOffsetX: number, newIndex: number) => {
+    setOffsetX(newOffsetX);
+
+    setIndex(newIndex);
+  };
+
+  const handlePrevSlide = () => {
+    const currentStep = step < index - frameSize
+      ? step
+      : index - frameSize;
+
+    updateSlide(
+      offsetX + (currentStep * (itemWidth + 20)),
+      index - currentStep,
+    );
+  };
+
+  const handleNextSlide = () => {
+    const currentStep = step < images.length - index
+      ? step
+      : images.length - index;
+
+    updateSlide(
+      offsetX - (currentStep * (itemWidth + 20)),
+      index + currentStep,
+    );
+  };
+
+  return (
+    <div className="container">
+      <button
+        onClick={handlePrevSlide}
+        type="button"
+        disabled={index <= frameSize}
+      >
+        <img
+          src="./img/arrow.jpg"
+          alt="arrow"
+          className="Carousel__btn-img"
+        />
+      </button>
+
+      <div
+        className="Carousel"
+        style={{ width: `${frameSize * itemWidth + (frameSize - 1) * 20}px ` }}
+      >
+        <ul
+          className="Carousel__list"
+          style={
+            {
+              transform: `translateX(${offsetX}px)`,
+              transition: `transform ${animationDuration}ms`,
+            }
+          }
+        >
+          {
+            images.map(image => (
+              <li key={getUniqueId()}>
+                <img
+                  className="Carousel__img"
+                  style={{ width: `${itemWidth}px` }}
+                  src={image}
+                  alt={image}
+                />
+              </li>
+            ))
+          }
+        </ul>
+      </div>
+
+      <button
+        onClick={handleNextSlide}
+        type="button"
+        data-cy="next"
+        disabled={index >= images.length}
+
+      >
+        <img
+          src="./img/arrow.jpg"
+          alt="arrow"
+          className="Carousel__btn-img Carousel__btn-img--transform-scaleX"
+        />
+      </button>
+    </div>
+  );
+};
 
 export default Carousel;
