d5cf4572c6504d63f93cb3c8d0b3a91ddae75b0b
Kseniia Chepur <kseniia.chepur@gmail.com>
2023-11-04 20:40:41 +0200

    implement solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,145 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
-import Carousel from './components/Carousel';
+import { Carousel } from './components/Carousel';
 
-interface State {
-  images: string[];
-}
+const images = [
+  './img/1.png',
+  './img/2.png',
+  './img/3.png',
+  './img/4.png',
+  './img/5.png',
+  './img/6.png',
+  './img/7.png',
+  './img/8.png',
+  './img/9.png',
+  './img/10.png',
+];
 
-class App extends React.Component<{}, State> {
-  state = {
-    images: [
-      './img/1.png',
-      './img/2.png',
-      './img/3.png',
-      './img/4.png',
-      './img/5.png',
-      './img/6.png',
-      './img/7.png',
-      './img/8.png',
-      './img/9.png',
-      './img/10.png',
-    ],
+export const App:React.FC = () => {
+  const [settings, setSettings] = useState({
+    step: 3,
+    frameSize: 3,
+    itemWidth: 130,
+    animationDuration: 1000,
+    infinite: false,
+  });
+
+  const {
+    step, frameSize, itemWidth, animationDuration, infinite,
+  } = settings;
+
+  const handleChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      name, value, type, checked,
+    } = e.target;
+
+    setSettings(prevSettings => (
+      {
+        ...prevSettings,
+        [name]: type === 'checkbox' ? checked : +value,
+      }
+    ));
   };
 
-  render() {
-    const { images } = this.state;
+  return (
+    <div className="App">
+      <h1
+        data-cy="title"
+        className="App__title"
+      >
+        {`Carousel with ${images.length} images`}
+      </h1>
 
-    return (
-      <div className="App">
-        {/* eslint-disable-next-line */}
-        <h1>Carousel with {images.length} images</h1>
+      <form
+        className="App__form"
+        action="#"
+      >
+        <div className="mb-2">
+          <label htmlFor="step" className="form-label">
+            Step:
+          </label>
+          <input
+            value={step}
+            onChange={handleChange}
+            type="number"
+            className="form-control form-control-sm"
+            name="step"
+            id="step"
+            min={1}
+            max={10}
+          />
+        </div>
+        <div className="mb-2">
+          <label htmlFor="frameSize" className="form-label">
+            Frame size:
+          </label>
+          <input
+            value={frameSize}
+            onChange={handleChange}
+            type="number"
+            className="form-control form-control-sm"
+            name="frameSize"
+            id="frameSize"
+            min={1}
+            max={10}
+          />
+        </div>
+        <div className="mb-2">
+          <label htmlFor="itemWidth" className="form-label">
+            Item width:
+          </label>
+          <input
+            value={itemWidth}
+            onChange={handleChange}
+            type="number"
+            className="form-control form-control-sm"
+            name="itemWidth"
+            id="itemWidth"
+            min={100}
+            max={260}
+            step={10}
+          />
+        </div>
+        <div className="mb-2">
+          <label htmlFor="animationDuration" className="form-label">
+            Animation duration:
+          </label>
+          <input
+            value={animationDuration}
+            onChange={handleChange}
+            type="number"
+            className="form-control form-control-sm"
+            name="animationDuration"
+            id="animationDuration"
+            min={500}
+            max={10000}
+            step={500}
+          />
+        </div>
+        <div className="form-check form-switch mb-2">
+          <label htmlFor="flexSwitchCheckDefault" className="form-check-label">
+            Infinite
+          </label>
+          <input
+            checked={infinite}
+            onChange={handleChange}
+            type="checkbox"
+            className="form-check-input"
+            role="switch"
+            name="infinite"
+            id="flexSwitchCheckDefault"
+          />
+        </div>
+      </form>
 
-        <Carousel />
-      </div>
-    );
-  }
-}
-
-export default App;
+      <Carousel
+        images={images}
+        step={step}
+        frameSize={frameSize}
+        itemWidth={itemWidth}
+        animationDuration={animationDuration}
+        infinite={infinite}
+      />
+    </div>
+  );
+};
--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,18 +1,92 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+type Props = {
+  images: string[],
+  step: number,
+  frameSize: number,
+  itemWidth: number,
+  animationDuration: number,
+  infinite: boolean,
+};
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+export const Carousel: React.FC<Props> = ({
+  images,
+  step,
+  frameSize,
+  itemWidth,
+  animationDuration,
+  infinite,
+
+}) => {
+  const [currentImgIndex, setCurrentImgIndex] = useState(0);
+  const maxIndex = images.length - frameSize;
+
+  const moveBack = () => (
+    currentImgIndex !== 0
+      ? setCurrentImgIndex(prevIndex => Math.max(prevIndex - step, 0))
+      : setCurrentImgIndex(maxIndex)
+  );
+
+  const moveForward = () => (
+    currentImgIndex !== maxIndex
+      ? setCurrentImgIndex(prevIndex => Math.min(prevIndex + step, maxIndex))
+      : setCurrentImgIndex(0)
+  );
+
+  return (
+    <div
+      className="Carousel"
+      style={{
+        width: `${itemWidth * frameSize}px`,
+        transition: `${animationDuration}ms`,
+      }}
+    >
+      <ul
+        className="Carousel__list"
+      >
+        {images.map((image, i) => (
+          <li
+            className="Carousel__item"
+            key={image}
+            style={{
+              transform: `translateX(${-(currentImgIndex * itemWidth)}px)`,
+              transition: `transform ${animationDuration}ms`,
+            }}
+          >
+            <img
+              className="Carousel__img"
+              src={image}
+              alt={`${i + 1}`}
+              width={itemWidth}
+              style={{ transition: `${animationDuration}ms` }}
+            />
+          </li>
+        ))}
+      </ul>
+
+      <div className="btn-block">
+        <button
+          className="btn btn-outline-primary"
+          type="button"
+          onClick={moveBack}
+          disabled={!infinite && currentImgIndex === 0}
+        >
+          Prev
+        </button>
+        <button
+          className="btn btn-outline-primary"
+          type="button"
+          data-cy="next"
+          onClick={moveForward}
+          disabled={!infinite && currentImgIndex === maxIndex}
+        >
+          Next
+        </button>
+      </div>
+
+    </div>
+  );
+};
 
 export default Carousel;
