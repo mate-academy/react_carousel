bcff930a1b8ede70a8fbc4a31a1a9920ae2c62e8
Roma <roma150269@gmail.com>
2023-11-11 19:44:17 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import Carousel from './components/Carousel';
 
 interface State {
   images: string[];
+  step: number;
+  frameSize: number,
+  itemWidth: number,
+  count: number,
+  animationDuration: number;
 }
 
 class App extends React.Component<{}, State> {
@@ -20,17 +25,113 @@ class App extends React.Component<{}, State> {
       './img/9.png',
       './img/10.png',
     ],
+    step: 3,
+    frameSize: 3,
+    itemWidth: 130,
+    count: 7,
+    animationDuration: 1000,
   };
 
+  handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    this.setState({
+      frameSize: +event.target.value,
+      count: this.state.images.length - +event.target.value,
+    });
+  }
+
+  widthItem(event: React.ChangeEvent<HTMLInputElement>) {
+    this.setState({ itemWidth: +event.target.value });
+  }
+
+  stepChange(event: React.ChangeEvent<HTMLInputElement>) {
+    this.setState({ step: +event.target.value });
+  }
+
+  animationChange(event: React.ChangeEvent<HTMLInputElement>) {
+    this.setState({ animationDuration: +event.target.value });
+  }
+
+  countChange(value: number) {
+    this.setState({ count: value });
+  }
+
   render() {
-    const { images } = this.state;
+    const {
+      images,
+      step,
+      frameSize,
+      itemWidth,
+      count,
+      animationDuration,
+    } = this.state;
 
     return (
       <div className="App">
         {/* eslint-disable-next-line */}
-        <h1>Carousel with {images.length} images</h1>
+        <h1 data-cy="title">Carousel with {images.length} images</h1>
 
-        <Carousel />
+        <form className="App__form" action="/">
+          <label htmlFor="item">
+            Number of images
+
+            <input
+              className="App__input"
+              type="number"
+              value={frameSize}
+              min={3}
+              max={10}
+              name="input1"
+              onChange={(event) => this.handleChange(event)}
+            />
+          </label>
+
+          <label htmlFor="item2">
+            Item Width
+
+            <input
+              className="App__input"
+              type="number"
+              value={itemWidth}
+              name="input2"
+              onChange={(event) => this.widthItem(event)}
+            />
+          </label>
+
+          <label htmlFor="item">
+            Number of images scrolled
+
+            <input
+              className="App__input"
+              type="number"
+              value={step}
+              min={1}
+              max={9}
+              name="input1"
+              onChange={(event) => this.stepChange(event)}
+            />
+          </label>
+
+          <label htmlFor="item">
+            Animation Duration
+
+            <input
+              className="App__input"
+              type="number"
+              value={animationDuration}
+              name="input1"
+              onChange={(event) => this.animationChange(event)}
+            />
+          </label>
+        </form>
+        <Carousel
+          images={images}
+          frameSize={frameSize}
+          itemWidth={itemWidth}
+          step={step}
+          count={count}
+          animationDuration={animationDuration}
+          countChange={(value) => this.countChange(value)}
+        />
       </div>
     );
   }
--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,18 +1,137 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+type CarouselProp = {
+  images: string[];
+  step: number
+  frameSize: number;
+  itemWidth: number;
+  count: number;
+  animationDuration:number;
+  countChange: (value: number) => void;
+};
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+const Carousel: React.FC<CarouselProp> = ({
+  images,
+  frameSize,
+  itemWidth,
+  step,
+  count,
+  animationDuration,
+  countChange,
+}) => {
+  const widthContainer = frameSize * itemWidth;
+  const muvStep = itemWidth * step;
+  let disabledButtonNext = false;
+  let disabledButtonPrev = false;
+  const prevValue = ((images.length - frameSize) - count);
+  const [muveNext, setMuveNext] = useState(0);
+
+  if (count <= 0) {
+    disabledButtonNext = true;
+  }
+
+  if (count === images.length - frameSize) {
+    disabledButtonPrev = true;
+  }
+
+  const muvNextEmoticon = () => {
+    let muv = 0;
+
+    if (count <= 0) {
+      return;
+    }
+
+    if (count < step) {
+      setMuveNext(muveNext + itemWidth * count);
+      countChange(0);
+    } else {
+      if (muveNext === 0) {
+        muv += muvStep;
+        countChange(count - step);
+      } else {
+        muv += muveNext + muvStep;
+        countChange(count - step);
+      }
+
+      setMuveNext(muv);
+    }
+  };
+
+  const muvPrevEmoticon = () => {
+    let muv = 0;
+
+    if (count < images.length - count) {
+      muv = muveNext - muvStep;
+      countChange(count + step);
+
+      if (muveNext > 0) {
+        setMuveNext(muv);
+      }
+    } else {
+      setMuveNext(0);
+      countChange(count + prevValue);
+    }
+  };
+
+  return (
+    <div className="Carousel">
+      <div
+        className="Carousel__container"
+        style={{
+          width: `${widthContainer}px`,
+        }}
+      >
+        <ul
+          style={{
+            transform: `translateX(-${muveNext}px)`,
+            transition: `all ${animationDuration}ms`,
+          }}
+          className="Carousel__list"
+        >
+          {
+            images.map((img: string, index: number) => (
+              <li
+                className="Carousel__item"
+                key={index}
+              >
+                <img
+                  className="Carousel__img"
+                  src={img}
+                  alt={`${index + 1}`}
+                  style={{
+                    width: `${itemWidth}px`,
+                    height: `${itemWidth}px`,
+                  }}
+                />
+              </li>
+            ))
+          }
+        </ul>
+      </div>
+      <div className="Carousel__wraper">
+        <button
+          className="Carousel__button"
+          type="button"
+          disabled={disabledButtonPrev}
+          onClick={() => muvPrevEmoticon()}
+        >
+          Prev
+        </button>
+
+        <button
+          className="Carousel__button"
+          data-cy="next"
+          type="button"
+          onClick={() => muvNextEmoticon()}
+          disabled={disabledButtonNext}
+        >
+          Next
+        </button>
+      </div>
+
+    </div>
+  );
+};
 
 export default Carousel;
