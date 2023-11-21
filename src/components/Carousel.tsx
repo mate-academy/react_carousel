153da57d8567b552b5ef547f7facc94d12bef985
Yuliia Bakun <bakunyuliia@gmail.com>
2023-11-21 09:25:34 +0100

    task carousel

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,18 +1,114 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+type Props = {
+  items: string[];
+  itemWidth: number;
+  frameSize: number;
+  step: number;
+  animation: number;
+  infinite: boolean;
+};
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+const Carousel: React.FC<Props> = ({
+  items,
+  itemWidth,
+  frameSize,
+  step,
+  animation,
+  infinite,
+}) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const showNextItems = () => {
+    let nextIndex = currentIndex + step;
+
+    if (infinite) {
+      nextIndex = nextIndex >= items.length ? 0 : nextIndex;
+
+      const remainingItems = items.length - nextIndex;
+
+      if (remainingItems < frameSize) {
+        nextIndex = Math.max(0, items.length - frameSize);
+      }
+    } else {
+      nextIndex = Math.min(nextIndex, items.length - frameSize);
+    }
+
+    setCurrentIndex(nextIndex);
+  };
+
+  const showPreviousItems = () => {
+    let prevIndex = currentIndex - step;
+
+    if (infinite) {
+      if (prevIndex <= 0 - frameSize) {
+        prevIndex = items.length - frameSize;
+      } else if (prevIndex > 0 - frameSize && prevIndex <= 0) {
+        prevIndex = 0;
+      }
+    } else {
+      prevIndex = Math.max(currentIndex - step, 0);
+    }
+
+    setCurrentIndex(prevIndex);
+  };
+
+  return (
+    <div
+      className="Carousel box"
+      style={{ width: `${(itemWidth * frameSize) + 40}px` }}
+    >
+
+      <div className="Carousel__wrapper">
+        <div
+          className="Carousel__container"
+          style={{
+            transform: `translateX(-${currentIndex * (itemWidth)}px)`,
+            transition: `transform ${animation}ms linear`,
+          }}
+        >
+          <ul className="Carousel__list">
+            {items.map((item) => (
+              <li key={item} className="Carousel__item">
+                <img
+                  className="Carousel__image"
+                  style={{ width: `${itemWidth}px` }}
+                  src={item}
+                  alt={item}
+                />
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+
+      <div className="Carousel__controls">
+        <button
+          type="button"
+          className="button"
+          onClick={showPreviousItems}
+          disabled={currentIndex <= 0 && !infinite}
+        >
+          <span className="icon is-small">
+            <i className="fa-solid fa-arrow-left"> </i>
+          </span>
+        </button>
+
+        <button
+          type="button"
+          data-cy="next"
+          className="button"
+          onClick={showNextItems}
+          disabled={currentIndex >= items.length - frameSize && !infinite}
+        >
+          <span className="icon is-small">
+            <i className="fa-solid fa-arrow-right"> </i>
+          </span>
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Carousel;
