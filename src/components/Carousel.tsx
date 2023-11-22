f0f08c9a8602f580553a3ba5714193e9ab247847
Andrii Rykal <andreuka79@gmail.com>
2023-11-22 09:45:26 +0200

    Solution

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,18 +1,125 @@
 import React from 'react';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+type Props = {
+  images: string[];
+  step: number;
+  frameSize: number;
+  itemWidth: number;
+  animationDuration: number;
+};
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+const Carousel: React.FC<Props> = ({
+  images,
+  step,
+  frameSize,
+  itemWidth,
+  animationDuration,
+}) => {
+  let lengthMove = 0;
+  const moveSlider = (event: React.MouseEvent<HTMLElement>) => {
+    const list = document.querySelector('.Carousel__list');
+    const currentButton = event.target;
+    const buttonNext = document
+      .querySelector<HTMLButtonElement>('[data-cy=next]');
+    const buttonPrev = document
+      .querySelector<HTMLButtonElement>('[data-cy=prev]');
+    const totalWidth = images.length * itemWidth;
+    const totalFrameSize = frameSize * itemWidth;
+    const totalStep = itemWidth * step;
+
+    if (currentButton instanceof HTMLElement
+      && list instanceof HTMLElement
+      && buttonNext instanceof HTMLElement
+      && buttonPrev instanceof HTMLElement) {
+      switch (currentButton.dataset.cy) {
+        case 'next':
+          lengthMove -= totalStep;
+
+          if (Math.abs(lengthMove) + totalStep >= totalWidth) {
+            lengthMove = -1 * (totalWidth - totalFrameSize);
+            list.style.transform = `translateX(${lengthMove}px)`;
+            buttonNext.disabled = true;
+          } else {
+            buttonPrev.disabled = false;
+            list.style.transform = `translateX(${lengthMove}px)`;
+          }
+
+          break;
+
+        case 'prev':
+          lengthMove += totalStep;
+
+          if (lengthMove >= 0) {
+            lengthMove = 0;
+            list.style.transform = `translateX(${lengthMove}px)`;
+            buttonPrev.disabled = true;
+          } else {
+            buttonNext.disabled = false;
+            list.style.transform = `translateX(${lengthMove}px)`;
+          }
+
+          break;
+
+        default:
+          break;
+      }
+    }
+  };
+
+  return (
+    <div
+      className="Carousel"
+      style={{
+        width: `${images.length * itemWidth}px`,
+      }}
+    >
+      <div
+        className="container"
+        style={{
+          width: `${frameSize * itemWidth}px`,
+        }}
+      >
+        <ul
+          className="Carousel__list"
+          style={{
+            transition: `all ${animationDuration}ms`,
+          }}
+        >
+          {images.map((image, index) => (
+            <li key={image}>
+              <img
+                src={image}
+                alt={`${index + 1}`}
+                style={{
+                  width: `${itemWidth}px`,
+                  height: `${itemWidth}px`,
+                }}
+              />
+            </li>
+          ))}
+        </ul>
+
+        <div className="Carousel__buttons">
+          <button
+            type="button"
+            data-cy="prev"
+            onClick={moveSlider}
+          >
+            Prev
+          </button>
+
+          <button
+            type="button"
+            data-cy="next"
+            onClick={moveSlider}
+          >
+            Next
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Carousel;
