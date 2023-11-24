eb3ee0076bc92dffab4f25c2abc77d5cbc1df173
Oksana Maruniak <syper.ksenka@gmail.com>
2023-11-24 16:09:41 +0200

    carousel_solution

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const images: string[] = [
   './img/10.png',
 ];
 
-const App: React.FC = () => {
+export const App: React.FC = () => {
   const [itemWidth, setBoxWidth] = useState(130);
   const [frameSize, setFrameSize] = useState(3);
   const [step, setStep] = useState(3);
