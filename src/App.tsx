2878c3b45399fc164782aeb930b87e6e8b85efb9
Daniil <danil.beshlebnov@gmail.com>
2023-11-25 16:26:57 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
-import Carousel from './components/Carousel';
+import Carousel from './components/Carousel/Carousel';
+import { Form } from './components/Form/Form';
 
-interface State {
-  images: string[];
-}
+const images: string[] = [
+  './img/1.png',
+  './img/2.png',
+  './img/3.png',
+  './img/4.png',
+  './img/5.png',
+  './img/6.png',
+  './img/7.png',
+  './img/8.png',
+  './img/9.png',
+  './img/10.png',
+];
 
-class App extends React.Component<{}, State> {
-  state = {
-    images: [
-      './img/1.png',
-      './img/2.png',
-      './img/3.png',
-      './img/4.png',
-      './img/5.png',
-      './img/6.png',
-      './img/7.png',
-      './img/8.png',
-      './img/9.png',
-      './img/10.png',
-    ],
-  };
+export const App: React.FC = () => {
+  const [inputWidth, setInputWidth] = useState(130);
+  const [frameSize, setFrameSize] = useState(3);
+  const [step, setStep] = useState(3);
+  const [animationD, setAnimationD] = useState(1000);
+  const [infinite, setInfinite] = useState(false);
 
-  render() {
-    const { images } = this.state;
+  return (
+    <div className="App">
+      <h1 data-cy="title">
+        Carousel with
+        {' '}
+        {images.length}
+        {' '}
+        images
+      </h1>
 
-    return (
-      <div className="App">
-        {/* eslint-disable-next-line */}
-        <h1>Carousel with {images.length} images</h1>
+      <Form
+        images={images}
+        inputWidth={inputWidth}
+        setInputWidth={setInputWidth}
+        frameSize={frameSize}
+        setFrameSize={setFrameSize}
+        step={step}
+        setStep={setStep}
+        animationD={animationD}
+        setAnimationD={setAnimationD}
+        infinite={infinite}
+        setInfinite={setInfinite}
+      />
 
-        <Carousel />
-      </div>
-    );
-  }
-}
+      <Carousel
+        images={images}
+        inputWidth={inputWidth}
+        frameSize={frameSize}
+        step={step}
+        animationD={animationD}
+        infinite={infinite}
+      />
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/Carousel/Carousel.tsx
@@ -0,0 +1,116 @@
+import React, { useState } from 'react';
+import './Carousel.scss';
+import cn from 'classnames';
+
+interface ICarouselProps {
+  images: string[];
+  inputWidth: number;
+  frameSize: number
+  step: number;
+  animationD: number;
+  infinite: boolean;
+}
+
+const changeSizeByinputWidth = (inputWidth: number) => ({
+  width: `${inputWidth * 3}px`,
+});
+
+const changeSizeByFrameSize = (frameSize: number, inputWidth: number) => ({
+  width: `${inputWidth * frameSize}px`,
+});
+
+const changeDuration = (animationD: number) => ({
+  transition: `transform ${animationD}ms`,
+});
+
+const Carousel: React.FC<ICarouselProps> = ({
+  images,
+  inputWidth,
+  frameSize,
+  step,
+  animationD,
+  infinite,
+}) => {
+  const [moveItems, setMoveItems] = useState(0);
+  const lastItem = images.length - frameSize;
+
+  const handleMoveRight = () => {
+    const remainingItems = images.length - (moveItems + frameSize);
+
+    if (infinite && lastItem <= moveItems) {
+      setMoveItems(0);
+    } else if (remainingItems > 0) {
+      setMoveItems(
+        (prevMoveRight) => prevMoveRight + Math.min(step, remainingItems),
+      );
+    }
+  };
+
+  const handleMoveLeft = () => {
+    if (infinite && moveItems === 0) {
+      setMoveItems(lastItem);
+    } else if (moveItems > 0) {
+      setMoveItems(
+        (prevMoveRight) => prevMoveRight - Math.min(step, prevMoveRight),
+      );
+    }
+  };
+
+  return (
+    <div
+      className="Carousel"
+      style={{
+        ...changeSizeByinputWidth(inputWidth),
+        ...changeSizeByFrameSize(frameSize, inputWidth),
+      }}
+    >
+      <ul className="Carousel__list">
+        {images.map(image => {
+          return (
+            <li
+              className="Carousel__item"
+              key={image}
+              style={{
+                transform: `translateX(-${moveItems * inputWidth}px)`,
+                ...changeDuration(animationD),
+              }}
+            >
+              <img
+                src={image}
+                alt={image}
+                style={{ width: `${inputWidth}px`, height: `${inputWidth}px` }}
+                width={inputWidth}
+              />
+            </li>
+          );
+        })}
+      </ul>
+
+      <div className="Carousel__buttons">
+        <button
+          type="button"
+          className={cn('Carousel__button-prev', {
+            'Carousel__button-prev--disabled': moveItems === 0 && !infinite,
+          })}
+          onClick={handleMoveLeft}
+        >
+          Prev
+        </button>
+
+        <button
+          type="button"
+          className={cn('Carousel__button-next', {
+            'Carousel__button-next--disabled':
+              moveItems === images.length - frameSize && !infinite,
+          })}
+          onClick={handleMoveRight}
+          data-cy="next"
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Carousel;
