de067ab95e5f4088120b8e4ef6907811babff41f
Maksym Svoboda <maksym.svoboda.kn.2021@lpnu.ua>
2023-11-26 17:44:20 +0200

    made solution for carousel

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,18 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+interface Props {
+  images: string[];
+  step: number;
+  frameSize: number;
+  itemWidth: number;
+  animationDuration: number;
+}
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+const Carousel: React.FC<Props> = ({
+  images,
+  step = 3,
+  frameSize = 3,
+  itemWidth = 130,
+  animationDuration = 1000,
+}) => {
+  const [translation, setTranslation] = useState(0);
+  const widthOfAllImages = itemWidth * images.length;
+  const stepWidth = itemWidth * step;
+  const frameWidth = itemWidth * frameSize;
+
+  const handleNext = () => {
+    if (Math.abs(translation) + stepWidth <= widthOfAllImages - frameWidth) {
+      setTranslation(translation - stepWidth);
+    } else {
+      setTranslation(frameWidth - widthOfAllImages);
+    }
+  };
+
+  const handlePrev = () => {
+    if (translation + stepWidth <= 0) {
+      setTranslation(translation + stepWidth);
+    } else {
+      setTranslation(0);
+    }
+  };
+
+  return (
+    <div className="Carousel" style={{ maxWidth: `${frameSize * itemWidth}px` }}>
+      <ul
+        className="Carousel__list"
+        style={{
+          transform: `translateX(${translation}px)`,
+          maxWidth: `${widthOfAllImages}px`,
+          transition: `transform ${animationDuration}ms`,
+        }}
+      >
+        {images.map((image, index) => (
+          <li key={`${index + 1}`}>
+            <img style={{ width: `${itemWidth}px` }} src={image} alt={`${index + 1}`} />
+          </li>
+        ))}
+      </ul>
+
+      <div className="Carousel__buttons">
+        <button type="button" onClick={handlePrev} disabled={translation === 0}>
+          {'<<<'}
+        </button>
+        <button
+          data-cy="next"
+          type="button"
+          onClick={handleNext}
+          disabled={translation === frameWidth - widthOfAllImages}
+        >
+          {'>>>'}
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Carousel;
