6874727e1dc19f688ef5de7ab624bcb8571e22a9
Andriy Fesych <andriyhimself@gmail.com>
2023-12-08 21:24:15 +0200

    save_0.2

--- src/App.tsx
+++ src/App.tsx
@@ -2,39 +2,29 @@ import React, { useState } from 'react';
 import './App.scss';
 import Carousel from './components/Carousel';
 
-/* type StateProps = {
-  images: string[],
+type StateProps = {
   step: number,
   frameSize: number,
   itemWidth: number,
   animationDuration: number,
   infinite: boolean,
-} */
+};
 
 export const App: React.FC = () => {
-/*   const [state, setState] = useState({
-    images: [
-      './img/1.png',
-      './img/2.png',
-      './img/3.png',
-      './img/4.png',
-      './img/5.png',
-      './img/6.png',
-      './img/7.png',
-      './img/8.png',
-      './img/9.png',
-      './img/10.png',
-    ],
+  const [state, setState] = useState<StateProps>({
     step: 3,
     frameSize: 3,
     itemWidth: 130,
     animationDuration: 1000,
     infinite: false,
-  } as StateProps); */
+  } as StateProps);
 
   const [pictures] = useState([
     './img/1.png',
     './img/2.png',
+    'https://media.glamourmagazine.co.uk/photos/64b6ae8fcd1ad7c51ecf045c/1:1/'
+    + 'w_1280,h_1280,c_limit/RYAN%20GOSLING%20EVA%20MENDES%20180723%20default'
+    + 'GettyImages-1527942629.jpg',
     './img/3.png',
     './img/4.png',
     './img/5.png',
@@ -45,47 +35,66 @@ export const App: React.FC = () => {
     './img/10.png',
   ]);
 
-  const [itemWidth, setItemWidth] = useState(130);
-
   document.documentElement.style.setProperty('--itemLength', `${pictures.length}px`);
-  document.documentElement.style.setProperty('--carouselWidth', `${pictures.length * itemWidth}px`);
+  document.documentElement.style.setProperty('--carouselWidth', `${pictures.length * state.itemWidth}px`);
+  document.documentElement.style.setProperty('--containerWidth', `${state.itemWidth * state.frameSize}px`);
 
-  // const [step, setStep] = useState(3);
-
- // const [frameSize, setFrameSize] = useState(3);
-
- // const [animationDuration, setAnimationDuration] = useState(1000);
- // const [infinite, setInfinite] = useState(false);
-
-/*   const changeHadler = (value: number, key: keyof StateProps) => {
-    setState(prevState => {
-      const newState: StateProps = { ...prevState };
-
-      newState[key] = value;
-
-      return newState;
-    });
-  }; */
+  const changeHadler = (value: number | boolean,
+    key: keyof StateProps) => {
+    setState(prevState => ({ ...prevState, [key]: value }));
+  };
 
   return (
     <div className="App">
-      {/* eslint-disable-next-line */}
-      <h1>Carousel with {pictures.length} images</h1>
+      <h1>{`Carousel with ${pictures.length} images, ${state.frameSize} frameSize and ${state.step} stepSize`}</h1>
 
-      <Carousel images={pictures} />
+      <Carousel
+        images={pictures}
+        step={state.step}
+        itemWidth={state.itemWidth}
+      />
       <input
         type="number"
         name="itemWidth"
         id="itemWidth"
-        value={itemWidth}
+        value={state.itemWidth}
         min={50}
         max={500}
         step={10}
         onChange={e => {
-          setItemWidth(+e.target.value);
+          changeHadler(+e.target.value, e.target.name as keyof StateProps);
           document.documentElement.style.setProperty('--itemWidth', `${+e.target.value}px`);
         }}
       />
+      <br />
+      <input
+        type="number"
+        name="frameSize"
+        id="frameSize"
+        value={state.frameSize}
+        min={1}
+        max={10}
+        step={1}
+        onChange={e => {
+          changeHadler(+e.target.value, e.target.name as keyof StateProps);
+          document.documentElement.style.setProperty('--containerWidth', `${state.itemWidth * state.frameSize}px`);
+        }}
+      />
+      frame size
+      <br />
+      <input
+        type="number"
+        name="step"
+        id="step"
+        value={state.step}
+        min={1}
+        max={10}
+        step={1}
+        onChange={e => {
+          changeHadler(+e.target.value, e.target.name as keyof StateProps);
+        }}
+      />
+      step size
     </div>
   );
 };
--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,27 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Carousel.scss';
 
 type Props = {
   images: string[],
+  step: number,
+  itemWidth: number,
 };
 
-const Carousel: React.FC<Props> = ({ images }) => {
+const Carousel: React.FC<Props> = ({ images, step, itemWidth }) => {
+  const [currentPos, setCurrentPos] = useState(0);
+
   const imagesToLi = (
     images.map((picture, index) => (
       <li className="Carousel__item">
-        <img src={picture} alt={index.toString()} />
+        <img className="Carousel__image" src={picture} alt={index.toString()} />
       </li>
     ))
   );
 
+  const carouselList = document.querySelector<HTMLElement>('.Carousel__list');
+
+  if (carouselList) {
+    carouselList.style.transform = `tranlateX(${currentPos * itemWidth}px)`;
+  }
+
+  const moveForward = () => {
+    if (currentPos + step < images.length - 1) {
+      setCurrentPos((prevPos) => prevPos + step);
+    }
+  };
+
+  const moveBackward = () => {
+    if (currentPos > 0) {
+      setCurrentPos((prevPos) => prevPos - step);
+    }
+  };
+
   return (
     <div className="Carousel">
-      <ul className="Carousel__list">
+      <ul className="Carousel__list" style={{ transform: `translateX(-${currentPos * itemWidth}px)` }}>
         {imagesToLi}
       </ul>
 
-      <button type="button">Prev</button>
-      <button type="button">Next</button>
+      <button
+        type="button"
+        disabled={currentPos === 0}
+        onClick={moveBackward}
+      >
+        Prev
+      </button>
+      <button type="button" onClick={moveForward}>Next</button>
     </div>
   );
 };
