3f509cb37241a0f2bbe65ba03ea1c4ee33aae571
Olena Shapkun <shapkunkh@gmail.com>
2023-12-20 19:58:15 +0200

    carousel solution

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,11 @@ import Carousel from './components/Carousel';
 
 interface State {
   images: string[];
+  step: number;
+  frameSize: number;
+  itemWidth: number;
+  animationDuration: number;
+  infinite: boolean;
 }
 
 class App extends React.Component<{}, State> {
@@ -20,17 +25,128 @@ class App extends React.Component<{}, State> {
       './img/9.png',
       './img/10.png',
     ],
+    itemWidth: 130,
+    frameSize: 3,
+    step: 3,
+    animationDuration: 1000,
+    infinite: false,
+  };
+
+  handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    switch (event.target.id) {
+      case 'stepId':
+        this.setState({ step: +event.target.value });
+        break;
+      case 'frameId':
+        this.setState({ frameSize: +event.target.value });
+        break;
+      case 'itemId':
+        this.setState({ itemWidth: +event.target.value });
+        break;
+      case 'animationDuration':
+        this.setState({ animationDuration: +event.target.value });
+        break;
+      case 'infinite':
+        this.setState((prevState) => ({
+          infinite: !prevState.infinite,
+        }));
+        break;
+
+      default:
+        break;
+    }
   };
 
   render() {
-    const { images } = this.state;
+    const {
+      images,
+      step,
+      frameSize,
+      itemWidth,
+      animationDuration,
+      infinite,
+    } = this.state;
+
+    const { handleInputChange } = this;
 
     return (
       <div className="App">
         {/* eslint-disable-next-line */}
-        <h1>Carousel with {images.length} images</h1>
+        <h1 data-cy="title">Carousel with {images.length} images</h1>
 
-        <Carousel />
+        <form action="#" className="App__form">
+          <label className="App__label" htmlFor="itemId">
+            Item Width
+            <input
+              type="number"
+              id="itemId"
+              name="itemId"
+              min="50"
+              max="200"
+              className="App__input"
+              step="5"
+              value={itemWidth}
+              onChange={handleInputChange}
+            />
+          </label>
+          <label className="App__label" htmlFor="frameId">
+            Frame size
+            <input
+              type="number"
+              id="frameId"
+              name="frameId"
+              min="1"
+              max="10"
+              className="App__input"
+              value={frameSize}
+              onChange={handleInputChange}
+            />
+          </label>
+          <label className="App__label" htmlFor="stepId">
+            Step
+            <input
+              type="number"
+              id="stepId"
+              name="stepId"
+              min="1"
+              max="9"
+              className="App__input"
+              value={step}
+              onChange={handleInputChange}
+            />
+          </label>
+          <label className="App__label" htmlFor="animationDuration">
+            Animation duration
+            <input
+              type="number"
+              id="animationDuration"
+              name="animationDuration"
+              className="App__input"
+              value={animationDuration}
+              onChange={handleInputChange}
+            />
+          </label>
+          <label className="App__label App__label--infinite" htmlFor="infinite">
+            Infinite
+            <input
+              type="checkbox"
+              id="infinite"
+              name="infinite"
+              className="App__input"
+              defaultChecked={infinite}
+              onChange={handleInputChange}
+            />
+          </label>
+        </form>
+
+        <Carousel
+          images={images}
+          step={step}
+          itemWidth={itemWidth}
+          frameSize={frameSize}
+          animationDuration={animationDuration}
+          infinite={infinite}
+        />
       </div>
     );
   }
--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,18 +1,122 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+type Props = {
+  images: string[],
+  step: number,
+  frameSize: number,
+  itemWidth: number,
+  animationDuration: number,
+  infinite: boolean,
+};
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+const Carousel: React.FC<Props> = ({
+  images,
+  step,
+  frameSize,
+  itemWidth,
+  animationDuration,
+  infinite,
+}) => {
+  const [currentPosition, setCurrentPosition] = useState(0);
+
+  const handlePrevClick = () => {
+    const newPosition = currentPosition - itemWidth * step;
+
+    if (infinite) {
+      setCurrentPosition(
+        newPosition < 0
+          ? (images.length - frameSize) * itemWidth
+          : newPosition,
+      );
+    } else {
+      setCurrentPosition(Math.max(newPosition, 0));
+    }
+  };
+
+  const handleNextClick = () => {
+    const remainingImages = images.length
+    - Math.ceil(currentPosition / itemWidth);
+
+    let newPosition;
+
+    if (infinite) {
+      if (remainingImages < step) {
+        newPosition = 0;
+      } else {
+        newPosition = currentPosition + itemWidth * step;
+        newPosition
+        = newPosition >= images.length * itemWidth
+            ? 0
+            : newPosition;
+      }
+    } else {
+      newPosition = currentPosition + itemWidth * step;
+      newPosition = Math.min(
+        newPosition,
+        (images.length - frameSize) * itemWidth,
+      );
+    }
+
+    setCurrentPosition(newPosition);
+  };
+
+  const carouselContainerStyles = {
+    width: `${itemWidth * frameSize}px`,
+    transitionDuration: `${animationDuration}ms`,
+  };
+
+  const carouselListStyles = {
+    width: `${itemWidth * images.length}px`,
+    height: `${itemWidth}px`,
+    transitionDuration: `${animationDuration}ms`,
+    transform: `translateX(-${currentPosition}px)`,
+  };
+
+  const carouselWrapperStyles = {
+    width: `${itemWidth * frameSize}px`,
+  };
+
+  return (
+    <div className="Carousel" style={carouselContainerStyles}>
+      <div className="Carousel__wrapper" style={carouselWrapperStyles}>
+        <ul className="Carousel__list" style={carouselListStyles}>
+          {images.map((image: string, index: number) => (
+            <li key={image} className="Carousel__item">
+              <img
+                src={image}
+                alt={`${index}`}
+                width={itemWidth}
+                className="Carousel__image"
+              />
+            </li>
+          ))}
+        </ul>
+        <div className="Carousel__btns">
+          <button
+            className="Carousel__btn"
+            type="button"
+            onClick={handlePrevClick}
+            disabled={!infinite && currentPosition === 0}
+          >
+            &lt;
+          </button>
+          <button
+            className="Carousel__btn"
+            type="button"
+            onClick={handleNextClick}
+            data-cy="next"
+            disabled={
+              !infinite
+                && currentPosition >= (itemWidth * (images.length - frameSize))
+            }
+          >
+            &gt;
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Carousel;
