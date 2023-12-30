afbda7a92f263e1abf04d68afaa9abb605b00c91
Yehor Korablin <yegor_ka2004@ukr.net>
2023-12-30 11:02:37 +0200

    Added step functional

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -3,6 +3,7 @@ import './Carousel.scss';
 
 const Carousel: React.FC = () => {
   const [currentImg, setCurrentImg] = useState(1);
+  const [step, setStep] = useState(1);
 
   return (
     <div className="Carousel">
@@ -14,18 +15,27 @@ const Carousel: React.FC = () => {
         <button
           type="button"
           className="Carousel__btn"
-          onClick={() => currentImg > 1 && setCurrentImg(currentImg - 1)}
+          onClick={() => currentImg > 1 && setCurrentImg(currentImg - step)}
         >
           Prev
         </button>
         <button
           type="button"
           className="Carousel__btn"
-          onClick={() => currentImg < 10 && setCurrentImg(currentImg + 1)}
+          onClick={() => currentImg < 10 && setCurrentImg(currentImg + step)}
         >
           Next
         </button>
       </ul>
+
+      <input
+        type="number"
+        className="Carousel__inp"
+        defaultValue={3}
+        min={1}
+        max={10}
+        onChange={() => setStep(2)}
+      />
     </div>
   );
 };
