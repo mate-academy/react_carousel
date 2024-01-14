68acca28508075f0dd86f9369caa09dbc06b0b37
Kseniia Nikolaienko <ksysha.nikolaenko778@gmail.com>
2024-01-15 00:52:40 +0200

    almost done

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,123 @@
-import React from 'react';
+import { useState } from 'react';
+
 import './App.scss';
 import Carousel from './components/Carousel';
+import type { Controls } from './types/Controls';
 
-interface State {
-  images: string[];
-}
+const defaultControls: Controls = {
+  step: 3,
+  frameSize: 3,
+  itemWidth: 130,
+  animationDuration: 1000,
+  isInfinite: false,
+};
 
-class App extends React.Component<{}, State> {
-  state = {
-    images: [
-      './img/1.png',
-      './img/2.png',
-      './img/3.png',
-      './img/4.png',
-      './img/5.png',
-      './img/6.png',
-      './img/7.png',
-      './img/8.png',
-      './img/9.png',
-      './img/10.png',
-    ],
+export const App = () => {
+  const [controls, setControls] = useState<Controls>(defaultControls);
+
+  const images = [
+    './img/1.png',
+    './img/2.png',
+    './img/3.png',
+    './img/4.png',
+    './img/5.png',
+    './img/6.png',
+    './img/7.png',
+    './img/8.png',
+    './img/9.png',
+    './img/10.png',
+  ];
+
+  const handleInputChange = (control: keyof Controls, value: number) => {
+    setControls(currentControls => ({ ...currentControls, [control]: value }));
   };
 
-  render() {
-    const { images } = this.state;
+  const toggleInfiniteCheckbox = () => {
+    setControls(currentControls => ({
+      ...currentControls, isInfinite: !currentControls.isInfinite,
+    }));
+  };
 
-    return (
-      <div className="App">
-        {/* eslint-disable-next-line */}
-        <h1 className='App__title'>Carousel with {images.length} images</h1>
+  return (
+    <div className="App">
+      <h1 className="App__title">
+        {`Carousel with ${images.length} images`}
+      </h1>
 
-        <Carousel
-          images={images}
-          step={2}
-          frameSize={3}
-          itemWidth={130}
-          animationDuration={1000}
-          infinite={false}
-        />
+      <div className="App__controls">
+        <label className="App__control">
+          Item width:
+          <input
+            type="number"
+            className="App__input"
+            value={controls.itemWidth}
+            onChange={(event) => {
+              handleInputChange('itemWidth', +event.target.value);
+            }}
+            step={10}
+          />
+        </label>
+
+        <label className="App__control">
+          Frame size:
+          <input
+            type="number"
+            className="App__input"
+            value={controls.frameSize}
+            min={1}
+            max={images.length}
+            onChange={(event) => {
+              handleInputChange('frameSize', +event.target.value);
+            }}
+          />
+        </label>
+
+        <label className="App__control">
+          Step:
+          <input
+            type="number"
+            className="App__input"
+            value={controls.step}
+            min={1}
+            max={images.length}
+            onChange={(event) => {
+              handleInputChange('step', +event.target.value);
+            }}
+          />
+        </label>
+
+        <label className="App__control">
+          Animation duration:
+          <input
+            type="number"
+            className="App__input"
+            value={controls.animationDuration}
+            min={0}
+            step={100}
+            onChange={(event) => {
+              handleInputChange('animationDuration', +event.target.value);
+            }}
+          />
+        </label>
+
+        <label className="App__control">
+          Infinite:
+          <input
+            type="checkbox"
+            className="App__check"
+            checked={controls.isInfinite}
+            onClick={toggleInfiniteCheckbox}
+          />
+        </label>
       </div>
-    );
-  }
-}
 
-export default App;
+      <Carousel
+        images={images}
+        step={controls.step}
+        frameSize={controls.frameSize}
+        itemWidth={controls.itemWidth}
+        animationDuration={controls.animationDuration}
+      />
+    </div>
+  );
+};
--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -20,61 +20,66 @@ const Carousel: React.FC<Props> = (props) => {
 
   const [translate, setTranslate] = useState(0);
 
-  const pixelStep = step * itemWidth;
+  const translateStep = step * itemWidth;
   const carouselWidth = frameSize * itemWidth;
   const maxTranslate = (images.length - frameSize) * itemWidth;
 
   const handlePrevClick = () => {
-    setTranslate(current => {
-      return current - pixelStep >= 0
-        ? current - pixelStep
+    setTranslate(currentTranslate => {
+      return currentTranslate - translateStep > 0
+        ? currentTranslate - translateStep
         : 0;
     });
   };
 
   const handleNextClick = () => {
     setTranslate(current => {
-      return current + pixelStep >= maxTranslate
-        ? maxTranslate
-        : current + pixelStep;
+      return current + translateStep < maxTranslate
+        ? current + translateStep
+        : maxTranslate;
     });
   };
 
   return (
     <div className="Carousel" style={{ width: `${carouselWidth}px` }}>
-      <ul
-        className="Carousel__list"
-        style={{
-          transform: `translateX(-${translate}px)`,
-          transition: `${animationDuration}ms transform`,
-        }}
-      >
-        {images.map((image, i) => (
-          <li key={image}>
-            <img
-              className="Carousel__img"
-              src={image}
-              alt={(i + 1).toString()}
-            />
-          </li>
-        ))}
-      </ul>
+      <div className="Carousel__frame">
+        <ul
+          className="Carousel__list"
+          style={{
+            transform: `translateX(-${translate}px)`,
+            transition: `${animationDuration}ms transform`,
+          }}
+        >
+          {images.map((image, i) => (
+            <li key={image}>
+              <img
+                className="Carousel__img"
+                src={image}
+                alt={(i + 1).toString()}
+                width={itemWidth}
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
 
       <div className="Carousel__buttons">
         <button
           type="button"
+          className="Carousel__button"
           onClick={handlePrevClick}
           disabled={!translate}
         >
-          Prev
+          {'<'}
         </button>
 
         <button
           type="button"
+          className="Carousel__button"
           onClick={handleNextClick}
           disabled={translate === maxTranslate}
         >
-          Next
+          {'>'}
         </button>
       </div>
     </div>
