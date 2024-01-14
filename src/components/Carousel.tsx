8a38d771d56d4247f651cd2aca1c5ff274ce0702
Kseniia Nikolaienko <ksysha.nikolaenko778@gmail.com>
2024-01-14 22:28:11 +0200

    start

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,18 +1,84 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+interface Props {
+  images: string[];
+  step: number;
+  frameSize: number;
+  itemWidth: number;
+  animationDuration: number;
+}
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+const Carousel: React.FC<Props> = (props) => {
+  const {
+    images,
+    step,
+    frameSize,
+    itemWidth,
+    animationDuration,
+  } = props;
+
+  const [translate, setTranslate] = useState(0);
+
+  const pixelStep = step * itemWidth;
+  const carouselWidth = frameSize * itemWidth;
+  const maxTranslate = (images.length - frameSize) * itemWidth;
+
+  const handlePrevClick = () => {
+    setTranslate(current => {
+      return current - pixelStep >= 0
+        ? current - pixelStep
+        : 0;
+    });
+  };
+
+  const handleNextClick = () => {
+    setTranslate(current => {
+      return current + pixelStep >= maxTranslate
+        ? maxTranslate
+        : current + pixelStep;
+    });
+  };
+
+  return (
+    <div className="Carousel" style={{ width: `${carouselWidth}px` }}>
+      <ul
+        className="Carousel__list"
+        style={{
+          transform: `translateX(-${translate}px)`,
+          transition: `${animationDuration}ms transform`,
+        }}
+      >
+        {images.map((image, i) => (
+          <li key={image}>
+            <img
+              className="Carousel__img"
+              src={image}
+              alt={(i + 1).toString()}
+            />
+          </li>
+        ))}
+      </ul>
+
+      <div className="Carousel__buttons">
+        <button
+          type="button"
+          onClick={handlePrevClick}
+          disabled={!translate}
+        >
+          Prev
+        </button>
+
+        <button
+          type="button"
+          onClick={handleNextClick}
+          disabled={translate === maxTranslate}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Carousel;
