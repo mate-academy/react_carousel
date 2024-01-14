408b147409d5994ba23bb69aa18f2f43ec4cb06f
unknown <bohdan.lesyk.kn.2021@lpnu.ua>
2024-01-14 16:44:05 +0200

    react-carousel

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,121 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import Carousel from './components/Carousel';
 
-interface State {
-  images: string[];
-}
+const images = [
+  './img/1.png',
+  './img/2.png',
+  './img/3.png',
+  './img/4.png',
+  './img/5.png',
+  './img/6.png',
+  './img/7.png',
+  './img/8.png',
+  './img/9.png',
+  './img/10.png',
+];
 
-class App extends React.Component<{}, State> {
-  state = {
-    images: [
-      './img/1.png',
-      './img/2.png',
-      './img/3.png',
-      './img/4.png',
-      './img/5.png',
-      './img/6.png',
-      './img/7.png',
-      './img/8.png',
-      './img/9.png',
-      './img/10.png',
-    ],
-  };
+const options = {
+  step: 3,
+  frame: 3,
+  width: 130,
+  animation: 1000,
+  infinite: false,
+};
 
-  render() {
-    const { images } = this.state;
+const MAX_CONTAINER_WIDTH = 1300;
 
-    return (
-      <div className="App">
-        {/* eslint-disable-next-line */}
-        <h1>Carousel with {images.length} images</h1>
+const App: React.FC = () => {
+  const [step, setStep] = useState(options.step);
+  const [frameSize, setFrameSize] = useState(options.frame);
+  const [itemWidth, setItemWidth] = useState(options.width);
+  const [animationDuration, setAnimationDuration] = useState(options.animation);
+  const [infinite, setInfinite] = useState(options.infinite);
 
-        <Carousel />
+  const getMaxFrames = Math.floor(MAX_CONTAINER_WIDTH / itemWidth);
+
+  return (
+    <div className="App">
+      <h1 data-cy="title">Carousel</h1>
+
+      <Carousel
+        images={images}
+        step={step}
+        frameSize={frameSize}
+        itemWidth={itemWidth}
+        animationDuration={animationDuration}
+        infinite={infinite}
+      />
+
+      <div
+        className="labels-container"
+        style={{
+          width: `${frameSize * itemWidth}px`,
+          transition: `${animationDuration}ms`,
+        }}
+      >
+        <label htmlFor="itemId" className="App__label">
+          {'Item Width: '}
+          <input
+            type="number"
+            id="itemId"
+            value={itemWidth}
+            min={130}
+            max={260}
+            step={10}
+            onChange={(event) => setItemWidth(+event.target.value)}
+          />
+        </label>
+
+        <label htmlFor="frameId" className="App__label">
+          {'Frame Size: '}
+          <input
+            type="number"
+            id="frameId"
+            value={frameSize}
+            min={1}
+            max={getMaxFrames}
+            step={1}
+            onChange={(event) => setFrameSize(+event.target.value)}
+          />
+        </label>
+
+        <label htmlFor="stepId" className="App__label">
+          {'Step: '}
+          <input
+            type="number"
+            id="stepId"
+            value={step}
+            min={1}
+            max={5}
+            onChange={(event) => setStep(+event.target.value)}
+          />
+        </label>
+
+        <label htmlFor="animationDurationId" className="App__label">
+          {'Animation duration: '}
+          <input
+            type="number"
+            id="animationDurationId"
+            value={animationDuration}
+            min={500}
+            max={5000}
+            step={500}
+            onChange={(event) => setAnimationDuration(+event.target.value)}
+          />
+        </label>
+
+        <label htmlFor="infiniteId" className="App__label">
+          {'Infinite '}
+          <input
+            type="checkbox"
+            id="infiniteId"
+            onChange={() => setInfinite(!infinite)}
+          />
+        </label>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
