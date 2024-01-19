87448c0ecca9cc2e9ed15a8c222c7911335ccfe5
Misha Martynov <martunmtm@gmail.com>
2024-01-19 16:27:08 +0200

    start_task

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,18 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+const STEP = 3;
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+type Props = {
+  images: string[]
+};
+
+const Carousel: React.FC<Props> = ({ images }) => {
+  const [startImage, setStartImage] = useState(0);
+  const preparedImages = images.slice(startImage, startImage + STEP);
+
+  return (
+    <div className="Carousel">
+      <div className="Carousel__container">
+        <ul className="Carousel__container__list">
+          {preparedImages.map((imageUrl, index) => (
+            <li className="Carousel__container__list__item" key={imageUrl}>
+              <img className="Carousel__container__list__img" src={imageUrl} alt={`${index + 1}`} />
+            </li>
+          ))}
+        </ul>
+        <div className="Carousel__container__button">
+          <button
+            onClick={() => setStartImage(
+              startImage !== 0
+                ? startImage - STEP
+                : startImage,
+            )}
+            type="button"
+          >
+            Prev
+          </button>
+          <button
+            onClick={() => setStartImage(
+              startImage < 10 - STEP
+                ? startImage + STEP
+                : startImage,
+            )}
+            type="button"
+          >
+            Next
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Carousel;
