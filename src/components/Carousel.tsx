4fc7434af53f83d1ec2b278b32828e68276cc250
Evhen Holub <golub0208@gmail.com>
2024-01-30 19:27:47 +0300

    carouse 1.0

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,18 +1,92 @@
 import React from 'react';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+type Props = {
+  images: string[];
+  step: number;
+  animDur: number;
+  frameSize: number;
+  itemWidth: number;
+  infinite: boolean
+  currentImage: number;
+  setCurrentImage: (value: number) => void;
+}
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+const Carousel: React.FC<Props> = ({
+  images,
+  step,
+  animDur,
+  frameSize,
+  itemWidth,
+  infinite,
+  currentImage,
+  setCurrentImage,
+}) => {
+  const imagesLeft = images.length - frameSize;
+
+  function goPrev() {
+    return (
+      currentImage !== 0
+        ? setCurrentImage(currentImage - step >= 0 ? currentImage - step : 0)
+        : setCurrentImage(imagesLeft)
+    );
+  }
+
+  function goNext() {
+    return (
+      currentImage !== imagesLeft
+        ? setCurrentImage(currentImage + step <= imagesLeft ? currentImage + step : imagesLeft)
+        : setCurrentImage(0)
+    );
+  }
+
+  const prevDisabled = (currentImage === 0) && !infinite;
+  const nextDisabled = (currentImage === imagesLeft) && !infinite;
+
+  return (
+    <div className="Carousel">
+      <ul className="Carousel__list">
+        {images.map((image, i) => (
+          <li
+            className='Carousel__image'
+          >
+            <img
+              src={image}
+              alt={`${i + 1}`}
+              width={itemWidth}
+            />
+            </li>
+        ))}
+      </ul>
+      <div className="Carousel__buttons">
+        <button
+          type="button"
+          className={
+            prevDisabled
+              ? 'Carousel__btn Carousel__btn--disabled'
+              : 'Carousel__btn Carousel__btn--active'
+          }
+          onClick={goPrev}
+          disabled={prevDisabled}
+        >
+          Prev
+        </button>
+        <button
+          type="button"
+          data-cy="next"
+          className={
+            nextDisabled
+              ? 'Carousel__btn Carousel__btn--disabled'
+              : 'Carousel__btn Carousel__btn--active'
+          }
+          onClick={goNext}
+          disabled={nextDisabled}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+}
 
 export default Carousel;
