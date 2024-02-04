1a4a7478fe09d491a11fd0686eae51f61fa9b607
Nikita Fesenko <fessenko.tr@gmail.com>
2024-02-04 09:26:30 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,117 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import Carousel from './components/Carousel';
 
-interface State {
-  images: string[];
-}
+const images = [
+  './img/1.png',
+  './img/2.png',
+  './img/3.png',
+  './img/4.png',
+  './img/5.png',
+  './img/6.png',
+  './img/7.png',
+  './img/8.png',
+  './img/9.png',
+  './img/10.png',
+];
 
-class App extends React.Component<{}, State> {
-  state = {
-    images: [
-      './img/1.png',
-      './img/2.png',
-      './img/3.png',
-      './img/4.png',
-      './img/5.png',
-      './img/6.png',
-      './img/7.png',
-      './img/8.png',
-      './img/9.png',
-      './img/10.png',
-    ],
+const App: React.FC = () => {
+  const [size, setSize] = useState(130);
+  const [offset, setOffset] = useState(0);
+  const [displayPicsQnty, setDisplayPicsQnty] = useState(3);
+  const [scrollStep, setScrollStep] = useState(3);
+  const [animationDuration, setAnimationDuration] = useState(1000);
+
+  const handleSizeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newSize = +e.target.value;
+    const currentPosition = offset / size;
+
+    setOffset(currentPosition * newSize);
+    setSize(newSize);
   };
 
-  render() {
-    const { images } = this.state;
+  const handlePicsQntyChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newPicsQty = +e.target.value;
+    const containerWidth = images.length * size;
+    const newVisibleArea = newPicsQty * size;
 
-    return (
-      <div className="App">
-        {/* eslint-disable-next-line */}
-        <h1>Carousel with {images.length} images</h1>
+    setDisplayPicsQnty(newPicsQty);
 
-        <Carousel />
-      </div>
-    );
-  }
-}
+    if (offset - newVisibleArea < -containerWidth) {
+      setOffset(offset + size);
+    }
+  };
+
+  return (
+    <div className="App">
+      {/* eslint-disable-next-line */}
+      <h1 data-cy="title" className="App__title">Carousel with {images.length} images</h1>
+
+      <Carousel
+        images={images}
+        width={size}
+        offset={offset}
+        onOffsetChange={setOffset}
+        picCount={displayPicsQnty}
+        step={scrollStep}
+        animationDuration={animationDuration}
+      />
+
+      <fieldset className="App__fieldset">
+        <legend>Carousel settings</legend>
+
+        <label className="App__label">
+          Picture width:
+          <input
+            className="App__input"
+            type="number"
+            min="10"
+            step="10"
+            value={size}
+            onChange={handleSizeChange}
+          />
+        </label>
+
+        <label className="App__label">
+          Frame size:
+          <input
+            className="App__input"
+            type="number"
+            min="1"
+            step="1"
+            max={images.length}
+            value={displayPicsQnty}
+            onChange={handlePicsQntyChange}
+          />
+        </label>
+
+        <label className="App__label">
+          Step:
+          <input
+            className="App__input"
+            type="number"
+            min="1"
+            step="1"
+            max={images.length}
+            value={scrollStep}
+            onChange={(e) => setScrollStep(+e.target.value)}
+          />
+        </label>
+
+        <label className="App__label">
+          Animation duration:
+          <input
+            className="App__input"
+            type="number"
+            min="0"
+            step="100"
+            value={animationDuration}
+            onChange={(e) => setAnimationDuration(+e.target.value)}
+          />
+        </label>
+      </fieldset>
+    </div>
+  );
+};
 
 export default App;
--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,18 +1,89 @@
-import React from 'react';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+type Props = {
+  images: string[],
+  width: number,
+  offset: number,
+  onOffsetChange: (newOffset:number) => void,
+  picCount: number,
+  step: number,
+  animationDuration: number,
+};
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+const Carousel: React.FC<Props> = ({
+  images,
+  width,
+  offset,
+  onOffsetChange,
+  picCount,
+  step,
+  animationDuration,
+}) => {
+  const liStyles = {
+    transform: `translate(${offset}px)`,
+    transition: `transform ${animationDuration}ms`,
+  };
+
+  const initialWidth = width * picCount;
+  const totalWidth = images.length * width;
+  const stepLength = width * step;
+  const nextOffsetTreshold = offset - initialWidth;
+  const prevOffsetTreshhold = 0;
+  const canMoveNext = nextOffsetTreshold - stepLength >= -totalWidth;
+  const canMovePrev = offset + stepLength <= prevOffsetTreshhold;
+
+  const handlePrevClick = () => {
+    onOffsetChange(canMovePrev ? offset + stepLength : offset - offset);
+  };
+
+  const handleNextClick = () => {
+    onOffsetChange(canMoveNext
+      ? offset - stepLength
+      : offset - (offset - initialWidth + totalWidth));
+  };
+
+  return (
+    <div className="Carousel">
+      <ul
+        className="Carousel__list"
+        style={{ width: `${initialWidth}px` }}
+      >
+        {images.map((img, i) => (
+          <li
+            key={img}
+            style={liStyles}
+          >
+            <img
+              src={img}
+              className="Carousel__img"
+              alt={`pic #${i + 1}`}
+              style={{ width: `${width}px` }}
+            />
+          </li>
+        ))}
+      </ul>
+
+      <div className="Carousel__btns">
+        <button
+          className="Carousel__btn"
+          type="button"
+          disabled={offset === prevOffsetTreshhold}
+          onClick={handlePrevClick}
+          data-cy="next"
+        >
+          &#8592;
+        </button>
+        <button
+          className="Carousel__btn"
+          type="button"
+          disabled={nextOffsetTreshold === -totalWidth}
+          onClick={handleNextClick}
+        >
+          &#8594;
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Carousel;
