3623672ccbc818c75d39cf0aff4d7dcce362e175
Andrii Solovei <solovey98.info@gmail.com>
2024-03-05 15:31:25 +0200

    create a carousel with pictures

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
 import Carousel from './components/Carousel';
 
-interface State {
-  images: string[];
-}
+const App: React.FC = () => {
+  const [images] = useState<string[]>([
+    './img/1.png',
+    './img/2.png',
+    './img/3.png',
+    './img/4.png',
+    './img/5.png',
+    './img/6.png',
+    './img/7.png',
+    './img/8.png',
+    './img/9.png',
+    './img/10.png',
+  ]);
 
-class App extends React.Component<{}, State> {
-  state = {
-    images: [
-      './img/1.png',
-      './img/2.png',
-      './img/3.png',
-      './img/4.png',
-      './img/5.png',
-      './img/6.png',
-      './img/7.png',
-      './img/8.png',
-      './img/9.png',
-      './img/10.png',
-    ],
+  const [step, setStep] = useState(3);
+  const [frame, setFrame] = useState(3);
+  const [width, setWidth] = useState(130);
+  const [animationDuration, setAnimationDuration] = useState(1000);
+  const [infinite, setInfinite] = useState(false);
+
+  const changeSettingsStep = (newStep: number) => {
+    setStep(newStep);
   };
 
-  render() {
-    const { images } = this.state;
+  const changeSettingsFrame = (newFrame: number) => {
+    setFrame(newFrame);
+  };
 
-    return (
-      <div className="App">
-        {/* eslint-disable-next-line */}
-        <h1>Carousel with {images.length} images</h1>
+  const changeSettingsWidth = (newWidth: number) => {
+    setWidth(newWidth);
+  };
 
-        <Carousel />
-      </div>
-    );
-  }
-}
+  const changeSettingsAnimationDuration = (newDuration: number) => {
+    setAnimationDuration(newDuration);
+  };
+
+  const changeSettingsInfinite = () => {
+    setInfinite(!infinite);
+  };
+
+  return (
+    <div className="App">
+      {/* eslint-disable-next-line */}
+      <h1 className="title">Carousel with {images.length} images</h1>
+
+      <Carousel
+        images={images}
+        step={step}
+        frameSize={frame}
+        animationDuration={animationDuration}
+        itemWidth={width}
+        infinite={infinite}
+        changeSettingsStep={changeSettingsStep}
+        changeSettingsFrame={changeSettingsFrame}
+        changeSettingsWidth={changeSettingsWidth}
+        changeSettingsAnimationDuration={changeSettingsAnimationDuration}
+        changeSettingsInfinite={changeSettingsInfinite}
+      />
+    </div>
+  );
+};
 
 export default App;
--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,26 +1,231 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './Carousel.scss';
+import { Setting } from './setting/Setting';
+import { SettingInfinite } from './setting/SettingInfinite';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li>
-        <img src="./img/1.png" alt="1" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="2" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="3" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="4" />
-      </li>
-    </ul>
+type Props = {
+  images: string[];
+  step: number;
+  frameSize: number;
+  animationDuration: number;
+  itemWidth: number;
+  infinite: boolean;
+  changeSettingsStep: (newStep: number) => void;
+  changeSettingsFrame: (newFrame: number) => void;
+  changeSettingsWidth: (newWidth: number) => void;
+  changeSettingsAnimationDuration: (newDuration: number) => void;
+  changeSettingsInfinite: () => void;
+};
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+const Carousel: React.FC<Props> = ({
+  images,
+  step,
+  frameSize,
+  animationDuration,
+  itemWidth,
+  infinite,
+  changeSettingsStep,
+  changeSettingsFrame,
+  changeSettingsWidth,
+  changeSettingsAnimationDuration,
+  changeSettingsInfinite,
+}) => {
+  const maxScroll = itemWidth * (10 - frameSize);
+  const [scroll, setScroll] = useState(0);
+  const [nextScroll, setNextScroll] = useState(scroll < maxScroll);
+  const [prevScroll, setPrevScroll] = useState(scroll === maxScroll);
+
+  const handleStepScroll = (event: React.ChangeEvent<HTMLInputElement>) => {
+    changeSettingsStep(Number(event.target.value));
+  };
+
+  const handleFrameOfSize = (event: React.ChangeEvent<HTMLInputElement>) => {
+    changeSettingsFrame(Number(event.target.value));
+    setScroll(0);
+  };
+
+  const handleWidthOfItem = (event: React.ChangeEvent<HTMLInputElement>) => {
+    changeSettingsWidth(Number(event.target.value));
+  };
+
+  const handleAnimationDuration = (
+    event: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    changeSettingsAnimationDuration(Number(event.target.value));
+  };
+
+  const handleChangeNext = () => {
+    const stepOfScroll = scroll + itemWidth * step;
+
+    if (scroll < maxScroll) {
+      setScroll(stepOfScroll > maxScroll ? maxScroll : stepOfScroll);
+    }
+  };
+
+  const handleChangePrev = () => {
+    const minScroll = 0;
+    const stepOfScroll = scroll - itemWidth * step;
+
+    if (scroll > minScroll) {
+      setScroll(stepOfScroll < minScroll ? minScroll : stepOfScroll);
+    }
+  };
+
+  const isInfinite = () => {
+    if (infinite) {
+      const moveNext = () => {
+        setTimeout(handleChangeNext, animationDuration);
+      };
+
+      const movePrev = () => {
+        setTimeout(handleChangePrev, animationDuration);
+      };
+
+      if (nextScroll) {
+        moveNext();
+        setNextScroll(scroll !== maxScroll);
+        setPrevScroll(scroll === maxScroll);
+      }
+
+      if (prevScroll) {
+        movePrev();
+        setPrevScroll(scroll !== 0);
+        setNextScroll(scroll === 0);
+      }
+    }
+  };
+
+  useEffect(() => {
+    isInfinite();
+  }, [infinite, scroll, prevScroll, nextScroll]);
+
+  return (
+    <>
+      <div
+        className="
+        Carousel
+        rounded
+        shadow
+        border
+        border-white-black
+        bg-white"
+      >
+        <ul
+          className="Carousel__list"
+          style={{
+            width: `${itemWidth * frameSize}px`,
+          }}
+        >
+          {images.map((el, index) => {
+            return (
+              <li
+                key={el}
+                style={{
+                  transform: `translateX(${-scroll}px)`,
+                  transition: `transform ${animationDuration}ms`,
+                }}
+              >
+                <img
+                  src={el}
+                  alt={`${index + 1}`}
+                  style={{
+                    width: `${itemWidth}px`,
+                  }}
+                />
+              </li>
+            );
+          })}
+        </ul>
+        <div className="Carousel__buttons">
+          <button
+            type="button"
+            className="Carousel__button btn btn-outline-secondary"
+            onClick={handleChangePrev}
+            disabled={scroll === 0}
+          >
+            Prev
+          </button>
+          <button
+            type="button"
+            className="Carousel__button btn btn-outline-secondary"
+            onClick={handleChangeNext}
+            disabled={scroll === maxScroll}
+          >
+            Next
+          </button>
+        </div>
+      </div>
+
+      <div
+        className="
+          Carousel__settings
+          rounded
+          d-flex
+          flex-column
+          mx-auto
+          shadow
+          border
+          border-white-black
+          bg-white
+          p-4"
+        style={{
+          width: '200px',
+        }}
+      >
+        <h1
+          className="pb-4 Carousel__settings--title"
+          style={{
+            fontSize: '35px',
+          }}
+        >
+          Settings
+        </h1>
+        <Setting
+          title="Step of scroll"
+          type="number"
+          id="scrollId"
+          min={1}
+          step="1"
+          defaultValue={3}
+          handleChange={handleStepScroll}
+        />
+        <Setting
+          title="Frame of size"
+          type="number"
+          id="frameSizeId"
+          min={1}
+          step="1"
+          defaultValue={3}
+          handleChange={handleFrameOfSize}
+        />
+        <Setting
+          title="Item Width"
+          type="number"
+          id="itemWidthId"
+          min={130}
+          step="10"
+          defaultValue={130}
+          handleChange={handleWidthOfItem}
+        />
+        <Setting
+          title="Animation Duration"
+          type="number"
+          id="animationDurationId"
+          min={0}
+          step="100"
+          defaultValue={1000}
+          handleChange={handleAnimationDuration}
+        />
+        <SettingInfinite
+          title="Infinite mode"
+          type="checkbox"
+          id="infiniteId"
+          handleChange={changeSettingsInfinite}
+        />
+      </div>
+    </>
+  );
+};
 
 export default Carousel;
