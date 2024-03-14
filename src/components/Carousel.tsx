bd373c2f80835b79f4294b02839959a10c93d566
Nirvacsh <nir.vlad.zu@gmail.com>
2024-03-14 09:23:36 +0200

    Solution

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,26 +1,86 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Carousel.scss';
+import { State } from '../types/State';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li>
-        <img src="./img/1.png" alt="1" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="2" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="3" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="4" />
-      </li>
-    </ul>
+const Carousel: React.FC<State> = ({
+  images,
+  step,
+  frameSize,
+  itemWidth,
+  animationDuration,
+  infinite,
+}) => {
+  const [startImage, setStartImage] = useState(0);
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+  const gap = 10;
+  const carouselWidth = frameSize * itemWidth + (frameSize - 1) * gap;
+
+  const handlePrevImage = () => {
+    if (infinite && startImage === 0) {
+      setStartImage(images.length - frameSize);
+    } else {
+      setStartImage(prevStartImage => Math.max(0, prevStartImage - step));
+    }
+  };
+
+  const handleNextImage = () => {
+    if (infinite && startImage >= images.length - frameSize) {
+      setStartImage(0);
+    } else {
+      setStartImage(prevStartImage =>
+        Math.min(images.length - frameSize, prevStartImage + step),
+      );
+    }
+  };
+
+  const translateX = startImage * (itemWidth + gap);
+
+  return (
+    <div className="Carousel">
+      <div
+        className="Carousel__container"
+        style={{
+          width: carouselWidth,
+        }}
+      >
+        <ul
+          className="Carousel__list"
+          style={{
+            gap,
+            transform: `translateX(-${translateX}px)`,
+            transition: `transform ${animationDuration}ms ease`,
+          }}
+        >
+          {images.map((image, index) => (
+            <li key={image}>
+              <img src={image} alt={`${index + 1}`} width={itemWidth} />
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="Carousel__buttons">
+        <button
+          type="button"
+          className="Carousel__button"
+          onClick={handlePrevImage}
+          disabled={!infinite && startImage === 0}
+        >
+          Prev
+        </button>
+
+        <button
+          type="button"
+          className="Carousel__button"
+          data-cy="next"
+          onClick={handleNextImage}
+          disabled={!infinite && startImage >= images.length - frameSize}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Carousel;
