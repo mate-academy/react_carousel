396e6f16c21eaf181d1c14e312c44683a07bad3f
Vlad Kach <kachevl@gmail.com>
2024-03-29 18:03:29 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,10 @@ import Carousel from './components/Carousel';
 
 interface State {
   images: string[];
+  step?: number;
+  frameSize?: number;
+  itemWidth?: number;
+  animationDuration?: number;
 }
 
 class App extends React.Component<{}, State> {
@@ -20,17 +24,96 @@ class App extends React.Component<{}, State> {
       './img/9.png',
       './img/10.png',
     ],
+
+    step: 3,
+    frameSize: 3,
+    itemWidth: 130,
+    animationDuration: 1000,
+  };
+
+  changeWidth = (event: React.ChangeEvent<HTMLInputElement>) => {
+    return this.setState({
+      ...this.state,
+      itemWidth: +event.target.value,
+    });
+  };
+
+  changeFrameSize = (event: React.ChangeEvent<HTMLInputElement>) => {
+    return this.setState({
+      ...this.state,
+      frameSize: +event.target.value,
+    });
+  };
+
+  changeStep = (event: React.ChangeEvent<HTMLInputElement>) => {
+    return this.setState({
+      ...this.state,
+      step: +event.target.value,
+    });
+  };
+
+  changeAnimationDuration = (event: React.ChangeEvent<HTMLInputElement>) => {
+    return this.setState({
+      ...this.state,
+      animationDuration: +event.target.value,
+    });
   };
 
   render() {
-    const { images } = this.state;
+    const { images, step, frameSize, itemWidth, animationDuration } =
+      this.state;
 
     return (
-      <div className="App">
+      <div className="App toCenter">
         {/* eslint-disable-next-line */}
-        <h1>Carousel with {images.length} images</h1>
+        <h1 data-cy="title">Carousel with {images.length} images</h1>
 
-        <Carousel />
+        <div className="inputs-container">
+          <label htmlFor="itemId">Width:</label>{' '}
+          <input
+            id="itemId"
+            type="number"
+            min={100}
+            max={200}
+            value={itemWidth}
+            onChange={this.changeWidth}
+          ></input>{' '}
+          <label htmlFor="frameId">Number:</label>{' '}
+          <input
+            id="frameId"
+            type="number"
+            min={1}
+            max={10}
+            value={frameSize}
+            onChange={this.changeFrameSize}
+          ></input>{' '}
+          <label htmlFor="stepId">Step:</label>{' '}
+          <input
+            id="stepId"
+            type="number"
+            min={1}
+            max={10}
+            value={step}
+            onChange={this.changeStep}
+          ></input>{' '}
+          <label htmlFor="animationDuration">Scrolling speed:</label>{' '}
+          <input
+            id="animationDuration"
+            type="number"
+            min={0}
+            max={5000}
+            value={animationDuration}
+            onChange={this.changeAnimationDuration}
+          ></input>
+        </div>
+
+        <Carousel
+          images={images}
+          itemWidth={itemWidth}
+          step={step}
+          frameSize={frameSize}
+          animationDuration={animationDuration}
+        />
       </div>
     );
   }
--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,26 +1,94 @@
 import React from 'react';
 import './Carousel.scss';
+import { useState } from 'react';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li>
-        <img src="./img/1.png" alt="1" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="2" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="3" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="4" />
-      </li>
-    </ul>
+type Props = {
+  images: string[];
+  itemWidth: number;
+  step: number;
+  frameSize: number;
+  animationDuration: number;
+};
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+const Carousel: React.FC<Props> = ({
+  images,
+  itemWidth,
+  frameSize,
+  step,
+  animationDuration,
+}) => {
+  const [firstIndex, setFirstIndex] = useState(0);
+  const transition = `transform ${animationDuration}ms ease-in-out`;
+  const nextImgCheck = firstIndex === images.length - frameSize;
+  const prevImgCheck = firstIndex === 0;
+
+  const changeIndexPrev = () => {
+    if (firstIndex > 0 && firstIndex - step >= 0) {
+      setFirstIndex(firstIndex - step);
+    } else {
+      setFirstIndex(0);
+    }
+  };
+
+  const changeIndexNext = () => {
+    if (
+      firstIndex < images.length - frameSize &&
+      firstIndex + step < images.length - frameSize
+    ) {
+      setFirstIndex(firstIndex + step);
+    } else {
+      setFirstIndex(images.length - frameSize);
+    }
+  };
+
+  return (
+    <div className="Carousel toCenter">
+      <div
+        className="container paddings"
+        style={{
+          width: `${itemWidth * frameSize}px`,
+          overflow: 'hidden',
+        }}
+      >
+        <ul
+          className="Carousel__list list"
+          style={{
+            transform: `translateX(-${firstIndex * itemWidth}px)`,
+            transition: transition,
+          }}
+        >
+          {images.map(image => {
+            return (
+              <li key={parseInt(image)}>
+                <img
+                  src={image}
+                  alt={parseInt(image).toString()}
+                  style={{ width: `${itemWidth}px` }}
+                />
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <div className="buttons-container paddings">
+        <button
+          type="button"
+          className={`button ${prevImgCheck ? 'disabled' : ''}`}
+          onClick={changeIndexPrev}
+        >
+          Prev
+        </button>
+        <button
+          data-cy="next"
+          type="button"
+          className={`button ${nextImgCheck ? 'disabled' : ''}`}
+          onClick={changeIndexNext}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Carousel;
