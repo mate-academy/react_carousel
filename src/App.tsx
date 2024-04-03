52b29367774fb68eddbeae33614274b310d3df08
Yuliia Nudyk <yulyayu833@gmail.com>
2024-04-03 05:36:24 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.scss';
 import Carousel from './components/Carousel';
+import { DefaultParameters } from './DefaultParameters';
 
 interface State {
   images: string[];
+  itemWidth: number;
+  frameSize: number;
+  step: number;
+  animationDuration: number;
 }
 
 class App extends React.Component<{}, State> {
@@ -20,17 +25,98 @@ class App extends React.Component<{}, State> {
       './img/9.png',
       './img/10.png',
     ],
+    itemWidth: DefaultParameters.ItemWidth,
+    frameSize: DefaultParameters.FrameSize,
+    step: DefaultParameters.Step,
+    animationDuration: DefaultParameters.AnimationDuration,
   };
 
   render() {
-    const { images } = this.state;
+    const { images, itemWidth, frameSize, step, animationDuration } =
+      this.state;
 
     return (
       <div className="App">
         {/* eslint-disable-next-line */}
-        <h1>Carousel with {images.length} images</h1>
+        <h1 data-cy="title">Carousel with {images.length} images</h1>
 
-        <Carousel />
+        <Carousel
+          images={images}
+          step={step}
+          frameSize={frameSize}
+          itemWidth={itemWidth}
+          animationDuration={animationDuration}
+          infinite={false}
+        />
+
+        <h2>Choose other parameters:</h2>
+
+        <form className="parametersForm">
+          <label className="parametersForm__input">
+            Item Width:
+            <input
+              type="number"
+              name="carouselParams"
+              id="itemWidth"
+              className="parametersForm__input-field"
+              min={50}
+              onChange={event => {
+                this.setState({
+                  itemWidth: +event.target.value || DefaultParameters.ItemWidth,
+                });
+              }}
+            />
+          </label>
+
+          <label className="parametersForm__input">
+            Frame Size:
+            <input
+              type="number"
+              name="carouselParams"
+              id="frameSize"
+              className="parametersForm__input-field"
+              min={1}
+              onChange={event => {
+                this.setState({
+                  frameSize: +event.target.value || DefaultParameters.FrameSize,
+                });
+              }}
+            />
+          </label>
+
+          <label className="parametersForm__input">
+            Step:
+            <input
+              type="number"
+              name="carouselParams"
+              className="parametersForm__input-field"
+              id="step"
+              min={1}
+              onChange={event => {
+                this.setState({
+                  step: +event.target.value || DefaultParameters.Step,
+                });
+              }}
+            />
+          </label>
+
+          <label className="parametersForm__input">
+            Animation Duration:
+            <input
+              type="number"
+              name="carouselParams"
+              className="parametersForm__input-field"
+              id="animationDuration"
+              min={1000}
+              onChange={event => {
+                this.setState({
+                  animationDuration:
+                    +event.target.value || DefaultParameters.AnimationDuration,
+                });
+              }}
+            />
+          </label>
+        </form>
       </div>
     );
   }
---
+++ src/DefaultParameters.ts
@@ -0,0 +1,6 @@
+export enum DefaultParameters {
+  ItemWidth = 130,
+  FrameSize = 3,
+  Step = 3,
+  AnimationDuration = 1000,
+}
