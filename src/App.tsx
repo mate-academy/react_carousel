1fa993e4b4820bd0f81a1dafc8c2cc9da47f6034
Katya <evil.666.buny@gmail.com>
2024-04-05 10:45:55 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,28 @@ import React from 'react';
 import './App.scss';
 import Carousel from './components/Carousel';
 
-interface State {
+type DefaultValues = {
+  itemWidth: number;
+  frameSize: number;
+  step: number;
+  animationDuration: number;
+  infinite: boolean;
+};
+
+const defaultValues: DefaultValues = {
+  itemWidth: 130,
+  frameSize: 3,
+  step: 3,
+  animationDuration: 1000,
+  infinite: false,
+};
+
+type State = DefaultValues & {
   images: string[];
-}
+};
 
 class App extends React.Component<{}, State> {
-  state = {
+  state: State = {
     images: [
       './img/1.png',
       './img/2.png',
@@ -20,17 +36,109 @@ class App extends React.Component<{}, State> {
       './img/9.png',
       './img/10.png',
     ],
+    step: defaultValues.step,
+    frameSize: defaultValues.frameSize,
+    itemWidth: defaultValues.itemWidth,
+    animationDuration: defaultValues.animationDuration,
+    infinite: defaultValues.infinite,
+  };
+
+  handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const name = event.target.name as keyof typeof this.state;
+
+    this.setState({
+      [name]: +event.target.value,
+    } as unknown as Pick<State, keyof State>);
   };
 
   render() {
-    const { images } = this.state;
+    const { images, step, frameSize, itemWidth, animationDuration, infinite } =
+      this.state;
 
     return (
       <div className="App">
         {/* eslint-disable-next-line */}
-        <h1>Carousel with {images.length} images</h1>
+        <h1 data-cy="title">Carousel with {images.length} images</h1>
 
-        <Carousel />
+        <form method="POST" action="#" className="control-panel">
+          <label htmlFor="itemId" className="control-panel__line-block">
+            Item Width:
+            <input
+              id="itemId"
+              name="itemWidth"
+              min="50"
+              step="10"
+              className="control-panel__input"
+              value={itemWidth}
+              onChange={this.handleChange}
+              type="number"
+            />
+          </label>
+
+          <label htmlFor="frameId" className="control-panel__line-block">
+            Frame Size:
+            <input
+              id="frameId"
+              name="frameSize"
+              min="1"
+              max={images.length}
+              className="control-panel__input"
+              value={frameSize}
+              onChange={this.handleChange}
+              type="number"
+            />
+          </label>
+
+          <label htmlFor="stepId" className="control-panel__line-block">
+            Step:
+            <input
+              id="stepId"
+              name="step"
+              min="1"
+              className="control-panel__input"
+              value={step}
+              onChange={this.handleChange}
+              type="number"
+            />
+          </label>
+
+          <label
+            htmlFor="animationDuration"
+            className="control-panel__line-block"
+          >
+            Animation Duration:
+            <input
+              id="animationDuration"
+              name="animationDuration"
+              min="100"
+              step="100"
+              className="control-panel__input"
+              value={animationDuration}
+              onChange={this.handleChange}
+              type="number"
+            />
+          </label>
+
+          <label htmlFor="infinite">
+            * infinite
+            <input
+              id="infinite"
+              name="infinite"
+              checked={infinite}
+              onChange={() => this.setState({ infinite: !infinite })}
+              type="checkbox"
+            />
+          </label>
+        </form>
+
+        <Carousel
+          images={images}
+          step={step}
+          frameSize={frameSize}
+          itemWidth={itemWidth}
+          animationDuration={animationDuration}
+          infinite={infinite}
+        />
       </div>
     );
   }
