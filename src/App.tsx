844f1751c01f625d344daf80ac14885216619d68
Hlib Kaponkin <glebkapenkin@gmail.com>
2024-04-07 22:43:12 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,10 @@ import Carousel from './components/Carousel';
 
 interface State {
   images: string[];
+  itemWidth: number;
+  frameSize: number;
+  step: number;
+  animationDuration: number;
 }
 
 class App extends React.Component<{}, State> {
@@ -20,17 +24,77 @@ class App extends React.Component<{}, State> {
       './img/9.png',
       './img/10.png',
     ],
+    itemWidth: 130,
+    frameSize: 3,
+    step: 3,
+    animationDuration: 1000,
+  };
+
+  changeValue = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { id, value } = event.target;
+
+    this.setState(prevState => ({
+      ...prevState,
+      [id]: +value,
+    }));
   };
 
   render() {
-    const { images } = this.state;
+    const { images, itemWidth, frameSize, step, animationDuration } =
+      this.state;
 
     return (
       <div className="App">
         {/* eslint-disable-next-line */}
         <h1>Carousel with {images.length} images</h1>
 
-        <Carousel />
+        <Carousel
+          images={images}
+          itemWidth={itemWidth}
+          frameSize={frameSize}
+          step={step}
+          animationDuration={animationDuration}
+        />
+        <div className="form">
+          <div>
+            <label htmlFor="itemWidth">
+              Enter width:{' '}
+              <input
+                type="number"
+                id="itemWidth"
+                value={itemWidth}
+                onChange={this.changeValue}
+              />
+            </label>
+          </div>
+          <div>
+            <label htmlFor="frameSize">Enter frame size</label>
+            <input
+              type="number"
+              id="frameSize"
+              value={frameSize}
+              onChange={this.changeValue}
+            />
+          </div>
+          <div>
+            <label htmlFor="step">Enter step</label>
+            <input
+              type="number"
+              id="step"
+              value={step}
+              onChange={this.changeValue}
+            />
+          </div>
+          <div>
+            <label htmlFor="animationDuration">Enter animation duration</label>
+            <input
+              type="number"
+              id="animationDuration"
+              value={animationDuration}
+              onChange={this.changeValue}
+            />
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,26 +1,81 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li>
-        <img src="./img/1.png" alt="1" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="2" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="3" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="4" />
-      </li>
-    </ul>
+type Props = {
+  images: string[];
+  itemWidth: number;
+  frameSize: number;
+  step: number;
+  animationDuration: number;
+};
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+const Carousel: React.FC<Props> = ({
+  images,
+  itemWidth,
+  frameSize,
+  step,
+  animationDuration,
+}) => {
+  const GAP = 20;
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const maxWidth = frameSize * itemWidth + (frameSize - 1) * GAP;
+  const handleNext = () => {
+    setCurrentIndex(Math.min(currentIndex + step, images.length - frameSize));
+  };
+
+  const handlePrevious = () => {
+    setCurrentIndex(Math.max(currentIndex - step, 0));
+  };
+
+  const styles = {
+    gap: `${GAP}px`,
+    maxWidth: `${maxWidth}px`,
+    transform: `translateX(-${currentIndex * (itemWidth + GAP)}px)`,
+    transition: `transform ${animationDuration}ms ease-out`,
+  };
+
+  useEffect(() => {
+    const maxIndex = images.length - frameSize;
+
+    if (currentIndex > maxIndex) {
+      setCurrentIndex(maxIndex);
+    }
+  }, [frameSize, images.length, currentIndex]);
+
+  return (
+    <div className="carousel">
+      <button
+        type="button"
+        onClick={handlePrevious}
+        disabled={currentIndex === 0}
+      >
+        Prev
+      </button>
+      <div className="carousel__container">
+        <ul className="carousel__list" style={styles}>
+          {images.map((image: string, index: number) => (
+            <li key={index}>
+              <img
+                src={image}
+                alt={`${index}`}
+                style={{
+                  width: `${itemWidth}px`,
+                }}
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <button
+        type="button"
+        onClick={handleNext}
+        disabled={currentIndex >= images.length - frameSize}
+      >
+        Next
+      </button>
+    </div>
+  );
+};
 
 export default Carousel;
