9d28ac43232dfb61fa88d33f9727766117350587
Krot Nikita <krotnikitaasd@gmail.com>
2024-04-12 15:06:23 +0300

    carousel

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,26 +1,79 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li>
-        <img src="./img/1.png" alt="1" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="2" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="3" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="4" />
-      </li>
-    </ul>
+type Props = {
+  images: string[];
+  step: number;
+  frameSize: number;
+  itemWidth: number;
+  animationDuration: number;
+};
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+const Carousel: React.FC<Props> = ({
+  images,
+  step,
+  frameSize,
+  itemWidth,
+  animationDuration,
+}) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const IMAGES_GAP = 10;
+  const wrapperWidth = itemWidth * frameSize + (frameSize - 1) * IMAGES_GAP;
+
+  const wrapperStyles = {
+    transform: `translateX(-${currentIndex * (itemWidth + IMAGES_GAP)}px)`,
+    transition: `transform ${animationDuration}ms ease-out`,
+    gap: `${IMAGES_GAP}px`,
+    width: `${wrapperWidth}px`,
+  };
+
+  const handleNextButton = () => {
+    setCurrentIndex(Math.min(currentIndex + step, images.length - frameSize));
+  };
+
+  const handlePrevButton = () => {
+    setCurrentIndex(Math.max(currentIndex - step, 0));
+  };
+
+  return (
+    <div className="Carousel">
+      <button
+        type="button"
+        onClick={handlePrevButton}
+        className="button"
+        disabled={currentIndex === 0}
+      >
+        Prev
+      </button>
+
+      <article className="Carousel__container">
+        <ul className="Carousel__list" style={wrapperStyles}>
+          {images.map((image: string) => (
+            <li key={image} className="Carousel__item">
+              <img
+                src={image}
+                alt={`emoji ${image}`}
+                width={itemWidth}
+                height={itemWidth}
+                className="Carousel__image"
+              />
+            </li>
+          ))}
+        </ul>
+      </article>
+
+      <button
+        type="button"
+        data-cy="next"
+        onClick={handleNextButton}
+        className="button"
+        disabled={currentIndex >= images.length - frameSize}
+      >
+        Next
+      </button>
+    </div>
+  );
+};
 
 export default Carousel;
