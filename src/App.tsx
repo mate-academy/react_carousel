3388d85bdf1aa077e3686c37d939a7bf4d0dc837
Denis Reznik <reznik.d.o12@gmail.com>
2024-04-30 22:50:02 +0300

    add solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import Carousel from './components/Carousel';
 
@@ -6,8 +6,14 @@ interface State {
   images: string[];
 }
 
-class App extends React.Component<{}, State> {
-  state = {
+export const App: React.FC = () => {
+  const [step, setStep] = useState(3);
+  const [frameSize, setFrameSize] = useState(3);
+  const [itemWidth, setItemwidth] = useState(130);
+  const [animationDuration, setAnimationDuration] = useState(1000);
+  const [infinite, setInfinite] = useState(false);
+
+  const state: State = {
     images: [
       './img/1.png',
       './img/2.png',
@@ -22,18 +28,140 @@ class App extends React.Component<{}, State> {
     ],
   };
 
-  render() {
-    const { images } = this.state;
+  const handleItemWidth = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const width = parseInt(event.target.value);
 
-    return (
-      <div className="App">
-        {/* eslint-disable-next-line */}
-        <h1>Carousel with {images.length} images</h1>
+    if (!isNaN(width)) {
+      setItemwidth(width);
+    }
+  };
 
-        <Carousel />
+  const handleFrameSize = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const size = parseInt(event.target.value);
+
+    if (!isNaN(size)) {
+      setFrameSize(size);
+    }
+  };
+
+  const handleStep = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const steps = parseInt(event.target.value);
+
+    if (!isNaN(steps)) {
+      setStep(steps);
+    }
+  };
+
+  const handleAnimationDuration = (
+    event: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    const value = parseInt(event.target.value);
+
+    if (!isNaN(value)) {
+      setAnimationDuration(value);
+    }
+  };
+
+  const handleInfinity = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.checked;
+
+    setInfinite(value);
+  };
+
+  return (
+    <div className="App">
+      {/* eslint-disable-next-line */}
+      <h1 className="App__title" data-cy="title">
+        Carousel with {state.images.length} images
+      </h1>
+
+      <div className="App__container">
+        <div className="App__container--element">
+          <label className="App__container-lable" htmlFor="itemId">
+            Item width:{' '}
+          </label>
+          <input
+            id="itemId"
+            name="itemId"
+            type="number"
+            min="50"
+            max="350"
+            onChange={handleItemWidth}
+            value={itemWidth}
+            className="App__container-input"
+          />
+        </div>
+        <div className="App__container--element">
+          <label className="App__container-lable" htmlFor="frameId">
+            Frame Size:{' '}
+          </label>
+          <input
+            id="frameId"
+            name="frameSize"
+            type="number"
+            min="1"
+            max={state.images.length}
+            onChange={handleFrameSize}
+            value={frameSize}
+            className="App__container-input"
+          />
+        </div>
+        <div className="App__container--element">
+          <label className="App__container-lable" htmlFor="stepId">
+            Step:{' '}
+          </label>
+          <input
+            id="stepId"
+            name="step"
+            type="number"
+            min="1"
+            max={state.images.length}
+            onChange={handleStep}
+            value={step}
+            className="App__container-input"
+          />
+        </div>
+        <div className="App__container--element">
+          <label className="App__container-lable" htmlFor="animationnId">
+            Animation Duration:{' '}
+          </label>
+          <input
+            id="animationnId"
+            name="animationDuration"
+            type="number"
+            min="500"
+            step="100"
+            max="3000"
+            onChange={handleAnimationDuration}
+            value={animationDuration}
+            className="App__container-input"
+          />
+        </div>
+        <div className="App__container--element">
+          <label className="App__container-lable" htmlFor="infiniteId">
+            Infinite:{' '}
+          </label>
+          <input
+            id="infiniteId"
+            name="infinite"
+            type="checkbox"
+            onChange={handleInfinity}
+            checked={infinite}
+            className="App__container-input"
+          />
+        </div>
       </div>
-    );
-  }
-}
+
+      <Carousel
+        images={state.images}
+        step={step}
+        frameSize={frameSize}
+        itemWidth={itemWidth}
+        animationDuration={animationDuration}
+        infinite={infinite}
+      />
+    </div>
+  );
+};
 
 export default App;
--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,26 +1,100 @@
-import React from 'react';
+import React, { useState } from 'react';
+import cn from 'classnames';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li>
-        <img src="./img/1.png" alt="1" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="2" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="3" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="4" />
-      </li>
-    </ul>
+type Props = {
+  images: string[];
+  step: number;
+  frameSize: number;
+  itemWidth: number;
+  animationDuration: number;
+  infinite: boolean;
+};
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+const Carousel: React.FC<Props> = ({
+  images,
+  step = 3,
+  frameSize = 3,
+  itemWidth = 130,
+  animationDuration = 1000,
+  infinite = false,
+}) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const widthContainer = itemWidth * frameSize;
+  const widthList = itemWidth * images.length;
+  const endIndex = images.length - frameSize;
+
+  const handleNextTransform = () => {
+    if (currentIndex < endIndex) {
+      setCurrentIndex(prev =>
+        prev + step <= endIndex ? prev + step : endIndex,
+      );
+    } else {
+      setCurrentIndex(0);
+    }
+  };
+
+  const handlePrevTransform = () => {
+    if (currentIndex > 0) {
+      setCurrentIndex(prev => (prev - step >= 0 ? prev - step : 0));
+    } else {
+      setCurrentIndex(endIndex);
+    }
+  };
+
+  return (
+    <div className="Carousel" style={{ width: widthContainer }}>
+      <div className="Carousel__wrapper">
+        <div
+          className="Carousel__container"
+          style={{ width: widthContainer, height: itemWidth }}
+        >
+          <ul
+            className="Carousel__list"
+            style={{
+              width: widthList,
+              height: itemWidth,
+              transform: `translate(-${currentIndex * itemWidth}px)`,
+              transitionDuration: `${animationDuration}ms`,
+            }}
+          >
+            {images.map((image, index) => (
+              <li key={index} style={{ width: itemWidth }}>
+                <img src={image} alt={`${index + 1}`} width={itemWidth} />
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="Carousel__button-container">
+          <button
+            className={cn('Carousel__button', 'Carousel__button--left', {
+              disabled: !infinite && currentIndex === 0,
+            })}
+            type="button"
+            disabled={!infinite && currentIndex === 0}
+            onClick={handlePrevTransform}
+          >
+            <svg className="Carousel__icon" width="32" height="32">
+              <use href="./img/icons/sprite.svg#arrow-left"></use>
+            </svg>
+          </button>
+          <button
+            className={cn('Carousel__button', 'Carousel__button--right', {
+              disabled: currentIndex + frameSize >= images.length && !infinite,
+            })}
+            type="button"
+            data-cy="next"
+            disabled={currentIndex + frameSize >= images.length && !infinite}
+            onClick={handleNextTransform}
+          >
+            <svg className="Carousel__icon" width="32" height="32">
+              <use href="./img/icons/sprite.svg#arrow-right"></use>
+            </svg>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Carousel;
