7e6d311e71fdf9a20c40dbcf2c26c63ffc4e9351
Slava Filimonenko <=>
2024-05-06 14:38:19 +0300

    fixed

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,4 @@
 import { createRoot } from 'react-dom/client';
 import App from './App';
-// import '@fortawesome/fontawesome-free/css/all.css';
-// import 'bulma';
 
 createRoot(document.getElementById('root') as HTMLElement).render(<App />);
