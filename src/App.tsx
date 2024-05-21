37405d05792eba67cda6447c445cfa0ec0bd0731
Tetyana Shatrova <shatrova.tetyana@gmail.com>
2024-05-21 14:34:51 +0300

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
+// import React, { useState } from 'react';
 import React from 'react';
 import './App.scss';
 import Carousel from './components/Carousel';
 
 interface State {
   images: string[];
+  itemWidth: number;
+  frameSize: number;
+  stepImage: number;
+  animationDuration: number;
+  infinite: boolean;
 }
 
 class App extends React.Component<{}, State> {
+  // state: Readonly<State> = {
   state = {
     images: [
       './img/1.png',
@@ -20,6 +27,11 @@ class App extends React.Component<{}, State> {
       './img/9.png',
       './img/10.png',
     ],
+    itemWidth: 130,
+    frameSize: 3,
+    stepImage: 3,
+    animationDuration: 1000,
+    infinite: false,
   };
 
   render() {
@@ -28,9 +40,53 @@ class App extends React.Component<{}, State> {
     return (
       <div className="App">
         {/* eslint-disable-next-line */}
-        <h1>Carousel with {images.length} images</h1>
+        <h1 data-cy="title">Carousel with {images.length} images</h1>
 
-        <Carousel />
+        <label htmlFor="step">stepImage</label>
+        <input
+          id="step"
+          type="text"
+          onChange={e => this.setState({ stepImage: parseInt(e.target.value) })}
+        />
+
+        <label htmlFor="frame">frameSize</label>
+        <input
+          id="frame"
+          type="text"
+          onChange={e => this.setState({ frameSize: parseInt(e.target.value) })}
+        />
+
+        <label htmlFor="width">itemWidth</label>
+        <input
+          id="width"
+          type="text"
+          onChange={e => this.setState({ itemWidth: parseInt(e.target.value) })}
+        />
+
+        <label htmlFor="animation">animationDuration</label>
+        <input
+          id="animation"
+          type="number"
+          onChange={e =>
+            this.setState({ animationDuration: parseInt(e.target.value) })
+          }
+        />
+
+        <label htmlFor="infinite">infinite </label>
+        <input
+          id="infinite"
+          type="checkbox"
+          onChange={e => this.setState({ infinite: e.target.checked })}
+        />
+
+        <Carousel
+          images={images}
+          step={this.state.stepImage}
+          frameSize={this.state.frameSize}
+          itemWidth={this.state.itemWidth}
+          animationDuration={this.state.animationDuration}
+          infinite={false}
+        />
       </div>
     );
   }
--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,26 +1,127 @@
 import React from 'react';
+import { useState } from 'react';
 import './Carousel.scss';
+// import { transform } from 'cypress/types/lodash';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li>
-        <img src="./img/1.png" alt="1" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="2" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="3" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="4" />
-      </li>
-    </ul>
+type Props = {
+  images: string[];
+  step: number;
+  frameSize: number;
+  itemWidth: number;
+  animationDuration: number;
+  infinite: boolean;
+};
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+const Carousel: React.FC<Props> = ({
+  images,
+  step,
+  frameSize,
+  itemWidth,
+  animationDuration,
+  infinite,
+}) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const transform = `translateX(${-currentIndex * itemWidth + -currentIndex * 10}px)`;
+  const transition = `transform ${animationDuration}ms ease-in-out`;
+
+  const imagesCopy = [...images];
+
+  if (infinite) {
+    imagesCopy.unshift(...images.slice(-step));
+    imagesCopy.push(...images.slice(0, step));
+  }
+
+  const handleNext = () => {
+    if (infinite) {
+      setCurrentIndex(
+        currentIndex + 2 * step > imagesCopy.length
+          ? imagesCopy.length - currentIndex + 2 * step
+          : currentIndex + step,
+      );
+    } else {
+      setCurrentIndex(
+        currentIndex + 2 * step > images.length
+          ? images.length - step
+          : currentIndex + step,
+      );
+    }
+  };
+
+  const handlePrev = () => {
+    if (infinite) {
+      setCurrentIndex(
+        currentIndex - step < 0
+          ? imagesCopy.length - step
+          : currentIndex - step,
+      );
+    } else {
+      setCurrentIndex(currentIndex - step < 0 ? 0 : currentIndex - step);
+    }
+  };
+
+  return (
+    <div className="Carousel" style={{ width: frameSize * itemWidth + 25 }}>
+      <ul
+        className="Carousel__list"
+        style={{ transform: transform, transition: transition }}
+      >
+        {images.map((image: string) => {
+          const alt = image.slice(6, -4);
+
+          return (
+            <li key={image}>
+              <img
+                src={image}
+                alt={alt}
+                className="Carousel__image"
+                style={{ width: itemWidth }}
+              />
+            </li>
+          );
+        })}
+      </ul>
+      <div className="Carousel__button">
+        <button
+          type="button"
+          className="Carousel__button--prev"
+          onClick={handlePrev}
+        ></button>
+        <button
+          type="button"
+          className="Carousel__button--next"
+          data-cy="next"
+          onClick={handleNext}
+        ></button>
+        {/* <button type="button" className='Carousel__button--prev' disabled={currentIndex === 0} onClick={handlePrev}></button>
+      <button type="button" className='Carousel__button--next' disabled={currentIndex === images.length - step} data-cy='next' 
+      onClick={handleNext}></button> */}
+      </div>
+    </div>
+  );
+};
 
 export default Carousel;
+
+// const [currentIndex, setCurrentIndex] = useState(0);
+// const transform = `translateX(${-currentIndex * itemWidth + (-currentIndex * 10)}px)`;
+// const transition = `transform ${animationDuration}ms ease-in-out`;
+// console.log(currentIndex);
+// return (
+// <div className="Carousel" style={{width: frameSize * itemWidth + 25}}>
+//   <ul className="Carousel__list" style={{ transform: transform, transition: transition}}>
+//     {images.map((image: string) => {
+//       const alt = image.slice(6, -4);
+//       return(
+//       <li key={image}>
+//       <img src={image} alt={alt} className='Carousel__image' style={{width: itemWidth}}/>
+//     </li>
+//     )})}
+//   </ul>
+//   <div className='Carousel__button'>
+//     <button type="button" className='Carousel__button--prev' disabled={currentIndex === 0} onClick={() => setCurrentIndex(currentIndex - step < 0 ? 0 : currentIndex - step)}></button>
+//     <button type="button" className='Carousel__button--next' disabled={currentIndex === images.length - step} data-cy='next' onClick={() => setCurrentIndex (currentIndex + 2 * step > images.length ? images.length - step : currentIndex + step)}></button>
+//   </div>
+// </div>
+// )};
+
+// export default Carousel;
