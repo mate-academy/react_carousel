8158f61b62ba405d3ad5e643cb3138e989dcaa92
Tetyana Shatrova <shatrova.tetyana@gmail.com>
2024-05-28 13:08:02 +0300

    add task solution

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -20,41 +20,37 @@ const Carousel: React.FC<Props> = ({
   infinite,
 }) => {
   const [currentIndex, setCurrentIndex] = useState(infinite ? step : 0);
-  // const [currentImages, setCurrentImages] = useState(images);
   const transform = `translateX(${-currentIndex * itemWidth + -currentIndex * 10}px)`;
   const transition = `transform ${animationDuration}ms ease-in-out`;
 
-  // const imagesCopy = [...images];
-
-  // if (infinite) {
-  //   imagesCopy.unshift(...images.slice(-step));
-  //   imagesCopy.push(...images.slice(0, step));
-  // }
-
   const handleNext = () => {
-    if (infinite) {
-      setCurrentIndex(
-        currentIndex + step > images.length ? step : currentIndex + step,
-      );
-    } else {
-      setCurrentIndex(
-        currentIndex + 2 * step > images.length
-          ? images.length - step
-          : currentIndex + step,
-      );
+    if (
+      (infinite && currentIndex === images.length - step) ||
+      currentIndex + frameSize === images.length
+    ) {
+      setCurrentIndex(0);
+
+      return;
     }
+
+    setCurrentIndex(
+      currentIndex + step + frameSize > images.length
+        ? images.length -
+            (step < frameSize || frameSize === 1 ? frameSize : step)
+        : currentIndex + step,
+    );
   };
 
   const handlePrev = () => {
-    if (infinite) {
+    if (infinite && currentIndex === 0 && frameSize !== 10) {
       setCurrentIndex(
-        currentIndex === 0 || currentIndex === -2
-          ? images.length - step
-          : currentIndex - step,
+        step > frameSize ? images.length - step : images.length - frameSize,
       );
-    } else {
-      setCurrentIndex(currentIndex - step < 0 ? 0 : currentIndex - step);
+
+      return;
     }
+
+    setCurrentIndex(currentIndex - step < 0 ? 0 : currentIndex - step);
   };
 
   const gap = 10;
@@ -96,36 +92,9 @@ const Carousel: React.FC<Props> = ({
           data-cy="next"
           onClick={handleNext}
         ></button>
-        {/* <button type="button" className='Carousel__button--prev' disabled={currentIndex === 0} onClick={handlePrev}></button>
-      <button type="button" className='Carousel__button--next' disabled={currentIndex === images.length - step} data-cy='next' 
-      onClick={handleNext}></button> */}
       </div>
     </div>
   );
 };
 
 export default Carousel;
-
-// const [currentIndex, setCurrentIndex] = useState(0);
-// const transform = `translateX(${-currentIndex * itemWidth + (-currentIndex * 10)}px)`;
-// const transition = `transform ${animationDuration}ms ease-in-out`;
-// console.log(currentIndex);
-// return (
-// <div className="Carousel" style={{width: frameSize * itemWidth + 25}}>
-//   <ul className="Carousel__list" style={{ transform: transform, transition: transition}}>
-//     {images.map((image: string) => {
-//       const alt = image.slice(6, -4);
-//       return(
-//       <li key={image}>
-//       <img src={image} alt={alt} className='Carousel__image' style={{width: itemWidth}}/>
-//     </li>
-//     )})}
-//   </ul>
-//   <div className='Carousel__button'>
-//     <button type="button" className='Carousel__button--prev' disabled={currentIndex === 0} onClick={() => setCurrentIndex(currentIndex - step < 0 ? 0 : currentIndex - step)}></button>
-//     <button type="button" className='Carousel__button--next' disabled={currentIndex === images.length - step} data-cy='next' onClick={() => setCurrentIndex (currentIndex + 2 * step > images.length ? images.length - step : currentIndex + step)}></button>
-//   </div>
-// </div>
-// )};
-
-// export default Carousel;
