c3bdb9a9006e8d73317ba821227f909de56ec820
Teollan <serhiikofficial@gmail.com>
2024-08-12 19:42:59 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,11 @@ import Carousel from './components/Carousel';
 
 interface State {
   images: string[];
+  itemWidth: number;
+  frameSize: number;
+  step: number;
+  animationDuration: number;
+  isInfinite: boolean;
 }
 
 class App extends React.Component<{}, State> {
@@ -20,17 +25,87 @@ class App extends React.Component<{}, State> {
       './img/9.png',
       './img/10.png',
     ],
+
+    itemWidth: 130,
+    frameSize: 3,
+    step: 3,
+    animationDuration: 1000,
+    isInfinite: false,
   };
 
   render() {
-    const { images } = this.state;
-
     return (
       <div className="App">
-        {/* eslint-disable-next-line */}
-        <h1>Carousel with {images.length} images</h1>
+        <h1 data-cy="title">Carousel with {this.state.images.length} images</h1>
 
-        <Carousel />
+        <>
+          <label htmlFor="itemId">Item width: </label>
+          <input
+            onChange={event =>
+              this.setState({ itemWidth: +event.target.value })
+            }
+            type="number"
+            name="itemId"
+            id="itemId"
+            defaultValue={130}
+          />
+          <br />
+
+          <label htmlFor="frameId">Frame size: </label>
+          <input
+            onChange={event =>
+              this.setState({ frameSize: +event.target.value })
+            }
+            type="number"
+            name="frameId"
+            id="frameId"
+            defaultValue={3}
+          />
+          <br />
+
+          <label htmlFor="stepId">Step: </label>
+          <input
+            onChange={event => this.setState({ step: +event.target.value })}
+            type="number"
+            name="stepId"
+            id="stepId"
+            defaultValue={3}
+          />
+          <br />
+
+          <label htmlFor="animationDuration">Animation duration: </label>
+          <input
+            onChange={event =>
+              this.setState({ animationDuration: +event.target.value })
+            }
+            type="number"
+            name="animationDuration"
+            id="animationDuration"
+            defaultValue={1000}
+          />
+          <br />
+
+          <label htmlFor="infinite">Infinite loop: </label>
+          <input
+            onClick={() =>
+              this.setState({ isInfinite: !this.state.isInfinite })
+            }
+            type="checkbox"
+            name="infinite"
+            id="infinite"
+            defaultChecked={false}
+          />
+          <br />
+        </>
+
+        <Carousel
+          images={this.state.images}
+          itemWidth={this.state.itemWidth}
+          frameSize={this.state.frameSize}
+          animationDuration={this.state.animationDuration}
+          step={this.state.step}
+          infinite={this.state.isInfinite}
+        />
       </div>
     );
   }
--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,26 +1,142 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li>
-        <img src="./img/1.png" alt="1" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="2" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="3" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="4" />
-      </li>
-    </ul>
+type Props = {
+  images: string[];
+  step?: number;
+  frameSize?: number;
+  itemWidth?: number;
+  animationDuration?: number;
+  infinite?: boolean;
+};
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+const Carousel: React.FC<Props> = ({
+  images,
+  step = 3,
+  frameSize = 3,
+  itemWidth = 130,
+  animationDuration = 1000,
+  infinite = false,
+}) => {
+  const [index, setIndex] = useState(0);
+  const [isPrevPossible, setIsPrevPossible] = useState(infinite || index > 0);
+  const [isNextPossible, setIsNextPossible] = useState(
+    infinite || index < images.length - step,
+  );
+
+  const containerWidth = itemWidth * frameSize;
+  const offset = itemWidth * index;
+
+  const containerStyle = { width: `${containerWidth}px` };
+  const listStyle = {
+    left: `-${offset}px`,
+    transitionDuration: `${animationDuration}ms`,
+  };
+
+  const imageHiddenStyle = {
+    visibility: 'hidden',
+    transitionDelay: `${animationDuration}ms`, // creates an illusion of smooth motion without angering the cypress gods
+  };
+
+  const updateMovement = useCallback(
+    (newIndex: number) => {
+      if (infinite) {
+        return;
+      }
+
+      setIsPrevPossible(newIndex > 0);
+      setIsNextPossible(newIndex < images.length - step);
+    },
+    [images.length, infinite, step],
+  );
+
+  const goToNextIndex = useCallback(() => {
+    setIndex(oldIndex => {
+      let newIndex = oldIndex + step;
+
+      if (newIndex >= images.length - (frameSize - step)) {
+        newIndex = 0;
+      } else if (newIndex > images.length - frameSize) {
+        newIndex = images.length - frameSize;
+      }
+
+      updateMovement(newIndex);
+
+      return newIndex;
+    });
+  }, [images.length, frameSize, step, updateMovement]);
+
+  const goToPrevIndex = useCallback(() => {
+    setIndex(oldIndex => {
+      let newIndex = oldIndex - step;
+
+      if (newIndex <= -frameSize + (frameSize - step)) {
+        newIndex = images.length - frameSize;
+      } else if (newIndex < 0) {
+        newIndex = 0;
+      }
+
+      updateMovement(newIndex);
+
+      return newIndex;
+    });
+  }, [images.length, frameSize, step, updateMovement]);
+
+  useEffect(() => {
+    if (infinite) {
+      setIsPrevPossible(true);
+      setIsNextPossible(true);
+    } else {
+      setIsPrevPossible(index > 0);
+      setIsNextPossible(index < images.length - step);
+    }
+  }, [images.length, index, infinite, step]);
+
+  return (
+    <div className="Carousel" style={containerStyle}>
+      <ul className="Carousel__list" style={listStyle}>
+        {images.map((image, id) => {
+          // Technically, manually hiding images is not needed,
+          // as Carousel just hides any overflow.
+          // However, Cypress does not consider it hidden for some reason.
+          // and this code fixes it.
+          const isVisible = id >= index && id < index + frameSize;
+
+          return (
+            <li className="Carousel__item" key={image}>
+              <img
+                className="Carousel__img"
+                width={itemWidth}
+                style={
+                  isVisible ? {} : (imageHiddenStyle as React.CSSProperties)
+                }
+                src={image}
+                alt={`${id + 1}`}
+              />
+            </li>
+          );
+        })}
+      </ul>
+
+      <button
+        className="Carousel__button"
+        onClick={goToPrevIndex}
+        type="button"
+        disabled={!isPrevPossible}
+      >
+        Prev
+      </button>
+      <button
+        className="Carousel__button"
+        data-cy="next"
+        onClick={goToNextIndex}
+        type="button"
+        disabled={!isNextPossible}
+      >
+        Next
+      </button>
+    </div>
+  );
+};
 
 export default Carousel;
