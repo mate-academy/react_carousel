4f0f30adde6b318818a7eedd6735eddf5c3bea8b
Maksym Mohyla <greenparksquad@gmail.com>
2024-09-28 15:20:13 +0300

    carousel done

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,26 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li>
-        <img src="./img/1.png" alt="1" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="2" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="3" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="4" />
-      </li>
-    </ul>
+// it will be used also in App file
+export type IProps = {
+  images: string[];
+  step: number;
+  frameSize: number;
+  itemWidth: number;
+  animationDuration: number;
+};
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+export const Carousel: React.FC<IProps> = ({
+  images,
+  step,
+  frameSize,
+  itemWidth,
+  animationDuration,
+}) => {
+  // index defines how much items we scroll at once
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handlePrev = () => {
+    setCurrentIndex(prevIndex =>
+      // if we try to scroll to the left more than images left, it will jump to the end
+      prevIndex - step < 0 ? images.length - step : prevIndex - step,
+    );
+  };
+
+  const handleNext = () => {
+    setCurrentIndex(prevIndex =>
+      // trying scroll to the right more than images left than you return to the beginning
+      prevIndex + step >= images.length ? 0 : prevIndex + step,
+    );
+  };
+
+  return (
+    <div
+      className="Carousel"
+      style={{
+        // width of the outer container has to be exact the same as visible items otherwise it doesn't work :(
+        width: `${itemWidth * frameSize}px`,
+      }}
+    >
+      <ul
+        className="Carousel__list"
+        style={{
+          // and that's where we need index
+          transform: `translateX(-${currentIndex * itemWidth}px)`,
+          transition: `transform ${animationDuration}ms ease-in-out`,
+        }}
+      >
+        {images.map((img, i) => (
+          <li key={img}>
+            <img src={img} alt={`${i} + 1`} width={itemWidth} />
+          </li>
+        ))}
+      </ul>
+
+      <button type="button" data-cy="prev" onClick={handlePrev}>
+        Prev
+      </button>
+      <button type="button" data-cy="next" onClick={handleNext}>
+        Next
+      </button>
+    </div>
+  );
+};
 
 export default Carousel;
