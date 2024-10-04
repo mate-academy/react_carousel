9eb3f1fed47c5452d1d1580b69a57e21c9173b45
Valerii Omelnyk <valeraaomelnik@gmail.com>
2024-10-04 18:31:42 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,10 @@ import Carousel from './components/Carousel';
 
 interface State {
   images: string[];
+  step: number;
+  frameSize: number;
+  itemWidth: number;
+  animDuration: number;
 }
 
 class App extends React.Component<{}, State> {
@@ -20,17 +24,75 @@ class App extends React.Component<{}, State> {
       './img/9.png',
       './img/10.png',
     ],
+    step: 3,
+    frameSize: 3,
+    itemWidth: 130,
+    animDuration: 1000,
   };
 
   render() {
-    const { images } = this.state;
+    const { images, step, frameSize, itemWidth, animDuration } = this.state;
 
     return (
       <div className="App">
         {/* eslint-disable-next-line */}
-        <h1>Carousel with {images.length} images</h1>
+        <h1 data-cy="title">Carousel with {images.length} images</h1>
 
-        <Carousel />
+        <div className="field">
+          <label htmlFor="itemId">Item Width:</label>
+          <input
+            id="itemId"
+            type="text"
+            value={itemWidth}
+            onChange={event =>
+              this.setState({ itemWidth: Number(event.target.value) })
+            }
+          />
+        </div>
+
+        <div className="field">
+          <label htmlFor="frameId">Frame Size:</label>
+          <input
+            id="frameId"
+            type="text"
+            value={frameSize}
+            onChange={event =>
+              this.setState({ frameSize: Number(event.target.value) })
+            }
+          />
+        </div>
+
+        <div className="field">
+          <label htmlFor="stepId">Step:</label>
+          <input
+            id="stepId"
+            type="text"
+            value={step}
+            onChange={event =>
+              this.setState({ step: Number(event.target.value) })
+            }
+          />
+        </div>
+
+        <div className="field">
+          <label htmlFor="animDurationInput">Animation Duration (ms):</label>
+          <input
+            id="animDurationInput"
+            type="text"
+            value={animDuration}
+            onChange={event =>
+              this.setState({ animDuration: Number(event.target.value) })
+            }
+          />
+        </div>
+
+        <Carousel
+          images={images}
+          step={step}
+          frameSize={frameSize}
+          itemWidth={itemWidth}
+          animationDuration={animDuration}
+        />
       </div>
     );
   }
--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,26 +1,104 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Carousel.scss';
+import { Direction } from '../types/Direction';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li>
-        <img src="./img/1.png" alt="1" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="2" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="3" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="4" />
-      </li>
-    </ul>
+type Props = {
+  images: string[];
+  step: number;
+  frameSize: number;
+  itemWidth: number;
+  animationDuration: number;
+};
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+const Carousel: React.FC<Props> = ({
+  images,
+  step,
+  frameSize,
+  itemWidth,
+  animationDuration,
+}) => {
+  const [slideRange, setSlideRange] = useState([1, frameSize]);
+
+  useEffect(() => {
+    setSlideRange([1, frameSize]);
+  }, [frameSize]);
+
+  const handleSetSlideRange = (direction: Direction) => {
+    setSlideRange(current => {
+      let newSlide;
+
+      let availableStep;
+
+      switch (direction) {
+        case Direction.Prev:
+          if (slideRange[0] - step < 1) {
+            availableStep = slideRange[0] - 1;
+          } else {
+            availableStep = step;
+          }
+
+          newSlide = [current[0] - availableStep, current[1] - availableStep];
+          break;
+        case Direction.Next:
+          if (slideRange[1] + step > images.length - 1) {
+            availableStep = images.length - 1 - slideRange[1];
+          } else {
+            availableStep = step;
+          }
+
+          newSlide = [current[0] + availableStep, current[1] + availableStep];
+          break;
+      }
+
+      return newSlide;
+    });
+  };
+
+  return (
+    <div className="Container">
+      <button
+        type="button"
+        disabled={slideRange[0] === 1}
+        className="Carousel__button"
+        onClick={() => handleSetSlideRange(Direction.Prev)}
+      >
+        Prev
+      </button>
+
+      <div className="Carousel" style={{ width: `${frameSize * itemWidth}px` }}>
+        <ul
+          className="Carousel__list"
+          style={{
+            transition: `transform ${animationDuration}ms`,
+            transform: `translateX(${-slideRange[0] * itemWidth}px)`,
+            width: `${frameSize * itemWidth}px`,
+          }}
+        >
+          {images.map((image, index) => (
+            <li key={image}>
+              <img
+                src={image}
+                alt={`${index + 1}`}
+                className="Carousel__img"
+                width={itemWidth}
+                height={itemWidth}
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <button
+        type="button"
+        disabled={slideRange[1] === images.length - 1}
+        className="Carousel__button"
+        data-cy="next"
+        onClick={() => handleSetSlideRange(Direction.Next)}
+      >
+        Next
+      </button>
+    </div>
+  );
+};
 
 export default Carousel;
