267f9717de0dd60df713a1eddd44643c1014dd77
RomanHasiuk <romanhasiuk1@gmail.com>
2025-02-12 17:41:17 +0200

    add task solution

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,26 +1,91 @@
-import React from 'react';
+import React, { useState } from 'react';
+import 'bulma/css/bulma.css';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li>
-        <img src="./img/1.png" alt="1" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="2" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="3" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="4" />
-      </li>
-    </ul>
+interface CarouselProps {
+  images: string[];
+  itemWidth?: number;
+  frameSize?: number;
+  step?: number;
+  animationDuration?: number;
+  infinite?: boolean;
+}
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+const Carousel: React.FC<CarouselProps> = ({
+  images,
+  itemWidth = 130,
+  frameSize = 3,
+  step = 3,
+  animationDuration = 1000,
+  infinite = false,
+}) => {
+  const [index, setIndex] = useState(0);
+
+  const itemGap = 10;
+  const visibleWidth = frameSize * (itemWidth + itemGap);
+  const totalWidth = images.length * (itemWidth + itemGap);
+
+  const nextSlide = () => {
+    const newIndex = index + step;
+
+    if (newIndex < images.length - frameSize + 1) {
+      setIndex(newIndex);
+    } else if (infinite) {
+      setIndex(0);
+    }
+  };
+
+  const prevSlide = () => {
+    const newIndex = index - step;
+
+    if (newIndex >= 0) {
+      setIndex(newIndex);
+    } else if (infinite) {
+      setIndex(images.length - frameSize);
+    }
+  };
+
+  return (
+    <div className="Carousel">
+      <button
+        className={`arrow left ${index === 0 && !infinite ? 'disabled' : ''}`}
+        onClick={prevSlide}
+        data-cy="prev"
+      >
+        &#9664;
+      </button>
+
+      <div className="Carousel__wrapper" style={{ width: visibleWidth }}>
+        <ul
+          className="Carousel__list"
+          style={{
+            transform: `translateX(-${index * (itemWidth + itemGap)}px)`,
+            transition: `transform ${animationDuration}ms ease-in-out`,
+            width: totalWidth,
+          }}
+        >
+          {images.map((src, i) => (
+            <li key={i} style={{ width: itemWidth }}>
+              <img
+                src={src}
+                alt={`Slide ${i + 1}`}
+                width={itemWidth}
+                height={itemWidth}
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <button
+        className={`arrow right ${index + frameSize >= images.length && !infinite ? 'disabled' : ''}`}
+        onClick={nextSlide}
+        data-cy="next"
+      >
+        &#9654;
+      </button>
+    </div>
+  );
+};
 
 export default Carousel;
