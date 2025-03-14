4ef486dc04bb3756884f8b9fd8e8c0be4e46896c
lublubuterbrodi <lublubuterbrodi@gmail.com>
2025-03-14 16:53:35 +0200

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,59 @@
-import React from 'react';
-import './App.scss';
+import React, { useState } from 'react';
 import Carousel from './components/Carousel';
 
-interface State {
-  images: string[];
-}
+const App: React.FC = () => {
+  const [itemWidth, setItemWidth] = useState(130);
+  const [frameSize, setFrameSize] = useState(3);
+  const [step, setStep] = useState(3);
+  const [animationDuration, setAnimationDuration] = useState(1000);
 
-class App extends React.Component<{}, State> {
-  state = {
-    images: [
-      './img/1.png',
-      './img/2.png',
-      './img/3.png',
-      './img/4.png',
-      './img/5.png',
-      './img/6.png',
-      './img/7.png',
-      './img/8.png',
-      './img/9.png',
-      './img/10.png',
-    ],
-  };
+  const images = Array.from({ length: 10 }, (_, i) => `./img/${i + 1}.png`);
 
-  render() {
-    const { images } = this.state;
+  return (
+    <div>
+      <h1 data-cy="title">Carousel</h1>
 
-    return (
-      <div className="App">
-        {/* eslint-disable-next-line */}
-        <h1>Carousel with {images.length} images</h1>
+      <label htmlFor="itemId">Item Width:</label>
+      <input
+        id="itemId"
+        type="number"
+        value={itemWidth}
+        onChange={e => setItemWidth(Number(e.target.value))}
+      />
 
-        <Carousel />
-      </div>
-    );
-  }
-}
+      <label htmlFor="frameId">Frame Size:</label>
+      <input
+        id="frameId"
+        type="number"
+        value={frameSize}
+        onChange={e => setFrameSize(Number(e.target.value))}
+      />
+
+      <label htmlFor="stepId">Step:</label>
+      <input
+        id="stepId"
+        type="number"
+        value={step}
+        onChange={e => setStep(Number(e.target.value))}
+      />
+
+      <label htmlFor="animationId">Animation Duration:</label>
+      <input
+        id="animationId"
+        type="number"
+        value={animationDuration}
+        onChange={e => setAnimationDuration(Number(e.target.value))}
+      />
+
+      <Carousel
+        images={images}
+        itemWidth={itemWidth}
+        frameSize={frameSize}
+        step={step}
+        animationDuration={animationDuration}
+      />
+    </div>
+  );
+};
 
 export default App;
