c15e4e54043d7aff181b72fc9ac0f2bab6852ec6
NataliaDavida <nataliadavida88@gmail.com>
2025-04-22 20:29:33 +0300

    add solution

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,14 @@ import Carousel from './components/Carousel';
 
 interface State {
   images: string[];
+  step: number;
+  frameSize: number;
+  itemWidth: number;
+  animationDuration: number;
 }
 
 class App extends React.Component<{}, State> {
-  state = {
+  state: State = {
     images: [
       './img/1.png',
       './img/2.png',
@@ -20,17 +24,63 @@ class App extends React.Component<{}, State> {
       './img/9.png',
       './img/10.png',
     ],
+
+    step: 3,
+    frameSize: 3,
+    itemWidth: 130,
+    animationDuration: 1000,
   };
 
   render() {
-    const { images } = this.state;
+    const { images, step, frameSize, itemWidth, animationDuration } =
+      this.state;
 
     return (
       <div className="App">
-        {/* eslint-disable-next-line */}
-        <h1>Carousel with {images.length} images</h1>
+        <h1 data-cy="title">Carousel with {images.length} images</h1>
 
-        <Carousel />
+        <form
+          action=""
+          style={{
+            width: `${itemWidth + itemWidth}px`,
+
+            display: 'flex',
+            flexDirection: 'column',
+            margin: 10,
+          }}
+        >
+          <label htmlFor="itemId">Item Width:</label>
+          <input
+            id="itemId"
+            type="number"
+            value={this.state.itemWidth}
+            onChange={e => this.setState({ itemWidth: Number(e.target.value) })}
+          />
+
+          <label htmlFor="frameId">Frame Size:</label>
+          <input
+            id="frameId"
+            type="number"
+            value={this.state.frameSize}
+            onChange={e => this.setState({ frameSize: Number(e.target.value) })}
+          />
+
+          <label htmlFor="stepId">Step:</label>
+          <input
+            id="stepId"
+            type="number"
+            value={this.state.step}
+            onChange={e => this.setState({ step: Number(e.target.value) })}
+          />
+        </form>
+
+        <Carousel
+          images={images}
+          step={step}
+          frameSize={frameSize}
+          itemWidth={itemWidth}
+          animationDuration={animationDuration}
+        />
       </div>
     );
   }
