7fbc6f8bd3a24f8f28906afb1cd62cd5671b652a
Krzysztof Piatkowski <krzysztofpiatkowski1987@gmail.com>
2025-05-14 14:48:31 +0200

    Task solution

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,26 +1,89 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li>
-        <img src="./img/1.png" alt="1" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="2" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="3" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="4" />
-      </li>
-    </ul>
+interface CarouselProps {
+  images: string[];
+  itemWidth?: number;
+  frameSize?: number;
+  step?: number;
+  animationDuration?: number;
+  infinite?: boolean;
+}
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+const Carousel: React.FC<CarouselProps> = ({
+  images,
+  itemWidth = 130,
+  frameSize = 3,
+  step = 1,
+  animationDuration = 1000,
+  infinite = false,
+}) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const translateX = -currentIndex * itemWidth;
+
+  return (
+    <div className="Carousel">
+      <button
+        className="Carousel__button Carousel__button--prev"
+        type="button"
+        disabled={currentIndex <= 0}
+        onClick={() => {
+          if (currentIndex > 0) {
+            setCurrentIndex(currentIndex - step);
+          }
+        }}
+      >
+        ‹
+      </button>
+
+      <div
+        className="Carousel__frame"
+        style={{ width: `${itemWidth * frameSize + 10 * (frameSize - 1)}px` }}
+      >
+        <ul
+          className="Carousel__list"
+          style={{
+            transform: `translateX(${translateX}px)`,
+            transition: `transform ${animationDuration}ms ease`,
+          }}
+        >
+          {images.map((src, index) => (
+            <li key={index}>
+              <img
+                data-cy="carousel-image"
+                src={src}
+                alt={`image-${index + 1}`}
+                width={itemWidth}
+                height={itemWidth}
+                style={{
+                  width: `${itemWidth}px`,
+                  height: `${itemWidth}px`,
+                }}
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <button
+        data-cy="next"
+        className="Carousel__button Carousel__button--next"
+        type="button"
+        disabled={currentIndex >= images.length - frameSize}
+        onClick={() => {
+          const maxIndex = images.length - frameSize;
+
+          if (currentIndex + step <= maxIndex) {
+            setCurrentIndex(currentIndex + step);
+          } else if (infinite) {
+            setCurrentIndex(0);
+          }
+        }}
+      >
+        ›
+      </button>
+    </div>
+  );
+};
 
 export default Carousel;
