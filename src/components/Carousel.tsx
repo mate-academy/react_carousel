0f35b8a526033e2887d458bf9477a611c12af481
Yura <yurii.st11@gmail.com>
2025-05-29 19:54:07 +0300

    add solution

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,26 +1,81 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li>
-        <img src="./img/1.png" alt="1" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="2" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="3" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="4" />
-      </li>
-    </ul>
+interface Props {
+  images: string[];
+  itemWidth: number;
+  animDuration: number;
+  step: number;
+  frameSize: number;
+  isInfinite: boolean;
+}
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+const Carousel: React.FC<Props> = ({
+  images,
+  itemWidth,
+  animDuration,
+  step,
+  frameSize,
+  isInfinite,
+}: Props) => {
+  const [offset, setOffset] = useState(0);
+
+  const maxOffset = Math.max(0, (images.length - frameSize) * itemWidth);
+
+  const handlePrev = () => {
+    setOffset(prev => {
+      const next = prev - step * itemWidth;
+
+      if (next < 0) {
+        return isInfinite ? maxOffset : 0;
+      }
+
+      return next;
+    });
+  };
+
+  const handleNext = () => {
+    setOffset(prev => {
+      const next = prev + step * itemWidth;
+
+      if (next > maxOffset) {
+        return isInfinite ? 0 : maxOffset;
+      }
+
+      return next;
+    });
+  };
+
+  return (
+    <div className="Carousel">
+      <button className="button is-dark is-small" onClick={handlePrev}>
+        Prev
+      </button>
+      <div className="viewport" style={{ width: `${itemWidth * frameSize}px` }}>
+        <ul
+          className="Carousel__list"
+          style={{
+            transform: `translateX(-${offset}px)`,
+            transition: `transform ${animDuration}ms ease`,
+          }}
+        >
+          {images.map((image, index) => (
+            <li key={image} className="Carousel__list__item">
+              <img src={image} alt={`${index + 1}`} width={itemWidth} />
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <button
+        className="button is-dark is-small"
+        onClick={handleNext}
+        data-cy="next"
+      >
+        Next
+      </button>
+    </div>
+  );
+};
 
 export default Carousel;
