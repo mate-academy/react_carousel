bb764ad1914db821f9edf82ddfd5c734d260d92e
Tania Kuzmenko <tanch.sergeevich@gmail.com>
2025-06-15 17:31:46 +0300

    Solution

---
+++ src/components/Carosel/Carousel.tsx
@@ -0,0 +1,107 @@
+import React, { useState } from 'react';
+import './Carousel.scss';
+
+interface Props {
+  images: string[];
+  itemWidth?: number;
+  frameSize?: number;
+  step?: number;
+  animationDuration?: number;
+  infinite?: boolean;
+}
+
+const Carousel: React.FC<Props> = ({
+  images,
+  itemWidth = 130,
+  frameSize = 3,
+  step = 3,
+  animationDuration = 1000,
+  infinite = false,
+}) => {
+  const [startIndex, setStartIndex] = useState(0);
+  const containerWidth = itemWidth * frameSize;
+  const maxIndex = Math.max(images.length - frameSize, 0);
+  const offsetX = startIndex * itemWidth;
+  const handleNext = () => {
+    if (startIndex + step > maxIndex) {
+      if (infinite) {
+        setStartIndex(0);
+      }
+    } else {
+      setStartIndex(startIndex + step);
+    }
+  };
+
+  const handlePrev = () => {
+    if (startIndex - step < 0) {
+      if (infinite) {
+        setStartIndex(maxIndex);
+      }
+    } else {
+      setStartIndex(startIndex - step);
+    }
+  };
+
+  return (
+    <div className="Carousel">
+      <div
+        className="Carousel__wrapper"
+        style={{ width: `${containerWidth}px` }}
+      >
+        <ul
+          className="Carousel__list"
+          style={{
+            transform: `translateX(-${offsetX}px)`,
+            transition: `transform ${animationDuration}ms`,
+            width: `${containerWidth}px`,
+          }}
+        >
+          {images.map((src, i) => {
+            const isVisible = i >= startIndex && i < startIndex + frameSize;
+
+            return (
+              <li
+                key={i}
+                className="Carousel__item"
+                style={{
+                  width: `${itemWidth}px`,
+                  visibility: isVisible ? 'visible' : 'hidden',
+                }}
+              >
+                <img
+                  src={src}
+                  alt={`Image ${i + 1}`}
+                  data-cy="carousel-image"
+                  width={itemWidth}
+                  height={130}
+                />
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <div className="Carousel__button_wrapper">
+        <button
+          type="button"
+          className={`Carousel__button Carousel__button--left ${startIndex === 0 ? 'disabled' : ''}`}
+          onClick={handlePrev}
+          disabled={startIndex === 0}
+        >
+          Previous
+        </button>
+
+        <button
+          type="button"
+          data-cy="next"
+          className={`Carousel__button Carousel__button--right ${startIndex >= maxIndex ? 'disabled' : ''}`}
+          onClick={handleNext}
+          disabled={startIndex >= maxIndex}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Carousel;
