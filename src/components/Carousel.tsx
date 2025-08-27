26ba1e3da1de9ad7e100825ed70640b077491af8
Veronica Shyian <veronica.shyian@gmail.com>
2025-08-27 20:11:58 +0300

    Solution

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,26 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li>
-        <img src="./img/1.png" alt="1" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="2" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="3" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="4" />
-      </li>
-    </ul>
+type Props = {
+  images: string[];
+  step?: number;
+  frameSize?: number;
+  itemWidth?: number;
+  animationDuration?: number;
+};
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+const Carousel: React.FC<Props> = ({
+  images,
+  step = 3,
+  frameSize = 3,
+  itemWidth = 130,
+  animationDuration = 1000,
+}) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  return (
+    <div className="Carousel">
+      <div
+        className="Carousel__wrap"
+        style={{ width: `${itemWidth * frameSize}px` }}
+      >
+        <ul
+          className="Carousel__list"
+          style={{
+            width: `${itemWidth * images.length}px`,
+            transform: `translateX(-${itemWidth * currentIndex}px)`,
+            transition: `transform ${animationDuration}ms linear`,
+          }}
+        >
+          {images.map((imageSrc, index) => (
+            <li key={imageSrc}>
+              <img
+                src={imageSrc}
+                alt={`Slide ${index + 1}`}
+                width={itemWidth}
+                height={itemWidth}
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <button
+        disabled={currentIndex === 0}
+        type="button"
+        data-cy="prev"
+        onClick={() => {
+          setCurrentIndex(prev => Math.max(prev - step, 0));
+        }}
+      >
+        Prev
+      </button>
+      <button
+        disabled={currentIndex === images.length - frameSize}
+        type="button"
+        data-cy="next"
+        onClick={() => {
+          setCurrentIndex(prev =>
+            Math.min(prev + step, images.length - frameSize),
+          );
+        }}
+      >
+        Next
+      </button>
+    </div>
+  );
+};
 
 export default Carousel;
