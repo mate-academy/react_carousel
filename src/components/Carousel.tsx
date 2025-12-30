c204b32e5e2fb37f2a07b7d1292d0ff3bc767a3e
Mykola <nyr3mvneov@gmail.com>
2025-12-30 19:55:19 +0200

    first commit 30_12_20_00

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,26 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li>
-        <img src="./img/1.png" alt="1" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="2" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="3" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="4" />
-      </li>
-    </ul>
+type Props = {
+  images: string[];
+};
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+const Carousel: React.FC<Props> = ({ images }) => {
+  const [currentPosition, setCurrentPosition] = useState(0);
+
+  const imagesToShow = 2;
+  const imageWidth = 130;
+  const imageGap = 24;
+  const itemWidth = imageWidth + imageGap;
+
+  const handlePrevImage = () => {
+    if (currentPosition > 0) {
+      setCurrentPosition(currentPosition - imagesToShow);
+    }
+  };
+
+  const handleNextImage = () => {
+    const maxPosition = images.length - imagesToShow;
+
+    if (currentPosition < maxPosition) {
+      setCurrentPosition(currentPosition + imagesToShow);
+    }
+  };
+
+  const translateX = -currentPosition * itemWidth;
+  const PrevDisabled = currentPosition <= 0;
+  const NextDisabled = currentPosition >= images.length - imagesToShow;
+
+  return (
+    <div className="Carousel">
+      <ul
+        className="Carousel__list"
+        style={{
+          transform: `translateX(${translateX}px)`,
+          transition: 'transform 0.3s ease',
+        }}
+      >
+        {images.map((src, index) => (
+          <li key={src}>
+            <img src={src} alt={`${index + 1}`} />
+          </li>
+        ))}
+      </ul>
+
+      <div className="Carousel__controls">
+        <button
+          className="Carousel__button Carousel__button--prev"
+          type="button"
+          onClick={handlePrevImage}
+          disabled={PrevDisabled}
+        >
+          {'<--'}
+        </button>
+        <button
+          className="Carousel__button Carousel__button--next"
+          type="button"
+          onClick={handleNextImage}
+          disabled={NextDisabled}
+        >
+          {'-->'}
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Carousel;
