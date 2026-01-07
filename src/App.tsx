7e33b4064152a5e4b619dd1bc0c2d4afa5ec6556
Dmytro Odnoroh <falcon121299@icloud.com>
2026-01-07 20:43:38 +0200

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ interface State {
 }
 
 class App extends React.Component<{}, State> {
-  state = {
+  state: State = {
     images: [
       './img/1.png',
       './img/2.png',
@@ -27,10 +27,8 @@ class App extends React.Component<{}, State> {
 
     return (
       <div className="App">
-        {/* eslint-disable-next-line */}
-        <h1>Carousel with {images.length} images</h1>
-
-        <Carousel />
+        <h1 data-cy="title">Carousel</h1>
+        <Carousel images={images} />;
       </div>
     );
   }
