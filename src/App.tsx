19e9b4a4fb264fec3474fb6941f3deac362c69c6
DevT <dev.tymofiichuk@gmail.com>
2026-03-06 12:19:50 +0200

    Carousel task

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import Carousel from './components/Carousel';
+import CarouselControls from './components/CarouselControls';
 
-interface State {
-  images: string[];
-}
+const App: React.FC = () => {
+  const [itemWidth, setItemWidth] = useState(130);
+  const [frameSize, setFrameSize] = useState(3);
+  const [step, setStep] = useState(3);
+  const [animationDuration, setAnimationDuration] = useState(1000);
 
-class App extends React.Component<{}, State> {
-  state = {
-    images: [
-      './img/1.png',
-      './img/2.png',
-      './img/3.png',
-      './img/4.png',
-      './img/5.png',
-      './img/6.png',
-      './img/7.png',
-      './img/8.png',
-      './img/9.png',
-      './img/10.png',
-    ],
-  };
+  const images = [
+    './img/1.png',
+    './img/2.png',
+    './img/3.png',
+    './img/4.png',
+    './img/5.png',
+    './img/6.png',
+    './img/7.png',
+    './img/8.png',
+    './img/9.png',
+    './img/10.png',
+  ];
 
-  render() {
-    const { images } = this.state;
+  return (
+    <div className="App">
+      <h1 data-cy="title">Carousel with {images.length} images</h1>
 
-    return (
-      <div className="App">
-        {/* eslint-disable-next-line */}
-        <h1>Carousel with {images.length} images</h1>
+      <CarouselControls
+        itemWidth={itemWidth}
+        frameSize={frameSize}
+        step={step}
+        animationDuration={animationDuration}
+        onItemWidthChange={setItemWidth}
+        onFrameSizeChange={setFrameSize}
+        onStepChange={setStep}
+        onAnimationDurationChange={setAnimationDuration}
+      />
 
-        <Carousel />
-      </div>
-    );
-  }
-}
+      <Carousel
+        images={images}
+        itemWidth={itemWidth}
+        frameSize={frameSize}
+        step={step}
+        animationDuration={animationDuration}
+        infinite={false}
+      />
+    </div>
+  );
+};
 
 export default App;
--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,26 +1,87 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li>
-        <img src="./img/1.png" alt="1" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="2" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="3" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="4" />
-      </li>
-    </ul>
+type Props = {
+  images: string[];
+  step?: number;
+  frameSize?: number;
+  itemWidth?: number;
+  animationDuration?: number;
+  infinite?: boolean;
+};
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+const Carousel: React.FC<Props> = ({
+  images,
+  step = 3,
+  frameSize = 3,
+  itemWidth = 130,
+  animationDuration = 1000,
+  infinite = false,
+}) => {
+  const [startIndex, setStartIndex] = useState(0);
+
+  const maxIndex = Math.max(images.length - frameSize, 0);
+  const frameWidth = frameSize * itemWidth;
+  const offset = -(startIndex * itemWidth);
+
+  useEffect(() => {
+    setStartIndex(prev => Math.min(prev, maxIndex));
+  }, [maxIndex]);
+
+  const onNextBtn = () => {
+    setStartIndex(prev =>
+      infinite && prev >= maxIndex ? 0 : Math.min(prev + step, maxIndex),
+    );
+  };
+
+  const onPrevBtn = () => {
+    setStartIndex(prev =>
+      infinite && prev <= 0 ? maxIndex : Math.max(prev - step, 0),
+    );
+  };
+
+  return (
+    <div className="Carousel">
+      <div className="Carousel__frame" style={{ width: `${frameWidth}px` }}>
+        <ul
+          className="Carousel__list"
+          style={{
+            transform: `translateX(${offset}px)`,
+            transition: `transform ${animationDuration}ms`,
+          }}
+        >
+          {images.map(image => {
+            const alt = image.split('/').pop()?.split('.')[0] || 'slide';
+
+            return (
+              <li
+                key={image}
+                className="Carousel__item"
+                style={{ width: `${itemWidth}px` }}
+              >
+                <img
+                  src={image}
+                  alt={alt}
+                  width={itemWidth}
+                  height={itemWidth}
+                />
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <div className="Carousel__buttons">
+        <button type="button" onClick={onPrevBtn}>
+          Previous
+        </button>
+
+        <button type="button" onClick={onNextBtn} data-cy="next">
+          Next
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Carousel;
