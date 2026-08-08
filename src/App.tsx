ec668f2c9649a0ea1df99f0f2117c30f354320de
vladahvala <80634246+vladahvala@users.noreply.github.com>
2026-08-08 22:25:36 +0300

    Implemented carousel

--- src/App.tsx
+++ src/App.tsx
@@ -4,33 +4,70 @@ import Carousel from './components/Carousel';
 
 interface State {
   images: string[];
+  step: number;
+  itemWidth: number;
+  frameSize: number;
 }
 
 class App extends React.Component<{}, State> {
   state = {
-    images: [
-      './img/1.png',
-      './img/2.png',
-      './img/3.png',
-      './img/4.png',
-      './img/5.png',
-      './img/6.png',
-      './img/7.png',
-      './img/8.png',
-      './img/9.png',
-      './img/10.png',
-    ],
+    images: Array.from(
+      { length: 10 },
+      (_, index) =>  (index + 1).toString(),
+    ),
+    step: 3,
+    itemWidth: 130,
+    frameSize: 3,
   };
 
   render() {
-    const { images } = this.state;
+    const { images, step, itemWidth, frameSize} = this.state;
+
 
     return (
       <div className="App">
+        <h1 data-cy="title">Carousel</h1>
         {/* eslint-disable-next-line */}
-        <h1>Carousel with {images.length} images</h1>
+          {/* ЕЛЕМЕНТИ УПРАВЛІННЯ - те, що шукають тести */}
+      <div>
+        <label htmlFor="stepId">Step: </label>
+        <input
+          id="stepId"
+          data-cy="step"
+          type="number"
+          value={step}
+          onChange={(e) => this.setState({ step: Number(e.target.value) })}
+        />
+      </div>
 
-        <Carousel />
+      <div>
+        <label htmlFor="itemId">Item Width: </label>
+        <input
+          id="itemId"
+          data-cy="item"
+          type="number"
+          value={itemWidth}
+          onChange={(e) => this.setState({ itemWidth: Number(e.target.value) })}
+        />
+      </div>
+
+      <div>
+        <label htmlFor="frameId">Frame Size: </label>
+        <input
+          id="frameId"
+          data-cy="frame"
+          type="number"
+          value={frameSize}
+          onChange={(e) => this.setState({ frameSize: Number(e.target.value) })}
+        />
+        </div>
+
+        <Carousel
+        images={images}
+        step={step}
+        frameSize={frameSize}
+        itemWidth={itemWidth}
+      />
       </div>
     );
   }
--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,26 +1,99 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li>
-        <img src="./img/1.png" alt="1" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="2" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="3" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="4" />
-      </li>
-    </ul>
+type Props = {
+  images: string[];
+  itemWidth?: number;
+  frameSize?: number;
+  step?: number;
+  animationDuration?: number;
+  infinite?: boolean;
+};
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+const Carousel: React.FC<Props> = ({
+  images,
+  step= 3,
+  frameSize = 3,
+  itemWidth = 130,
+  animationDuration = 1000,
+  infinite = false,
+}) => {
+  const [currentWidth, setCurrentWidth] = useState(0);
+
+  const diff = -(images.length - frameSize) * itemWidth;
+  const remaining = Math.abs(diff - currentWidth);
+  const stepWidth = itemWidth * step;
+
+  return (
+    <div className="Carousel">
+      <div className="Carousel__window"
+        style={{ width: `${frameSize * itemWidth}px` }}
+      >
+        <ul
+          className="Carousel__list"
+          style={{
+            transform: `translateX(${currentWidth}px)`,
+            transition: `transform ${animationDuration}ms`,
+          }}
+        >
+          {images.map(img => (
+            <li key={img}>
+              <img
+                src={`./img/${img}.png`}
+                alt="4"
+                width={itemWidth}
+                style={{ width: `${itemWidth}px`, height: `${itemWidth}px` }}
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <button
+        type="button"
+        data-cy="prev"
+        onClick={() => {
+          if (currentWidth === 0) {
+            if (infinite) {
+              setCurrentWidth(diff)
+            } else {
+              setCurrentWidth(0);
+            }
+          } else if (currentWidth < 0) {
+            if (Math.abs(currentWidth) < stepWidth) {
+              setCurrentWidth(0);
+            } else {
+              setCurrentWidth(currentWidth + stepWidth);
+            }
+          }
+        }}
+      >
+        Prev
+      </button>
+
+      <button
+        type="button"
+        data-cy="next"
+        onClick={() => {
+          if (currentWidth === diff) {
+            if (infinite) {
+              setCurrentWidth(0);
+            } else {
+              setCurrentWidth(diff);
+            }
+          } else if (currentWidth > diff) {
+            if (remaining < stepWidth) {
+              setCurrentWidth(diff);
+            } else {
+              setCurrentWidth(currentWidth - stepWidth);
+            }
+          }
+        }}
+      >
+        Next
+      </button>
+    </div>
+  );
+};
 
 export default Carousel;
