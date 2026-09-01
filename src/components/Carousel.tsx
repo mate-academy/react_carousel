c4da1bb8d904c5902d691f029782e8999726f860
ivanov-serg <ivanovsereza26@gmail.com>
2026-09-01 15:42:18 +0300

    react_carousel

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,26 +1,81 @@
 import React from 'react';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li>
-        <img src="./img/1.png" alt="1" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="2" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="3" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="4" />
-      </li>
-    </ul>
+interface Props {
+  images: string[];
+  itemWidth?: number;
+  frameSize?: number;
+  step?: number;
+  animationDuration?: number;
+  infinite?: boolean;
+}
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+const Carousel: React.FC<Props> = (props: Props) => {
+  const {
+    images,
+    itemWidth = 130,
+    frameSize = 3,
+    step = 3,
+    animationDuration = 1000,
+    infinite = false,
+  } = props;
+
+  const [currentIndex, setCurrentIndex] = React.useState(0);
+
+  const handleNext = () => {
+    const maxIndex = images.length - frameSize;
+    const nextIndex = currentIndex + step;
+
+    if (infinite && nextIndex >= maxIndex) {
+      setCurrentIndex(0);
+    } else if (nextIndex >= maxIndex) {
+      setCurrentIndex(maxIndex);
+    } else {
+      setCurrentIndex(nextIndex);
+    }
+  };
+
+  const handlePrev = () => {
+    const maxIndex = images.length - frameSize;
+    const previousIndex = currentIndex - step;
+
+    if (infinite && previousIndex < 0) {
+      setCurrentIndex(maxIndex);
+    } else if (previousIndex >= 0) {
+      setCurrentIndex(previousIndex);
+    }
+  };
+
+  return (
+    <div className="Carousel">
+      <div
+        className="Carousel__frame"
+        style={{ width: frameSize * itemWidth + 'px' }}
+      >
+        <ul
+          className="Carousel__list"
+          style={{
+            transform: `translateX(-${currentIndex * itemWidth}px)`,
+            transition: `transform ${animationDuration}ms`,
+          }}
+        >
+          {images.map((image: string, index: number) => (
+            <li key={image}>
+              <img src={image} alt={'Image ' + (index + 1)} width={itemWidth} />
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <button type="button" onClick={handlePrev}>
+        Prev
+      </button>
+
+      <button type="button" data-cy="next" onClick={handleNext}>
+        Next
+      </button>
+    </div>
+  );
+};
 
 export default Carousel;
